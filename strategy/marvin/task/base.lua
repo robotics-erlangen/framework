f56ad827c94fe5c6272fa05efb2e1ac5cfb205d8
Michael Eischer <michael.eischer@robotics-erlangen.de>
2012-11-01 20:00:48 +0100

    marvin: Update Task.Base

--- strategy/marvin/task/base.lua
+++ strategy/marvin/task/base.lua
@@ -2,20 +2,29 @@ local Base = (require "base/class").new("Task.Base")
 
 Base._priority = 0
 
-function Base:init(tm, robot, ...)
-	self._taskmanager = tm
+function Base:init(robot, ...)
 	self._robot = robot
 	self:_init(...)
+	if self._priority == 0 then
+		error("priority not set")
+	end
 end
 
-function Base:_run()
+function Base:_init(...)
+	error("stub")
+	-- handle params
+end
+
+function Base:_run(priorityMessages, notifications)
 	error("stub")
 	-- hysteresis
 end
 
-function Base:run()
+function Base:run(priorityMessages, notifications)
 	-- TODO: prepare logging
-	local msg = self:_run()
+	local msg = self:_run(priorityMessages, notifications)
 	-- TODO: finish logging
 	return msg -- TODO: message to share with other tasks
 end
+
+return Base
