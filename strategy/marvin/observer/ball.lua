b98a86087a17c4a9365de198d1647c48dd7583cf
Alexander Danzer <alexander.danzer@robotics-erlangen.de>
2012-12-08 00:29:05 +0100

    save changes

--- strategy/marvin/observer/ball.lua
+++ strategy/marvin/observer/ball.lua
@@ -4,25 +4,32 @@ local Ball = {}
 -- TODO: who is the first at the ball
 
 local World = require "../base/world"
-
-
-
-
+local Settings = require "settings"
 
 
 local lastBallOwner
 
+-- returns the ballie
 function Ball.ballOwner()
+	-- tests if the current ball owner still got the ball
 	if lastBallOwner then
-		-- test if lastBallOwner still owns the ball
-	else
-		local minDistSq = 1000000 -- 1km
-		for _,r in World.Robots do
-			local distSq = (r.pos - World.Ball.pos).lengthSq()
-			if distSq < minDist then
-				minDist = 
+		local distSq = (lastBallOwner.pos - World.Ball.pos):lengthSq()
+		if distSq > (Settings.ballOwnDistance + Settings.ballOwnHysterese)^2 then
+			lastBallOwner = nil
 		end
 	end
+	-- searches for a new ball owner
+	if not lastBallOwner then
+		local minDistSq = math.huge
+		for _,r in World.Robots do
+			local distSq = (r.pos - World.Ball.pos):lengthSq()
+			if distSq < minDistSq and distSq <= Settings.ballOwnDistance^2 then
+				minDistSq = distSq
+				lastBallOwner = r
+			end
+		end
+	end
+	return lastBallOwner
 end
 
 
--- strategy/marvin/settings.lua
+++ strategy/marvin/settings.lua
@@ -1,7 +1,10 @@
 local Settings = {}
 -- TODO: add settings
 
-	Settings.positionPadding = 0.02
-	Settings.forceKeeperId = nil -- set to robot id to force using this robot as keeper
+Settings.positionPadding = 0.02
+Settings.forceKeeperId = nil -- set to robot id to force using this robot as keeper
+
+Settings.ballOwnDistance = 0.05
+Settings.ballOwnHysterese = 0.1
 
 return Settings
