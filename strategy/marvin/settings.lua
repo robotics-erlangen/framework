8b868a6cd330cab4e9219c26f6d3f2129679fb7d
André Pscherer <andre.pscherer@robotics-erlangen.de>
2012-12-13 19:01:18 +0100

    Hysteresis not Hysterese

--- strategy/marvin/settings.lua
+++ strategy/marvin/settings.lua
@@ -5,6 +5,6 @@ Settings.positionPadding = 0.02
 Settings.forceKeeperId = nil -- set to robot id to force using this robot as keeper
 
 Settings.ballOwnDistance = 0.05
-Settings.ballOwnHysterese = 0.1
+Settings.ballOwnHysteresis = 0.1
 
 return Settings
