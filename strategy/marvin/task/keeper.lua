b1d2d482f484834fdda568dfaba29842d17b8d25
Michael Eischer <michael.eischer@robotics-erlangen.de>
2012-12-26 15:11:47 +0100

    Marvin: Ported ball shot prediction from wopr + some crash fixes for the keeper

--- strategy/marvin/task/keeper.lua
+++ strategy/marvin/task/keeper.lua
@@ -13,20 +13,21 @@ end
 
 --moves keeper do defending possition
 function Keeper:_run(priorityMessages, notifications)
-	local atkPos, atkDir, isShot = Observer.--todo predictShot
+	--TODO: add obstacles if outside keeper area
+	local atkPos, atkDir, isShot = Observer.predictShot()
 	--Defending possition if ball is allready shot: shortest way to stop the ball
 	if isShot then
 		local movTo = self._robot.pos:nearestPosOnLine(atkPos, atkPos+atkDir)
-		local faceBall = World.Ball.Pos-movTo
-		self._robot.trajectory:update(ToTarget, movTo, faceBall)
+		local faceBall = World.Ball.pos-movTo
+		self._robot.trajectory:update(ToTarget, movTo, faceBall:angle())
 	--Defending possition to block possible Goal shots: Moves along a straigt line in front of the goal: distance to goal: Settings.keeperGoalDistance
 	else
 		local goalLinePos = Vector.create(0 ,World.Geometry.FriendlyGoal.y + Settings.keeperGoalDistance)
 		local goalLineDir = Vector.create(1,0)
 		local movTo = geom.intersectLineLine(atkPos, atkDir, goalLinePos, goalLineDir)
-		movTo.x = math.bound(-World.Geometry.GoalWidth.x / 2, movTo.x, World.Geometry.GoalWidth.x / 2) --don't move out of the goal
+		movTo.x = math.bound(-World.Geometry.GoalWidth / 2, movTo.x, World.Geometry.GoalWidth / 2) --don't move out of the goal
 		local faceBall = World.Ball.pos-movTo
-		self._robot.trajectory:update(ToTarget, movTo, faceBall)
+		self._robot.trajectory:update(ToTarget, movTo, faceBall:angle())
 	end
 end
 
