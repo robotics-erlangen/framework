26051d2903049b9ff66c144b019dc6c36c777a5e
Pavel Marchenko <pavel.marchenko@robotics-erlangen.de>
2013-01-22 20:28:18 +0100

    finished opponent max acceleration evaluator

--- strategy/marvin/observer/robot.lua
+++ strategy/marvin/observer/robot.lua
@@ -1,7 +1,30 @@
 local Robot = {}
+local World = require "../base/world"
 
-function Robot.estimatePos(robot, time)
-	-- body
+local lastspeed = {}
+local accelerationSmoothed = {}
+local alpha = 0.1
+local accelerationSmoothedMax = {}
+
+function Robot.estimateAcceleration() -- opponent robots actual and maximal acceleration estimation 
+	for _, robot in pairs(World.OpponentRobots) do
+		if lastspeed[robot.id] then
+			local accel = (robot.speed - lastspeed[robot.id]) / World.TimeDiff -- classic derivative without smoothing
+			accelerationSmoothed[robot.id] = alpha * accel + (1 - alpha) * accelerationSmoothed[robot.id] -- smoothed acceleration curve
+		end
+		lastspeed[robot.id]=robot.speed
+		if accelerationSmoothedMax[robot.id] then
+			if accelerationSmoothedMax[robot.id] < accelerationSmoothed[robot.id] then
+				accelerationSmoothedMax[robot.id] = accelerationSmoothed[robot.id]
+			end
+		else
+			accelerationSmoothedMax[robot.id] = accelerationSmothed[robot.id]
+		end
+	end
+end
+
+function Robot.getAcceleration(rID)
+	return accelerationSmoothedMax[rID]
 end
 
 function Robot.minTimeToBall(robot, ball)
