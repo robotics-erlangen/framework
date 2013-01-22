050589b1fd44cac4bab238b8374e714c712de651
André Pscherer <andre.pscherer@robotics-erlangen.de>
2013-01-22 21:39:55 +0100

    added currentRating to ShootGoal (wird eh wieder gelöscht, sobald die conditions ausgereift sind)

--- strategy/marvin/play/shootgoal.lua
+++ strategy/marvin/play/shootgoal.lua
@@ -51,4 +51,42 @@ function ShootGoal.startRating(attackers, defenders, minRating)
 	end
 end
 
+function ShootGoal:currentRating()
+	if World.RefereeState ~= "Game" then
+		return Base.rating.no
+	end										-- same as startRating from here
+	local ballOwner = Observer.Ball.ballOwner()
+	if ballOwner and ballOwner.isFriendly then
+		local ball = World.Ball
+		if ballOwner:hasBall(ball) then
+			local robots = {}
+			for _,r in ipairs(World.Robots) do
+				if r.pos.y > ball.pos.y then
+					table.insert(robots, r)
+				end
+			end
+			local freeSectors = Observer.Goal.freeSectors(ball.pos, robots, true)
+			local bestRating, bestSector = 0, 1
+			for k, fs in ipairs(freeSectors) do
+				local rating = (fs[2] - fs[1])*(10 - geom.getAngleDiff(ballOwner.dir, 0.5*(fs[1] + fs[2]))^2)
+				if rating > bestRating then
+					bestRating = rating
+					--bestSector = k
+				end
+			end
+			if bestRating > 1.50861 then -- OBACHT! never tested magic constant
+				return Base.rating.yes
+			elseif bestRating > 0.87350 then -- OBACHT! never tested magic constant
+				return Base.rating.perhaps
+			else
+				return Base.rating.no
+			end
+		else
+			return Base.rating.no
+		end
+	else
+		return Base.rating.no
+	end
+end
+
 return ShootGoal
