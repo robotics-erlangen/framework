ce4115b8c1e76657fa39d48108ecf5e5555db909
Christian Lobmeier <christian.lobmeier@robotics-erlangen.de>
2013-01-22 22:12:38 +0100

    test function for Ball.atTime

--- strategy/marvin/tests/observer/ball.lua
+++ strategy/marvin/tests/observer/ball.lua
@@ -1,6 +1,7 @@
 local BallTest = {}
 
 local Ball = require "observer/ball"
+local World = require "../base/world"
 local vis = require "../base/vis"
 
 function BallTest.testBallOwner()
@@ -11,7 +12,11 @@ function BallTest.testBallOwner()
 
 end
 
-
-
+function BallTest.testAtTime()
+	local moments = {0.2, 0.5, 1}
+	for _,t in pairs(moments) do
+		vis.addCircle("Future Ball Pos", Ball.atTime(t).pos, World.Ball.radius, vis.colors.orangeHalf, true)
+	end
+end
 
 return BallTest
