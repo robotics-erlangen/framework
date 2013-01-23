59207ee60f0821dda96a81415717b6f03d0b1017
Michael Eischer <michael.eischer@robotics-erlangen.de>
2013-01-23 22:10:38 +0100

    Marvin: Better Robot.minToToBall estimation

--- strategy/marvin/observer/robot.lua
+++ strategy/marvin/observer/robot.lua
@@ -1,5 +1,6 @@
 local Robot = {}
 local World = require "../base/world"
+local Constants = require "../base/constants"
 
 local lastspeed = {}
 local accelerationSmoothed = {}
@@ -28,10 +29,54 @@ function Robot.getAcceleration(rID)
 end
 
 function Robot.minTimeToBall(robot, ball)
-	local posDiff = robot.pos - ball.pos
-	 -- only add ball speed if it moves towards us
-	local ballSpeedToRobot = math.max(0, ball.speed:dot(posDiff) / posDiff:length())
-	return math.max(0, posDiff:length() - robot.radius - ball.radius) / (robot.maxSpeed + ballSpeedToRobot)
+	local posDiff = (ball.pos - robot.pos):normalize()
+	local dist = ball.pos:distanceTo(robot.pos) - ball.radius - robot.radius
+	-- speed of ball and robot towards each other
+	local robotSpeed = posDiff:dot(robot.speed)
+	local robotAccel = robot.maxAcceleration
+	
+	local ballSpeed = -posDiff:dot(ball.speed)
+	local ballAccel
+	if ballSpeed == 0 then -- prevent division by zero for timeBall
+		ballAccel = 1 -- only used together with ballSpeed
+	else
+		ballAccel = (ballSpeed / ball.speed:length()) * Constants.ballDeceleration
+	end
+	
+	-- x(t) = x0 - integral(0 to t, v_r(t) + v_b(t) dt)
+	-- solve x(t) = 0 for t
+	-- v_r(t) = v_r_0 + a_r*t  if t < (v_max - v_r_0)/a_r
+	--          v_max          otherwise
+	-- v_b(t) = v_b_0 + a_b*t  if t < v_b_0 / a_b
+	--          0              otherwise
+	-- a_r is robot acceleration
+	-- a_b is ball deceleration along direction towards the robot
+	
+	-- times until full acceleration / stop and distances traveled until then
+	local timeRobot = math.max(0, (robot.maxSpeed - robotSpeed) / robotAccel)
+	local distRobot = robotSpeed * timeRobot + robotAccel * timeRobot^2 * 0.5
+	local timeBall = math.max(0, -ballSpeed / ballAccel)
+	local distBall = ballSpeed * timeBall + ballAccel * timeBall^2 * 0.5
+	
+	-- Solve equations for each interval and check that the result is in it
+	local t = math.solveSq((robotAccel+ballAccel)*0.5, robotSpeed+ballSpeed, -dist)
+	if t and t <= math.min(timeRobot, timeBall) then
+		return t < 0 and 0 or t
+	end
+	
+	if timeRobot < timeBall then
+		local distLeft = dist - distRobot + timeRobot * robot.maxSpeed
+		t = math.solveSq(ballAccel * 0.5, robot.maxSpeed + ballSpeed, -distLeft)
+	elseif timeBall < timeRobot then
+		local distLeft = dist - distBall
+		t = math.solveSq(robotAccel * 0.5, robotSpeed, -distLeft)
+	end
+	if t and t >= math.min(timeRobot, timeBall) and t <= math.max(timeRobot, timeBall) then
+		return t
+	end
+	
+	local distLeft = dist - distRobot - distBall + timeRobot * robot.maxSpeed
+	return distLeft / robot.maxSpeed
 end
 
 return Robot
