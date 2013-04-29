45cf6507431980474f12e5badd8fa9183d3a7817
Werner Sembach <werner.sembach@robotics-erlangen.de>
2013-04-29 18:51:15 +0200

    rating fuction implemented

--- strategy/marvin/task/receivepass.lua
+++ strategy/marvin/task/receivepass.lua
@@ -4,6 +4,8 @@ local World = require "../base/world"
 local ToTarget = require "trajectory/totarget"
 local Observer = require "observer/ball"
 local geom = require "../base/geom"
+local Rating = require "util/rating"
+local vis = require "../base/vis"
 
 ReceivePass.priority = 5
 
@@ -14,18 +16,8 @@ function ReceivePass:_run(priorityMessages, notifications)
 	self._robot.path:setDefaultObstacles(self._robot, true)
 	self._robot.path:addRobotObstacles(self._robot)
 	
-	local ballSpeed = World.Ball.speed:length()
-	-- bei schnellen Baellen in den Weg stellen und abfangen
-	if ballSpeed > Settings.slowBall then
-		local movTo = self._robot.pos:nearestPosOnLine(World.Ball.pos, World.Ball.pos+(World.Ball.speed * 30))
-		local faceBall = (World.Ball.pos-movTo):angle()
-		self._robot.trajectory:update(ToTarget, movTo, faceBall)
-	--bei langsamen Baellen entgegenbewegen
-	else
-		local movTo = World.Ball.pos - (World.Ball.pos - self._robot.pos):setLength(self._robot.shootRadius)
-		local faceBall = (World.Ball.pos-movTo):angle()
-		self._robot.trajectory:update(ToTarget, movTo, faceBall)
-	end
+	local faceBall = (World.Ball.pos-self.moveTo):angle()
+	self._robot.trajectory:update(ToTarget, self.moveTo, faceBall)
 end
 
 function ReceivePass.factory(position)
@@ -40,6 +32,21 @@ function ReceivePass.test(id)
 		return nil
 	end
 	return ReceivePass.factory(1), 1
+end
+
+function ReceivePass:_rate()
+	local ballSpeed = World.Ball.speed:length()
+	-- bei schnellen Baellen in den Weg stellen und abfangen
+	if ballSpeed > Settings.slowBall then
+		self.moveTo = self._robot.pos:nearestPosOnLine(World.Ball.pos, World.Ball.pos+(World.Ball.speed * 30))
+	--bei langsamen Baellen entgegenbewegen
+	else
+		self.moveTo = World.Ball.pos - (World.Ball.pos - self._robot.pos):setLength(self._robot.shootRadius)
+	end
+	
+	vis.addCircle("RecivePassMoveTo", self.moveTo, 0.03, blue, true)
+	
+	return Rating.posToRating(self._robot, self.moveTo)
 end
 
 return ReceivePass
