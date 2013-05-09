8b19ef68968690784e64bb3994172757c2146535
Michael Eischer <michael.eischer@robotics-erlangen.de>
2013-05-09 23:30:21 +0200

    Marvin: Fix shoot-Task shooting with successProbability 0 and not doing anything if it can't shoot yet

--- strategy/marvin/task/shoot.lua
+++ strategy/marvin/task/shoot.lua
@@ -17,18 +17,20 @@ function Shoot:_shoot(targetPos, targetSpeed, linearShoot, probabilityThreshold)
 		-- TODO: check future to see whether probability will decrease
 		-- TODO: test whether to add a delay when probability decreases
 		if successProbability > probabilityThreshold
-				or successProbability <= self._lastSuccessProbability then
+				or successProbability < self._lastSuccessProbability then
 			self._shootHysteresis = self._shootHysteresis + 2
 		else
 			self._shootHysteresis = math.max(self._shootHysteresis - 1, 0)
 		end
 		self._lastSuccessProbability = successProbability
+		
+		-- FIXME drive towards hit point and not where the ball currently is
+		local speed = World.Ball.pos - self._robot.pos
+		speed:setLength(Settings.shootDriveSpeed)
+		local targetDir = (targetPos-self._robot.pos):angle()
+		self._robot.trajectory:update(TrajectoryDirect, speed, targetDir)
+		
 		if self._shootHysteresis > 0 then
-			local speed = World.Ball.pos - self._robot.pos
-			speed:setLength(Settings.shootDriveSpeed)
-			local targetDir = (targetPos-self._robot.pos):angle()
-			self._robot.trajectory:update(TrajectoryDirect, speed, targetDir)
-			
 			local dist = (targetPos-self._robot.pos):length()
 			self._robot:shoot(targetSpeed, dist)
 		end
