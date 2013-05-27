b960abbf6ffb0d43edf4b99a6189805e7e884e6f
Michael Eischer <michael.eischer@robotics-erlangen.de>
2013-05-27 20:03:57 +0200

    Marvin: Fix shoot dir calculation

--- strategy/marvin/task/shoot.lua
+++ strategy/marvin/task/shoot.lua
@@ -27,7 +27,7 @@ function Shoot:_shoot(targetPos, targetSpeed, linearShoot, probabilityThreshold)
 		-- FIXME drive towards hit point and not where the ball currently is
 		local speed = World.Ball.pos - self._robot.pos
 		speed:setLength(Settings.shootDriveSpeed)
-		local targetDir = (targetPos-self._robot.pos):angle()
+		local targetDir = (targetPos-World.Ball.pos):angle()
 		self._robot.trajectory:update(TrajectoryDirect, speed, targetDir, 0)
 		
 		if self._shootHysteresis > 0 then
