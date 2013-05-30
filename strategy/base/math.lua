9eb8772e9d7d0acfe22a6fc2a8cc78d4a7adf86b
Christian Lobmeier <christian.lobmeier@robotics-erlangen.de>
2013-05-30 22:05:29 +0200

    quartic equation solution algorithm, even works for some inputs

--- strategy/base/math.lua
+++ strategy/base/math.lua
@@ -3,6 +3,12 @@
 module "math"
 ]]--
 
+local suc, Complex = pcall(require, "base/complex")
+if not suc then
+	Complex = require "complex"
+end
+
+
 --- Limits value to interval [min, max].
 -- @name bound
 -- @param min number - lower bound of interval
@@ -56,7 +62,21 @@ function math.round(val, digits)
 	return math.floor(val * fac + 0.5) / fac
 end
 
---- Solves a*t^2+b*t+c for t
+
+--- Solves a*t + b for t
+--@name solveLin
+--@param a number
+--@param b number
+--@return [number]
+function math.solveLin(a, b)
+	if a == 0 then
+		return 
+	end
+	return -b/a
+end
+
+
+--- Solves a*t^2 + b*t + c for t
 -- @name solveSq
 -- @param a number
 -- @param b number
@@ -64,6 +84,10 @@ end
 -- @return [number - smallest positive solution or largest
 -- @return [number]]
 function math.solveSq(a, b, c)
+	if a == 0 then
+		solveLin(b, c)
+	end
+	
 	local det = b*b - 4*a*c
 	if det < 0 then
 		return
@@ -83,6 +107,46 @@ function math.solveSq(a, b, c)
 		return t2, t1
 	end
 end
+
+--- Solves a*t^4 + b*t^3 + c*t^2 + d*t + e for t
+-- @name solveQrt
+-- @param a number
+-- @param b number
+-- @param c number
+-- @param d number
+-- @param e number
+-- @return ?
+function math.solveQrt(a, b, c, d, e)
+	--http://de.wikipedia.org/wiki/Quartische_Gleichung 
+	local alpha = -(3*b*b)/(8*a*a) + c/a
+	local beta = (b*b*b)/(8*a*a*a) - (b*c)/(2*a*a) + d/a
+	local gamma = -(3*b*b*b*b)/(256*a*a*a*a) + (b*b*c)/(16*a*a*a) - (b*d)/(4*a*a) + e/a
+	local P = -(alpha*alpha)/12 - gamma
+	local Q = -(alpha*alpha*alpha)/108 + (alpha*gamma)/3 - (beta*beta)/8
+	local qproot = (Q*Q)/4 + (P*P*P)/27
+	if qproot < 0 then
+		return --no real solution
+	end
+	
+	local calpha = Complex.create(alpha, 0)
+	local cbeta = Complex.create(beta, 0)
+	local cgamma = Complex.create(gamma, 0)
+	
+	local U = math.pow(-Q/2+math.sqrt(qproot),1/3)
+	local y = -5/6 * alpha + U - P/(3*U)
+
+	local cwsq = Complex.create(alpha + 2*y, 0)
+	local cw = cwsq:sqrt()
+	local sposrpos, sposrneg = (-(calpha + 2*y) - (calpha + cbeta/cw)*2):sqrt()
+	local x1 = (cw + sposrpos)/2 - b/4*a
+	local x2 = (cw + sposrneg)/2 - b/4*a
+	local snegrpos, snegrneg = (-(calpha + 2*y) - (calpha - cbeta/cw)*2):sqrt()
+	local x3 = (-cw + snegrpos)/2 - b/4*a
+	local x4 = (-cw + snegrneg)/2 - b/4*a
+		
+	return x1, x2, x3, x4
+end
+
 
 --- "Calculates" the signum of a number
 -- @name sign
