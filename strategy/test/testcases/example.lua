ab37170802773c8a49294b1b5440d12872b3dedf
Michael Eischer <michael.eischer@robotics-erlangen.de>
2013-06-03 20:55:03 +0200

    Strategy: Add simple geom test as an example

--- strategy/test/testcases/example.lua
+++ strategy/test/testcases/example.lua
@@ -1,3 +1,5 @@
+local geom = require "../base/geom"
+
 return function()
 context("A context", function()
 	before(function() end)
@@ -14,6 +16,22 @@ context("A context", function()
 	end)
 	test("A test in the top-level context", function()
 		assert_equal(3, 1)
+	end)
+	test("geom test", function()
+		local ret, l1, l2 = geom.intersectLineLine(Vector.create(0, 0), Vector.create(0, 1), Vector.create(0, 1), Vector.create(0, -1))
+		assert_equal(ret, Vector.create(0, 0))
+		assert_equal(l1, 0)
+		assert_equal(l2, 0)
+		
+		local ret, l1, l2 = geom.intersectLineLine(Vector.create(0, 0), Vector.create(0, 1), Vector.create(0.1, 1), Vector.create(0, -1))
+		assert_equal(ret, nil)
+		assert_equal(l1, nil)
+		assert_equal(l2, nil)
+		
+		local ret, l1, l2 = geom.intersectLineLine(Vector.create(0, 0), Vector.create(0, 1), Vector.create(1, 1), Vector.create(1, 0))
+		assert_equal(ret, Vector.create(0, 1))
+		assert_equal(l1, 1)
+		assert_equal(l2, -1)
 	end)
 end)
 end
