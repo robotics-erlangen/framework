d56faab2230e26aea2820396802368f1cb199a1f
Christian Lobmeier <christian.lobmeier@robotics-erlangen.de>
2013-06-10 22:17:38 +0200

    fixed task switching in a/k/handleball

--- strategy/marvin/agent/keeper/handleball.lua
+++ strategy/marvin/agent/keeper/handleball.lua
@@ -7,6 +7,7 @@ local Referee = require "util/referee"
 local ChipAway = require "task/chipaway"
 local AggressiveKeeper = require "task/aggressivekeeper"
 
+
 function HandleBall:_check()
 	if Referee.isStopState() then
 		return Base.State.Inactive
@@ -16,41 +17,26 @@ function HandleBall:_check()
 	return active and Base.State.Active or Base.State.Inactive
 end
 
-function HandleBall:_run() 
-	--hysteresis constant
-	local extraDistance = 0.1
-
-	local stillDanger = false
-	if not self.badrobots then
-		self.badrobots = {}
-	else
-		for r,_ in pairs(self.badrobots) do
-			if Field.distanceToFriendlyDefenseArea(r.pos, r.radius) <= extraDistance then
-				--if a dangerous robot is still near the defense area
-				stillDanger = true
-			else
-				--if too far away, remove that robot
-				self.badrobots[r] = nil
-			end
-		end
-	end
-
+function HandleBall:_run()	
 	--track opponent robots in defense area
+	local danger = false
 	for _,r in pairs(World.OpponentRobots) do
 		if Field.isInFriendlyDefenseArea(r.pos, r.radius) then
-			--if any robot is inside the defense area, it becomes a dangerous one
-			self.badrobots[r] = true
-			stillDanger = true
+			danger = true
+			
+			--set the task to nil to ensure that a new task (AggressiveKeeper) will be created
+			self._task = nil
 		end
 	end
 	
 	--decide whether to chip away or move aggressively to the ball
-	-- FIXME don't recreate the task in every frame
-	if not stillDanger then
-		self._task = ChipAway.create(self._robot)
-	else
-		self._task = AggressiveKeeper.create(self._robot)
-	end
+	if not self._task then
+		if danger then
+			self._task = AggressiveKeeper.create(self._robot)
+		else
+			self._task = ChipAway.create(self._robot)
+		end
+	end	
 end
 
 return HandleBall
