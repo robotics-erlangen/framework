b920a4a86853251643855d65a071bcd2cf54f6a4
Michael Eischer <michael.eischer@robotics-erlangen.de>
2013-06-13 19:31:21 +0200

    Marvin: Shoot: compensate ball movement, should turn with dontMoveWithBall flag

--- strategy/marvin/task/shoot.lua
+++ strategy/marvin/task/shoot.lua
@@ -29,17 +29,21 @@ function Shoot:_shoot(targetPos, targetSpeed, linearShoot, probabilityThreshold,
 		end
 		self._lastSuccessProbability = successProbability
 		
-		local distToBall = (self._robot:posToBall(World.Ball)).x
+		-- FIXME drive towards hit point and not where the ball currently is
+		local distToBall = self._robot:posToBall(World.Ball)
+		local targetDir = (targetPos - World.Ball.pos):angle()
+		local speed = World.Ball.speed
+		if math.abs(distToBall.y) >= 0.01 then
+			speed = Vector.fromAngle(targetDir):perpendicular():setLength(distToBall.y / 10) -- correct pos error in 100ms
+		end
 
-		if not (distToBall < 0.005 and dontMoveWithBall) then
-			-- FIXME drive towards hit point and not where the ball currently is
+		if not (distToBall.x < 0.005 and dontMoveWithBall) then
 			local speedDir = (World.Ball.pos - self._robot.pos):angle()
-			local targetDir = (targetPos - World.Ball.pos):angle()
 			-- double angle between targetDir and speedDir, but limit to 90 degree
 			local speedAngle = targetDir + math.bound(-math.pi/2, 2*geom.getAngleDiff(targetDir, speedDir), math.pi/2)
-			local speed = Vector.fromAngle(speedAngle):setLength(Settings.shootDriveSpeed)
-			self._robot.trajectory:update(TrajectoryDirect, speed, targetDir)
+			speed = speed + Vector.fromAngle(speedAngle):setLength(Settings.shootDriveSpeed)
 		end
+		self._robot.trajectory:update(TrajectoryDirect, speed, targetDir)
 
 		if self._shootHysteresis > 0 then
 			local dist = (targetPos-self._robot.pos):length()
