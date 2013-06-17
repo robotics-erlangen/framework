99eb1a28a0545d429917f11b40a698c135e7c38d
Christian Lobmeier <christian.lobmeier@robotics-erlangen.de>
2013-06-18 01:06:37 +0200

    implemented aggressive keeper

--- strategy/marvin/task/aggressivekeeper.lua
+++ strategy/marvin/task/aggressivekeeper.lua
@@ -1,6 +1,8 @@
 local AggressiveKeeper = (require "../base/class").new("Task.AggressiveKeeper", require "task/base")
 
 local World = require "../base/world"
+local Ball = require "observer/ball"
+local ToTarget = require "trajectory/totarget"
 
 AggressiveKeeper.priority = 6
 
@@ -8,7 +10,13 @@ function AggressiveKeeper:_init()
 end
 
 function AggressiveKeeper:_run() 
-	--TODO
+	local tpos, ttime = Ball.toBall(self._robot, World.Ball)
+	
+	local fromGoal = (tpos - World.Geometry.FriendlyGoal):angle()
+
+	self._robot:chip(1)
+	self._robot.path:setDefaultObstacles(self._robot, true)
+	self._robot.trajectory:update(ToTarget, tpos, fromGoal, nil, 1) --FIXME magic constant for end speed: 1m/s 
 end
 
 function AggressiveKeeper:_rate()
