8d5b16a67d03f74cdb9b3e7e01f2184af879e09c
Alexander Danzer <alexander.danzer@robotics-erlangen.de>
2013-06-21 12:21:31 +0200

    Marvin: Replace ManMark with FarMirror when there is no Opponent in own half of field

--- strategy/marvin/agent/defender/default.lua
+++ strategy/marvin/agent/defender/default.lua
@@ -1,15 +1,31 @@
 local Base = require "agent/base/behaviour"
 local Default = (require "../base/class").new("Agent.Defender.Default", Base)
 
+local World = require "../base/world"
+local Class = require "../base/class"
 local ManMark = require "task/manmark"
+local FarMirror = require "task/farmirror"
 
 function Default:_check()
 	return Base.State.Active
 end
 
 function Default:_run()
-	if not self._task then
-		self._task = ManMark.create(self._robot)
+	local opponentsInOurHalf = false
+	for _, robot in ipairs(World.OpponentRobots) do
+		if robot.pos.y < 0 then
+			opponentsInOurHalf = true
+		end
+	end
+
+	if opponentsInOurHalf then
+		if not self._task or Class.name(self._task, true) ~= "ManMark" then
+			self._task = ManMark.create(self._robot)
+		end
+	else
+		if not self._task or Class.name(self._task, true) ~= "FarMirror"then
+			self._task = FarMirror.create(self._robot)
+		end
 	end
 end
 
