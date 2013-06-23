df90ffb585273706ece4590f228c1672762d7697
Michael Eischer <michael.eischer@robotics-erlangen.de>
2013-06-24 01:07:58 +0200

    Revert "Marvin: Kepper Task: Use last known ball position if invisible"
    This reverts commit 96965664a0c3dd9f22c71784a60b34c25d1d3c9e.

--- strategy/marvin/task/keeper.lua
+++ strategy/marvin/task/keeper.lua
@@ -11,7 +11,6 @@ local Field = require "util/field"
 Keeper.priority = 6
 
 function Keeper:_init()
-	self._ballPos = World.Ball.pos -- save position for vision problems
 end
 
 --moves keeper do defending possition
@@ -59,10 +58,7 @@ function Keeper:_run(priorityMessages, notifications)
 	moveTo.y = moveTo.y - (math.abs(moveTo.x) - (World.Geometry.GoalWidth / 2 - Settings.keeperGoalDistance))
 	end
 	
-	if World.Ball:isPositionValid() then
-		self._ballPos = World.Ball.pos
-	end
-	local faceBall = self._ballPos-moveTo
+	local faceBall = World.Ball.pos-moveTo
 	self._robot.trajectory:update(ToTarget, moveTo, faceBall:angle())
 end
 
