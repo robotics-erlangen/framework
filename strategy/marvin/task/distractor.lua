7314b93f4a9af67a6c17df6c26c463f88e9d9ac5
Christian Lobmeier <christian.lobmeier@robotics-erlangen.de>
2013-06-24 16:07:01 +0200

    Marvin: new distractor task for skuba freekick

---
+++ strategy/marvin/task/distractor.lua
@@ -0,0 +1,61 @@
+local Distractor = (require "../base/class").new("Task.Distractor", require "task/base")
+
+local World = require "../base/world"
+local ToTarget = require "trajectory/totarget"
+local Rating = require "util/rating"
+
+Distractor.priority = 3
+
+local distractionX = {0.8, 1.0, 1.2}
+local distractionY = {2.0, 2.2, 2.4}
+
+function Distractor:_init()
+end
+
+function Distractor:_run(priorityMessages, notifications)
+	self._robot.path:setDefaultObstacles(self._robot)
+	self._robot.path:addRobotObstacles(self._robot)
+
+	self._preferredDir = (World.Geometry.OpponentGoal - self._targetPos):angle()
+	self._robot.trajectory:update(ToTarget, self._targetPos, self._preferredDir)
+	
+	return { distractedIndex = self._index }
+end
+
+function Distractor:_rate(priorityMessages, notifications)
+	local indices = {}
+	for _, msg in pairs(priorityMessages) do
+		local index = msg.task.distractedIndex
+		if index then
+			indices[index] = true
+		end
+	end
+	local targetIndex = 1
+	while indices[targetIndex] do
+		targetIndex = targetIndex + 1
+	end
+	if targetIndex > #distractionX then
+		error("too many distractors / too few distraction positions")
+	end
+	
+	self._index = targetIndex
+	self._targetPos = Vector.create((World.Ball.pos.x > 0 and -1 or 1) * distractionX[self._index], distractionY[self._index])
+
+	return Rating.posToRating(self._robot, self._targetPos)
+end
+
+function Distractor.factory(position)
+	local f = function (robots)
+		return Distractor.create(robots[position])
+	end
+	return f
+end
+
+function Distractor.test(id)
+	if id > 2 then
+		return nil
+	end
+	return Distractor.factory(1), 1
+end
+
+return Distractor
