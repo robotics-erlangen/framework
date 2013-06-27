3107789a50510d5476ae2557ee687c2b0fd42b4d
Christian Lobmeier <christian.lobmeier@robotics-erlangen.de>
2013-06-27 14:53:34 +0200

    Marvin: duel task gets active more frequently

--- strategy/marvin/agent/attacker/duel.lua
+++ strategy/marvin/agent/attacker/duel.lua
@@ -5,7 +5,20 @@ local Ball = require "observer/ball"
 local TaskDuel = require "task/duel"
 
 function Duel:_check()
-	if Ball.opponentBallOwner() then
+	local timeAdvance = 0.2
+	
+	local robot, time = Ball.firstAtBall()
+	local friendly = true
+	if robot then
+		friendly = robot.isFriendly
+	end
+	if self._active and friendly and time > timeAdvance then
+		self._active = false
+	elseif not self._active and not friendly then
+		self._active = true
+	end
+	
+	if self._active then
 		return Base.State.Active
 	elseif self._state == Base.State.Active and Ball.friendlyBallOwner() == self._robot then
 		return Base.State.Active
