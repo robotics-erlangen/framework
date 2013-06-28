a1d566608afa2f544b2e46ea70b77d7631128fbe
Michael Eischer <michael.eischer@robotics-erlangen.de>
2013-06-28 12:54:39 +0200

    Marvin: Hopefully fixed ReceivePass task

--- strategy/marvin/agent/attacker/receivepass.lua
+++ strategy/marvin/agent/attacker/receivepass.lua
@@ -1,37 +1,48 @@
 local Base = require "agent/base/behaviour"
 local ReceivePass = (require "../base/class").new("Agent.Attacker.ReceivePass", Base)
 local World = require "../base/world"
-local Ball = require "observer/ball"
-local Referee = require "util/referee"
+local Class = require "../base/class"
 
+local Ball = require "observer/ball"
+local Robot = require "observer/robot"
+local Rating = require "util/rating"
+local Referee = require "util/referee"
 
 local PassReceiver = require "task/passreceiver"
 local PassTarget = require "task/passtarget"
 
+-- the agent can become passReceiver if the last passTarget notification
+-- isn't older than this timeout
+local passTargetTimeout = 0.2
+
 function ReceivePass:_check()
-	local isPassTarget = false
 	for robot, msg in pairs(self._messages) do
 		if msg.task.passTarget == self._robot then
-			isPassTarget = true
+			self._targetTimer = World.Time
 			break
 		end
 	end
 
-	if isPassTarget then
-		if Ball.isShot() then
-			self._catchingPass = true
-			self._task = nil
-		end
-		return Base.State.Active
-	elseif self._catchingPass then
-		if Ball.opponentBallOwner() or Ball.friendlyBallOwner() or World.Ball.speed:length() < Settings.slowBall then
+	if self._catchingPass then
+		-- abort if someone has the ball, the ball is slow or the game is stopped
+		if Ball.opponentBallOwner() or Ball.friendlyBallOwner() or World.Ball.speed:length() < Settings.slowBall
+				or Referee.isStopState() then
 			return Base.State.Inactive
 		end
-		-- force being mainAttacker
-		local message = { specialTask = { mainAttacker = 2 } }
-		if not Referee.isStopState() then
-			return Base.State.Active, message
+		-- force being mainAttacker, suppress other passReceivers
+		return Base.State.Active, { specialTask = { mainAttacker = 2, passReceiver = 2 } }
+
+	elseif self._targetTimer and World.Time - self._targetTimer < passTargetTimeout then
+		-- apply for becoming pass receiver
+		local timeToBall = Robot.minTimeToBall(self._robot, World.Ball)
+		local passReceiverRating = Rating.timeToRating(timeToBall)
+		message = { specialTask = { passReceiver = passReceiverRating } }
+
+		local passReceiver = self._trainerMessage.specialTask.passReceiver
+		if (not passReceiver or passReceiver == self._robot) and Ball.isShot() then
+			self._catchingPass = true
 		end
+		return Base.State.Active, message
 	end
 	return Base.State.Inactive
 end
@@ -41,10 +52,12 @@ function ReceivePass:_stop(isAborted)
 end
 
 function ReceivePass:_run()
-	if not self._task then
-		if self._catchingPass then
+	if self._catchingPass then
+		if not self._task or Class.instanceOf(self._task, PassReceiver) then
 			self._task = PassReceiver.create(self._robot)
-		else
+		end
+	else
+		if not self._task or Class.instanceOf(self._task, PassTarget) then
 			self._task = PassTarget.create(self._robot)
 		end
 	end
