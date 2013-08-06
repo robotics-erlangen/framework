49f8a1110d12198316c8b1e9bbd3e8c89240b3ce
Michael Eischer <michael.eischer@robotics-erlangen.de>
2013-08-06 23:03:22 +0200

    Marvin: Much more powerful and faster checker for unexpected globals

--- strategy/marvin/init.lua
+++ strategy/marvin/init.lua
@@ -1,26 +1,35 @@
+local validGlobals = {
+	amun = true,
+	log = true, -- amun and log musn't be reported, otherwise the checker will crash
+	path = true,
+	Vector = true,
+	Settings = true,
+	Entrypoints = true
+}
+
+local globalsChecker = {
+	-- there should be only a fixed set of globals, thus this causes no performance hit
+	__newindex = function (t, k, v)
+		if not validGlobals[k] then
+			-- always show a warning
+			log("Setting global " .. tostring(k) .. " to value " .. tostring(v))
+			-- treat as error while debugging
+			if amun and amun.isDebug then
+				error("Unexpected global")
+			end
+		end
+		rawset(t, k, v)
+	end
+}
+setmetatable(_G, globalsChecker)
+
 require "../base/base"
 require "settings"
 require "base/path" -- extend path module
 local World = require "../base/world"
 
-local globals = {}
-local function saveGlobals()
-	globals = {}
-	for k, _ in pairs(_G) do
-		globals[k] = true
-	end
-end
-
-local function checkGlobals()
-	for k, _ in pairs(_G) do
-		if not globals[k] then
-			log("Unexpected global: " .. k)
-		end
-	end
-end
 
 Entrypoints = {}
-saveGlobals()
 -- require "task/tasks"
 require "control/coordinator"
 require "tests/tests"
@@ -29,12 +38,10 @@ require "tests/tests"
 local debug = require "../base/debug"
 local Cache = require "../base/cache"
 local Observer = require "observer/observer"
-checkGlobals()
 
 for name, func in pairs(Entrypoints) do
 	Entrypoints[name] = function ()
 		-- require "../test/debug/enable"
-		saveGlobals()
 		World.update()
 		Observer.observe()
 		if not func() then -- Entrypoint has to return true if robots shouldn't be stopped on halt
@@ -45,7 +52,6 @@ for name, func in pairs(Entrypoints) do
 		World.setRobotCommands()
 		debug.resetStack()
 		Cache.resetFrame()
-		checkGlobals()
 	end
 end
 
