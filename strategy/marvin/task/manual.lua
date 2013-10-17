5f294588dfa9371624f7ac1811d04212c940238c
Christian Lobmeier <christian.lobmeier@robotics-erlangen.de>
2013-10-17 22:55:59 +0200

    Marvin: add some hysteresis to catch ball in manual

--- strategy/marvin/task/manual.lua
+++ strategy/marvin/task/manual.lua
@@ -21,11 +21,18 @@ function Manual:run()
 		local magic = speedAngleDiff * input.speed:length()
 		local viewAngleDiff = math.abs(self._robot.dir - (World.Ball.pos - self._robot.pos):angle())
 		if Ball.friendlyBallOwner() ~= self._robot
-				and magic < 0.5 -- if the robot is about to catch the ball, or something like that
-				and t < 0.3 -- if the ball can be catched quickly
-				and viewAngleDiff < math.pi/2 then -- if the robot looks towards the ball
+			and ((self._cbactive
+				and magic < 1 -- if the robot is about to catch the ball, or something like that
+				and t < 0.5 -- if the ball can be catched quickly
+				and viewAngleDiff < math.pi/2) or -- if the robot looks towards the ball
+			(not self._cbactive
+				and magic < 0.5
+				and t < 0.3
+				and viewAngleDiff < math.pi/1.5)) then
+			self._cbactive = true
 			self:_catchBall(World.Ball.pos*2 - self._robot.pos, Settings.shootDriveSpeed, Constants.positionError)
 		else
+			self._cbactive = false
 			self._robot.trajectory:update(Direct, input.speed, nil, input.omega)
 		end
 	else
