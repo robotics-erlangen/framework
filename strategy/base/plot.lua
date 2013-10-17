1294d71512ebacf3418a20ebbd19f6660305ecb4
Michael Eischer <michael.eischer@robotics-erlangen.de>
2013-10-17 20:30:17 +0200

    Comments for plot module

--- strategy/base/plot.lua
+++ strategy/base/plot.lua
@@ -1,6 +1,14 @@
+--[[
+--- Send plot data to ra
+module "plot"
+]]--
 local plot = {}
 local amun = amun
 
+--- Add data to a plot. Value is used to create a point at the current time
+-- @name addPlot
+-- @param name string - Plot name, seperated layers by '.'
+-- @param value number - value for data point
 function plot.addPlot(name, value)
 	amun.addPlot(name, value)
 end
