51c037ae6e2997a42c85b885f171f00530735053
Christian Lobmeier <christian.lobmeier@robotics-erlangen.de>
2013-10-17 17:36:20 +0200

    Marvin: lineup dodges opponent robots

--- strategy/marvin/util/lineup.lua
+++ strategy/marvin/util/lineup.lua
@@ -4,11 +4,10 @@ local MoveToPos = require "task/movetopos"
 
 
 local function generateLineup(lineStart, lineupDir) 
-	local lineEnd = Vector.fromAngle(lineupDir) -- not really the "end" of the line, more a direction
-	local lineupDistance = 0.08 -- keep 8 cm distance to make taking the robots more comfortable
-	local opponentDistance = 0.03 -- always keep at least 3 cm safety distance to opponent robots
+	local lineEnd = lineStart + Vector.fromAngle(lineupDir) -- not really the "end" of the line, more a direction
+	local lineupDistance = 0.05 -- keep 5 cm distance to make taking the robots more comfortable
+	local mindist = lineupDistance + 0.18 
 	
-	--FIXME
 	local viewDir = lineupDir + math.pi/2 
 	
 	local distances = {}
@@ -16,36 +15,40 @@ local function generateLineup(lineStart, lineupDir)
 	-- filter and sort opponent robots
 	local filter = function(r)
 		local proj, orthDist = r.pos:orthogonalProjection(lineStart, lineEnd)
+		log("orthdist = "..orthDist)
 		distances[r] = proj:distanceTo(lineStart) -- just for calculating the value once, required in compare()
-		return orthDist < 0.09 + opponentDistance + r.radius -- 9 cm = max robot radius
+		return orthDist * orthDist < mindist * mindist -- 9 cm = max robot radius
 	end
 	local compare = function(a, b)
 		return distances[a] < distances[b]
 	end
-	local sortedOpps = table.copy(World.OpponentRobots)
-	table.filter(sortedOpps, filter)
+	local sortedOpps = table.filter(World.OpponentRobots, filter)
 	table.sort(sortedOpps, compare)
-	
-	-- place frienly robots along the given line
+
+	log(#sortedOpps)
+	for i = 1, #sortedOpps do
+	    log(sortedOpps[i])
+	end
+
+	-- place friendly robots along the given line
 	local distToStart = 0
 	local friendlyIndex = 1
 	local opponentIndex = 1
 	while friendlyIndex <= #World.FriendlyRobots do
 		local r = World.FriendlyRobots[friendlyIndex]
-		local opp = World.OpponentRobots[opponentIndex]
-		
-		local intendedPos = lineStart + lineEnd * distToStart
-		if opp.pos:distanceTo(intendedPos) < r.radius + opponentDistance + opp.radius then
-			local distIncrease = (distances[opp] - distToStart) * 2
-			assert(distIncrease > 0, "BUG")
-			distToStart = distToStart + distIncrease
+		local opp = #sortedOpps > 0 and sortedOpps[opponentIndex]
+		local intendedPos = lineStart + (lineEnd - lineStart) * distToStart
+
+		if opp and opp.pos:distanceTo(intendedPos) < mindist then
+			local xdiff = opp.pos.x - lineStart.x
+			distToStart = opp.pos.y - lineStart.y + math.sqrt(mindist * mindist - xdiff * xdiff)	
 			opponentIndex = opponentIndex + 1
 		else
 			local pseudoagent = {robot = function(...) return r end} --FIXME hack
 			local task = MoveToPos.create(pseudoagent, intendedPos, viewDir)
 			task.run(task)
 			friendlyIndex = friendlyIndex + 1
-			distToStart = distToStart + lineupDistance + 2*r.radius
+			distToStart = distToStart + mindist
 		end
 	end
 end
