ffe82e29b860306c3dc8b477874e4d0422242843
Werner Sembach <werner.sembach@robotics-erlangen.de>
2013-12-18 16:24:04 +0100

    Fixes intersections outside the playing field with new boundary and adds new boundary that robot doesn't move through shooting line. TODO: calculating shooting line if ball is not shot.

--- strategy/marvin/task/assistant.lua
+++ strategy/marvin/task/assistant.lua
@@ -21,14 +21,17 @@ function Assistant:_init(pos, radius)
 end
 
 function Assistant:run()
+	--referee handling
 	local offFreeKick = World.RefereeState == "IndirectOffensive" 
 		or World.RefereeState == "DirectOffensive"
 	self.numMapping[3] = (Referee.isStopState() or offFreeKick) and 1.25 or 1.4
-
-	--special case robot is on a line with ball and enemy robot too
+	
+	--some variables
 	local lineY = World.Geometry.FieldHeightQuarter * self.numMapping[self.lineNum]
 	local linePos = Vector.create(-World.Geometry.FieldWidthHalf, lineY)
 	local robotsInWay = false
+	
+	--special case: blocking enemy robot on assistance line
 	for _, robot in ipairs(World.OpponentRobots) do
 		if World.Ball.pos.y < (linePos.y + 0.2) and World.Ball.pos.y > (linePos.y - 0.2) and robot.pos.y < (linePos.y + 0.2) and robot.pos.y > (linePos.y - 0.2) and ((robot.pos.x > World.Ball.pos.x and robot.pos.x < self._robot.pos.x) or (robot.pos.x < World.Ball.pos.x and robot.pos.x > self._robot.pos.x)) then
 			robotsInWay = true
@@ -42,35 +45,35 @@ function Assistant:run()
 		end
 	end
 	
-	-- macht eh nix, da nachrichten nicht existieren?
-	-- local atkPos
-	-- for _, msg in pairs(priorityMessages) do
-	-- 	if msg.task.shotTarget then
-	-- 		atkPos = msg.task.shotTarget
-	-- 	end
-	-- end
-	-- local atkRobot
-	-- for _, msg in pairs(priorityMessages) do
-	-- 	if msg.task.passRobot then
-	-- 		atkRobotPos = msg.task.passRobot.pos
-	-- 	end
-	-- end
-	
+	--bounding of playing field and shooting Line
 	self.shotPos = World.Ball.pos
 	local shotDir = World.Ball.speed:copy():setLength(World.Geometry.FieldHeightHalf)
-	self.shotTarget = --[[atkPos or atkRobotPos or]] (self.shotPos+shotDir)
+	self.shotTarget =self.shotPos+shotDir
+	--TODO shotTarget if ball is not shot
+	local lineStart = linePos
+	local lineEnd = linePos + self.lineDir
+	local northBound = World.Geometry.FieldWidthHalf - self._robot.radius
+	local southBound = -northBound
+	local shotIntersect = geom.intersectLinesByPoints(lineStart, lineEnd, self.shotPos, self.shotTarget)
+	if shotIntersect and shotIntersect.x >= self._robot.pos.x then
+		northBound = math.min(northBound, shotIntersect.x)
+	elseif shotIntersect and shotIntersect.x < self._robot.pos.x then
+		southBound = math.max(southBound, shotIntersect.x)
+	end
 	
+	--referee handling
 	if World.RefereeState == "PenaltyOffensivePrepare" or World.RefereeState == "PenaltyOffensive" then
 		linePos.y = World.Geometry.PenaltyLine - Settings.penaltyLineDistance
 	end
-	local lineStart = linePos
-	local lineEnd = linePos + self.lineDir
-
+	
+	--collection of all intersections
 	local occupiedSectors = {}
-	if World.Ball.pos.y < lineStart.y then -- don't position between ball and goal
+	
+	-- don't position between ball and goal
+	if World.Ball.pos.y < lineStart.y then
 		local goalLeft = geom.intersectLinesByPoints(lineStart, lineEnd, World.Ball.pos, World.Geometry.OpponentGoalLeft)
 		local goalRight = geom.intersectLinesByPoints(lineStart, lineEnd, World.Ball.pos, World.Geometry.OpponentGoalRight)
-		table.insert(occupiedSectors, {goalLeft.x - self._robot.radius, goalRight.x + self._robot.radius})
+		table.insert(occupiedSectors, {math.bound(southBound, goalLeft.x - self._robot.radius, northBound), math.bound(southBound, goalRight.x + self._robot.radius, northBound)})
 	end
 
 	-- always keep distance to ball
@@ -80,7 +83,7 @@ function Assistant:run()
 		if cut1 and cut2 then
 			local min = math.min(cut1.x, cut2.x)
 			local max = math.max(cut1.x, cut2.x)
-			table.insert(occupiedSectors, {min, max})
+			table.insert(occupiedSectors, {math.bound(southBound, min, northBound), math.bound(southBound, max, northBound)})
 		end
 	end
 
@@ -89,7 +92,7 @@ function Assistant:run()
 		if robot.pos.y < lineStart.y and robot.pos.y > World.Ball.pos.y then
 			local tmp = geom.intersectLinesByPoints(lineStart, lineEnd, World.Ball.pos, robot.pos)
 			if tmp then
-				table.insert(occupiedSectors, {tmp.x - self._robot.radius, tmp.x + self._robot.radius})
+				table.insert(occupiedSectors, {math.bound(southBound, tmp.x - self._robot.radius, northBound), math.bound(southBound, tmp.x + self._robot.radius, northBound)})
 			end
 		end
 	end
@@ -101,7 +104,7 @@ function Assistant:run()
 	end
 	for robot, pos in pairs(self._inbox.moveDest()) do
 		if otherAssistants[robot] and math.abs(pos.y - lineStart.y) < 0.5 then
-			table.insert(occupiedSectors, {pos.x - 2*robot.radius, pos.x + 2*robot.radius})
+			table.insert(occupiedSectors, {math.bound(southBound, pos.x - 2*robot.radius, northBound), math.bound(southBound, pos.x + 2*robot.radius, northBound)})
 		end
 	end
 
@@ -109,7 +112,7 @@ function Assistant:run()
 	Interval.sort(occupiedSectors)
 	Interval.merge(occupiedSectors)
 	local widthLimit = World.Geometry.FieldWidthHalf - 2 * self._robot.radius
-	local freeSectors = Interval.negate(occupiedSectors, -widthLimit, widthLimit)
+	local freeSectors = Interval.negate(occupiedSectors, southBound, northBound)
 	
 	--some debug output
 	vis.addPath("ShootingLine", {self.shotPos, self.shotTarget}, vis.colors.blueHalf, true)
@@ -118,24 +121,23 @@ function Assistant:run()
 		vis.addPath("AssistantIntersections" .. self._robot.id, {Vector.create(pos[1], lineStart.y), Vector.create(pos[2], lineStart.y)}, vis.colors.blueHalf, true)
 	end
 	
+	--takes the biggest free space
 	local best = nil
 	local bestSpace = -1
 
-	--takes the biggest free space
 	for _, sector in ipairs(freeSectors) do
 		local space = sector[2] - sector[1]
 		local pos = Vector.create((sector[1] + sector[2]) / 2, lineStart.y)
 		local distance = pos:distanceTo(self._robot.pos)
-		space = space / math.log(distance+math.exp(1)) -- weigth by distance
+		space = space / math.log(distance+math.exp(1)) -- weight by distance
 		if space > bestSpace then
 			best = sector
 			bestSpace = space
 		end
 	end
-
+	
+	--calculate target pos and move
 	local midX = best and (best[1] + best[2]) / 2 or 0 -- fallback to mid
-	local bound = World.Geometry.FieldWidthHalf - self._robot.radius
-	midX = math.bound(-bound, midX, bound)
 	self.targetPos = Vector.create(midX, lineStart.y)
 	self.targetDir = (World.Ball.pos - self.targetPos):angle()
 	self.rating = Rating.posToRating(self._robot, self.targetPos)
