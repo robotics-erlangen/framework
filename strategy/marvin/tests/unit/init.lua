7d49702c45177ed7a7605b06ff8567ebf73e0293
Alexander Danzer <alexander.danzer@robotics-erlangen.de>
2014-01-23 18:24:12 +0100

    Make unit tests individudally selectable

--- strategy/marvin/tests/unit/init.lua
+++ strategy/marvin/tests/unit/init.lua
@@ -2,64 +2,83 @@ local Entrypoints = require "../base/entrypoints"
 local World = require "../base/world"
 
 local tests = require "tests/unit/tests"
-
--- logs have to run as a whole strategy run 
--- because they are printed after the strategy is done
 local total = 0
 for _ in pairs(tests) do
 	total = total + 1
 end
+
+local initialized = false
+local testCount = 0
 local successfull = 0
-local testCount = 1
-local logCount = 1 
 
+-- Note:
+-- message printing and tests have to run in subsequent strategy runs 
+-- because the printing happens after a strategy run is complete
+local namePrinted = false -- set true on the second call of runSingle
 
-local isInitialized = false
-
-local function setupUnitTesting()
-	if not pcall(require, 'debug') then 
-		error("Debugging not enabled!") 
+local function runSingle(name, test)
+	if not namePrinted then
+		log("(" .. (testCount+1) .. "/" .. total .. ") Testing " .. name .. " ... ")
+		namePrinted = true
+	else
+		local startTime = amun.getCurrentTime()
+		local fine, message = pcall(test)
+		local runTime = math.round((amun.getCurrentTime() - startTime)*1000000)/1000
+		if fine then
+			successfull = successfull + 1
+			log("<font color=\"darkgreen\">success (" .. runTime .. "ms)</font>")
+		else
+			log("<font color=\"red\">fail (" .. runTime .. "ms): " .. message .. "</font>")
+		end
+		testCount = testCount + 1
+		namePrinted = false
 	end
-
-	local timeout = 1000000000 -- number of instructions before hook is called
-	debug.sethook(function() error("Timeout (adjustable in tests/unit/init.lua)") end, "", timeout)
-	isInitialized = true
-end
-
-local testName, testFunction = next(tests)
-local function runTest()
-	if testCount == total+1 then -- all tests done
+	if testCount == total then
 		local failing = total-successfull
 		if failing == 0 then
-			log("<font color=\"darkgreen\"> All testcases successfull:)</font>")
+			log("<font color=\"darkgreen\"> All testcases successfull :)</font>")
 		else
 			log("<font color=\"red\">" .. failing .. " failing testcase" 
 				.. (failing > 1 and "s" or "") ..  "!</font>")
 		end
-		testCount = testCount + 1
-		logCount = logCount + 2
-	elseif logCount <= testCount then
-		log("(" .. (logCount) .. "/" .. total .. ") Testing " .. testName .. " ... ")
-		logCount = logCount + 1
-	elseif testCount <= total then
-		local startTime = amun.getCurrentTime()
-		local fine, message = pcall(testFunction)
-		local runTime = math.round((amun.getCurrentTime() - startTime)*1000000)/1000
-		if fine then
-			successfull = successfull + 1
-			log("<font color=\"darkgreen\">ok (" .. runTime .. "ms)</font>")
-		else
-			log("<font color=\"red\">fail (" .. runTime .. "ms): " .. message .. "</font>")
-		end
-		log("")
-		testCount = testCount + 1
-		testName, testFunction = next(tests, testName)
 	end
 end
 
-Entrypoints.add("Unit Tests", function()
-	if not isInitialized then
-		setupUnitTesting()
+local curTestName, curTestFn = next(tests)
+local function runNextTest() -- for entrypoint all
+	runSingle(curTestName, curTestFn)
+	if not namePrinted then
+		curTestName, curTestFn = next(tests, curTestName)
 	end
-	runTest()
+end
+
+local function init()
+	if not pcall(require, 'debug') then 
+		error("Debugging not enabled!") 
+	end
+	local timeout = 1000000000 -- number of instructions before hook is called
+	debug.sethook(function() error("Timeout (adjustable in tests/unit/init.lua)") end, "", timeout)
+	initialized = true
+end
+
+Entrypoints.add("Unit Tests/ all", function()
+	if not initialized then
+		init()
+	end
+	if curTestName then
+		runNextTest()
+	end
 end)
+
+for name, test in pairs(tests) do
+	Entrypoints.add("Unit Tests/" .. name, function()
+		if not initialized then
+			init()
+			total = 1
+			runSingle(name, test)
+		end
+		if namePrinted then
+			runSingle(name, test)
+		end
+	end)
+end
