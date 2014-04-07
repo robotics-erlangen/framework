fc8efe11602b95b9d9f924effdde6f2b394f7ece
Michael Eischer <michael.eischer@robotics-erlangen.de>
2014-04-07 22:47:19 +0200

    Marvin: Use path.setBoundary for nav challenge borders

--- strategy/marvin/challenge/iranopen2014/navigationtask.lua
+++ strategy/marvin/challenge/iranopen2014/navigationtask.lua
@@ -140,15 +140,9 @@ function Navigation:run()
 	}
 	vis.addPolygon("NavigationObstacle", corridor, vis.colors.black, false)
 
-	-- field boarders
-	self._robot.path:addLine(-g.FieldWidthHalf-lineWidth/2, -g.FieldHeightHalf-lineWidth/2,
-		-g.FieldWidthHalf-lineWidth/2, g.FieldHeightHalf+lineWidth/2, lineWidth)
-	self._robot.path:addLine(g.FieldWidthHalf+lineWidth/2, -g.FieldHeightHalf-lineWidth/2,
-		g.FieldWidthHalf+lineWidth/2, g.FieldHeightHalf+lineWidth/2, lineWidth)
-	self._robot.path:addLine(-g.FieldWidthHalf-lineWidth/2, -g.FieldHeightHalf-lineWidth/2,
-		g.FieldWidthHalf+lineWidth/2, -g.FieldHeightHalf-lineWidth/2, lineWidth)
-	self._robot.path:addLine(-g.FieldWidthHalf-lineWidth/2, g.FieldHeightHalf+lineWidth/2,
-		g.FieldWidthHalf+lineWidth/2, g.FieldHeightHalf+lineWidth/2, lineWidth)
+	-- field borders
+	self._robot.path:setBoundary( -g.FieldWidthHalf, -g.FieldHeightHalf,
+			g.FieldWidthHalf, g.FieldHeightHalf)
 
 	self._robot.trajectory:update(ToTarget, self._moveDest, (self._moveDest - self._robot.pos):angle(), 1)
 end
