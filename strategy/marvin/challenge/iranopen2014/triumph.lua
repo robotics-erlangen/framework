9c427ace176af4ea8cac1a0f352a485c88be1913
Christian Lobmeier <christian.lobmeier@robotics-erlangen.de>
2014-04-09 11:36:59 +0430

    Marvin: adrian wanted a move-in-square-behavior

---
+++ strategy/marvin/challenge/iranopen2014/triumph.lua
@@ -0,0 +1,27 @@
+local TestAgent = require "agent/testagent"
+local TriumphTask = require "challenge/iranopen2014/triumphtask"
+local Entrypoints = require "../base/entrypoints"
+local World = require "../base/world"
+local Messaging = require "control/messaging"
+
+local initialized
+local agents = {}
+
+local function run()
+	if not initialized then
+		for i, robot in ipairs(World.FriendlyRobots) do
+			if i > 4 then break end
+			table.insert(agents, TestAgent.create(robot, {
+				task = TriumphTask,
+				parameters = { i }
+			}))
+		end
+		initialized = true
+	end
+	Messaging.deliverMessages()
+	for _, agent in ipairs(agents) do
+		agent:run()
+	end
+end
+
+Entrypoints.add("Technical Challenge/Triumph", run)
---
+++ strategy/marvin/challenge/iranopen2014/triumphtask.lua
@@ -0,0 +1,51 @@
+local Triumph = (require "../base/class").new("Task.Triumph", require "task/base")
+local World = require "../base/world"
+local ToTarget = require "trajectory/totarget"
+
+Triumph.priority = 5 -- no meaning
+Triumph.i = 0
+Triumph.j = 0
+
+local points = {
+	Vector.create(-1, -2.5),
+	Vector.create(1, -2.5),
+	Vector.create(1, -0.5),
+	Vector.create(-1, -0.5),
+}
+
+function Triumph:_init(num)
+	if num < 1 or num > 4 then
+		error("invalid num")
+	end
+
+	self._num = num
+	self._moveDest = points[num]
+	self._limit = 0.05
+end
+
+function Triumph:run()
+	if World.RefereeState == "Stop" then
+		if self._robot.pos:distanceTo(self._moveDest) < self._limit then
+			if not self._ready then
+				Triumph.i = Triumph.i + 1
+			end
+			self._ready = true
+		end
+		if Triumph.i == 4 then
+			Triumph.j = 0
+		end
+		if Triumph.j < 4 then
+			self._num = self._num + 1
+			if self._num > 4 then self._num = 1 end
+			self._moveDest = points[self._num]
+			Triumph.j = Triumph.j + 1
+			Triumph.i = 0
+			self._ready = false
+		end
+--		log(Triumph.i .. "  " .. Triumph.j)
+	end
+	self._robot.path:setDefaultObstacles(self._robot, true, true, true)
+	self._robot.trajectory:update(ToTarget, self._moveDest, math.pi/2 * (self._num+2), 0.6)
+end
+
+return Triumph
