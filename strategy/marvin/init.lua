ef560d0c61d4efc3f649c878d9ed1c8136b95fec
André Pscherer <andre.pscherer@robotics-erlangen.de>
2014-04-24 21:32:22 +0200

    new process: BallAnalyzer

--- strategy/marvin/init.lua
+++ strategy/marvin/init.lua
@@ -34,6 +34,8 @@ function preproc:isFinished()
     return false
 end
 Processor.addPre(preproc)
+local BallAnalyzer = require "observer/ballAnalyzer"
+Processor.addPre(BallAnalyzer.create())
 
 local wrapper = function (func)
 	return function()
