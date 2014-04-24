20e43ccdba6e3ae11b3b16f6f123be8d6c048442
André Pscherer <andre.pscherer@robotics-erlangen.de>
2014-04-24 21:30:45 +0200

    new file for BallAnalyzer

---
+++ strategy/marvin/observer/ballAnalyzer.lua
@@ -0,0 +1,158 @@
+local BallAnalyzer = (require "../base/class").new("Observer.BallAnalyzer", require "../base/process")
+
+local World = require "../base/world"
+local MovingAverage = require "learning/movingaverage"
+local Ball = require "observer/ball"
+local debug = require "../base/debug"
+
+function BallAnalyzer:init(ball, movingAverageSlipping, movingAverageRolling, slippingFrictionStart, rollingFrictionStart)
+	self._ball = ball or World.Ball
+	self._movingAverageSlipping = movingAverageSlipping or MovingAverage.get("SlippingFriction", 50000, -3.47)
+	self._movingAverageRolling = movingAverageRolling or MovingAverage.get("RollingFriction", 50000, -0.305)
+	self._slippingFriction = slippingFrictionStart or -3.47
+	self._rollingFriction = rollingFrictionStart or -0.305
+	self._recording = false
+	self._record = {}
+	self._stopTime = World.Time
+end
+
+function BallAnalyzer:update(slippingFriction, rollingFriction)
+	self._slippingFriction = slippingFriction
+	self._rollingFriction = rollingFriction
+end
+
+function BallAnalyzer:run()
+	local resultsSlipping, resultsRolling
+	if Ball.isShot() then
+		log("isShot")
+		if self._recording then
+			resultsSlipping, resultsRolling = self:analyze()
+			self._record = {}
+		else
+			self._recording = true
+		end
+		self._stopTime = World.Time + 8	-- stop one acquisition and start the next, when the ball is shot again before the 8 sec countdown
+	end
+	if self._recording then
+		--log("add "..tostring(self._ball.speed))
+		table.insert(self._record, self._ball.speed)
+		if World.Time > self._stopTime then
+			self._recording = false
+			resultsSlipping, resultsRolling = self:analyze()
+			self._record = {}
+		end
+	end
+	if resultsSlipping then
+		log("Ergebnisse")
+		for _, v in ipairs(resultsSlipping) do
+			self._movingAverageSlipping.addValue(v)
+		end
+		for _, v in ipairs(resultsRolling) do
+			self._movingAverageRolling.addValue(v)
+		end
+		BallAnalyzer:update(self._movingAverageSlipping:value(), self._movingAverageRolling:value())
+		log("Slipping: "..self._movingAverageSlipping:value().." | Rolling: "..self._movingAverageRolling:value())
+	end
+	debug.set("slipping friction", self._movingAverageSlipping:value())
+	debug.set("rolling friction", self._movingAverageRolling:value())
+end
+
+function BallAnalyzer:analyze()
+	local accelerationArray = {}
+	for i = #self._record-1, 1, -1 do
+		accelerationArray[i] = (self._record[i+1] - self._record[i]):length()*100
+	end
+	local deviation, limit = math.huge, #accelerationArray/10
+	local overallFriction = math.average(accelerationArray)
+	local slippingFriction, rollingFriction = self._slippingFriction, self._rollingFriction
+	local startRolling
+	local lastDeviation = math.huge
+	repeat
+		local ratio = (slippingFriction - overallFriction)/(rollingFriction - overallFriction)
+		startRolling = math.bound(0.5, #accelerationArray/(1-ratio), #accelerationArray)
+		if startRolling >= 1 then
+			slippingFriction = math.average(accelerationArray, 0, math.floor(startRolling))
+		end
+		if startRolling <= #accelerationArray-1 then
+			log("length: "..#accelerationArray.." | start: "..math.ceil(startRolling))
+			rollingFriction = math.average(accelerationArray, math.ceil(startRolling))
+		end
+		deviation = 0
+		for i = 1, math.floor(startRolling) do
+			local diff = slippingFriction - accelerationArray[i]
+			deviation = deviation + diff*diff
+		end
+		for i = math.ceil(startRolling), #accelerationArray do
+			local diff = rollingFriction - accelerationArray[i]
+			deviation = deviation + diff*diff
+		end
+		-- vorsicht, bei starkem rauschen kann deviation gar nicht beliebig klein werden, daher eher mit der ableitung von deviation nach der anzahl der iterationen arbeiten
+	until lastDeviation - deviation > 0.01
+	local slFrSmoothed, roFrSmoothed
+	if startRolling <= #accelerationArray-3 then
+		if startRolling >= 3 then
+			slippingFriction, rollingFriction = table.split(accelerationArray, math.floor(startRolling))
+			slFrSmoothed = BallAnalyzer.cutAndSmoothen(slippingFriction)
+			roFrSmoothed = BallAnalyzer.cutAndSmoothen(rollingFriction)
+		else
+			_, rollingFriction = table.split(accelerationArray, math.max(0, math.floor(startRolling)))
+			slFrSmoothed = {}
+			roFrSmoothed = BallAnalyzer.cutAndSmoothen(rollingFriction)
+		end
+	else
+		slippingFriction, _ = table.split(accelerationArray, math.min(math.floor(startRolling), #accelerationArray))
+		slFrSmoothed = BallAnalyzer.cutAndSmoothen(slippingFriction)
+		roFrSmoothed = {}
+	end
+	return slFrSmoothed, roFrSmoothed
+end
+
+function BallAnalyzer.cutAndSmoothen(array)
+	local sum = 0
+	local arraySmoothed = {}
+	for i = #array-2, 1, -1 do
+		arraySmoothed[i] = 0.25*(array[i] + 2*array[i+1] + array[i+2])
+		sum = sum + arraySmoothed[i]
+	end
+	local avgSmoothed, maxDeviation, diff = sum/#arraySmoothed, 0, {}
+	for k, v in ipairs(arraySmoothed) do
+		diff[k] = avgSmoothed - v
+		diff[k] = diff[k]*diff[k]
+		maxDeviation = maxDeviation + diff[k]
+	end
+	local deviation = maxDeviation
+	local avgDeviationHalf, closeEnough, cutStart, cutEnd, cutIndex = maxDeviation/(2*#diff), false, true, true, 1
+	repeat
+		if deviation < 0.5*maxDeviation then
+			closeEnough = true
+		else
+			if cutStart then
+				if not table.remove(arraySmoothed, cutIndex) or diff[cutIndex+1] < avgDeviationHalf then
+					cutStart = false
+				end
+			end
+			if cutEnd then
+				if not table.remove(arraySmoothed, #arraySmoothed-cutIndex+1) or diff[#arraySmoothed-cutIndex] < avgDeviationHalf then
+					cutEnd = false
+				end
+			end
+			if not (cutStart or cutEnd) or #arraySmoothed == 0 then
+				break
+			end
+			avgSmoothed = math.average(arraySmoothed)
+			deviation = 0
+			for k, v in ipairs(arraySmoothed) do
+				diff[k] = avgSmoothed - v
+				diff[k] = diff[k]*diff[k]
+				deviation = deviation + diff[k]
+			end
+		end
+	until closeEnough
+	return arraySmoothed
+end
+
+function BallAnalyzer:isFinished()
+	return false
+end
+
+return BallAnalyzer
