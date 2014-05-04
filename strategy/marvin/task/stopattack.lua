a650a364bf6c239dab4b59bd472c7aaaa9d1da7a
Christian Lobmeier <christian.lobmeier@robotics-erlangen.de>
2014-05-04 21:34:49 +0200

    Marvin: rewrite of stopattack (still crashes into the defenders, will be fixed soon)

--- strategy/marvin/task/stopattack.lua
+++ strategy/marvin/task/stopattack.lua
@@ -1,143 +1,35 @@
 local StopAttack = (require "../base/class").new("Task.StopAttack", require "task/base")
 
-local Constants = require "../base/constants"
 local World = require "../base/world"
-local ToTarget = require "trajectory/totarget"
-local Goal = require "observer/goal"
-local Rating = require "util/rating"
+local Constants = require "../base/constants"
 local Field = require "util/field"
-local debug = require "../base/debug"
-local RobotList = require "util/robotlist"
+local ToTarget = require "trajectory/totarget"
 
 StopAttack.priority = 4
 
 function StopAttack:_init()
-	self._pos = nil
-	self._dir = nil
-	self.offensive = World.Ball.pos.y > 0
-	self.lastLargestIntervalMid = 0
-	self.lastLargestIntervalSize = 0
+	self._focusPoint = Vector.create(0, -World.Geometry.FieldHeightHalf + 4 * self._robot.radius)
+	self._side = World.Ball.pos.x < 0 and "left" or "right"
 end
 
 function StopAttack:run()
-	--hysteresis for offensive switch
-	if World.Ball.pos.y > 0.2 then
-		self.offensive = true
-	elseif World.Ball.pos.y < -0.2 then
-		self.offensive = false
-	end
-	debug.set("OffensiveStop", self.offensive)
-	
-	--offensive: aim torwards enemy goal, else place in front of own goal
-	local largestInterval = nil
-	if self.offensive then
-		largestInterval = Goal.largestFreeSector(World.Ball.pos, World.OpponentRobots, true)
-		if largestInterval == nil then --middle of goal as fallback
-			largestInterval = {(World.Geometry.OpponentGoal - World.Ball.pos):angle(), (World.Geometry.OpponentGoal - World.Ball.pos):angle()}
-		end
-
-		--Hysterese
-		local largestIntervalSize = largestInterval[2] - largestInterval[1]
-		local freeSectors = Goal.freeSectors(World.Ball.pos, World.OpponentRobots, true)
-		for _, sector in pairs(freeSectors) do
-			if sector[1] - math.pi <= self.lastLargestIntervalMid and sector[2] - math.pi >= self.lastLargestIntervalMid and self.lastLargestIntervalSize > largestIntervalSize - 2 then --2 is magic constant
-				largestInterval = sector
-				break
-			end
-		end
-		self.lastLargestIntervalSize = largestInterval[2]-largestInterval[1]
-		
-		largestInterval[1] = largestInterval[1] - math.pi
-		largestInterval[2] = largestInterval[2] - math.pi
-	else
-
-		largestInterval = Goal.largestFreeSector(World.Ball.pos, RobotList.excludeRobot(World.FriendlyRobots, self._robot), false)
-		if largestInterval == nil then --middle of goal as fallback
-			largestInterval = {(World.Geometry.FriendlyGoal - World.Ball.pos):angle(), (World.Geometry.OpponentGoal - World.Ball.pos):angle()}
-		end
-		
-		--Hysterese
-		local largestIntervalSize = largestInterval[2] - largestInterval[1]
-		local freeSectors = Goal.freeSectors(World.Ball.pos, RobotList.excludeRobot(World.FriendlyRobots, self._robot), false)
-		for _, sector in pairs(freeSectors) do
-			if sector[1] <= self.lastLargestIntervalMid and sector[2] >= self.lastLargestIntervalMid and self.lastLargestIntervalSize > largestIntervalSize - 2 then --2 is magic constant
-				largestInterval = sector
-				break
-			end
-		end
-		self.lastLargestIntervalSize = largestInterval[2]-largestInterval[1]
-	end
-	
-	local targetAngle = (largestInterval[1] + largestInterval[2]) / 2
-	self.lastLargestIntervalMid = targetAngle
-	local minDist = World.Ball.radius + self._robot.radius + Constants.stopBallDistance + Settings.positionPadding
-	
-	local target = World.Ball.pos + Vector.fromAngle(targetAngle) * minDist
-	
-	-- stop attacker moving out to the left / right 
-	local changedY = false
-	if target.y > World.Geometry.FieldHeightHalf then 
-		target.y = World.Geometry.FieldHeightHalf
-		changedY = true
-	end 
-	
-	if target.y < -World.Geometry.FieldHeightHalf then 
-		target.y = -World.Geometry.FieldHeightHalf
-		changedY = true
-	end 
-	if changedY then 
-		if target.x - World.Ball.pos.x > 0 then 
-			target.x = target.x + minDist
-		else 
-			target.x = target.x - minDist
-		end 
+	if self._side == "left" and World.Ball.pos.x > 0.3 then
+		self._side = "right"
+	elseif self._side == "right" and World.Ball.pos.x < -0.3 then
+		self._side = "left"
 	end
 
-	-- stop attacker moving out to the left / right 
-	local changedX = false
-	if target.x > World.Geometry.FieldWidthHalf then 
-		target.x = World.Geometry.FieldWidthHalf
-		changedX = true
-	end 
-	
-	if target.x < -World.Geometry.FieldWidthHalf then 
-		target.x = -World.Geometry.FieldWidthHalf
-		changedX = true
-	end 
-	if changedX then 
-		if target.y - World.Ball.pos.y > 0 then 
-			target.y = World.Ball.pos.y + minDist
-		else 
-			target.y = World.Ball.pos.y - minDist
-		end 
-	end 
+	local pos = World.Ball.pos + (self._focusPoint - World.Ball.pos):
+			setLength(Constants.stopBallDistance + self._robot.radius + Settings.positionPadding)
 
-	-- check if ball is in corner (if true, move towards goal) 
-	if target.y > World.Geometry.FieldHeightHalf then 
-		target.y = World.Geometry.FieldHeightHalf
-		if target.x > 0 then 
-			target.x = World.Ball.pos.x - minDist
-		else
-			target.x = World.Ball.pos.x + minDist
-		end 
-	end 
-	if target.y < -World.Geometry.FieldHeightHalf then 
-		target.y = -World.Geometry.FieldHeightHalf
-		if target.x > 0 then 
-			target.x = World.Ball.pos.x - minDist
-		else
-			target.x = World.Ball.pos.x + minDist
-		end 
-	end 
+	-- TODO 
+	-- avoid crashing into the centerbacks using self._side as evasion direction
+	-- probably by implementing Field.intersectCircleDefenseArea
 
-	self._pos = target
-	self._pos = Field.limitToAllowedField(self._pos, 0.03, true)
-	self._dir = (World.Ball.pos - self._pos):angle()
-	
 	self._robot.path:setDefaultObstacles(self._robot)
 	self._robot.path:addRobotObstacles(self._robot)
 	
-	self._robot.trajectory:update(ToTarget, self._pos, self._dir)
+	self._robot.trajectory:update(ToTarget, pos, (World.Ball.pos - pos):angle())
 end
 
 return StopAttack
