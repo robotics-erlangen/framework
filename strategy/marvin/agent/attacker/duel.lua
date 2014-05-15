5a8294d9e0afa0747b6f1ddd60038493393dd234
Christian Lobmeier <christian.lobmeier@robotics-erlangen.de>
2014-05-15 22:27:39 +0200

    Marvin: fixed duel

--- strategy/marvin/agent/attacker/duel.lua
+++ strategy/marvin/agent/attacker/duel.lua
@@ -8,27 +8,8 @@ function Duel:check()
 	if not (self._inbox.mainAttacker().trainer == self._robot) then
 		return false
 	end
-	
-	local timeAdvance = 0.2
-	
-	local robot, time = Ball.firstAtBall()
-	local friendly = true
-	if robot then
-		friendly = robot.isFriendly
-	end
-	if self.active and friendly and time > timeAdvance then
-		self.active = false
-	elseif not self._active and not friendly then
-		self.active = true
-	end
-	
-	if self.active then
-		return true
-	elseif self._active and Ball.friendlyBallOwner() == self._robot then
-		return true -- prevents change to shoot?
-	else
-		return false
-	end
+
+	return Ball.opponentBallOwner()
 end
 
 function Duel:_updateTask()
--- strategy/marvin/task/duel.lua
+++ strategy/marvin/task/duel.lua
@@ -1,39 +1,26 @@
-local Duel = (require "../base/class").new("Task.Duel", require "task/directpass")
+local Duel = (require "../base/class").new("Task.Duel", require "task/base")
 
 local World = require "../base/world"
 local geom = require "../base/geom"
 local debug = require "../base/debug"
 local Direct = require "trajectory/direct"
-local DirectPass = require "task/directpass"
 local Shoot = require "observer/shoot"
 local ClearBall = require "task/clearball"
-local Ball = require "observer/ball"
 
 Duel.priority = 4
-
 
 
 function Duel:_init()
 end
 
-function Duel:_canShoot()
-	return true
-end
-
 function Duel:run()
-	self.opposer = Ball.opponentBallOwner()
-	if self.opposer then
-		if self._robot:hasBall(World.Ball) then
-			self:_contest()
-			debug.set("duel-state", "contest")
-		else
-			local cb = ClearBall.create(self._agent)
-			ClearBall._clearBall(cb)
-			debug.set("duel-state", "clear ball")
-		end
+	if self._robot:hasBall(World.Ball) then
+		self:_contest()
+		debug.set("duel-state", "contest")
 	else
-		self:_passAway()
-		debug.set("duel-state", "pass away")
+		local cb = ClearBall.create(self._agent)
+		ClearBall._clearBall(cb)
+		debug.set("duel-state", "clear ball")
 	end
 end
 
@@ -45,25 +32,6 @@ function Duel:_contest()
 	local ccw = intersection and math.sign(intersection.x) or 1 --positive = ccw, negative = cw
 	local toBall = (World.Ball.pos - self._robot.pos):setLength(0.2)
 	self._robot.trajectory:update(Direct, toBall, nil, ccw * 2 * 2*math.pi) -- 2 turns per second
-end
-
-function Duel:_passAway()
-	local bestAssistant = Shoot.bestFreeAssistant(self._robot)
-	
-	local rating = bestAssistant and Shoot.rateAssistant(bestAssistant) or 0
-	local oldRating = self._bestAssistant and Shoot.rateAssistant(self._bestAssistant) or -1 
-	local hyst = World.Geometry.FieldHeightQuarter / 2
-
-	if self._bestAssistant ~= bestAssistant and rating > oldRating+hyst then
-		self._bestAssistant = bestAssistant
-	end
-		
-	if self._bestAssistant then
-		DirectPass._init(self, self._bestAssistant, true)
-		DirectPass.run(self)
-	else
-		self:_shoot(World.Geometry.OpponentGoal, math.huge, false) 
-	end
 end
 
 return Duel
