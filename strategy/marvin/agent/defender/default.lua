d345fdd5ca969f88dc97d925b15f986bc200cf92
Christian Lobmeier <christian.lobmeier@robotics-erlangen.de>
2014-05-19 18:53:02 +0200

    Marvin: centerback uses Goal.predictShot() instead of Ball.pos

--- strategy/marvin/agent/defender/default.lua
+++ strategy/marvin/agent/defender/default.lua
@@ -1,7 +1,10 @@
 local Base = require "agent/base/behavior"
 local Default = (require "../base/class").new("Agent.Defender.Default", Base)
 
+
+local World = require "../base/world"
 local CenterBack = require "task/centerback"
+local Goal = require "observer/goal"
 
 function Default:check()
 	return true
@@ -9,11 +12,12 @@ end
 
 function Default:_updateTask()
 	local role = self._inbox.roleAssignment().trainer
+	local futureBall = {["pos"] = Goal.predictShot()}
 	if role and role.name == "CenterBack" then
-		return CenterBack
+		return CenterBack, futureBall
 	else
 		-- TODO consider something new (for example zonal defense)
-		return CenterBack
+		return CenterBack, futureBall
 	end
 end
 
