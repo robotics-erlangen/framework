d07ec255598591e06cfa304c12fc29d388dda8c7
Alexander Danzer <alexander.danzer@robotics-erlangen.de>
2014-05-23 00:25:44 +0200

    ManMarkers: consider movement of opponents

--- strategy/marvin/util/defense.lua
+++ strategy/marvin/util/defense.lua
@@ -24,6 +24,13 @@ function Defense.manMarkPos(opponent)
 
 	local dist = opponent.radius + Constants.maxRobotRadius + Settings.markingDistance
 	local targetPos = opponent.pos + (orientationPos - opponent.pos):setLength(dist)
+	
+	-- extend position with speed of opponent, parameters can be improved
+	local maxPosExtension = Constants.maxRobotRadius
+	local extensionTime = 0.1
+	local posExtension = math.min(maxPosExtension, opponent.speed:length()*extensionTime)
+	targetPos = targetPos + opponent.speed:copy():setLength(posExtension)
+
 	targetPos = Field.limitToAllowedField(targetPos, Constants.maxRobotRadius, true)
 	if Referee.isStopState() then
 		local minDist = World.Ball.radius + Constants.maxRobotRadius + Constants.stopBallDistance + Settings.positionPadding
