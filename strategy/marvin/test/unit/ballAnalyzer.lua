b46e7eb93c16daa2d9b4d0aaf84fcd469cb091ee
André Pscherer <andre.pscherer@robotics-erlangen.de>
2014-05-22 21:38:35 +0200

    fixed test shoot generator

--- strategy/marvin/test/unit/ballAnalyzer.lua
+++ strategy/marvin/test/unit/ballAnalyzer.lua
@@ -1,15 +1,33 @@
 local BallAnalyzer = require "observer/ballAnalyzer"
 
-local jitter = 2
-return function()
-	BallAnalyzer:init(nil, nil, nil, -2.75, -0.272)
+local jitter = 1
+local function CreateShot()
+	local speedArray = {}
 	for i = 1, 50 do
-		BallAnalyzer._record[i] = Vector.create(5 - (3.5*i + jitter*(0.5 - math.random()))/100, 0)
+		speedArray[i] = Vector.create(5 - (3.5*i + jitter*(0.5 - math.random()))/100, 0)
 	end
 	for i = 51, 250 do
-		BallAnalyzer._record[i] = BallAnalyzer._record[i-1] - Vector.create((0.3 + jitter*(0.5 - math.random()))/100, 0)
+		speedArray[i] = speedArray[i-1] - Vector.create((0.3 + jitter*(0.5 - math.random()))/100, 0)
 	end
+	return speedArray
+end
+
+local function CreateShot2()
+	local speedArray = {Vector.create(5, 0)}
+	for i = 2, 250 do
+		speedArray[i] = speedArray[i-1] - Vector.create(((math.atan((50-i)/5) + jitter*(0.5 - math.random())) + 1.9)/100, 0)
+		--log("accelerationArray["..(i-1).."] = "..(speedArray[i-1].x - speedArray[i].x)*100)
+	end
+	return speedArray
+end
+
+return function()
+	BallAnalyzer:init(nil, nil, nil, -2.25, -0,5)
+	BallAnalyzer._record = CreateShot()
 	local sl, ro
+	sl, ro = BallAnalyzer:analyze()
+	log("Sliding friction: "..math.average(sl).." | Rolling friction: "..math.average(ro))
+	BallAnalyzer._record = CreateShot2()
 	sl, ro = BallAnalyzer:analyze()
 	log("Sliding friction: "..math.average(sl).." | Rolling friction: "..math.average(ro))
 end
