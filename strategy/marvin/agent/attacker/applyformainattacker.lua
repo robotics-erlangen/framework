1e827a92a3b058a9d35a4ad40b23eab132e01769
Christian Lobmeier <christian.lobmeier@robotics-erlangen.de>
2014-07-10 22:08:41 +0200

    Marvin: cooldown in a/a/applyformainattacker (no more double touch!)

--- strategy/marvin/agent/attacker/applyformainattacker.lua
+++ strategy/marvin/agent/attacker/applyformainattacker.lua
@@ -1,9 +1,28 @@
 local Base = require "agent/base/behavior"
 local ApplyForMainattacker = (require "../base/class").new("Agent.Attacker.ApplyForMainattacker", Base)
 local Referee = require "../base/referee"
+local Ball = require "observer/ball"
+local World = require "../base/world"
+
+local cooldown = 0.5
 
 function ApplyForMainattacker:check()
-	if self._inbox.mainAttacker().trainer == self._robot then
+	if self._lastShot and World.Time - self._lastShot < cooldown then
+		return false
+	end
+	if Ball.isShot() == self._robot then
+		self._lastShot = World.Time
+	end
+	if self._freekickFlag and self._maFlag and not Referee.isFriendlyFreeKickState() then
+		self._lastShot = World.Time
+		return false
+	end
+
+
+	self._freekickFlag = Referee.isFriendlyFreeKickState()
+	self._maFlag = self._inbox.mainAttacker().trainer == self._robot
+	
+	if self._maFlag then
 		self._forceKeepingInPool = true
 	end
 	if not Referee.isOpponentPenaltyState() then
