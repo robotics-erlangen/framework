c1f957b1e45d1a9c394563057d6b13d8dc927559
Michael Eischer <michael.eischer@robotics-erlangen.de>
2014-07-21 10:47:30 -0300

    Fix robot parameters

--- strategy/base/robots/generation2012.lua
+++ strategy/base/robots/generation2012.lua
@@ -28,6 +28,9 @@ function Gen2012:_shoot(speed)
 	local speedRatio = speed/self.maxShotLinear*8
 	local lim = math.bound(0, 162827-20000*speedRatio, 162827)
 	local power = -(math.sqrt(3)*math.sqrt(lim)-741)/1000
+	if self.id == 7 then
+		power = 0.0135*speed*speed+0.0723*speed+0.181
+	end
 	self:shootLinear(math.bound(0.1, power, 1))
 end
 
