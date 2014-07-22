7a300489bdef11c17adb303c23029433a6b85823
Alexander Danzer <alexander.danzer@robotics-erlangen.de>
2014-07-22 10:42:06 -0300

    AutoRef: Add multiple defenders detection

---
+++ strategy/autoref/multipledefender.lua
@@ -0,0 +1,48 @@
+local MultipleDefender = {}
+
+local World = require "../base/world"
+local Field = require "../marvin/util/field"
+
+local offendingTeam =""
+local occupation = ""
+
+local function robotIsOffending(robot, team, testOccupation)
+    local defAreaDistThreshold = (testOccupation == "partial") and robot.radius or -robot.radius
+    if Field["isIn"..team.."DefenseArea"](robot.pos, defAreaDistThreshold) then
+        local touchDistance = World.Ball.radius + robot.radius
+        if robot.pos:distanceTo(World.Ball.pos) < touchDistance then
+            if (World.TeamIsBlue and team == "Opponent")
+                or (not World.TeamIsBlue and team == "Friendly")
+            then
+                offendingTeam = "<font color=\"#C9C60D\">yellow</font>"
+            else
+                offendingTeam = "<font color=\"blue\">blue</font>"
+            end
+            occupation = testOccupation
+            return true
+        end
+    end
+end
+
+local function checkTeam(team)
+    for _, robot in ipairs(World[team .. "Robots"]) do
+        if robot ~= World[team .. "Goalie"] then
+            if robotIsOffending(robot, team, "full") then return true end
+            if robotIsOffending(robot, team, "partial") then return true end
+        end
+    end
+end
+
+function MultipleDefender:occuring()
+    if checkTeam("Opponent") or checkTeam("Friendly") then
+        return true
+    end
+
+    return false
+end
+
+function MultipleDefender:print()
+    log("Multiple defenders by " .. offendingTeam .. " team: " .. occupation .. " occupation")
+end
+
+return MultipleDefender
