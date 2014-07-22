0e698541f0b64f4cd0d3d45425af9f5bc3e25851
Christian Lobmeier <christian.lobmeier@robotics-erlangen.de>
2014-07-22 14:08:09 -0300

    Marvin: TechnicalChallenge: implement random moving CenterBacks

---
+++ strategy/marvin/test/task/randomdefense.lua
@@ -0,0 +1,48 @@
+local TestAgent = require "agent/testagent"
+local Entrypoints = require "../base/entrypoints"
+local CenterBack = require "task/centerback"
+local Messaging = require "control/messaging"
+local World = require "../base/world"
+local G = World.Geometry
+
+
+local N = 3
+
+local destPositions = {}
+local agents = {}
+
+
+-----------------------------
+-------- ! CAUTION ! --------
+-----------------------------
+-- in task/centerback:     --
+-- increase "getImportant" --
+-- drastically to avoid    --
+-- collisions              --
+-----------------------------
+
+
+local function run()
+	for i = 1,N do
+		local r = World.FriendlyRobots[i]
+		if not destPositions[i] or math.random() < 0.003 then
+			local x = math.random() * G.FieldWidth - G.FieldWidthHalf
+			local y = - math.random() * G.FieldHeightHalf
+			destPositions[i] = Vector.create(x, y)
+
+			agents[i] = TestAgent.create(r, {
+				task = CenterBack,
+				parameters = { { pos = destPositions[i] } }
+			})
+		end
+
+	end
+
+	Messaging.deliverMessages()
+
+	for i = 1,N do
+		agents[i]:run()
+	end
+end
+
+Entrypoints.add("Technical Challenge/Random Defense", run)
