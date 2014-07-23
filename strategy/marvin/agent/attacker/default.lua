115418241be65a98c9e496bfba49a85c6c303489
Alexander Danzer <alexander.danzer@robotics-erlangen.de>
2014-07-22 23:36:13 -0300

    Fix pool-switching of pass receivers + some direct pass stuff

--- strategy/marvin/agent/attacker/default.lua
+++ strategy/marvin/agent/attacker/default.lua
@@ -2,13 +2,20 @@ local Base = require "agent/base/behavior"
 local Default = (require "../base/class").new("Agent.Attacker.Default", Base)
 
 local Striker = require "task/striker"
+local DirectPassTarget = require "task/directpasstarget"
 
 function Default:check()
+	self._forceKeepingInPool = next(self._inbox.passPos()) ~= nil
 	return true
 end
 
 function Default:_updateTask()
-	return Striker
+	local _, passPos = next(self._inbox.passPos())
+	if passPos and self._robot.pos:distanceTo(passPos) < 3*self._robot.radius then
+		return DirectPassTarget -- just hold position to receive the direct pass
+	else
+		return Striker
+	end
 end
 
 return Default
