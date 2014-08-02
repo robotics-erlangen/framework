33546122e40f5810d9816d2f9d099d94297289f4
Alexander Danzer <alexander.danzer@robotics-erlangen.de>
2014-08-02 13:28:07 -0300

    AutoRef: Fix team color output in outOfField

--- strategy/autoref/outoffield.lua
+++ strategy/autoref/outoffield.lua
@@ -41,8 +41,12 @@ function OutOfField.occuring()
 end
 
 function OutOfField.print()
-    log("Ball out field. Last touch: " .. ((World.TeamIsBlue and Referee.friendlyTouchedLast())
-        and "<font color=\"blue\">Blue</font>" or "<font color=\"#C9C60D\">Yellow</font>"))
+    local faulingTeam = "<font color=\"#C9C60D\">Yellow</font>"
+    if (World.TeamIsBlue and Referee.friendlyTouchedLast()) or
+            not World.TeamIsBlue and not Referee.friendlyTouchedLast() then
+        faulingTeam = "<font color=\"blue\">Blue</font>"
+    end
+    log("Ball out field. Last touch: " .. faulingTeam)
 end
 
 return OutOfField
