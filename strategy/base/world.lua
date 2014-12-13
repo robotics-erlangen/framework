9e805a4856068c277170b73ee3ff329e68861ed4
Michael Eischer <michael.eischer@robotics-erlangen.de>
2014-12-13 12:10:00 +0100

    Prevent strategy from running on broken 32-bit ra builds

--- strategy/base/world.lua
+++ strategy/base/world.lua
@@ -183,6 +183,7 @@ function World._updateWorld(state)
 		World.TimeDiff = 0
 	end
 	World.Time = state.time * 1E-9
+	assert(World.Time > 0, "Invalid World.Time. Outdated ra version!")
 	if World.IsSimulated ~= state.is_simulated then
 		World.IsSimulated = state.is_simulated
 		Constants.switchSimulatorConstants(World.IsSimulated)
