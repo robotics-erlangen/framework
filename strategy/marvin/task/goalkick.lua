51c0d8449b3e58c1de0b1f14c9addd81dca3a4c6
Alexander Danzer <alexander.danzer@robotics-erlangen.de>
2015-04-08 16:46:16 +0430

    Marvin: Shoot GoalKick to opponent penalty spot
    Old strategy was to provocate corner kicks,
    but the icing rule makes this to dangerous

--- strategy/marvin/task/goalkick.lua
+++ strategy/marvin/task/goalkick.lua
@@ -5,11 +5,8 @@ local GoalKick = Class("Task.GoalKick", Task, Shoot)
 local World = require "../base/world"
 
 function GoalKick:run()
-	local leftUpperCorner = Vector(World.Ball.pos.x + 0.05, -1)
-	local rightUpperCorner = Vector(World.Ball.pos.x - 0.05, -1)
-	local shootPos = World.Ball.pos.x > 0 and rightUpperCorner or leftUpperCorner
-	local linear = false
-	self:_shoot(shootPos, 1, linear, 3 * math.pi/180)
+	local shootPos = World.Geometry.OpponentPenaltySpot
+	self:_shoot(shootPos, 1, false, 3 * math.pi/180)
 end
 
 return GoalKick
