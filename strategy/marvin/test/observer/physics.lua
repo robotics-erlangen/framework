e4a8bd8abcb8e760d5cfdcf342f0cdef7900d6f5
Christian Lobmeier <christian.lobmeier@robotics-erlangen.de>
2015-04-25 14:25:54 +0200

    Marvin: expand T/O/P/testBallVsRobotTime by the RMTTB component

--- strategy/marvin/test/observer/physics.lua
+++ strategy/marvin/test/observer/physics.lua
@@ -4,10 +4,14 @@ local Physics = require "observer/physics"
 local IO = require "util/io"
 
 function PhysicsTest.testBallVsRobotTime()
-	local ball = {pos = Vector(0, 0), speed = Vector(0, 2), maxSpeed = 4}
-	local robot = {pos = Vector(1, 0), speed = Vector(0, 0), maxSpeed = 3}
+	local ball = {pos = Vector(0, 0), speed = Vector(0, 1.5), maxSpeed = 8, radius = 0.021}
+	local robot = {pos = Vector(0.2, 2.5), speed = Vector(0, -1), maxSpeed = 3, radius = 0.09}
 	local s_max = 4
 	local s_step = 0.01
+
+
+	local mintime = Physics.robotMinTimeToBall(robot, ball)
+	local balldist = Physics.ballAtTime(ball, mintime).pos.y
 
 	local values = {}
 	for s = s_step,s_max,s_step do
@@ -15,11 +19,18 @@ function PhysicsTest.testBallVsRobotTime()
 		local t_ball = Physics.ballRollTime(ball, s)
 		local t_robot = Physics.robotTimeToPos(robot, Vector(0, s), endspeed)
 		local t_diff = t_ball - t_robot
-		table.insert(values, t_diff)
+
+		local mttb_flag = "NaN"
+		if s < balldist and s + s_step > balldist then
+			mttb_flag = 0
+		end
+		table.insert(values, t_diff .. " " .. mttb_flag)
 	end
 
 	IO.save("physicstest", values)
-	log("done")
+
+	local x_ball = Physics.ballAtTime(ball, mintime).pos
+	log(mintime .. "s -- " .. x_ball.y .. "m")
 end
 
 return PhysicsTest
