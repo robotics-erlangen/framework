1cbc696803ffe684aae045b345f19e28d2ebf268
Michael Niebisch <michael.niebisch@robotics-erlangen.de>
2015-05-29 22:35:11 +0200

    added possibility of kickoffs task

--- strategy/marvin/agent/attacker/kickoffassistant.lua
+++ strategy/marvin/agent/attacker/kickoffassistant.lua
@@ -1,13 +1,15 @@
 local Base = require "agent/base/behavior"
 local KickoffAssistant = Class("Agent.Attacker.KickoffAssistant", Base)
-
+local debug = require "../base/debug"
 local World = require "../base/world"
 local G = World.Geometry
 local Ball = require "observer/ball"
 
 local MoveToPos = require "task/movetopos"
+local KickoffPass = require "task/kickoffpass"
 
 function  KickoffAssistant:_stop(d)
+	self._behind = false
 	self._moveDest = nil
 	self._movePos = nil
 end
@@ -16,7 +18,7 @@ function KickoffAssistant:check()
 	-- try every position in random order, take first free one
 	local positionClash = false
 	for _, pos in pairs(self._inbox.moveDest()) do
-		if pos == self._moveDest then
+		if self._moveDest~=nil and (pos == self._moveDest or pos.x == self._moveDest.x or (pos.y==self._moveDest.y and math.abs(pos.y) >= 29 * self._robot.radius ))  then
 			positionClash = true
 		end
 	end
@@ -25,11 +27,22 @@ function KickoffAssistant:check()
 		local positions = {
 			Vector(-G.FieldWidthHalf * 0.75, -3 * self._robot.radius),
 			Vector(-G.FieldWidthHalf * 0.5, -3 * self._robot.radius),
+			Vector(-G.FieldWidthHalf * 0.75, -30 * self._robot.radius),
 			Vector(G.FieldWidthHalf * 0.75, -3 * self._robot.radius),
-			Vector(G.FieldWidthHalf * 0.5, -3 * self._robot.radius)
+			Vector(G.FieldWidthHalf * 0.5, -3 * self._robot.radius),
+			Vector(G.FieldWidthHalf * 0.75, -30 * self._robot.radius),
 		}
 		self._moveDest = table.shuffle(positions)[1]
+		debug.set("pos", self._moveDest.x)
+		if self._moveDest == positions[3] or self._moveDest== positions[6] then
+			--self._send.kop("all", self._moveDest)
+			--debug.set("t3", 1)
+			self._behind = true
+		else
+			self._behind=false
+		end
 	end
+	--self._send.kop("all", self._moveDest)
 
 	self._send.moveDest("all", self._moveDest)
 
@@ -39,7 +52,23 @@ function KickoffAssistant:check()
 end
 
 function KickoffAssistant:_updateTask()
-	if self._movePos ~= self._moveDest then
+	debug.set("t", self._behind)
+	debug.set("t2", self._moveDest)
+
+	if self._behind then -- player is in the back and wants a pass at kickoff
+		if self._movePos ~= self._moveDest then
+			--self._send.testMessage("all", 2)
+
+			self._movePos = self._moveDest
+			self._task = nil -- make sure a new task will be created
+		end
+		--self._send.testMessage("all", 1)
+		return KickoffPass, {self._moveDest, (G.OpponentGoal-self._moveDest):angle()}
+	end
+
+	if self._movePos ~= self._moveDest then  -- player is on the goalline
+		--self._send.testMessage("all", 2)
+
 		self._movePos = self._moveDest
 		self._task = nil -- make sure a new task will be created
 	end
