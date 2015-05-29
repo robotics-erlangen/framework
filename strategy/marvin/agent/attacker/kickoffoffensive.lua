b2711e7000c696968f1c75fedbcd051930cedaa5
Michael Niebisch <michael.niebisch@robotics-erlangen.de>
2015-05-29 22:36:14 +0200

    added communication with kickoffpass

--- strategy/marvin/agent/attacker/kickoffoffensive.lua
+++ strategy/marvin/agent/attacker/kickoffoffensive.lua
@@ -1,10 +1,12 @@
 local Base = require "agent/base/behavior"
 local KickoffOffensive = Class("Agent.Attacker.KickoffOffensive", Base)
 
+local debug = require "../base/debug"
 local World = require "../base/world"
 local G = World.Geometry
 local vis = require "../base/vis"
 local ShootGoal = require "task/shootgoal"
+local Pass = require "task/pass"
 local MoveToStaticBall = require "task/movetostaticball"
 
 
@@ -15,8 +17,34 @@ end
 
 
 function KickoffOffensive:_updateTask()
-	if World.RefereeState == "KickoffOffensivePrepare" then
+	local rob, loc = next(self._inbox.kickoffPass())
+	if World.RefereeState == "KickoffOffensivePrepare" then -- wait for kickoff
 		return MoveToStaticBall, { math.pi/2, 0.05 }
+	elseif rob then --~= nil then-- kickoff
+		-- is a player waiting for a kickoffpass
+		
+		self._send.kickoffStart("all", 1)
+		debug.set("kickoffPassSuggestion", "true")
+		--end
+		
+		
+		if  self._inbox.targetTime() ~= nil then
+			--debug.set("time", self._inbox.targetTime())
+			for r, time in pairs(self._inbox.targetTime()) do
+				debug.set("r", r)
+				debug.set("time", time)
+				--if time <= 2.0 then
+					self._send.kickoffStart("all", 1)
+					debug.set("targetTime", "true")
+					--return ShootGoal
+					for rr, k in pairs(self._inbox.kickoffPass()) do
+						return Pass, {r, Vector(k.x, -k.y*0.35)}
+					end			
+				--end
+			end
+		end
+		return MoveToStaticBall, { math.pi/2, 0.05 }
+			
 	else
 		return ShootGoal
 	end
