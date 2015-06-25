2c7b1fae099077d5abb7c0ac397cf76a9af2e29a
Christian Lobmeier <christian.lobmeier@robotics-erlangen.de>
2015-06-25 20:41:45 +0200

    Marvin: only activate duel if an opponent controls the ball

--- strategy/marvin/agent/attacker/duel.lua
+++ strategy/marvin/agent/attacker/duel.lua
@@ -1,15 +1,36 @@
 local Base = require "agent/base/behavior"
 local Duel = Class("Agent.Attacker.Duel", Base)
+
+local World = require "../base/world"
 local Ball = require "observer/ball"
 
 local TaskDuel = require "task/duel"
+
+function Duel:_stop()
+	self._active = false
+end
 
 function Duel:check()
 	if not (self._inbox.mainAttacker().trainer == self._robot) then
 		return false
 	end
 
-	return Ball.opponentBallOwner()
+	local opponentHasBall = false
+	for _,r in pairs(World.OpponentRobots) do
+		if r:hasBall(World.Ball) then
+			opponentHasBall = true
+			break
+		end
+	end
+
+	if opponentHasBall and not Ball.receivesPass(self._robot) then
+		self._active = true
+	end
+	if not Ball.opponentBallOwner() then
+		self._active = false
+	end
+
+	return self._active
 end
 
 function Duel:_updateTask()
