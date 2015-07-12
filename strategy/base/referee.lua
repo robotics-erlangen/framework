e79093801847f05ce3194c8d64c960e59bf73abf
Alexander Danzer <alexander.danzer@robotics-erlangen.de>
2015-07-12 23:35:45 +0200

    Fix last commit b5389035

--- strategy/base/referee.lua
+++ strategy/base/referee.lua
@@ -136,11 +136,15 @@ end
 
 local lastTeam = true -- true for the friendly team, false for the opponent
 local touchDist = World.Ball.radius+robotRadius
+local fieldHeightHalf = World.Geometry.FieldHeightHalf
+local fieldWidthHalf = World.Geometry.FieldWidthHalf
 --- Update the status of which team touched the ball last
 -- @name checkTouching
 function Referee.checkTouching()
+	local ballPos = World.Ball.pos
 	-- only consider touches when playing
-	if World.RefereeState == "Stop" or not Field.isInField(ballPos) then
+	if World.RefereeState == "Stop" or
+			ballPos.x > fieldWidthHalf or ballPos.y > fieldHeightHalf then
 		return
 	end
 
