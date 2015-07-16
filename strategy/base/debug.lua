f07f4c68ad1105daf8ec3fee3d5b6f3e8e3d5398
Michael Eischer <michael.eischer@robotics-erlangen.de>
2015-07-16 16:42:35 +0800

    Fix debug not showing empty tables

--- strategy/base/debug.lua
+++ strategy/base/debug.lua
@@ -89,11 +89,15 @@ function debug.set(name, value, visited)
 		else
 			debug.push(tostring(name))
 			local class = Class.toClass(value, true)
-			if class then
-				debug.set(nil, Class.name(class))
-			end
+			local hasValues = false
 			for k, v in pairs(value) do
 				debug.set(k, v, visited)
+				hasValues = true
+			end
+			if class then
+				debug.set(nil, Class.name(class))
+			elseif not hasValues then
+				debug.set(nil, "empty table")
 			end
 			debug.pop()
 			return
