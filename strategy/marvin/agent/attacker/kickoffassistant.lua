c8c3916a6dd70da3146f2f037844ff939b421be1
Michael Niebisch <michael.niebisch@robotics-erlangen.de>
2015-07-17 20:57:33 +0800

    Marvin: /a/a/kickoffassistant Keep Attacker immediately after Kickoff

--- strategy/marvin/agent/attacker/kickoffassistant.lua
+++ strategy/marvin/agent/attacker/kickoffassistant.lua
@@ -72,6 +72,7 @@ function KickoffAssistant:_updateTask()
 			self._movePos = self._moveDest
 			self._task = nil -- make sure a new task will be created
 		end
+		self._forceKeepingInPool = true
 		return KickoffPass, {self._moveDest, (G.OpponentGoal-self._moveDest):angle()}
 	end
 	--currently not used, because there will always be a player in the back
