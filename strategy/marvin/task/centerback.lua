9bd7f511ca543e0c82391ef445cc03774ec5d5b4
Alexander Danzer <alexander.danzer@robotics-erlangen.de>
2015-07-20 22:00:43 +0800

    Decrease length of aimless chip kicks

--- strategy/marvin/task/centerback.lua
+++ strategy/marvin/task/centerback.lua
@@ -291,7 +291,7 @@ function CenterBack:run()
 			and World.Ball.pos:distanceTo(self._robot.pos) < 1 then
 		debug.set("chip", true)
 		self:_doForceShoot()
-		self._robot:chip(4)
+		self._robot:chip(2)
 	end
 
 	local ignoreOpponents
