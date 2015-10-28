e508d3ca95f1bca4c983057bbf89a377201e3bc5
Michael Eischer <michael.eischer@robotics-erlangen.de>
2015-10-29 00:22:20 +0100

    Marvin: t/manmark: Add hysteresis when intercepting the opponent

--- strategy/marvin/task/manmark.lua
+++ strategy/marvin/task/manmark.lua
@@ -7,15 +7,21 @@ local Defense = require "util/defense"
 function ManMark:_init(targetRobot)
 	assert(targetRobot, "ManMark task needs a target robot")
 	self._targetRobot = targetRobot
+	self._leadTarget = false
 end
 
 function ManMark:run()
 	local preferredPos = Defense.manMarkPos(self._targetRobot)
 	local preferredDir = (World.Ball.pos - self._robot.pos):angle()
 
-	-- If the opponent is driving in the opposite direction of me than change the pos into his movement
+	-- If the opponent is driving towards me then aim before him
 	local oppSpeed = self._targetRobot.speed:copy()
 	if oppSpeed:length() > 1 and oppSpeed:absoluteAngleDiff(self._robot.speed) > math.pi / 2 then
+		self._leadTarget = true
+	elseif oppSpeed:length() < 0.8 or oppSpeed:absoluteAngleDiff(self._robot.speed) < 0.42 * math.pi then
+		self._leadTarget = false
+	end
+	if self._leadTarget then
 		preferredPos = preferredPos + oppSpeed:setLength(oppSpeed:length()*0.6)
 	end
 
