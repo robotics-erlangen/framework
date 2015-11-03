e285a514c00c105f70ca355983632e02fdfc4966
André Pscherer <andre.pscherer@robotics-erlangen.de>
2015-11-03 20:54:56 +0100

    Added testcase for Goal.allFreeSectors()

--- strategy/marvin/test/observer/goal.lua
+++ strategy/marvin/test/observer/goal.lua
@@ -4,6 +4,7 @@ local Goal = require "observer/goal"
 local World = require "../base/world"
 local vis = require "../base/vis"
 local RobotList = require "util/robotlist"
+local debug = require "../base/debug"
 
 function GoalTest.testFreeSectors()
 	local freeSectors = Goal.freeSectors(World.Ball.pos, World.OpponentRobots, true)
@@ -17,20 +18,13 @@ function GoalTest.testFreeSectors()
 end
 
 function GoalTest.testCustomFreeSectors()
-	if #World.FriendlyRobots < 2 then
-		log("Needs at least two friendly robots")
-		return
+	local freeSectors = Goal.allFreeSectors(World.Ball.pos, World.OpponentRobots)
+	for i,sector in pairs(freeSectors) do
+		debug.set("sector["..i.."]", "{"..sector[1]..", "..sector[2].."}")
 	end
-
-	local startAngle = (World.FriendlyRobots[1].pos - World.Ball.pos):angle()
-	local endAngle = (World.FriendlyRobots[2].pos - World.Ball.pos):angle()
-	local freeSectors = Goal.getFreeSectors(World.Ball.pos, World.OpponentRobots, startAngle, endAngle)
 	vis.setColor(vis.colors.orangeHalf, true)
 	for _, s in ipairs(freeSectors) do
-		--log(tostring(s[1]) .. " "..tostring(s[2]))
-		local pointRight = World.Ball.pos + Vector.fromAngle(s[1])*10
-		local pointLeft = World.Ball.pos + Vector.fromAngle(s[2])*10
-		vis.addPolygon("test: Custom Free Sectors", {World.Ball.pos, pointRight, pointLeft})
+		vis.addPizza("test: Custom Free Sectors", World.Ball.pos, 5, s[2], s[1])
 	end
 end
 
