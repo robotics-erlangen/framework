effa5f01e21907909ff670b940e4d723203ac065
Michael Niebisch <michael.niebisch@robotics-erlangen.de>
2015-12-08 21:55:13 +0100

    Marvin: /test/t/duel Test for duel

---
+++ strategy/marvin/test/task/duel.lua
@@ -0,0 +1,45 @@
+local Entrypoints = require "../base/entrypoints"
+local World = require "../base/world"
+local AgentPool = require "control/agentpool"
+local Coordinator = require "control/coordinator"
+local Duel = require "task/duel"
+local MoveToStaticBall = require "task/movetostaticball"
+local Trainer = require "trainer/trainer"
+ 
+
+-- needs one yellow and one blue robot, must be run for both strategies
+
+local Dueler = Class("Test.Task.Duel.Duel", require "agent/base/behavior")
+function Dueler:check()
+	return true
+end
+
+function Dueler:_updateTask()
+	local otherRobot = next(self._inbox.attackerFlag())
+	if World.TeamIsBlue then
+		return Duel, {}
+	else
+		return MoveToStaticBall, {0 * math.pi, 0}
+	end
+	
+end
+
+
+local DuelAgent = Class("Test.Task.DuelAgent", require "agent/base/simpleagent")
+DuelAgent._behaviors = {
+	Dueler
+}
+
+local coord = nil
+
+local function run()
+	if coord == nil then
+		local trainer = Trainer()
+		local pools = { pass = AgentPool(DuelAgent, 1) }
+		local poolGroups = { { pools.pass } }
+		coord = Coordinator(trainer, pools, poolGroups)
+	end
+	coord:run()
+end
+
+Entrypoints.add("TaskTest/Duel", run)
