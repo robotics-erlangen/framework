430252d0670738d8e37fd28089e685301304510b
Michael Niebisch <michael.niebisch@robotics-erlangen.de>
2015-12-08 20:43:00 +0100

    Marvin: /t/duel block the shot on goal by driving to a position with the shortest time

--- strategy/marvin/task/duel.lua
+++ strategy/marvin/task/duel.lua
@@ -4,16 +4,19 @@ local World = require "../base/world"
 local geom = require "../base/geom"
 local vis = require "../base/vis"
 local debug = require "../base/debug"
+local math = require "../base/math"
 local Physics = require "observer/physics"
 local Ball = require "observer/ball"
 local Robot = require "observer/robot"
 local Direct = require "trajectory/direct"
 local ToTarget = require "trajectory/totarget"
+local Field = require "../base/field"
 
 
 function Duel:_init()
 	self._opposer = nil
 	self._blockingBall = false
+	self._oldPosition = nil
 end
 
 function Duel:run()
@@ -37,6 +40,54 @@ function Duel:run()
 	else
 		self:_moveToBall()
 		debug.set("duel-state", "move to ball")
+	end
+end
+
+-- this function searches for a position between boundaryOne and boundaryTwo to which the robot will take
+-- the shortest ammount of time, up to a precission value, using a tenery algorithm
+function Duel:_findBestPointToBlockOpponentShot(boundaryOne, boundaryTwo, precission)
+
+	-- time to the boundaries
+	local timeToBoundaryOne = Physics.robotTimeToPos(self._robot, boundaryOne, Vector(0, 0), false, false)
+	local timeToBoundaryTwo = Physics.robotTimeToPos(self._robot, boundaryTwo, Vector(0, 0), false, false)
+	
+	-- time diff between the two bounds
+	if math.abs(timeToBoundaryOne - timeToBoundaryTwo) < precission then
+		return boundaryOne
+	end
+
+	-- calculate two new positions on the line
+	local leftThird = (boundaryOne * 2 + boundaryTwo) / 3
+	local rightThird = (boundaryOne + boundaryTwo * 2) / 3
+
+	-- calculate time to the new positions
+	local timeToLeftThird = Physics.robotTimeToPos(self._robot, leftThird, Vector(0, 0), false, false)
+	local timeToRightThird = Physics.robotTimeToPos(self._robot, rightThird, Vector(0,0), false, false)
+
+	-- depending on which time is smaller recursively call the function with new boundaries
+	if timeToLeftThird < timeToRightThird then
+		return self:_findBestPointToBlockOpponentShot(boundaryOne, rightThird, precission)
+	else
+		return self:_findBestPointToBlockOpponentShot(leftThird, boundaryTwo, precission)
+	end
+end
+
+-- this method calculates a new position between boundaryOne and boundaryTwo regarding the oldPosition
+function Duel:_newPosRegardingOldPosition(boundaryOne, boundaryTwo, precission)
+	local oldPos = nil
+	
+	if self._oldPosition then
+		oldPos = self._oldPosition:nearestPosOnLine(boundaryOne, boundaryTwo)
+	end
+	
+	local newPos = self:_findBestPointToBlockOpponentShot(boundaryOne, boundaryTwo, 0.01)
+	local  alpha = 0.1
+	
+	-- don't let the postion jump to much between frames
+	if oldPos then
+		return newPos * alpha + oldPos * (1-alpha)
+	else
+		return newPos
 	end
 end
 
@@ -82,8 +133,27 @@ function Duel:_moveToBall()
 		local viewDirBall = World.Ball.pos - closestOpponentRobot.pos
 	 	moveBall = moveBall + viewDirBall:setLength(self._robot.radius + World.Ball.radius + 0.02)
 	end
-	moveDest = self._robot.pos:nearestPosOnLine(moveBall, World.Geometry.FriendlyGoal)
 
+	-- plumb to the ball goal line
+	--moveDest = self._robot.pos:nearestPosOnLine(moveBall, World.Geometry.FriendlyGoal)
+	
+	-- pos before the defense area; the possibility of crashing into centerbacks was considered 
+	-- but disregarded because blocking a shot on the goal is more important,
+	-- and the probabilty of it being the final position is small
+	local intersectionDefenseArea = Field.intersectRayDefenseArea(World.Ball.pos, (World.Geometry.FriendlyGoal-World.Ball.pos):copy():normalize(), self._robot.radius + 0.04, false, false)
+
+	local moveDest = nil
+
+	if intersectionDefenseArea then
+		-- calculate new position between ball (regarding robot shootRadius) and the intersection with defence area
+		moveDest = self:_newPosRegardingOldPosition(World.Ball.pos + ((intersectionDefenseArea - World.Ball.pos):setLength(self._robot.shootRadius + World.Ball.radius)), intersectionDefenseArea, 0.01)
+	else
+		-- case for there not being an intersection with defence area
+		moveDest = World.Ball.pos + (self._robot.pos-World.Ball.pos):setLength(self._robot.shootRadius + World.Ball.radius)
+	end
+
+	-- remember position for the next calculation
+	self._oldPosition = moveDest
 
 	local distToLine = moveDest:distanceTo(self._robot.pos)
 	if distToLine <= DIFF then
