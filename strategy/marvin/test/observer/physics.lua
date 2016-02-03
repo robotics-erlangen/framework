3409e602ad9eea1395ff1c17192796645bef1c05
Michael Eischer <michael.eischer@robotics-erlangen.de>
2016-02-03 13:45:49 +0100

    Marvin: te/o/physics: Fix parameters of ball vs robot test

--- strategy/marvin/test/observer/physics.lua
+++ strategy/marvin/test/observer/physics.lua
@@ -5,18 +5,29 @@ local IO = require "util/io"
 
 
 function PhysicsTest.testBallVsRobotTime()
-	local ball = {pos = Vector(0, -1), speed = Vector(0, 5), maxSpeed = 7, radius = 0.021}
-	local robot = {pos = Vector(0, 0), speed = Vector(0, 2), maxSpeed = 3, shootRadius = 0.08, dribblerWidth = 0.06}
+	local ball = {
+		pos = Vector(0.168249,-1.50264),
+		speed = Vector(2.7468,1.88377),
+		maxSpeed = 7,
+		radius = 0.021
+	}
+	local robot = {
+		pos = Vector(0.971402,-0.894273),
+		speed = Vector(-0.791324,-0.469037),
+		maxSpeed = 3, shootRadius = 0.08, dribblerWidth = 0.07,
+		acceleration = { aSpeedupFMax = 3.3, aBrakeFMax = 3.5 }
+	}
+	local targetPos = Vector(0, 4.04)
+
 	local s_max = 4
 	local s_step = 0.01
 
-	local targetPos = Vector(0, -3)
 	local mintime = Physics.robotTimeToBall(robot, ball, targetPos, robot.maxSpeed)
 	local balldist = Physics.ballAtTime(ball, mintime).pos.y
 
 	local values = {}
 	for s = s_step,s_max,s_step do
-		local endspeed = (ball.pos - robot.pos):setLength(3)
+		local endspeed = (ball.pos - robot.pos):setLength(0)
 		local t_ball = Physics.ballRollTime(ball, s)
 		local t_robot = Physics.robotTimeForBallTime(robot, ball, targetPos, robot.maxSpeed, t_ball)
 		local t_diff = t_ball - t_robot
