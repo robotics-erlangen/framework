e185f1a476bfdfcccdc9d78ab11b9b92e6040b3d
Alexander Danzer <alexander.danzer@robotics-erlangen.de>
2016-02-04 15:59:44 +0100

    Fix "Publish ball placement position to strategy"

--- strategy/base/world.lua
+++ strategy/base/world.lua
@@ -313,7 +313,7 @@ function World._updateGameState(state)
 		World.RefereeState = "Halt"
 	end
 
-	if state.designated_position.x and
+	if state.designated_position and state.designated_position.x and
 			(not World.BallPlacementPos or World.BallPlacementPos.y ~= state.designated_position.y
 			or World.BallPlacementPos.x ~= state.designated_position.x) then
 		World.BallPlacementPos = Vector(state.designated_position.x, state.designated_position.y)
