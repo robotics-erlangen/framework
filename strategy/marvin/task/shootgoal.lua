076be29203fd6cf45b1172ee8e338387a4febdda
Christian Lobmeier <christian.lobmeier@robotics-erlangen.de>
2016-02-23 21:48:00 +0100

    Marvin: t/shootgoal: updateVolleyShootPos: lock time instead of lock distance

--- strategy/marvin/task/shootgoal.lua
+++ strategy/marvin/task/shootgoal.lua
@@ -189,8 +189,10 @@ function ShootGoal:_updateVolleyShootPos()
 	end
 
 	-- keep the old position if the ball about to arrive
-	if oldPos and World.Ball.pos:distanceTo(self._robot.pos + Vector.fromAngle(self._robot.dir):setLength(
-			self._robot.shootRadius + World.Ball.radius)) < 0.25 then
+	local dribblerPos = self._robot.pos + Vector.fromAngle(self._robot.dir):setLength(
+		self._robot.shootRadius + World.Ball.radius)
+	local preparationTime = Physics.checkedBallRollTime(World.Ball, dribblerPos)
+	if oldPos and preparationTime > 0 and preparationTime < 0.25 then
 		debug.set("volley pos", "keep (lock)")
 		return
 	end
