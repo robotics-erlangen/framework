850feeee67722fdeeaeea9e2db37c36e77a1e66f
Michael Eischer <michael.eischer@robotics-erlangen.de>
2016-03-11 00:32:53 +0100

    Marvin: t/placeball: Partially working version

--- strategy/marvin/task/placeball.lua
+++ strategy/marvin/task/placeball.lua
@@ -1,5 +1,6 @@
 local PlaceBall = Class("Task.PlaceBall", require "task/base")
 
+local debug = require "../base/debug"
 local World = require "../base/world"
 local PathHelper = require "trajectory/pathhelper"
 local ToTarget = require "trajectory/totarget"
@@ -15,15 +16,35 @@ function PlaceBall:run()
     local dir = (World.Ball.pos - self._robot.pos):angle() -- towards ball
 
     if self._goToBall then
-        self._robot.trajectory:update(ToTarget, World.Ball.pos, dir)
-        -- TODO
-        -- if close to ball, activate dribbler
-        -- if ball is in light brake: self._goToBall to false
-    else -- move ball into position, possible approach:
-        -- first, turn slowly
-        -- then move directly to position, with reduced speed
-	   self._robot.trajectory:update(ToTarget, World.BallPlacementPos, dir)
+        local ofs = (World.Ball.pos - World.BallPlacementPos):setLength(World.Ball.radius + self._robot.shootRadius - 0.02)
+        local targetPos = World.Ball.pos - ofs
+        self._robot.trajectory:update(ToTarget, targetPos, dir)
+
+        local dist = targetPos:distanceTo(self._robot.pos)
+        if dist < 0.05 then
+            self._robot:setDribblerSpeed(1.0)
+        end
+        if dist < 0.01 or not World.Ball:isPositionValid() then
+            -- FIXME: if ball is in light brake
+            -- Push ball a little bit the move backwards
+            self._goToBall = false
+        end
+    else -- move ball into position
+        -- FIXME: stabilize direction if near target
+        local ofs = (World.Ball.pos - World.BallPlacementPos):setLength(World.Ball.radius + self._robot.shootRadius)
+        local targetPos = World.BallPlacementPos - ofs
+        self._robot.trajectory:update(ToTarget, targetPos, dir, 1, nil, true)
+
+        if World.Ball.pos:distanceTo(self._robot.pos) > World.Ball.radius + self._robot.shootRadius + 0.05 then
+            self._goToBall = true
+        end
+        if World.Ball.pos:distanceTo(World.BallPlacementPos) > 0.02 then
+            self._robot:setDribblerSpeed(1.0)
+        else
+            self._robot:setDribblerSpeed(0)
+        end
     end
+    debug.set("goToBall", self._goToBall)
 end
 
 return PlaceBall
