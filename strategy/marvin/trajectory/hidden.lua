2f115b0a694c0f51d4c19d5a5db8838e4c4cd630
Michael Eischer <michael.eischer@robotics-erlangen.de>
2016-03-16 13:31:33 +0100

    Marvin: traj/hidden: Add omega as parameter

--- strategy/marvin/trajectory/hidden.lua
+++ strategy/marvin/trajectory/hidden.lua
@@ -7,10 +7,10 @@ local Coordinates = require "../base/coordinates"
 function Hidden:_init()
 end
 
-function Hidden:update(speedForward, speedSide)
+function Hidden:update(speedForward, speedSide, omega)
 	assert(not self._robot.isVisible, "can only control invisible robots")
 	local robotPos = Coordinates.toGlobal(self._robot.pos)
-	return { v_f = speedForward, v_s = speedSide }, self._robot.pos, 0
+	return { v_f = speedForward, v_s = speedSide, omega = omega }, self._robot.pos, 0
 end
 
 function Hidden:canHandle(speed, targetDir)
