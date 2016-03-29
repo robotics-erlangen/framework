2b86f7bed3640222d8aa816cfd14f036485c3b57
Michael Eischer <michael.eischer@robotics-erlangen.de>
2016-03-29 16:17:16 +0200

    Revert "mainAttacker applications: change order of execution"
    This reverts commit 4286d8b55788d24e78f7ef4144f3e2be1c7d4d44.

--- strategy/marvin/agent/attacker/shoot.lua
+++ strategy/marvin/agent/attacker/shoot.lua
@@ -37,7 +37,6 @@ function Shoot:check()
 		self._forceKeepingInPool = true
 		self._send.exclusiveRole("trainer", {mainAttacker = 2})
 	end
-	debug.set("catching pass", self._isCatchingPass)
 
 	return mainAttackerFlag
 end
