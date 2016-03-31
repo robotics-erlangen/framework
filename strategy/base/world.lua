f4ebcd266244e6a48a54d96761bc2ea65db03a3b
Alexander Danzer <alexander.danzer@robotics-erlangen.de>
2016-03-31 16:25:56 +0200

    Marvin: Fix Ball Placement position unit

--- strategy/base/world.lua
+++ strategy/base/world.lua
@@ -322,11 +322,12 @@ function World._updateGameState(state)
 		World.RefereeState = "Halt"
 	end
 
-	if state.designated_position and state.designated_position.x and
-			(not World.BallPlacementPos or World.BallPlacementPos.y ~= state.designated_position.y
-			or World.BallPlacementPos.x ~= state.designated_position.x) then
-		World.BallPlacementPos = Coordinates.toLocal(
-				Vector.createReadOnly(state.designated_position.x, state.designated_position.y))
+	if state.designated_position and state.designated_position.x then
+		World.BallPlacementPos = Coordinates.toLocal(Vector.createReadOnly(
+			-- refbox position message uses millimeters
+			-- ssl-vision's coordinate system is rotated by 90 degrees
+			-state.designated_position.y / 1000,
+			state.designated_position.x / 1000))
 	end
 
 	World.GameStage = World.gameStageMapping[state.stage]
