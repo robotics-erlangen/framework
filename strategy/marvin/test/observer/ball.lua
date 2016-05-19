3307423338c6b34f9894f1bf0a9297a51fb2589b
Christian Lobmeier <christian.lobmeier@robotics-erlangen.de>
2016-05-19 20:18:21 +0200

    Marvin: t/o/b/testBallCatchProbability()

--- strategy/marvin/test/observer/ball.lua
+++ strategy/marvin/test/observer/ball.lua
@@ -1,6 +1,8 @@
 local BallTest = {}
 
 local vis = require "../base/vis"
+local Constants = require "../base/constants"
+local Field = require "../base/field"
 local World = require "../base/world"
 local Ball = require "observer/ball"
 local Physics = require "observer/physics"
@@ -15,6 +17,22 @@ function BallTest.testBallOwner()
 	local oowner = Ball.opponentBallOwner()
 	if oowner then
 		vis.addCircle("test: Ball Owner", oowner.pos, 0.2, vis.colors.blueHalf, true)
+	end
+end
+
+
+function BallTest.testBallCatchProbability()
+	if World.Ball.speed:length() > 0.1 then
+		local endOfField = Field.nextLineCut(World.Ball.pos, World.Ball.speed)
+		local corridorHalf = World.Ball.speed:perpendicular():setLength(World.Ball.radius + Constants.positionError) * 2
+		for _,robot in ipairs(World.OpponentRobots) do
+			local pointOnLine = robot.pos:nearestPosOnLine(World.Ball.pos, endOfField)
+			local ballRollTime = Physics.ballRollTime(World.Ball, (pointOnLine - World.Ball.pos):length())
+			local chance = Ball.ballCatchProbability(robot, 0, ballRollTime, pointOnLine, corridorHalf)
+			if chance == chance then
+				vis.addCircle("test: BallCatchProb", robot.pos, 0.2, vis.fromTemperature(chance), true)
+			end
+		end
 	end
 end
 
