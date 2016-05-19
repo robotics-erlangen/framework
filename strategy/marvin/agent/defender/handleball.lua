2ff6f331eb438ea1b4d1db51e44e480ea170d2e2
Christian Lobmeier <christian.lobmeier@robotics-erlangen.de>
2016-05-19 21:57:15 +0200

    Marvin: reworked a/d/handleball, still a lot to test and tweak

--- strategy/marvin/agent/defender/handleball.lua
+++ strategy/marvin/agent/defender/handleball.lua
@@ -4,17 +4,75 @@ local HandleBall = Class("Agent.Defender.HandleBall", Base)
 local Field = require "../base/field"
 local Referee = require "../base/referee"
 local World = require "../base/world"
-local Robot = require "observer/robot"
+local Ball = require "observer/ball"
 local Physics = require "observer/physics"
 local DefUtil = require "util/defense"
 local Duel = require "task/duel"
 local InterceptPass = require "task/interceptpass"
 local debug = require "../base/debug"
 
+--[[
+	timeAdvance ist doof, weil
+		- geht kaputt, wenn wir knapp vor dem gegner in der ballinie stehen
+			-> kleiner advance, interceptPass wird aktiv :(
+		- beachtet nicht die zeit die wir zum ball überhaupt brauchen
+		- man sollte sich auch mal für MA bewerben, wenn man nicht unbedingt schneller da ist (zB force duel)
+
+	stattdessen
+		- MA bewerbung schicken, falls
+			- ManMark
+			- CenterBack
+				- nicht falls ein Gegner den Ball früher abfangen wird
+					-> winkel zwischen ballrichtung und gegner-tor beachten
+					-> ballgeschwindigkeit beachten
+				- falls wir den Ball locker abfangen können
+				- falls wir einen gefährlichen Querpass ablenken können
+]]
 
 function HandleBall:_stop()
-	self._timeAdvance = -math.huge
 	self._mainAttackerApplicationSent = false
+end
+
+function HandleBall:_shouldSendMAApplication()
+	local role = self._inbox.roleAssignment().trainer
+
+	-- Manmarks should always apply for MA
+	if role and role.name == "ManMark" then
+		return true
+	end
+
+	local interceptionTime = math.huge
+	local interceptionDist = math.huge
+	if Ball.receivesPass(self._robot) then
+		local posOnLine = self._robot.pos:nearestPosOnLine(World.Ball.pos, World.Ball.pos + World.Ball.speed)
+		local timeToLine = Physics.robotTimeToPos(self._robot, posOnLine, Vector(0, 0), false)
+		local dist = World.Ball.pos:distanceTo(posOnLine)
+		if timeToLine < 0.4 or timeToLine + 0.1 < Physics.ballRollTime(World.Ball, dist) then
+			interceptionTime = timeToLine
+			interceptionDist = dist
+		end
+	end
+
+	-- if we cannot reach the ball in a reasonable time, we stay CB
+	if interceptionTime > 0.7 then
+		return false
+	end
+
+	-- if an opponent robot will catch the ball before us, we stay CB
+	for _,r in ipairs(World.OpponentRobots) do
+		if r.pos.y > -1 and Ball.receivesPass(r) then
+			local posOnLine = self._robot.pos:nearestPosOnLine(World.Ball.pos, World.Ball.pos + World.Ball.speed)
+			local timeToLine = Physics.robotTimeToPos(self._robot, posOnLine, 
+				(World.Geometry.FriendlyGoal - r.pos):setLength(r.maxSpeed), false)
+			local dist = World.Ball.pos:distanceTo(posOnLine)
+			if dist < interceptionDist + self._robot.radius and timeToLine < 1
+					and (r.pos - World.Geometry.FriendlyGoal):absoluteAngleDiff(World.Ball.speed) < 10 * math.pi/360 then
+				return false
+			end
+		end
+	end
+
+	return true
 end
 
 function HandleBall:check()
@@ -24,22 +82,8 @@ function HandleBall:check()
 	end
 
 	local mainAttacker = self._inbox.mainAttacker().trainer
-	local role = self._inbox.roleAssignment().trainer
-	local closestOppAtBall = DefUtil.getClosestRobot(World.OpponentRobots, World.Ball.pos)
-	local forceDuel = role and role.name == "ManMark" and role.params == closestOppAtBall and
-		mainAttacker and Physics.robotTimeToBall(mainAttacker, World.Ball, closestOppAtBall.pos, 0) >
-		Physics.robotTimeToBall(self._robot, World.Ball, closestOppAtBall.pos, 0)
-	debug.set("force duel", forceDuel)
 
-	local selfTime = Robot.minTimeToBall(self._robot)
-	local _, opponentTime = Robot.fastestOpponentAtBall()
-	self._timeAdvance = opponentTime - selfTime
-	debug.set("timeAdvance (HandleBall)", self._timeAdvance)
-
-	if mainAttacker == self._robot
-			or (self._mainAttackerApplicationSent and self._timeAdvance > 0)
-			or (not self._mainAttackerApplicationSent and self._timeAdvance > 0.2)
-			or forceDuel then
+	if mainAttacker == self._robot or self:_shouldSendMAApplication() then
 		self:_applyForMainAttacker()
 		self._mainAttackerApplicationSent = true
 	else
@@ -50,16 +94,12 @@ function HandleBall:check()
 end
 
 function HandleBall:_updateTask()
-	if self._timeAdvance > 0.5 and not DefUtil.dangerousBallTowardsDefense() then
+	local role = self._inbox.roleAssignment().trainer
+	if role and role.name == "ManMark" and not DefUtil.dangerousBallTowardsDefense() then
 		self._send.poolChangeRequest("trainer")
 	end
 
-	if self._timeAdvance > 0 and World.Ball.speed:length() > 1.5
-			and not DefUtil.dangerousBallTowardsDefense() then
-		return InterceptPass
-	else
-		return Duel
-	end
+	return Duel
 end
 
 return HandleBall
