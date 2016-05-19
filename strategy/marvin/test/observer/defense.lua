a6788c4fc34c342af75087920015e236c1cb08c1
Christian Lobmeier <christian.lobmeier@robotics-erlangen.de>
2016-05-19 19:41:24 +0200

    Marvin: t/o/d/testDangerousness: use vis.fromTemperature()

--- strategy/marvin/test/observer/defense.lua
+++ strategy/marvin/test/observer/defense.lua
@@ -8,10 +8,7 @@ local Defense = require "util/defense"
 function DefenseTest.testDangerousness()
 	local ratings = Defense.rateOpponentDangerousness()
 	for robot, rating in pairs(ratings) do
-		local red = math.bound(0, 255 * rating, 255)
-		local green = math.bound(0, 255 * (1- rating), 255)
-		local color = vis.fromRGBA(red, green, 0, 128)
-		vis.addCircle("test: Dangerousness", robot.pos, 0.2, color, true)
+		vis.addCircle("test: Dangerousness", robot.pos, 0.2, vis.fromTemperature(rating), true)
 	end
 end
 
