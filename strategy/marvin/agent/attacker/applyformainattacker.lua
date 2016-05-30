bd2348ceec40850441f4c8077098a783ca80a4ef
Alexander Danzer <alexander.danzer@robotics-erlangen.de>
2016-05-30 21:37:16 +0200

    Marvin: Fix mainAttacker change during pass attempts

--- strategy/marvin/agent/attacker/applyformainattacker.lua
+++ strategy/marvin/agent/attacker/applyformainattacker.lua
@@ -12,6 +12,7 @@ local cooldown = 3
 
 function ApplyForMainattacker:_init()
 	self._lastIncomingPassTime = 0
+	self._lastIncomingPassPos = nil
 	self._lastIncomingPassSender = nil
 end
 
@@ -25,6 +26,7 @@ function ApplyForMainattacker:check()
 		if msg.robot == self._robot then
 			self._lastIncomingPassTime = World.Time
 			self._lastIncomingPassSender = sender
+			self._lastIncomingPassPos = msg.pos
 		else -- pass to other robot
 			self._lastIncomingPassTime = 0
 		end
@@ -49,8 +51,14 @@ function ApplyForMainattacker:check()
 	end
 
 	local passTargetOverrideTime = 0.7
-	if Ball.wasShot(passTargetOverrideTime) == self._lastIncomingPassSender and
-			World.Time - self._lastIncomingPassTime < passTargetOverrideTime then
+	local angleDiff = math.huge
+	if self._lastIncomingPassPos then
+		angleDiff = World.Ball.speed:absoluteAngleDiff(self._lastIncomingPassPos - World.Ball.pos)
+	end
+	if Ball.wasShot(passTargetOverrideTime) == self._lastIncomingPassSender
+			and World.Time - self._lastIncomingPassTime < passTargetOverrideTime
+			and angleDiff < 30*math.pi/180 then
+		debug.set("angle diff", angleDiff)
 		self:_applyForMainAttacker(nil, nil, 2)
 		self._agent.beOffensive = true
 	else
