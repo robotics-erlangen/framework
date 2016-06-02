cd5f8c20111bf76b5dd2b6de67119cb7f04fd1bc
Alexander Danzer <alexander.danzer@robotics-erlangen.de>
2016-06-02 19:45:32 +0200

    Marvin: Unit test for pools

---
+++ strategy/marvin/test/unit/pools.lua
@@ -0,0 +1,62 @@
+local MainCoordinator = require "control/maincoordinator"
+local MainTrainer = require "trainer/maintrainer"
+local World = require "../base/world"
+local Robot = require "../base/robot"
+
+
+local function robotStub(id)
+	local r = Robot(id, true, { FieldWidthHalf = 1, BoundaryWidth = 0.2, FieldHeightHalf = 1 })
+	r.isVisible = true
+	r.pos = Vector(0,0)
+	return r
+end
+
+test("base.pools", function()
+    local allFriendlyRobotsOrig = World.FriendlyRobotsAll
+    World.FriendlyRobotsAll = { robotStub(1), robotStub(2) }
+
+    MainTrainer.attackRatio = function()
+        return 1, 1
+    end
+
+	local coordinator = MainCoordinator()
+
+    coordinator:run()
+
+    local attackerRobot = next(coordinator._trainer._inbox.attackerFlag())
+    local defenderRobot = next(coordinator._trainer._inbox.defenderFlag())
+    local attackerAgent = nil
+    local defenderAgent = nil
+    for _, agent in ipairs(coordinator._pools.attack._agents) do
+        if agent:robot() == attackerRobot then
+            attackerAgent = agent
+        end
+    end
+    for _, agent in ipairs(coordinator._pools.defense._agents) do
+        if agent:robot() == defenderRobot then
+            defenderAgent = agent
+        end
+    end
+    assert_not_nil(attackerAgent)
+    assert_not_nil(defenderAgent)
+
+	local defenderBefore, attackerBefore = defenderRobot, attackerRobot
+	attackerAgent._send.poolChangeRequest("trainer")
+	coordinator:run()
+
+	attackerRobot = next(coordinator._trainer._inbox.attackerFlag())
+    defenderRobot = next(coordinator._trainer._inbox.defenderFlag())
+	assert_equal(attackerRobot, defenderBefore)
+	assert_equal(defenderRobot, attackerBefore)
+
+	defenderBefore, attackerBefore = defenderRobot, attackerRobot
+	defenderAgent._send.poolChangeRequest("trainer")
+	coordinator:run()
+
+	attackerRobot = next(coordinator._trainer._inbox.attackerFlag())
+	defenderRobot = next(coordinator._trainer._inbox.defenderFlag())
+	assert_equal(attackerRobot, defenderBefore)
+	assert_equal(defenderRobot, attackerBefore)
+
+    World.FriendlyRobotsAll = allFriendlyRobotsOrig
+end)
