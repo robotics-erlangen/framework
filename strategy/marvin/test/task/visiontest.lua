e9912af09318ee72e4368fd00926c729c865f932
Michael Eischer <michael.eischer@robotics-erlangen.de>
2016-06-30 20:24:52 +0200

    Marvin: te/t/visitiontest: Cleanup code, should be more reliable

--- strategy/marvin/test/task/visiontest.lua
+++ strategy/marvin/test/task/visiontest.lua
@@ -8,42 +8,53 @@ local PathHelper = require "trajectory/pathhelper"
 local ToTarget = require "trajectory/totarget"
 
 
-local START_POS = Vector(-1.8, -3.9)
 local CENTER_DIST = 7.8
 local START_ANGLE = 90/180*math.pi
 local ANGLE_STEP = 360/180*math.pi
 local WAIT_TIME = 3
 local ROBOT_ORIENTATION = 90/180*math.pi
 local ROBOT_ORIENTATION_STEP = 0/180*math.pi
-local STATE = 0
+local POS_LIST = { Vector(-1.8, 3.9), Vector(0, 3.9), Vector(0, -3.9), Vector(1.8, -3.9), Vector(1.8, 3.9), Vector(-1.8, -3.9) }
 
+
+local function indexCalculation(inbox, robotId)
+	local idx = 0
+	local total = 0
+	for robot, _ in pairs(inbox.attackerFlag("broadcast")) do
+		if robotId > robot.id then
+			idx = idx + 1
+		end
+		total = total + 1
+	end
+	return idx, total
+end
 
 local VisionTestTask = Class("Test.Task.VisionTest.Task", require "task/base")
-function VisionTestTask:_init(idx, total)
+function VisionTestTask:_init()
 	self._dest = nil
 	self._atTargetSince = nil
-	self._startPos = START_POS
+	self._startPos = POS_LIST[1]
 	self._centerDist = CENTER_DIST
 	self._angle = START_ANGLE
 	self._angleStep = ANGLE_STEP
 	self._robotOrientation = ROBOT_ORIENTATION
 	self._robbotOrientationStep = ROBOT_ORIENTATION_STEP
 	self._orientation = ROBOT_ORIENTATION
-	self._robotState = STATE
-	-- line up robots
-	self._startPos = START_POS + Vector((idx - total/2) * 0.25, 0)
-	self._myTotal = total
-	self._myIdx = idx
+	self._robotState = 0
 end
 
 function VisionTestTask:run()
 	PathHelper.setDefaultObstacles(self._robot.path, self._robot,true , false, true, nil, nil, nil, true)
 	PathHelper.addRobotObstacles(self._robot.path, self._robot)
+
+	local idx, total = indexCalculation(self._inbox, self._robot.id)
+	local offset = Vector((idx - total/2) * 0.25, 0)
+
 	local pos
 	if self._dest then
-		pos = self._dest
+		pos = self._dest + offset
 	else
-		pos = self._startPos
+		pos = self._startPos + offset
 	end
 	local dir = self._orientation
 
@@ -59,29 +70,13 @@ function VisionTestTask:run()
 		self._send.defenderFlag("all")
 		synchronized = (table.count(self._inbox.attackerFlag("broadcast")) - table.count(self._inbox.defenderFlag("broadcast"))) == 0
 	end
+
 	if synchronized then
 		if self._dest then
 			self._dest = nil
 		else
-			if self._robotState == 0 then
-				self._robotState = 1
-				self._startPos = Vector(-1.8, 3.9)+ Vector((self._myIdx - self._myTotal/2) * 0.25, 0)
-			elseif self._robotState == 1 then
-				self._robotState = 2
-				self._startPos = Vector(0, 3.9)+ Vector((self._myIdx - self._myTotal/2) * 0.25, 0)
-			elseif self._robotState == 2 then
-				self._robotState = 3
-				self._startPos = Vector(0, -3.9)+ Vector((self._myIdx - self._myTotal/2) * 0.25, 0)
-			elseif self._robotState == 3 then
-				self._robotState = 4
-				self._startPos = Vector(1.8, -3.9)+ Vector((self._myIdx - self._myTotal/2) * 0.25, 0)
-			elseif self._robotState == 4 then
-				self._robotState = 5
-				self._startPos = Vector(1.8, 3.9)+ Vector((self._myIdx - self._myTotal/2) * 0.25, 0)
-			elseif self._robotState == 5 then
-				self._robotState = 0
-				self._startPos = Vector(-1.8, -3.9)+ Vector((self._myIdx - self._myTotal/2) * 0.25, 0)
-			end
+			self._startPos = POS_LIST[self._robotState+1]
+			self._robotState = (self._robotState + 1) % #POS_LIST
 		end
 		self._orientation = self._orientation + ROBOT_ORIENTATION_STEP
 	end
@@ -99,15 +94,7 @@ function Position:check()
 end
 
 function Position:_updateTask()
-	local idx = 0
-	local total = 0
-	for robot, _ in pairs(self._inbox.attackerFlag()) do
-		if self._robot.id > robot.id then
-			idx = idx + 1
-		end
-		total = total + 1
-	end
-	return VisionTestTask, { idx, total }
+	return VisionTestTask, {}
 end
 
 
