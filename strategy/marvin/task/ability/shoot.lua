2502f5542e85e803b0eb850bf5d1ce4827c46846
Christian Lobmeier <christian.lobmeier@robotics-erlangen.de>
2016-07-01 17:33:50 +0200

    Marvin: t/a/shoot: increase block hysteresis

--- strategy/marvin/task/ability/shoot.lua
+++ strategy/marvin/task/ability/shoot.lua
@@ -35,7 +35,7 @@ local EXTRA_MOVE_SPEED_LIMIT = 0.5
 local SAFETY_TIME = 0.2
 local SAFETY_TIME_HYSTERESIS = 0.2
 local BLOCK_ANGLE = 65 / 180 * math.pi
-local BLOCK_HYSTERESIS = 10 / 180 * math.pi
+local BLOCK_HYSTERESIS = 20 / 180 * math.pi
 local OPP_TIME_HYSTERESIS = 0.1
 local IN_THE_RUN = 1.5
 
