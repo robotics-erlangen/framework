8d729c112c06e8413a9414a4eb31be0f2bee8b44
Andreas Wendler <andreas.wendler@robotics-erlangen.de>
2016-07-28 17:01:33 +0200

    Marvin: test/task: Added task test for performing a number of different chips/kicks in a row

---
+++ strategy/marvin/test/task/chiplearningtest.lua
@@ -0,0 +1,55 @@
+local Entrypoints = require "../base/entrypoints"
+local Field = require "../base/field"
+local World = require "../base/world"
+local Shoot = require "task/ability/shoot"
+local PathHelper = require "trajectory/pathhelper"
+local ToTarget = require "trajectory/totarget"
+local TestHelper = require "test/helper/agent"
+
+
+local ChipLearningTest = Class("Test.Task.ChipLearningTest", require "task/base", Shoot)
+
+local DO_LINEAR_SHOOT = false
+
+function ChipLearningTest:_init()
+	self._framesSinceMove = 0
+	self._shootSpeed = 0
+	self._shootPos = Vector(0, 0)
+	self._maxShootSpeed = 5
+end
+
+function ChipLearningTest:run()
+	local stayOnPos = false
+	if World.Ball.speed:length() < 0.05  and math.abs(World.Ball.pos.x) < World.Geometry.FieldWidthHalf and
+		math.abs(World.Ball.pos.y) < World.Geometry.FieldHeightHalf then
+		self._framesSinceMove = self._framesSinceMove + 1
+		if self._framesSinceMove == 100 then
+			local randX = World.Geometry.FieldWidthHalf * (math.random() * 2 - 1) * 0.8
+			local randY = World.Geometry.FieldHeightHalf * (math.random() * 2 - 1) * 0.8
+			self._shootPos = Vector(randX, randY)
+			self._shootSpeed = math.random() * self._maxShootSpeed
+		end
+		if self._framesSinceMove < 150 then
+			stayOnPos = true
+		else
+			self:_shoot(self._shootPos, self._shootSpeed, DO_LINEAR_SHOOT, 3 * math.pi/180, false)
+		end
+	else
+		self._framesSinceMove = 0
+		stayOnPos = true
+	end
+	if stayOnPos then
+		PathHelper.setDefaultObstacles(self._robot.path, self._robot)
+		self._robot.trajectory:update(ToTarget, self._robot.pos, math.pi/2)
+	end
+end
+
+
+local Agent = Class("Test.Task.ChipLearningTest.Agent", require "agent/base/simpleagent")
+Agent._behaviors = {
+	TestHelper.staticBehavior(ChipLearningTest, { 1 })
+}
+
+
+local run = TestHelper.defaultCoordinator("attack", Agent, 1)
+Entrypoints.add("TaskTest/ChipLearning", run)
