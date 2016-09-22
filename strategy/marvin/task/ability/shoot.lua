f0c91e22e2a9fc2facab7888d551aa45b7c2bdc1
Michael Eischer <michael.eischer@robotics-erlangen.de>
2016-09-22 15:04:37 +0200

    Marvin: t/a/shoot: Remove shoot skew hack

--- strategy/marvin/task/ability/shoot.lua
+++ strategy/marvin/task/ability/shoot.lua
@@ -291,7 +291,7 @@ function Shoot:_calculateMovementSpeed(lastBallSpeed, distToBall, shotAtRobot)
 	return speed
 end
 
-function Shoot:_calculateShootDirection(targetPos, targetSpeed, distToBall, linearShoot)
+function Shoot:_calculateShootDirection(targetPos, targetSpeed)
 	-- calculate shoot direction
 	local ballTouchPos = self._robot.pos + Vector.fromAngle(self._robot.dir)*(self._robot.shootRadius+World.Ball.radius)
 	local ballRollTime = Physics.ballRollTime(World.Ball, World.Ball.pos:distanceTo(ballTouchPos))
@@ -300,15 +300,6 @@ function Shoot:_calculateShootDirection(targetPos, targetSpeed, distToBall, line
 				targetPos, targetSpeed)
 	kickSpeed = math.max(MIN_SHOOT_SPEED, kickSpeed)
 	local rawPhi = (targetPos - futureBall.pos):angle()
-
-	vis.addPath("t/a/shoot: Direction", { self._robot.pos, self._robot.pos + Vector.fromAngle(targetDir)*20 }, vis.colors.redHalf)
-	-- handle robot shoot direction problem
-	-- if not World.IsSimulated and linearShoot then
-	-- 	-- 4.2 degree / cm off
-	-- 	local SHOOT_SKEW = 4.2 / 180 * math.pi * 100
-	-- 	local SHOOT_SKEW_LIMIT = 4 / 180 * math.pi
-	-- 	targetDir = targetDir - math.bound(-SHOOT_SKEW_LIMIT, distToBall.y * SHOOT_SKEW, SHOOT_SKEW_LIMIT)
-	-- end
 
 	vis.addPath("t/a/shoot: Direction", { self._robot.pos, self._robot.pos + Vector.fromAngle(self._robot.dir)*20 }, vis.colors.blue)
 	vis.addPath("t/a/shoot: Direction", { self._robot.pos, self._robot.pos + Vector.fromAngle(targetDir)*20 }, vis.colors.pink)
@@ -357,7 +348,7 @@ function Shoot:_doShoot(targetPos, targetSpeed, linearShoot, maxAngleError, dont
 
 	local distToBall, shotAtRobot = self:_calculateDistToBall()
 	local speed = self:_calculateMovementSpeed(self._lastBallSpeed, distToBall, shotAtRobot)
-	local targetDir, kickSpeed = self:_calculateShootDirection(targetPos, targetSpeed, distToBall, linearShoot)
+	local targetDir, kickSpeed = self:_calculateShootDirection(targetPos, targetSpeed)
 
 	if self._movingBallHysteresis and World.Ball.speed:length() >= IN_THE_RUN
 			and World.Ball.speed:dot(World.Ball.pos - self._robot.pos) < 0 then
