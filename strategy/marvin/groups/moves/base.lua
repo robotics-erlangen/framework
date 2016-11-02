65597205b9b5172454bd872be7ca6c07f388d36d
Christian Lobmeier <christian.lobmeier@robotics-erlangen.de>
2016-11-02 15:40:11 +0100

    Marvin: move base class

---
+++ strategy/marvin/groups/moves/base.lua
@@ -0,0 +1,26 @@
+local Base = Class("Group.Move.Base")
+
+Base.MIN_ROBOTS = -1
+
+
+function Base.canStart()
+	error("stub")
+end
+
+function Base.chooseRobots(availableRobots)
+	error("stub")
+end
+
+function Base:init(robots)
+end
+
+function Base:canContinue()
+	error("stub")
+end
+
+function Base:updateTasks()
+	error("stub")
+end
+
+
+return Base
