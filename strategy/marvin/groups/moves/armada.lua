186fca1ebc56a92bdce2cd161a99195d78310532
Tobias Heineken <tobias.heineken@robotics-erlangen.de>
2016-11-17 17:38:30 +0100

    Marvin: gr/m/armadatask: remove globals

--- strategy/marvin/groups/moves/armada.lua
+++ strategy/marvin/groups/moves/armada.lua
@@ -10,13 +10,45 @@ local G = World.Geometry
 
 Armada.N_ROBOTS = 5
 
+-- the armada has 4 steps to form stairs, depending on ball distance
+local X_POSITIONS_ORIG = {
+	G.FieldWidthHalf * 5/8,
+	G.FieldWidthHalf * 1/4,
+	-G.FieldWidthHalf * 1/4,
+	-G.FieldWidthHalf * 5/8
+}
+local Y_BALL_DISTS_RIGHT_ORIG = {
+	-G.FieldHeightHalf * 1/4,
+	0,
+	G.FieldHeightHalf * 1/4,
+	G.FieldHeightHalf * 1/2,
+}
+local Y_BALL_DISTS_LEFT_ORIG = {
+	G.FieldHeightHalf * 1/2,
+	G.FieldHeightHalf * 1/4,
+	0,
+	-G.FieldHeightHalf * 1/4
+}
+
+local MAX_RANDOM_POSITION_OFFSET = 0.3
+local CIRCLE_CENTER_ORIG = Vector(0,-1)
+
 function Armada.canStart()
 	return  World.Ball.pos.y > G.FieldHeightHalf / 5 --and Referee.opponentTouchedLast()
 		and math.abs(World.Ball.pos.x) > G.FieldWidthHalf / 2
 		and World.RefereeState == "Stop"
 end
 
+local function getRandomOffsetVector()
+	local result = Vector(0,0)
+	result.x = (math.random()-0.5)*2 * MAX_RANDOM_POSITION_OFFSET
+	result.y = (math.random()-0.5)*2 * MAX_RANDOM_POSITION_OFFSET
+	return result
+end
+
 function Armada:_init()
+	self._circleCenter = Vector(0,0) + getRandomOffsetVector()
+	self._yPosOrig = World.Ball.pos.x > 0 and Y_BALL_DISTS_RIGHT_ORIG or Y_BALL_DISTS_LEFT_ORIG
 end
 
 function Armada:_canContinue()
@@ -31,11 +63,15 @@ end
 function Armada:_updateTasks()
 	local taskAssignments = {}
 	taskAssignments[self._robots[1]] = { class = StopAttack, params = { } }
-	taskAssignments[self._robots[2]] = { class = ArmadaTask, params = { 1 } }
-	taskAssignments[self._robots[3]] = { class = ArmadaTask, params = { 2 } }
-	taskAssignments[self._robots[4]] = { class = ArmadaTask, params = { 3 } }
-	taskAssignments[self._robots[5]] = { class = ArmadaTask, params = { 4 } }
-	return taskAssignments
+	taskAssignments[self._robots[2]] = { class = ArmadaTask, params = { 1, self._circleCenter,
+		Vector(X_POSITIONS_ORIG[1], self._yPosOrig[1]) + getRandomOffsetVector() } }
+	taskAssignments[self._robots[3]] = { class = ArmadaTask, params = { 2, self._circleCenter,
+		Vector(X_POSITIONS_ORIG[2], self._yPosOrig[2]) + getRandomOffsetVector() } }
+	taskAssignments[self._robots[4]] = { class = ArmadaTask, params = { 3, self._circleCenter,
+		Vector(X_POSITIONS_ORIG[3], self._yPosOrig[3]) + getRandomOffsetVector() } }
+	taskAssignments[self._robots[5]] = { class = ArmadaTask, params = { 4, self._circleCenter,
+		Vector(X_POSITIONS_ORIG[4], self._yPosOrig[4]) + getRandomOffsetVector() } }
+	return taskAssignments 
 end
 
 return Armada
--- strategy/marvin/groups/moves/armadatask.lua
+++ strategy/marvin/groups/moves/armadatask.lua
@@ -9,60 +9,17 @@ local ToTarget = require "trajectory/totarget"
 
 local G = World.Geometry
 
--- the armada has 4 steps to form stairs, depending on ball distance
-local X_POSITIONS_ORIG = {
-	G.FieldWidthHalf * 5/8,
-	G.FieldWidthHalf * 1/4,
-	-G.FieldWidthHalf * 1/4,
-	-G.FieldWidthHalf * 5/8
-}
-local Y_BALL_DISTS_RIGHT_ORIG = {
-	-G.FieldHeightHalf * 1/4,
-	0,
-	G.FieldHeightHalf * 1/4,
-	G.FieldHeightHalf * 1/2,
-}
-local Y_BALL_DISTS_LEFT_ORIG = {
-	G.FieldHeightHalf * 1/2,
-	G.FieldHeightHalf * 1/4,
-	0,
-	-G.FieldHeightHalf * 1/4
-}
-
-local X_POSITIONS = {}
-local Y_BALL_DISTS_RIGHT = {}
-local Y_BALL_DISTS_LEFT = {}
-local CIRCLE_CENTER_ORIG = Vector(0,-1)
-local CIRCLE_CENTER = Vector(0,0)
-local MAX_RANDOM_POSITION_OFFSET = 0.3
-
-local function shufflePositions()
-	for i, pos in ipairs (X_POSITIONS_ORIG) do
-		X_POSITIONS[i] = pos + (math.random()-0.5)*2 * MAX_RANDOM_POSITION_OFFSET
-	end
-	for i, pos in ipairs (Y_BALL_DISTS_RIGHT_ORIG) do
-		Y_BALL_DISTS_RIGHT[i] = pos + (math.random()-0.5)*2 * MAX_RANDOM_POSITION_OFFSET
-	end
-	for i, pos in ipairs (Y_BALL_DISTS_LEFT_ORIG) do
-		Y_BALL_DISTS_LEFT[i] = pos + (math.random()-0.5)*2 * MAX_RANDOM_POSITION_OFFSET
-	end
-	CIRCLE_CENTER.x = CIRCLE_CENTER_ORIG.x + (math.random()-0.5)*2 * MAX_RANDOM_POSITION_OFFSET
-	CIRCLE_CENTER.y = CIRCLE_CENTER_ORIG.y + (math.random()-0.5)*2 * MAX_RANDOM_POSITION_OFFSET
-end
-
-function ArmadaTask:_init(posIndex)
+function ArmadaTask:_init(posIndex, circleCenter, target)
 	self._posIndex = posIndex
 	self._moveDest = nil
-	self._armadaPosTaken = false
-	if posIndex == 1 then
-		shufflePositions()
-	end
+	self._circleCenter = circleCenter
+	self._target = target
 end
 
 function ArmadaTask:_pointOnCircle()
 	local angle = (World.Time % 1000) % (math.pi*2)
 	local individualAngle = angle + (math.pi/2)*self._posIndex
-	local pos = CIRCLE_CENTER + Vector.fromAngle(individualAngle)*0.5
+	local pos = self._circleCenter + Vector.fromAngle(individualAngle)*0.5
 	return pos
 end
 
@@ -71,14 +28,7 @@ function ArmadaTask:run()
 	if World.RefereeState == "Stop" then
 		self._moveDest = self:_pointOnCircle()
 	else -- Direct or Indirect Freekick
-		if not self._armadaPosTaken then
-			local yPos = Y_BALL_DISTS_LEFT[self._posIndex]
-			if World.Ball.pos.x > 0 then
-				yPos = Y_BALL_DISTS_RIGHT[self._posIndex]
-			end
-			self._moveDest = Vector(X_POSITIONS[self._posIndex], yPos)
-			self._armadaPosTaken = true
-		end
+		self._moveDest = self._target
 		self:_suggestPass(self._moveDest)
 	end
 	PathHelper.setDefaultObstacles(self._robot.path, self._robot)
