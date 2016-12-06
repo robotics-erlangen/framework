2b18c80dc22757ac455c6556353c7278bcc9c67c
Linda Stadter <linda.stadter@robotics-erlangen.de>
2016-12-06 19:59:37 +0100

    marvin: a/attacker/doubletouchguard no longer active after successful freekick

--- strategy/marvin/agent/attacker/doubletouchguard.lua
+++ strategy/marvin/agent/attacker/doubletouchguard.lua
@@ -2,13 +2,17 @@ local Base = require "agent/base/behavior"
 local DoubleTouchGuard = Class("Agent.Attacker.DoubleTouchGuard", Base)
 
 local Referee = require "../base/referee"
+local World = require "../base/world"
+local Ball = require "observer/ball"
 local Robot = require "observer/robot"
 local StopAttack = require "task/stopattack"
 
+
 --prevents freekicking robot from moving away after failed shot
 
-function DoubleTouchGuard:check()	
-	if not Referee.isFriendlyFreeKickState() and Robot.ownFreeKickShooter() == self._robot then
+function DoubleTouchGuard:check()
+
+	if World.RefereeState =="Game" and Robot.ownFreeKickShooter() == self._robot and not Ball.wasShot(World.Time-Referee.lastStateChangeTime()) then
 		return true
 	end
 	return false
