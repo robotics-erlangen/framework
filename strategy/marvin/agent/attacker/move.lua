f7cc7fb8dfbeffac61bded4b3e4408cd41ee32db
Linda Stadter <linda.stadter@robotics-erlangen.de>
2017-01-26 17:24:16 +0100

    Marvin: a/a/move forceKeepingInPool is now only for the target robot true

--- strategy/marvin/agent/attacker/move.lua
+++ strategy/marvin/agent/attacker/move.lua
@@ -18,8 +18,13 @@ function Move:check()
 end
 
 function Move:_updateTask()
-	self._forceKeepingInPool = next(self._inbox.passPos()) ~= nil
-
+	local _, msg = next(self._inbox.passPos())
+	if msg ~= nil then
+		if msg.robot == self._robot then
+			self._forceKeepingInPool = true
+		end
+	end
+	
 	local assignment = self._inbox.moveAssignment().trainer
 	if assignment.behavior then
 		if not self._behavior then
