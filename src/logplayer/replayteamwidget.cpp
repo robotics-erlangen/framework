0e5c4cff1890bbe80f3e0b9a06ca0da99728bc19
Andreas Wendler <andreas.wendler@robotics-erlangen.de>
2017-01-26 23:11:17 +0100

    Ra: create ReplayTeamWidget

---
+++ src/logplayer/replayteamwidget.cpp
@@ -0,0 +1,60 @@
+#include <QSettings>
+#include "replayteamwidget.h"
+#include "ui_replayteamwidget.h"
+
+ReplayTeamWidget::ReplayTeamWidget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::ReplayTeamWidget)
+{
+    ui->setupUi(this);
+    ui->blue->init(true);
+    ui->yellow->init(false);
+    QSettings s;
+    s.beginGroup("Strategy");
+    m_recentScripts = s.value("RecentScripts").toStringList();
+    s.endGroup();
+
+    ui->blue->setRecentScripts(&m_recentScripts);
+    ui->yellow->setRecentScripts(&m_recentScripts);
+    ui->blue->load();
+    ui->yellow->load();
+
+    connect(ui->replayBlue, SIGNAL(clicked(bool)), ui->blue, SLOT(setEnabled(bool)));
+    connect(ui->replayYellow, SIGNAL(clicked(bool)), ui->yellow, SLOT(setEnabled(bool)));
+    connect(ui->replayBlue, SIGNAL(clicked(bool)), this, SIGNAL(enableStrategyBlue(bool)));
+    connect(ui->replayYellow, SIGNAL(clicked(bool)), this, SIGNAL(enableStrategyYellow(bool)));
+
+    connect(ui->blue, SIGNAL(sendCommand(Command)), this, SIGNAL(sendCommand(Command)));
+    connect(ui->yellow, SIGNAL(sendCommand(Command)), this, SIGNAL(sendCommand(Command)));
+    connect(this, SIGNAL(gotStatusBlue(Status)), ui->blue, SLOT(handleStatus(Status)));
+    connect(this, SIGNAL(gotStatusYellow(Status)), ui->yellow, SLOT(handleStatus(Status)));
+}
+
+ReplayTeamWidget::~ReplayTeamWidget()
+{
+    QSettings s;
+    s.beginGroup("Strategy");
+    s.setValue("RecentScripts", m_recentScripts);
+    s.endGroup();
+    delete ui;
+}
+
+void ReplayTeamWidget::handleStatus(const Status &status)
+{
+    if (ui->replayBlue->isChecked()) {
+        emit gotStatusBlue(status);
+    }
+    if (ui->replayYellow->isChecked()) {
+        emit gotStatusYellow(status);
+    }
+}
+
+bool ReplayTeamWidget::replayBlueEnabled() const
+{
+    return ui->replayBlue->isChecked();
+}
+
+bool ReplayTeamWidget::replayYellowEnabled() const
+{
+    return ui->replayYellow->isChecked();
+}
---
+++ src/logplayer/replayteamwidget.h
@@ -0,0 +1,39 @@
+#ifndef REPLAYTEAMWIDGET_H
+#define REPLAYTEAMWIDGET_H
+
+#include <QWidget>
+#include <QStringList>
+#include "protobuf/status.h"
+#include "protobuf/command.h"
+
+namespace Ui {
+class ReplayTeamWidget;
+}
+
+class ReplayTeamWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit ReplayTeamWidget(QWidget *parent = 0);
+    ~ReplayTeamWidget();
+    bool replayBlueEnabled() const;
+    bool replayYellowEnabled() const;
+
+public slots:
+    void handleStatus(const Status &status);
+
+signals:
+    void sendCommand(const Command &command);
+    void gotStatusBlue(const Status & status);
+    void gotStatusYellow(const Status & status);
+    void enableStrategyBlue(bool enabled);
+    void enableStrategyYellow(bool enabled);
+
+private:
+    Ui::ReplayTeamWidget *ui;
+
+    QStringList m_recentScripts;
+};
+
+#endif // REPLAYTEAMWIDGET_H
