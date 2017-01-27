9895436a0e970c1e9b639e5c388aa8bc4b45d802
Christian Lobmeier <christian.lobmeier@robotics-erlangen.de>
2017-01-27 19:49:45 +0100

    Marvin: t/pass: targetPos is an optional parameter now

--- strategy/marvin/task/pass.lua
+++ strategy/marvin/task/pass.lua
@@ -1,14 +1,30 @@
 local Shoot = require "task/ability/shoot"
 local Pass = Class("Task.Pass", require "task/base", Shoot)
 
+local debug = require "../base/debug"
+
 function Pass:_init(targetRobot, targetPos, chip, passSpeed)
 	self._targetRobot = targetRobot
 	self._targetPos = targetPos
 	self._chip = chip
 	self._passSpeed = passSpeed or self._targetRobot.constants.passSpeed
+
+	-- retrieve targetPos from messages if no argument was given
+	if not targetPos then
+		local sugg = self._inbox.passSuggestion()[targetRobot]
+		if sugg then
+			self._targetPos = sugg.pos
+		else
+			self._targetPos = targetRobot.pos +
+				Vector.fromAngle(targetRobot.dir) * targetRobot.shootRadius
+		end
+	end
 end
 
 function Pass:run()
+	debug.set("targetRobot", self._targetRobot)
+	debug.set("targetPos", self._targetPos)
+
 	self:_shoot(self._targetPos, self._passSpeed, not self._chip, 3 * math.pi/180, false)
 end
 
