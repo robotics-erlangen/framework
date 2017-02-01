fab8bd88bf3f3c88cd600d9f2ba3a293cae9faad
Tobias Heineken <tobias.heineken@robotics-erlangen.de>
2017-02-01 08:29:52 +0100

    a/a/doubletouchguard: enabled after direct freekicks, too

--- strategy/marvin/agent/attacker/doubletouchguard.lua
+++ strategy/marvin/agent/attacker/doubletouchguard.lua
@@ -1,6 +1,7 @@
 local Base = require "agent/base/behavior"
 local DoubleTouchGuard = Class("Agent.Attacker.DoubleTouchGuard", Base)
 
+local Referee = require "../base/referee"
 local World = require "../base/world"
 local Ball = require "observer/ball"
 local Robot = require "observer/robot"
@@ -10,7 +11,7 @@ local StopAttack = require "task/stopattack"
 --prevents freekicking robot from moving away after failed shot
 local lastFreekickTime = 0
 function DoubleTouchGuard:check()
-	if World.RefereeState == "IndirectOffensive" then
+	if Referee.isFriendlyFreeKickState() then
 		lastFreekickTime = World.Time
 	end
 	if World.RefereeState == "Game" and Robot.ownStandardShooter() == self._robot and not Ball.wasShot(World.Time-lastFreekickTime) then
