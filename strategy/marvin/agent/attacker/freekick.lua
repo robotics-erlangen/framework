bd7e4feafc9dc9f2e3a21d49c8f52fbbc02d1a5e
Christian Lobmeier <christian.lobmeier@robotics-erlangen.de>
2017-03-01 20:19:37 +0100

    Marvin: a/a/freekick: reduce wait time to 1.5 seconds

--- strategy/marvin/agent/attacker/freekick.lua
+++ strategy/marvin/agent/attacker/freekick.lua
@@ -72,7 +72,7 @@ function FreeKick:_updateTask()
 
 	-- wait -> shootgoal
 	-- wait -> pass_prepare
-	local MIN_PASS_WAIT_TIME = 2.5
+	local MIN_PASS_WAIT_TIME = 1.5
 	local MAX_TIMEFRAME = 8
 	local timeRunningOut = World.Time - Referee.lastStateChangeTime() >= MAX_TIMEFRAME
 	if self._state == "wait" then
