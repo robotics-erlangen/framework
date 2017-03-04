ba0915dcbd5c50428c08363ba24620e3bdfd7532
Tobias Heineken <tobias.heineken@robotics-erlangen.de>
2017-03-04 17:35:19 +0100

    marvin: a/s/error: show collected values

--- strategy/marvin/agent/shared/error.lua
+++ strategy/marvin/agent/shared/error.lua
@@ -16,34 +16,34 @@ function Error:errorMsg()
 	local msgParts = {}
 	local errorData = ErrorObserver.getErrorTable(self._robot)
 	if errorData.motor_1_error then
-		table.insert(msgParts, "motor 1 error")
+		table.insert(msgParts, "motor 1 error" .. tostring(errorData.motor_1_error))
 	end
 	if errorData.motor_2_error then
-		table.insert(msgParts, "motor 2 error")
+		table.insert(msgParts, "motor 2 error" .. tostring(errorData.motor_2_error))
 	end
 	if errorData.motor_3_error then
-		table.insert(msgParts, "motor 3 error")
+		table.insert(msgParts, "motor 3 error" .. tostring(errorData.motor_3_error))
 	end
 	if errorData.motor_4_error then
-		table.insert(msgParts, "motor 4 error")
+		table.insert(msgParts, "motor 4 error" .. tostring(errorData.motor_4_error))
 	end
 	if errorData.dribbler_error then
-		table.insert(msgParts, "dribber error")
+		table.insert(msgParts, "dribber error" .. tostring(errorData.dribbler_error))
 	end
 	if errorData.kicker_error then
-		table.insert(msgParts, "kicker error")
+		table.insert(msgParts, "kicker error" .. tostring(errorData.kicker_error))
 	end
 	if errorData.motorOverheatedError then
-		table.insert(msgParts, "motor overheat")
+		table.insert(msgParts, "motor overheat" .. tostring(errorData.motorOverheatedError))
 	end
 	if errorData.motor_encoder_error then
-		table.insert(msgParts, "motor encoder")
+		table.insert(msgParts, "motor encoder" .. tostring(errorData.motor_encoder_error))
 	end
 	if errorData.main_sensor_error then
-		table.insert(msgParts, "main sensor")
+		table.insert(msgParts, "main sensor" .. tostring(errorData.main_sensor_error))
 	end
 	if errorData.kicker_beak_beam_error then
-		table.insert(msgParts, "kicker beam error")
+		table.insert(msgParts, "kicker beam error" .. tostring(errorData.kicker_beak_beam_error))
 	end
 	if errorData.temperature then
 		table.insert(msgParts, "temperature: " .. tostring(errorData.temperature))
