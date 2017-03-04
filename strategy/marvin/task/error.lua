4fc8e40f5a8f10c6ec415a74bd1a015c528c4595
Tobias Heineken <tobias.heineken@robotics-erlangen.de>
2017-03-04 19:39:46 +0100

    marvin: t/error: move to exchange pos instead of rotating

--- strategy/marvin/task/error.lua
+++ strategy/marvin/task/error.lua
@@ -1,8 +1,16 @@
 local Error = Class("Task.Error", require "task/base")
+local World = require "../base/world"
 local Direct = require "trajectory/direct"
+local ToTarget = require "trajectory/totarget"
+
+local EXCHANGE_TARGET = Vector(World.Geometry.FieldWidthHalf - 1, -1)
 
 function Error:run()
-	self._robot.trajectory:update(Direct, Vector(0, 0), nil, 2*math.pi)
+	if self._robot.pos:distanceTo(EXCHANGE_TARGET) > 0.5 then
+		self._robot.trajectory:update(ToTarget, EXCHANGE_TARGET, 0)
+	else
+		self._robot.trajectory:update(Direct, Vector(0, 0), nil, 2*math.pi)
+	end
 end
 
 return Error
