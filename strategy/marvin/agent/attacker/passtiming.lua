5375538d0270bff11994c6749ea16c665445c7c6
Linda Stadter <linda.stadter@robotics-erlangen.de>
2017-06-11 17:22:08 +0200

    Marvin: a/a/passtiming robot receiving the ball waits if there is enough time

---
+++ strategy/marvin/agent/attacker/passtiming.lua
@@ -0,0 +1,32 @@
+local Base = require "agent/base/behavior"
+local PassTiming = Class("Agent.Attacker.PassTiming", Base)
+
+local World = require "../base/world"
+local Physics = require "observer/physics"
+local MoveToPos = require "task/movetopos"
+local Attack = require "util/attack"
+
+function PassTiming:check()
+	local lastIncomingPassInfo = Attack.lastIncomingPassInfo(self._robot, self._inbox.passInfo())
+	local lastIncomingPassInfoPos = nil
+
+	if lastIncomingPassInfo then
+		lastIncomingPassInfoPos = lastIncomingPassInfo.ballPos
+	end
+
+	if lastIncomingPassInfoPos then
+		local robotTimeToPassPos = Physics.robotTimeToPos(self._robot, lastIncomingPassInfoPos, Vector(0,0))
+		local ballTimeToPassPos = Physics.ballTravelTime(World.Ball, World.Ball.pos:distanceTo(lastIncomingPassInfoPos))
+		if robotTimeToPassPos + 0.035 <= ballTimeToPassPos then
+			if not Attack.checkPassInfos(self._robot, lastIncomingPassInfo) then
+				return true
+			end
+		end
+	end
+end
+
+function PassTiming:_updateTask()
+	return MoveToPos, {self._robot.pos}
+end
+
+return PassTiming
