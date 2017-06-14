93db78c0e62e4b19823429db0b08fd9c3d49dbe9
Christian Lobmeier <christian.lobmeier@robotics-erlangen.de>
2017-06-14 23:51:00 +0200

    Marvin: o/p/robotTimeToPos: tweaking

--- strategy/marvin/test/move/timetopos.lua
+++ strategy/marvin/test/move/timetopos.lua
@@ -1,5 +1,6 @@
 local TimeToPos = Class("Test.Move.TimeToPos", require "group/move/base")
 
+local plot = require "../base/plot"
 local vis = require "../base/vis"
 local World = require "../base/world"
 
@@ -16,9 +17,12 @@ function TimeToPos:_init()
 	self._state = 1
 
 	self._positions = {
-		Vector(1, -2),
-		Vector(-3, -2),
-		Vector(1, 0),
+		-- Vector(1, -2), Vector(-3, -2), Vector(1, -2)
+		Vector(1, -2), Vector(-3, -2), Vector(1, 3)
+		-- Vector(0.2, -2), Vector(-3, -2), Vector(-0.4, -2)
+		-- Vector(1, -2), Vector(-2, -2), Vector(2, -2),
+		-- Vector(1, -2), Vector(-2, -2), Vector(-1, -1.7),
+		-- Vector(0.1, -2), Vector(-1, -2), Vector(-0.07, -1.7)
 	}
 
 	self._endSpeedLength = 0
@@ -37,17 +41,18 @@ end
 
 function TimeToPos:_updateTasks()
 	local taskAssignments = {}
-
+	local plotVal = 0
 	local pos = self._robots[1].pos
 	local state = self._state
-	if self._state == 1 and pos:distanceTo(self._positions[1]) < 0.01 then
+	if self._state == 1 and pos:distanceTo(self._positions[1]) < 0.005 then
 		state = 2
 	elseif self._state == 2 and pos.x < 0 then
 		state = 3
 		self._startTime = World.Time
 		self._estimation2, self._brakeTime, self._curveTime = Physics.robotTimeToPos(self._robots[1], self._positions[3], Vector(0, self._endSpeedLength))
+		plotVal = 0.1
 		log("Estimation 2: " .. tostring(self._estimation2))
-	elseif self._state == 3 and pos:distanceTo(self._positions[3]) < 0.001 and self._robots[1].speed:length() <= self._endSpeedLength + 0.1 then
+	elseif self._state == 3 and pos:distanceTo(self._positions[3]) < 0.005 and self._robots[1].speed:length() <= self._endSpeedLength + 0.1 then
 		local measuredTime = World.Time - self._startTime
 		log("Measurement: " .. tostring(measuredTime))
 		log("Error 2: " .. tostring(self._estimation2 - measuredTime))
@@ -57,6 +62,9 @@ function TimeToPos:_updateTasks()
 		self._brakePos = nil
 		self._curvePos = nil
 	end
+
+	plot.addPlot("RTTP", plotVal)
+	plot.addPlot("RobotSpeed", self._robots[1].speed:length())
 
 	if not self._brakePos and self._brakeTime and World.Time > self._startTime + self._brakeTime then
 		self._brakePos = self._robots[1].pos
