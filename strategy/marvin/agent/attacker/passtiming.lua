d566bd32ce27b133542a0e2395b6511c69548f46
Christian Lobmeier <christian.lobmeier@robotics-erlangen.de>
2017-06-22 16:08:58 +0200

    Marvin: a/a/passtiming: add mainAttacker check

--- strategy/marvin/agent/attacker/passtiming.lua
+++ strategy/marvin/agent/attacker/passtiming.lua
@@ -1,12 +1,17 @@
 local Base = require "agent/base/behavior"
 local PassTiming = Class("Agent.Attacker.PassTiming", Base)
 
+local debug	 = require "../base/debug"
 local World = require "../base/world"
 local Physics = require "observer/physics"
 local MoveToPos = require "task/movetopos"
 local Attack = require "util/attack"
 
 function PassTiming:check()
+	if self._inbox.mainAttacker().trainer ~= self._robot then
+		return false
+	end
+
 	local lastIncomingPassInfo = Attack.lastIncomingPassInfo(self._robot, self._inbox.passInfo())
 	local lastIncomingPassInfoPos = nil
 
@@ -19,10 +24,14 @@ function PassTiming:check()
 		local ballTimeToPassPos = Physics.ballTravelTime(World.Ball, World.Ball.pos:distanceTo(lastIncomingPassInfoPos))
 		if robotTimeToPassPos + 0.035 <= ballTimeToPassPos then
 			if not Attack.checkPassInfos(self._robot, lastIncomingPassInfo) then
+				debug.set("PassTiming/robotTime", robotTimeToPassPos)
+				debug.set("PassTiming/ballTime", ballTimeToPassPos)
 				return true
 			end
 		end
 	end
+
+	return false
 end
 
 function PassTiming:_updateTask()
