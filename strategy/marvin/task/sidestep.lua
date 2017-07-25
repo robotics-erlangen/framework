18c41d9e5d0e8755b272743d34d2545488f37944
Christian Lobmeier <christian.lobmeier@robotics-erlangen.de>
2017-07-25 10:14:43 +0200

    Marvin: t/sidestep: send striker application and pass suggestion

--- strategy/marvin/task/sidestep.lua
+++ strategy/marvin/task/sidestep.lua
@@ -1,5 +1,5 @@
 local SuggestPass = require "task/ability/suggestpass"
-local SideStep = Class("Task.SideStep", require "task/base")
+local SideStep = Class("Task.SideStep", require "task/base", SuggestPass)
 
 local PathHelper = require "trajectory/pathhelper"
 local ToTarget = require "trajectory/totarget"
@@ -9,6 +9,23 @@ function SideStep:_init()
 end
 
 function SideStep:run()
+	local groupApplication = { name = "striker", payload = {}}
+	self._send.groupApplication("trainer", groupApplication)
+
+	local _, passInfoTable = next(self._inbox.passInfo())
+	local passInfo = nil
+	for _, pass in ipairs(passInfoTable) do
+		if pass.target == self._robot then
+			passInfo = pass
+			break
+		end
+	end
+
+	local _, attackPosition = next(self._inbox.attackPosition())
+	if attackPosition then
+		self:_suggestPass(passInfo.ballPos, attackPosition, passInfo.time)
+	end
+
 	PathHelper.setDefaultObstacles(self._robot.path, self._robot)
 	PathHelper.addRobotObstacles(self._robot.path, self._robot)
 
