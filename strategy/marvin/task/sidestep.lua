f0e4471bddb609713bd76546036a655d43bb1dd3
Lukas Wegmann <lukas.wegmann@robotics-erlangen.de>
2017-07-26 06:34:25 +0200

    Marvin: t/sidestep changed evasion pos, added rating, still needs testing, contains minor bugs

--- strategy/marvin/task/sidestep.lua
+++ strategy/marvin/task/sidestep.lua
@@ -3,13 +3,109 @@ local SideStep = Class("Task.SideStep", require "task/base", SuggestPass)
 
 local PathHelper = require "trajectory/pathhelper"
 local ToTarget = require "trajectory/totarget"
+local Rating = require "util/rating"
+local Field = require "../base/field"
+local geom = require "../base/geom"
 local World = require "../base/world"
+local vis = require "../base/vis"
+local debug = require "../base/debug"
+local G = World.Geometry
+
+local MANMARK_DISTANCE_THRESHOLD = 0.2
+
+function SideStep:_projectBotsOnLine(point1, point2)
+	local bestDist = math.huge
+	for _, r in ipairs(World.OpponentRobots) do
+		local dist = r.pos:distanceToLineSegment(point1, point2)
+		if dist < bestDist then
+			bestDist = dist
+		end
+	end
+	return bestDist
+end
+
+function SideStep:_rateLine(line)
+	local h = G.FieldHeightHalf
+	local w = G.FieldWidthHalf
+	local topLeft = Vector(-w, h)
+	local topRight = Vector(w, h)
+	local bottomLeft = Vector(-w, -h)
+	local bottomRight = Vector(w, -h)
+	local horizontal = Vector(0, 1)
+	local vertical = Vector(1, 0)
+	local point, lambda = geom.intersectLineLine(self._passInfo.ballPos, line, topLeft, -horizontal)
+	if not lambda or lambda < 0 or (point:distanceToLineSegment(topLeft, bottomLeft) > 0.01) then
+		point, lambda = geom.intersectLineLine(self._passInfo.ballPos, line, bottomRight, horizontal)
+	end
+	if not lambda or lambda < 0 or (point:distanceToLineSegment(topRight, bottomLeft) > 0.01) then
+		local intersection = Field.intersectRayDefenseArea(self._passInfo.ballPos, line, 0.2, true)
+		intersection = intersection or Field.intersectRayDefenseArea(self._passInfo.ballPos, line, 0.2)
+		if intersection then
+			lambda = intersection:distanceTo(self._passInfo.ballPos)
+		end
+	end
+	if not lambda or lambda < 0 then
+		point, lambda = geom.intersectLineLine(self._passInfo.ballPos, line, topLeft, vertical)
+	end
+	if not lambda or lambda < 0 or (point:distanceToLineSegment(topLeft, topRight) > 0.01) then
+		local _
+		_, lambda = geom.intersectLineLine(self._passInfo.ballPos, line, bottomRight, -vertical)
+	end
+	local rating = 1 - Rating.valueToRating(lambda, 2, 0)/2
+	local dist = self:_projectBotsOnLine(self._passInfo.ballPos, self._passInfo.ballPos + line)
+	local distRating = Rating.valueToRating(dist, 1, MANMARK_DISTANCE_THRESHOLD)
+
+	rating = rating - (1 - distRating) / 10
+	return lambda, rating
+end
 
 function SideStep:_init(passInfo)
 	self._passInfo = passInfo
+	self._feintPos = nil
+	local passBlocked = self:_projectBotsOnLine(self._robot.pos, passInfo.ballPos) > MANMARK_DISTANCE_THRESHOLD
+	local line
+	if passBlocked then
+		line = (passInfo.ballPos - World.Ball.pos):setLength(1)
+	else
+		line = (G.OpponentGoal - passInfo.ballPos):setLength(1)
+	end
+	local clockwise = line:copy():perpendicular()
+	local counterClockwise = line:copy():rotate(math.pi / 2)
+	local ccwDist, ccwRating = self:_rateLine(counterClockwise)
+	local cwDist, cwRating = self:_rateLine(clockwise)
+	if cwRating > ccwRating then
+		self._feintPos = passInfo.ballPos + clockwise:setLength(cwDist)
+	else
+		self._feintPos = passInfo.ballPos + counterClockwise:setLength(ccwDist)
+	end
+	self._debugTable = {	startingPoint = self._passInfo.ballPos,
+							ballPos = passInfo.ballPos,
+							passBlocked = passBlocked, 
+							line = line, cw = clockwise,
+							cwDist = cwDist,
+							cwRating = cwRating,
+							ccw = counterClockwise,
+							ccwDist = ccwDist,
+							ccwRating = ccwRating,
+							feintPos = self._feintPos}
+end
+
+local function draw(table)
+	local t = table
+	debug.push("sideStep Debug")
+	for a, b in pairs(t) do
+		debug.set(a, b)
+	end
+	debug.pop()
+	log("startingPoint2: "..tostring(t.startingPoint))
+	vis.addCircle("sideStep", t.startingPoint, 0.05, vis.colors.blue, true)
+	vis.addCircle("sideStep", t.feintPos, 0.05, vis.colors.red, true)
+	vis.addPath("sideStep", {t.ballPos, t.ballPos + t.cw:setLength(t.cwDist)}, vis.fromTemperature(t.cwRating))
+	vis.addPath("sideStep", {t.ballPos, t.ballPos + t.ccw:setLength(t.ccwDist)}, vis.fromTemperature(t.ccwRating))
 end
 
 function SideStep:run()
+	draw(self._debugTable)
 	local groupApplication = { name = "striker", payload = {}}
 	self._send.groupApplication("trainer", groupApplication)
 
@@ -22,7 +118,7 @@ function SideStep:run()
 	PathHelper.addRobotObstacles(self._robot.path, self._robot)
 
 	local dir = (World.Geometry.OpponentGoal - self._robot.pos):angle()
-	self._robot.trajectory:update(ToTarget, Vector(0, 0), dir)
+	self._robot.trajectory:update(ToTarget, self._feintPos, dir)
 end
 
 return SideStep
