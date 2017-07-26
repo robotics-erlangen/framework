ceffe607ef6e4c88382a2d8533d958cba1be48d2
Alexander Danzer <alexander.danzer@robotics-erlangen.de>
2017-07-26 11:35:05 +0900

    Marvin: chip challenge

---
+++ strategy/marvin/test/task/chipchallengereceiver.lua
@@ -0,0 +1,48 @@
+local Entrypoints = require "../base/entrypoints"
+local World = require "../base/world"
+local Shoot = require "task/ability/shoot"
+local PathHelper = require "trajectory/pathhelper"
+local ToTarget = require "trajectory/totarget"
+local TestHelper = require "test/helper/agent"
+local debug = require "../base/debug"
+
+local ChipChallengeReceiver = Class("Test.Task.ChipChallengeReceiver", require "task/base", Shoot)
+
+
+function ChipChallengeReceiver:_init()
+    self._ballKicked = false
+	self._moveDest =  World.Geometry.FriendlyGoal -- random fallback
+end
+
+function ChipChallengeReceiver:run()
+    local ball = World.Ball
+
+    if World.Ball.posZ > 0 then
+		self._ballKicked = true
+	end
+
+
+    local kickingRobot = World.OpponentRobots[1]
+    if self._ballKicked and kickingRobot and (kickingRobot.pos-World.Ball.pos):length() > 1.5 then
+        self._moveDest = World.Ball.touchdownPos
+    elseif kickingRobot and World.Ball.speed:length() < 0.3 then
+        debug.set("ball speed", World.Ball.speed:length())
+        local bp = World.Ball.pos
+		self._moveDest = bp + Vector.fromAngle(kickingRobot.dir):setLength(3.1)
+    end
+
+    local toBall = (ball.pos - self._robot.pos):angle()
+
+    PathHelper.setDefaultObstacles(self._robot.path, self._robot, false, true, true, 0, 0.1)
+	self._robot.trajectory:update(ToTarget, self._moveDest, toBall)
+end
+
+
+local Agent = Class("Test.Task.ChipChallengeReceiver.Agent", require "agent/base/simpleagent")
+Agent._behaviors = {
+	TestHelper.staticBehavior(ChipChallengeReceiver, { 1 })
+}
+
+
+local run = TestHelper.defaultCoordinator("attack", Agent, 1)
+Entrypoints.add("TaskTest/ChipChallengeReceiver", run)
