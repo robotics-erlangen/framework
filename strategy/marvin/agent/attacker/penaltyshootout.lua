694ce091cff2ab9503e2d2dbb3aff0be778a7cd7
Lukas Wegmann <lukas.wegmann@robotics-erlangen.de>
2017-07-27 06:35:39 +0200

    Marvin: a/a/penaltyshootout removed logspam, improved behaviour

--- strategy/marvin/agent/attacker/penaltyshootout.lua
+++ strategy/marvin/agent/attacker/penaltyshootout.lua
@@ -8,15 +8,17 @@ local G = World.Geometry
 local Goal = require "observer/goal"
 local Robot = require "observer/robot"
 local MoveToStaticBall = require "task/movetostaticball"
-local ShootPenalty = require "task/shootpenalty"
 local StopAttack = require "task/stopattack"
 local ShootGoal = require "task/shootgoal"
 
 local Dribble = require "task/dribble"
 
+local vis = require "../base/vis"
 
-local DISTANCE_TO_DEFENSE_AREA = 0.40 -- the furthest we'll go before we shoot
-local MAX_DIST_PER_DEGREE = 0.05
+
+local DISTANCE_TO_DEFENSE_AREA = 0.6 -- the furthest we'll go before we shoot
+local MIN_RELATIVE_SECTOR_SIZE = 1/3
+local DRIBBLING_DISTANCE = 0.075 -- Ball and Robot must be at least this far apart to reset dribbling
 
 
 function PenaltyShootout:_stop()
@@ -25,26 +27,30 @@ function PenaltyShootout:_stop()
 	self._shootGoalFlag = false
 end
 
-function PenaltyShootout:_start()
-	self._penaltyStartTime = World.Time
-end
-
 function PenaltyShootout:check()
 	local mainAttacker = self._inbox.mainAttacker().trainer == self._robot
 	local isPenalty = World.RefereeState == "PenaltyOffensivePrepare" or World.RefereeState == "PenaltyOffensive"
 	local isShootout = World.GameStage == "PenaltyShootout"
-	log(tostring(mainAttacker)..", "..tostring(isPenalty)..", "..tostring(isShootout)..", "..tostring(self:_checkPenaltyOngoing()))
-	return false and mainAttacker and isShootout and (isPenalty or self:_checkPenaltyOngoing())
+	-- log("")
+	-- log("check")
+	-- log("mainAttacker: "..tostring(mainAttacker))
+	-- log("isPenalty: "..tostring(isPenalty))
+	-- log("isShootout: "..tostring(isShootout))
+	-- log("onGoing: "..tostring(self:_checkPenaltyOngoing()))
+	return mainAttacker and isShootout and (isPenalty or self:_checkPenaltyOngoing())
 end
 
 function PenaltyShootout:_checkPenaltyOngoing()
-	return self._penaltyStartTime and World.Time - self._penaltyStartTime < 15 and Referee.lastStateChangeTime == self._penaltyStartTime
+	return self._penaltyStartTime and World.Time - self._penaltyStartTime < 15 and not Referee.isStopState()
 end
 
 function PenaltyShootout:_updateDribbling()
+	-- log("update")
 	if not self._contactPoint and Robot.hadBall(self._robot, 0) then
+		-- log("1")
 		self._contactPoint = self._robot.pos
-	elseif self._contactPoint and not Robot.hadBall(self._robot, 0) then
+	elseif self._contactPoint and World.Ball.pos:distanceTo(self._robot.pos) > DRIBBLING_DISTANCE + self._robot.radius then
+		-- log("2")
 		self._contactPoint = nil
 	end
 end
@@ -52,10 +58,22 @@ end
 function PenaltyShootout:_updateShootGoal()
 	local sector = Goal.largestFreeSector(World.Ball.pos, World.Robots, true)
 	local width = sector and math.abs(sector[1] - sector[2]) or 0
+
+	-- if (self._penltyStartTime and World.Time - self._penaltyStartTime > 8) then
+	-- 	log("2")
+	-- elseif World.Ball.pos.y > G.FieldHeightHalf - G.DefenseRadius - DISTANCE_TO_DEFENSE_AREA then
+	-- 	log("3")
+	-- elseif width < 2 * math.tan((G.GoalWidth / 2) / (G.FieldHeightHalf - self._robot.pos.y)) * MIN_RELATIVE_SECTOR_SIZE then
+	-- 	log("4")
+	-- 	log("width = "..tostring(width * 180))
+	-- 	log("sector = "..tostring(sector))
+	-- 	log("threshold = "..tostring(2 * math.tan((G.GoalWidth / 2) / (G.FieldHeightHalf - self._robot.pos.y)) * MIN_RELATIVE_SECTOR_SIZE) * 180)
+	-- end
+
 	if self._shootGoalFlag
 			or (self._penltyStartTime and World.Time - self._penaltyStartTime > 8)
-			or G.FieldHeightHalf - World.Ball.pos.y > width * MAX_DIST_PER_DEGREE
-			or World.Ball.pos.y > G.FieldHeightHalf - G.DefenseRadius - DISTANCE_TO_DEFENSE_AREA then
+			or World.Ball.pos.y > G.FieldHeightHalf - G.DefenseRadius - DISTANCE_TO_DEFENSE_AREA 
+			or width < 2 * math.tan((G.GoalWidth / 2) / (G.FieldHeightHalf - self._robot.pos.y)) * MIN_RELATIVE_SECTOR_SIZE then
 		self._shootGoalFlag = true
 	end
 end
@@ -63,20 +81,22 @@ end
 function PenaltyShootout:_updateTask()
 	self:_updateDribbling()
 	self:_updateShootGoal()
-	local annoyingKeeper = World.OpponentKeeper and World.OpponentKeeper.pos.y < G.FieldHeightHalf - 0.3 or false
-	if self._contactPoint and self._contactPoint:distanceTo(self._robot.pos) > 1 then
-		return StopAttack
-	elseif self._shootGoalFlag then
-		return ShootGoal
+	if self._contactPoint then
+		vis.addCircle("1test", self._contactPoint, 0.05, vis.colors.green, true)
 	end
+
+	if World.RefereeState == "PenaltyOffensive" and not self._penaltyStartTime then
+		-- log("Start Time set")
+		self._penaltyStartTime = World.Time
+	end
+
 	if World.RefereeState == "PenaltyOffensivePrepare" then
 		return MoveToStaticBall, {math.pi / 2, 0.1}
 	elseif self._shootGoalFlag then
 		return ShootGoal
-	elseif self._contactPoint and self._contactPoint:distanceTo(self._robot.pos) > 1 then
+	elseif self._contactPoint and self._contactPoint:distanceTo(World.Ball.pos) > 1 then
+		--log("distance: "..self._contactPoint:distanceTo(self._robot.pos))		
 		return StopAttack
-	elseif not annoyingKeeper and World.Ball.pos.y > G.FieldHeightHalf - G.DefenseRadius - DISTANCE_TO_DEFENSE_AREA then
-		return ShootPenalty
 	else
 		return Dribble, {Vector(0, G.FieldHeightHalf - G.DefenseRadius - 0.2)}
 	end
