e79551a2ed3f356c4ea4cf8d998ee9eb5634031a
Christian Lobmeier <christian.lobmeier@robotics-erlangen.de>
2017-07-30 01:17:03 +0200

    Marvin: a/a/applyformainattacker: only disable during a goal shot when the robot is between the ball and the opponent goal

--- strategy/marvin/agent/attacker/applyformainattacker.lua
+++ strategy/marvin/agent/attacker/applyformainattacker.lua
@@ -11,27 +11,40 @@ function ApplyForMainattacker:_init()
 end
 
 function ApplyForMainattacker:_stop()
+	self._applying = false
 end
 
 function ApplyForMainattacker:check()
 	if Referee.isOpponentPenaltyState() then
+		self._applying = false
 		return false
 	end
 
 	-- prevent double touches after a failed freekick by preventing the freekicking robot as mainattacker
 	if not Referee.isFriendlyFreeKickState() and Robot.ownStandardShooter() == self._robot then
+		self._applying = false
 		return false
 	end
+
+	local applying = false
 	local sender, passInfoTable = next(self._inbox.passInfo())
 	if Attack.currentPlannedMainAttacker(sender, passInfoTable) == self._robot then
 		self:_applyForMainAttacker(nil, nil, 2)
 		self._agent.beOffensive = true
+		applying = true
 	else
 		if not Defense.dangerousBallTowardsDefense(true) then
-			self:_applyForMainAttacker()
-			self._agent.beOffensive = false
+			local robotDistToGoal = self._robot.pos:distanceTo(World.Geometry.OpponentGoal)
+			local ballDistToGoal = World.Ball.pos:distanceTo(World.Geometry.OpponentGoal)
+			local maxDistDiff = (self._applying and -1 or 1) * (World.Ball.radius + self._robot.shootRadius)
+			if robotDistToGoal - ballDistToGoal > maxDistDiff then
+				self:_applyForMainAttacker()
+				self._agent.beOffensive = false
+				applying = true
+			end
 		end
 	end
+	self._applying = applying
 	return false
 end
 
