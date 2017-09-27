65056115b67bf1e07d6b4ad018cf322e3eeacee0
Andreas Wendler <andreas.wendler@robotics-erlangen.de>
2017-09-27 08:22:53 +0200

    Ra: remove compilation errors in Debug

--- src/amun/processor/tracking/abstractballfilter.h
+++ src/amun/processor/tracking/abstractballfilter.h
@@ -23,11 +23,11 @@ struct VisionFrame
     VisionFrame(const SSL_DetectionBall& b, qint64 t, qint32 c, RobotInfo r)
         : cameraId(c), ballArea(b.area()), x(-b.y()/1000), y(b.x()/1000), time(t), dribblerPos(r.dribblerPos), robotPos(r.robotPos), chipCommand(r.chipCommand), linearCommand(r.linearCommand) {}
     // b.area is optional in the protobuf but defaults to 0, so nothing bad can happen
-    const qint32 cameraId;
-    const quint32 ballArea;
-    const float x;
-    const float y;
-    const qint64 time;
+    qint32 cameraId;
+    quint32 ballArea;
+    float x;
+    float y;
+    qint64 time;
     Eigen::Vector2f dribblerPos;
     Eigen::Vector2f robotPos;
     bool chipCommand;
