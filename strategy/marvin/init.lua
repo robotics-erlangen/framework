30577ab0fd4fc38415342d6eb9e509a3be6753e5
Andreas Wendler <andreas.wendler@robotics-erlangen.de>
2017-10-25 11:43:44 +0200

    Marvin: seed randomness by world time

--- strategy/marvin/init.lua
+++ strategy/marvin/init.lua
@@ -56,6 +56,7 @@ local wrapper = function (func)
 			end
 			return -- skip processing if no vision data is available yet
 		end
+		math.randomseed(World.Time)
 		debug.set("frame", frameCount)
 		--local time0 = amun.getCurrentTime()
 		Processor.pre()
