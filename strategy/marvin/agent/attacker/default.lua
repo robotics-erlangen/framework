2aa98a19c4aa42377d34ec14e0f3c0236ecd3f45
Christian Lobmeier <christian.lobmeier@robotics-erlangen.de>
2017-11-02 17:10:59 +0100

    Marvin: a/a/default: remove dead and broken code

--- strategy/marvin/agent/attacker/default.lua
+++ strategy/marvin/agent/attacker/default.lua
@@ -24,18 +24,6 @@ function Default:check()
 		end
 	end
 
-	-- if there is a defender further away from the own goal than we are,
-	-- request a pool change
-	if not self._forceKeepingInPool then
-		local ownGoal = World.Geometry.FriendlyGoal
-		for defender, _ in ipairs(self._inbox.defenderFlag()) do
-			if defender.pos:distanceTo(ownGoal) < self._robot.pos:distanceTo(ownGoal) and
-					defender.pos:distanceTo(self._robot.pos) < MIN_DIST_FOR_POOL_CHANGE then
-				self._send.poolChangeRequest("trainer", "defender")
-			end
-		end
-	end
-
 	return true
 end
 
