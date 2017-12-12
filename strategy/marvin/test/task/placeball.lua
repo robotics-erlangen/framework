346c07b620ea1175874d5037e8e08b4fe3a0e3b1
Paul Bergmann <paul.bergmann@robotics-erlangen.de>
2017-12-12 17:07:02 +0100

    Add m/t/t/placeball.lua to ease testing of PlaceBall

---
+++ strategy/marvin/test/task/placeball.lua
@@ -0,0 +1,34 @@
+local Entrypoints = require "../base/entrypoints"
+local PlaceBall = require "task/placeball2"
+local Coordinator = require "control/coordinator"
+local Trainer = require "trainer/trainer"
+local AgentPool = require "control/agentpool"
+
+local Placer = Class("Test.Task.PlaceBall.Placer", require "agent/base/behavior")
+
+function Placer:check()
+	return true
+end
+
+function Placer:_updateTask()
+	return PlaceBall
+end
+
+local PlacerAgent = Class("Test.Task.PlaceBall.PlacerAgent", require "agent/base/simpleagent")
+PlacerAgent._behaviors = {
+	Placer
+}
+
+local coord = nil
+
+local function run()
+	if coord == nil then
+		local trainer = Trainer()
+		local pools = { pass = AgentPool(PlacerAgent, 1) }
+		local poolGroups = { { pools.pass } }
+		coord = Coordinator(trainer, pools, poolGroups)
+	end
+	coord:run()
+end
+
+Entrypoints.add("TaskTest/PlaceBall", run)
