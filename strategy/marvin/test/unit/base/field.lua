87ccd4307b66ba62c3c7ff385b574258501d4119
Michael Eischer <michael.eischer@robotics-erlangen.de>
2018-01-03 21:27:36 +0100

    Marvin: te/u/b/field: Cleanup unit test

--- strategy/marvin/test/unit/base/field.lua
+++ strategy/marvin/test/unit/base/field.lua
@@ -1,11 +1,16 @@
 local Injector = require "test/unit/injector"
 
 context("base.field", function()
-	local Referee, Field, World
-	local injector = Injector(nil)
+	local Field, Referee, World, G
+	local injector
+
+	local function initHelper(ruleversion)
+		World.RULEVERSION = ruleversion
+		Field = injector:load("../base/field")
+	end
 
 	before(function()
-
+		injector = Injector(nil)
 		injector:load("../base/math")
 		injector:load("../base/geom")
 
@@ -15,18 +20,13 @@ context("base.field", function()
 				FieldWidthHalf = 3,
 				FreeKickDefenseDist = 0.2,
 				DefenseStretch = 0.5,
-				-- DefenseStretchHalf = DefenseStretch / 2,
 				DefenseRadius = 1,
 				GoalWidth = 1,
 				GoalDepth = 0.18,
-				-- DefenseHeight = DefenseRadius,
-				-- DefenseWidth = DefenseHeight * 2,
-				-- DefenseWidthHalf = DefenseWidth / 2,
-
 			},
-			RULEVERSION = "2018"
+			RULEVERSION = nil -- keep unset for now
 		}
-		local G = World.Geometry
+		G = World.Geometry
 		G.DefenseStretchHalf = G.DefenseStretch / 2
 		G.DefenseHeight = G.DefenseRadius
 		G.DefenseWidth = G.DefenseHeight * 2
@@ -46,36 +46,32 @@ context("base.field", function()
 	end)
 
 	test("distanceToDefenseArea_2018", function()
-		local G = World.Geometry
-		World.RULEVERSION = "2018"
-		Field = injector:load("../base/field")
+		initHelper("2018")
 
-		local pos = Vector(0,0)
-		assert_equal(Field.distanceToDefenseArea(pos, 3,false), 0.5)
-		assert_equal(Field.distanceToDefenseArea(pos, 3,true), 0.5)
-		assert_false(Field.isInDefenseArea(pos,0.18,false))
-		assert_false(Field.isInDefenseArea(pos,0.18,true))
+		local pos = Vector(0, 0)
+		assert_equal(Field.distanceToDefenseArea(pos, 3, false), 0.5)
+		assert_equal(Field.distanceToDefenseArea(pos, 3, true), 0.5)
+		assert_false(Field.isInDefenseArea(pos, 0.18, false))
+		assert_false(Field.isInDefenseArea(pos, 0.18, true))
 		pos = Vector(0.1, G.FieldHeightHalf)
-		assert_true(Field.isInDefenseArea(pos,0.18,false))
-		assert_false(Field.isInDefenseArea(pos,0.18,true))
-		pos = Vector(G.DefenseWidthHalf + 0.2,-G.FieldHeightHalf+0.4)
+		assert_true(Field.isInDefenseArea(pos, 0.18, false))
+		assert_false(Field.isInDefenseArea(pos, 0.18, true))
+		pos = Vector(G.DefenseWidthHalf + 0.2, -G.FieldHeightHalf + 0.4)
 		assert_true(Field.isInDefenseArea(pos, 0.3, true))
 		assert_false(Field.isInDefenseArea(pos, 0.3, false))
-		pos = Vector(G.DefenseWidthHalf + 0.1, G.FieldHeightHalf-G.DefenseHeight-0.1)
-		assert_true(Field.isInDefenseArea(pos, 0.2,false))
+		pos = Vector(G.DefenseWidthHalf + 0.1, G.FieldHeightHalf - G.DefenseHeight - 0.1)
+		assert_true(Field.isInDefenseArea(pos, 0.2, false))
 	end)
 	test("distanceToDefenseArea_2017", function()
-		local G = World.Geometry
-		World.RULEVERSION = "2017"
-		Field = injector:load("../base/field")
+		initHelper("2017")
 
-		local pos = Vector(0,0)
-		assert_equal(Field.distanceToDefenseArea(pos, 3,false), 0.5)
-		assert_equal(Field.distanceToDefenseArea(pos, 3,true), 0.5)
-		assert_false(Field.isInDefenseArea(pos,0.18,false))
-		assert_false(Field.isInDefenseArea(pos,0.18,true))
+		local pos = Vector(0, 0)
+		assert_equal(Field.distanceToDefenseArea(pos, 3, false), 0.5)
+		assert_equal(Field.distanceToDefenseArea(pos, 3, true), 0.5)
+		assert_false(Field.isInDefenseArea(pos, 0.18, false))
+		assert_false(Field.isInDefenseArea(pos, 0.18, true))
 		pos = Vector(0.1, G.FieldHeightHalf)
-		assert_true(Field.isInDefenseArea(pos,0.18,false))
-		assert_false(Field.isInDefenseArea(pos,0.18,true))
+		assert_true(Field.isInDefenseArea(pos, 0.18, false))
+		assert_false(Field.isInDefenseArea(pos, 0.18, true))
 	end)
 end)
