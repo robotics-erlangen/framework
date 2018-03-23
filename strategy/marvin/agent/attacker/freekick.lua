0ee009ca1483795f0184e792152c5243f6972b77
Christian Lobmeier <christian.lobmeier@robotics-erlangen.de>
2018-03-23 03:15:50 +0100

    Marvin: a/a/freekick: don't try to shoot goals from our half of the field

--- strategy/marvin/agent/attacker/freekick.lua
+++ strategy/marvin/agent/attacker/freekick.lua
@@ -56,7 +56,7 @@ function FreeKick:_updateTask()
 		< distanceToBall + self._robot.radius + World.Ball.radius + 0.02
 
 	local _; _, _, self._dirty = ShootGoalUtil.updateTarget(self._robot, nil, self._dirty, World.Ball.pos)
-	local shootgoalPossible = not self._dirty and
+	local shootgoalPossible = not self._dirty and World.Ball.pos.y > -0.2 and
 		(World.RefereeState == "DirectOffensive" or World.RefereeState == "KickoffOffensive")
 
 	-- prepare -> wait
