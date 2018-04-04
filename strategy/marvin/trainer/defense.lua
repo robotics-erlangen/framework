195a786b2249735dd3c3e1a4803d1c2c44cdfa68
Lukas Wegmann <lukas.wegmann@robotics-erlangen.de>
2018-04-04 21:17:53 +0200

    Marvin: tr/defense fix piggy/manmark flickering

--- strategy/marvin/trainer/defense.lua
+++ strategy/marvin/trainer/defense.lua
@@ -198,7 +198,8 @@ local function determineNumberOfPiggies(defenderCount, manmarkTargets, piggyTarg
 	end
 
 	local piggieCount = 0
-	if not Ball.ballHeadingForGoal(World.Ball) then
+	if not Ball.ballHeadingForGoal(World.Ball, true) 
+			or World.Ball.speed:length() < 3 or (World.Ball.pos + World.Ball.speed).y > -1 then
 		local nRelevantManMarkTargets = 0
 		for _, dangerousness in pairs(manmarkTargets) do
 			if dangerousness > dangerousnessThreshold then
