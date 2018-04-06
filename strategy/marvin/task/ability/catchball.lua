2fb6a40ffeeef59aa23cca0e6dd1f459a8aba1c5
Tobias Heineken <tobias.heineken@robotics-erlangen.de>
2018-04-06 12:54:16 +0200

    marvin. t/a/catchball: improve quickfix not to annoy the keeper

--- strategy/marvin/task/ability/catchball.lua
+++ strategy/marvin/task/ability/catchball.lua
@@ -93,7 +93,7 @@ function CatchBall:_catchBall(targetPos, distanceToBall, targetSpeed, maxSpeed)
 	end
 
 	-- QUICKFIX to prevent hish speed movement towards defenseArea
-	if predictedBall.pos and not Field.isInAllowedField(predictedBall.pos, World.Ball.radius) then
+	if predictedBall.pos and not Field.isInAllowedField(predictedBall.pos, World.Ball.radius) and self._robot ~= World.FriendlyKeeper then
 		predictedBall.pos = Field.limitToAllowedField(predictedBall.pos, World.Ball.radius)
 	end
 	local moveDest = predictedBall.pos - Vector.fromAngle(viewDir):scaleLength(
