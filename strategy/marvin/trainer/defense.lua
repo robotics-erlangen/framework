56a88d58e295362977f49f94766b17928f39bc36
Andreas Wendler <andreas.wendler@robotics-erlangen.de>
2018-06-17 01:42:27 +0200

    Marvin: tr/defense also assign centerback when ball is close

--- strategy/marvin/trainer/defense.lua
+++ strategy/marvin/trainer/defense.lua
@@ -368,7 +368,8 @@ function Defense:_assignBallCenterbacks(defenders)
 			ROBOT_TIME_MARGIN_LOW or ROBOT_TIME_MARGIN_HIGH
 		if robotTime + robotTimeMargin < rollTime or
 				robotTime < rollTime and rollTime < ROBOT_TIME_MARGIN_HIGH or
-				#self._centerbackAssignments == 0 and intersectsGoal then
+				#self._centerbackAssignments == 0 and intersectsGoal or
+				rollTime < 0.25 and robotTime < 0.4 then
 			table.insert(self._centerbackAssignments, closestRobot)
 			table.removeValue(defenders, closestRobot)
 			self._send.roleAssignment(closestRobot,
