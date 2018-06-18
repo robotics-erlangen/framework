d63b7bc64b5868cc896b19190b491b52c5a1d3c7
Tobias Heineken <tobias.heineken@robotics-erlangen.de>
2018-06-18 18:17:32 +0200

    base: robot: parameter adjustments against Minibots

--- strategy/base/robot.lua
+++ strategy/base/robot.lua
@@ -80,15 +80,15 @@ function Robot:init(data, isFriendly)
 		self.shootRadius = 0.067 -- shoot radius of 2014 generation
 		self.generation = -1
 		self.id = data
-		self.maxSpeed = 2.5 -- Init max speed and acceleration for opponents
+		self.maxSpeed = 2.75 -- Init max speed and acceleration for opponents
 		self.maxAngularSpeed = 4
 
 		self.acceleration = {}
-		self.acceleration.aSpeedupFMax = 2.5
-		self.acceleration.aSpeedupSMax = 2.5
+		self.acceleration.aSpeedupFMax = 2.0
+		self.acceleration.aSpeedupSMax = 2.0
 		self.acceleration.aSpeedupPhiMax = 20
-		self.acceleration.aBrakeFMax = 4
-		self.acceleration.aBrakeSMax = 4
+		self.acceleration.aBrakeFMax = 2.5
+		self.acceleration.aBrakeSMax = 2.5
 		self.acceleration.aBrakePhiMax = 20
 	end
 	self.lostSince = 0
