5933d25b0b74539979e365438d009de426cac390
Lukas Wegmann <lukas.wegmann@robotics-erlangen.de>
2018-06-20 23:26:04 +0200

    Marvin: t/sh/chiptopos prevent name collision with mixin

--- strategy/marvin/task/shared/chipToPos.lua
+++ strategy/marvin/task/shared/chipToPos.lua
@@ -7,7 +7,7 @@ function ChipToPos:_init(firstContactPos, targetTime, ballReceiptPos, precision)
 	self._firstContactPos = firstContactPos
 	self._targetTime = targetTime
 	self._ballReceiptPos = ballReceiptPos
-	self._precision = precision
+	self._chipPrecision = precision
 end
 
 function ChipToPos:run()
@@ -15,7 +15,7 @@ function ChipToPos:run()
 		inbox = self._inbox
 	}
 	PathHelper.setDefaultObstaclesByTable(self._robot.path, self._robot, obstacleTable)
-	self:_chipToPos(self._firstContactPos, self._targetTime, self._ballReceiptPos, self._precision)
+	self:_chipToPos(self._firstContactPos, self._targetTime, self._ballReceiptPos, self._chipPrecision)
 end
 
 return ChipToPos
