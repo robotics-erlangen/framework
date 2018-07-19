ad90e61f4a04f9e8395d605a780293a3ef4b95c4
Tobias Heineken <tobias.heineken@robotics-erlangen.de>
2018-07-19 19:53:04 +0200

    marvin: tr/defense: improved condition readability

--- strategy/marvin/trainer/defense.lua
+++ strategy/marvin/trainer/defense.lua
@@ -388,7 +388,7 @@ function Defense:_assignBallCenterbacks(defenders)
 				robotTime < rollTime and rollTime < ROBOT_TIME_MARGIN_HIGH or
 				#self._centerbackAssignments == 0 and intersectsGoal or
 				rollTime < 0.25 and robotTime < 0.4) and
-				not (toGoalLineDistance < 0.25 and not intersectsGoal) then
+				(toGoalLineDistance >= 0.25 or intersectsGoal) then
 			table.insert(self._centerbackAssignments, closestRobot)
 			table.removeValue(defenders, closestRobot)
 			self._send.roleAssignment(closestRobot,
