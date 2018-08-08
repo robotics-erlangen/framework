3c78673733a644a74cdd2ecadb187443d56efd94
Andreas Wendler <andreas.wendler@robotics-erlangen.de>
2018-08-08 18:35:10 +0200

    Fix paths more cmake test target as the lua strategy was moved

--- cmake/AddTests.cmake
+++ cmake/AddTests.cmake
@@ -1,10 +1,18 @@
 add_test(NAME luacheck-base
     COMMAND luacheck -q .
-    WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}/strategy/base")
+    WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}/strategy/lua/base")
 
-#add_test(NAME unittest
-#    COMMAND amun-cli "strategy/strategy/init.lua" "Unit Tests/ all"
-#    WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}")
+add_test(NAME luacheck-marvin
+    COMMAND luacheck -q .
+    WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}/strategy/lua/marvin")
+
+add_test(NAME unittest-marvin
+    COMMAND amun-cli "strategy/lua/marvin/init.lua" "Unit Tests/ all"
+    WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}")
+
+add_test(NAME unittest-marvin-blue
+    COMMAND amun-cli "--as-blue" "strategy/lua/marvin/init.lua" "Unit Tests/ all"
+    WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}")
 
 # show what went wrong by default
 add_custom_target(check COMMAND ${CMAKE_CTEST_COMMAND} --output-on-failure
