5a343c8df4e303bff280a74a9f8638da3100f385
Andreas Wendler <andreas.wendler@robotics-erlangen.de>
2018-08-16 17:57:18 +0200

    Glados: init.ts that controls a robot for testing purposes

--- strategy/typescript/glados/init.ts
+++ strategy/typescript/glados/init.ts
@@ -1,10 +1,10 @@
-/*require("../base/globalschecker").enable()
-require "../base/base"
+/*require("base/globalschecker").enable()
+require "base/base"
 // luacheck: push globals Class
-Class = require "../base/class"
+Class = require "base/class"
 // luacheck: pop
-let Entrypoints = require "../base/entrypoints"
-let World = require "../base/world"
+let Entrypoints = require "base/entrypoints"
+let World = require "base/world"
 
 require "control/maincoordinator"
 require "observer/initReplay"
@@ -15,18 +15,18 @@ require "test/task/index"
 require "test/unit/index"
 require "util/lineup"
 
-let Cache = require "../base/cache"
-let debug = require "../base/debug"
-let debugger = require "../base/debugger"
-let Processor = require "../base/processor"
-let Referee = require "../base/referee"
+let Cache = require "base/cache"
+let debug = require "base/debug"
+let debugger = require "base/debugger"
+let Processor = require "base/processor"
+let Referee = require "base/referee"
 let Ball = require "observer/ball"
 let Robot = require "observer/robot"
 let Error = require "observer/error"
 let Goal = require "observer/goal"
-let plot = require "../base/plot"
+let plot = require "base/plot"
 
-let preproc = Class("Process.PreProc", require "../base/process")
+let preproc = Class("Process.PreProc", require "base/process")
 function preproc:run () {
 	Ball._update()
 	Robot._update()
@@ -94,24 +94,58 @@ let wrapper = function (func)
 return {name = "Marvin", entrypoints = Entrypoints.get(wrapper)}
 *///
 
-import * as Entrypoints from "../base/entrypoints";
-import {log} from "../base/globals";
-import * as amunFunctions from "../base/amun";
+import "base/base";
+import * as Entrypoints from "base/entrypoints";
+import {log, Vector} from "base/globals";
+import * as debug from "base/debug";
+declare var path: any;
+let pathLocal = path;
+import * as amunFunctions from "base/amun";
+import {Coordinates, _setIsBlue} from "base/coordinates";
+_setIsBlue(true);
+import * as vis from "base/vis";
+import "base/trajectory";
+import "base/ball";
+import "base/timing";
+import * as World from "base/world";
+import "base/referee";
+import * as Field from "base/field";
+import {CurvedMaxAccel} from "glados/trajectory/curvedmaxaccel";
+import {FriendlyRobot} from "base/robot";
+//import * as test from "glados/control/messaging";
+//import * as test from "glados/observer/physics";
 
-amunFunctions._hideFunctions();
+let counter = 0;
+function initTest (): boolean {
+	//let limited = Field.limitToAllowedField(World.Ball.pos);
+	//vis.addCircle("1Test", limited, 0.1, vis.colors.red);
+	let robot = World.FriendlyRobots[0];
+	let moveTo = World.Ball.pos;
+	robot.path.clearObstacles();
+	robot.path.setRadius(robot.radius);
+	for (let i = 1;i<World.FriendlyRobots.length;i++) {
+		robot.path.addCircle(World.FriendlyRobots[i].pos.x, World.FriendlyRobots[i].pos.y, 0.2, "asdf");
+		vis.addCircle("1Test", World.FriendlyRobots[i].pos, 0.2, vis.colors.red);
+	}
+	robot.trajectory.update(CurvedMaxAccel, moveTo, 0);
+	vis.addCircle("1Test", World.Ball.pos, 0.5, vis.colors.redHalf, true);
 
-function initTest () { {
-	log("test");
 	return true;
 }
 
 Entrypoints.add("test", initTest);
 
-function wrapper (func: () {=> boolean) {
+function wrapper (func: ()=> boolean) {
 	return function() {
+		counter++;
+		World.update();
+		if (counter < 5) {
+			return;
+		}
 		func();
+		World.setRobotCommands();
 	}
 }
 
 let result = {name: "GLaDOS", entrypoints: Entrypoints.get(wrapper)};
-result
+export const scriptInfo = result;
