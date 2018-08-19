1fa5619e703dea9ea40e7b22801ec7704255d3ac
Paul Bergmann <paul.bergmann@robotics-erlangen.de>
2018-08-19 16:30:06 +0200

    Unify cmake test names to {language}-{task}-{target}

--- cmake/AddTests.cmake
+++ cmake/AddTests.cmake
@@ -1,20 +1,20 @@
-add_test(NAME luacheck-base
+add_test(NAME lua-luacheck-base
     COMMAND luacheck -q .
     WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}/strategy/lua/base")
 
-add_test(NAME luacheck-marvin
+add_test(NAME lua-luacheck-marvin
     COMMAND luacheck -q .
     WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}/strategy/lua/marvin")
 
-add_test(NAME tslint-base
+add_test(NAME typescript-tslint-base
 	COMMAND tslint -p tsconfig.json -c ../tslint.yaml
 	WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}/strategy/typescript/base")
 
-add_test(NAME unittest-marvin
+add_test(NAME lua-unittest-marvin
     COMMAND amun-cli "strategy/lua/marvin/init.lua" "Unit Tests/ all"
     WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}")
 
-add_test(NAME unittest-marvin-blue
+add_test(NAME lua-unittest-marvin-blue
     COMMAND amun-cli "--as-blue" "strategy/lua/marvin/init.lua" "Unit Tests/ all"
     WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}")
 
