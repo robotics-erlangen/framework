47118af44d855d9af8fa36b1421971316b7fdc73
Tobias Heineken <tobias.heineken@robotics-erlangen.de>
2018-08-27 18:18:26 +0200

    typescript/base: added seedable PRNG to mathUtil

--- strategy/typescript/base/random.ts
+++ strategy/typescript/base/random.ts
@@ -1,22 +1,20 @@
 /**
- * @author bennyl, https://stackoverflow.com/a/32537932, with slight adaptations by Tobias Heineken <tobias.heineken@robotics-erlangen.de>
+ * @author bennyl, https://stackoverflow.com/a/32537932
  * copied almost directly from Mersenne Twister implementation found in https://gist.github.com/banksean/300494
  * all rights reserved to him.
  */
-
-/* eslint-disable */
 
 /*
   I've wrapped Makoto Matsumoto and Takuji Nishimura's code in a namespace
   so it's better encapsulated. Now you can have multiple random number generators
   and they won't stomp all over eachother's state.
-
+  
   If you want to use this as a substitute for Math.random(), use the random()
   method like so:
-
+  
   var m = new MersenneTwister();
   var randomNumber = m.random();
-
+  
   You can also call the other genrand_{foo}() methods on the instance.
 
   If you want to use a specific seed in order to get a repeatable random
@@ -29,31 +27,31 @@
   Sean McCullough (banksean@gmail.com)
 */
 
-/*
+/* 
    A C-program for MT19937, with initialization improved 2002/1/26.
    Coded by Takuji Nishimura and Makoto Matsumoto.
-
-   Before using, initialize the state by using init_genrand(seed)
+ 
+   Before using, initialize the state by using init_genrand(seed)  
    or init_by_array(init_key, key_length).
-
+ 
    Copyright (C) 1997 - 2002, Makoto Matsumoto and Takuji Nishimura,
-   All rights reserved.
-
+   All rights reserved.                          
+ 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions
    are met:
-
+ 
      1. Redistributions of source code must retain the above copyright
         notice, this list of conditions and the following disclaimer.
-
+ 
      2. Redistributions in binary form must reproduce the above copyright
         notice, this list of conditions and the following disclaimer in the
         documentation and/or other materials provided with the distribution.
-
-     3. The names of its contributors may not be used to endorse or promote
-        products derived from this software without specific prior written
+ 
+     3. The names of its contributors may not be used to endorse or promote 
+        products derived from this software without specific prior written 
         permission.
-
+ 
    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
    "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
    LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -65,8 +63,8 @@
    LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
    NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-
+ 
+ 
    Any feedback is very welcome.
    http://www.math.sci.hiroshima-u.ac.jp/~m-mat/MT/emt.html
    email: m-mat @ math.sci.hiroshima-u.ac.jp (remove space)
@@ -86,7 +84,11 @@ export class Random {
     mti = Random.N + 1;
     /* mti==N+1 means mt[N] is not initialized */
 
-    constructor(seed: number) {
+    constructor(seed:number = null) {
+        if (seed == null) {
+            seed = new Date().getTime();
+        }
+
         this.init_genrand(seed);
     }
 
@@ -148,16 +150,16 @@ export class Random {
 
     /**
      * generates an int32 pseudo random number
-     * @param range: an optional [from, to] range (from and to inclusive), if not specified the result will be in range [0,0xffffffff]
+     * @param range: an optional [from, to] range, if not specified the result will be in range [0,0xffffffff]
      * @return {number}
      */
-    nextInt32(range?:[number, number]):number {
-        let result = this._nextInt32();
-        if (range == undefined) {
+    nextInt32(range:[number, number] = null):number {
+        var result = this._nextInt32();
+        if (range == null) {
             return result;
         }
 
-        return (result % (range[1] - range[0] + 1)) + range[0];
+        return (result % (range[1] - range[0])) + range[0];
     }
 
     /**
