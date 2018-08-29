8220c0789247e8a9a9257a3078a1b31b5227991d
Andreas Wendler <andreas.wendler@robotics-erlangen.de>
2018-08-29 17:44:38 +0200

    glados: port init, tr/roles

--- strategy/typescript/glados/init.ts
+++ strategy/typescript/glados/init.ts
@@ -1,149 +1,72 @@
-/*require("base/globalschecker").enable()
-require "base/base"
-// luacheck: push globals Class
-Class = require "base/class"
-// luacheck: pop
-let Entrypoints = require "base/entrypoints"
-let World = require "base/world"
+import "base/base";
+import * as Entrypoints from "base/entrypoints";
+import * as World from "base/world";
 
-require "control/maincoordinator"
-require "observer/initReplay"
-require "test/move/index"
-require "test/observer/index"
-require "test/situation/index"
-require "test/task/index"
-require "test/unit/index"
-require "util/lineup"
+import "glados/control/maincoordinator";
+import "glados/observer/initReplay";
+// require "test/move/index";
+// require "test/observer/index";
+// require "test/situation/index";
+// require "test/task/index";
+// require "test/unit/index";
+import "glados/util/lineup";
 
-let Cache = require "base/cache"
-let debug = require "base/debug"
-let debugger = require "base/debugger"
-let Processor = require "base/processor"
-let Referee = require "base/referee"
-let Ball = require "observer/ball"
-let Robot = require "observer/robot"
-let Error = require "observer/error"
-let Goal = require "observer/goal"
-let plot = require "base/plot"
+import * as Cache from "base/cache";
+import * as debug from "base/debug";
+import {log} from "base/globals";
+import {Process} from "base/process";
+import * as Processor from "base/processor";
+import * as plot from "base/plot";
+import * as  Referee from "base/referee";
+import * as Ball from "glados/observer/ball";
+import * as Robot from "glados/observer/robot";
+import * as Error from "glados/observer/error";
+import * as Goal from "glados/observer/goal";
 
-let preproc = Class("Process.PreProc", require "base/process")
-function preproc:run () {
-	Ball._update()
-	Robot._update()
-	Referee.check()
-	Referee.illustrateRefereeStates()
-	Error._update()
-	Goal._update()
-}
-function preproc:isFinished () {
-	return false
-}
-Processor.addPre(preproc)
-// local BallAnalyzer = require "observer/ballAnalyzer"
-// Processor.addPre(BallAnalyzer())
 
-let lastMemoryUsage = 0
-let deferredGarbageCollection = function () {
-	let currentMemoryUsage = collectgarbage("count")
-	// plot.addPlot("memInGB", currentMemoryUsage/1024/1024)
-	let gcPauseThreshold = 2
-	if (currentMemoryUsage > gcPauseThreshold * lastMemoryUsage) {
-		let debt = currentMemoryUsage - lastMemoryUsage
-		// trigger collection of some amount of memory
-		let cycleCompleted = collectgarbage("step", debt)
-		// disable gc again, it should only run after the strategy commands are passed on
-		collectgarbage("stop")
-		if (cycleCompleted) {
-			lastMemoryUsage = collectgarbage("count")
-		}
+class PreProc implements Process {
+	run () {
+		Ball._update();
+		Robot._update();
+		Referee.check();
+		Referee.illustrateRefereeStates();
+		Error._update();
+		Goal._update();
+	}
+
+	isFinished (): boolean {
+		return false;
 	}
 }
+Processor.addPre(new PreProc)
+// import {BallAnalyzer} from "glados/observer/ballAnalyzer";
+// Processor.addPre(new BallAnalyzer)
 
-
-let frameCount = 0
-let wrapper = function (func)
-	let f = function()
+let frameCount = 0;
+function wrapper (func: ()=> boolean) {
+	return function() {
 		frameCount = frameCount + 1
-		if (not World.update()) {
+		if (!World.update()) {
 			if ((frameCount % 100) == 0) {
 				log("Waiting for vision data...")
 			}
 			return // skip processing if no vision data is available yet
 		}
 		debug.set("frame", frameCount)
-		//local time0 = amun.getCurrentTime()
+		// let time0 = amun.getCurrentTime();
 		Processor.pre()
-		//local time1 = amun.getCurrentTime()
-		//plot.addPlot("preproc time", (time1 - time0))
-		if (not func()) { // Entrypoint has to return true if robots shouldn't be stopped on halt
-			if (World.RefereeState == "Halt") {
+		// let time1 = amun.getCurrentTime();
+		// plot.addPlot("preproc time", (time1 - time0));
+		if (!func()) { // Entrypoint has to return true if robots shouldn't be stopped on halt
+			if (World.RefereeState === "Halt") {
 				World.haltOwnRobots()
 			}
 		}
-		World.setRobotCommands()
-		Processor.post()
-		debug.resetStack()
-		Cache.resetFrame()
-		plot._plotAggregated()
-		deferredGarbageCollection()
-	}
-
-	return debugger.dumpLocalsOnError(f)
-}
-
-return {name = "Marvin", entrypoints = Entrypoints.get(wrapper)}
-*///
-
-import "base/base";
-import * as Entrypoints from "base/entrypoints";
-import {log, Vector} from "base/globals";
-import * as debug from "base/debug";
-declare var path: any;
-let pathLocal = path;
-import * as amunFunctions from "base/amun";
-import {Coordinates, _setIsBlue} from "base/coordinates";
-_setIsBlue(true);
-import * as vis from "base/vis";
-import "base/trajectory";
-import "base/ball";
-import "base/timing";
-import * as World from "base/world";
-import "base/referee";
-import * as Field from "base/field";
-import {CurvedMaxAccel} from "glados/trajectory/curvedmaxaccel";
-import {FriendlyRobot} from "base/robot";
-//import * as test from "glados/control/messaging";
-//import * as test from "glados/observer/physics";
-
-let counter = 0;
-function initTest (): boolean {
-	//let limited = Field.limitToAllowedField(World.Ball.pos);
-	//vis.addCircle("1Test", limited, 0.1, vis.colors.red);
-	let robot = World.FriendlyRobots[0];
-	let moveTo = World.Ball.pos;
-	robot.path.clearObstacles();
-	robot.path.setRadius(robot.radius);
-	for (let i = 1;i<World.FriendlyRobots.length;i++) {
-		robot.path.addCircle(World.FriendlyRobots[i].pos.x, World.FriendlyRobots[i].pos.y, 0.2, "asdf");
-		vis.addCircle("1Test", World.FriendlyRobots[i].pos, 0.2, vis.colors.red);
-	}
-	robot.trajectory.update(CurvedMaxAccel, moveTo, 0);
-	vis.addCircle("1Test", World.Ball.pos, 0.5, vis.colors.redHalf, true);
-
-	return true;
-}
-
-Entrypoints.add("test", initTest);
-
-function wrapper (func: ()=> boolean) {
-	return function() {
-		counter++;
-		World.update();
-		if (counter < 5) {
-			return;
-		}
-		func();
 		World.setRobotCommands();
+		Processor.post();
+		debug.resetStack();
+		Cache.resetFrame();
+		plot._plotAggregated();
 	}
 }
 
--- strategy/typescript/glados/trainer/roles.ts
+++ strategy/typescript/glados/trainer/roles.ts
@@ -1,57 +1,60 @@
-let Roles = {}
-
 import * as Referee from "base/referee";
+import {FriendlyRobot} from "base/robot";
 import * as vis from "base/vis";
 import * as World from "base/world";
 
+import {MessageBox, MessageType, ExclusiveRole} from "glados/control/messaging";
 
-let ROLE_HYSTERESIS = 0.05
 
-function Roles:init () {
-	this._exclusiveRoles = {}
-}
+const ROLE_HYSTERESIS = 0.05;
 
-function Roles:_chooseExclusiveRoles () {
-	let roleHysteresis = ROLE_HYSTERESIS
-	if (Referee.isStopState()) {
-		roleHysteresis = 1
+export class Roles {
+	_exclusiveRoles: Map<ExclusiveRole, FriendlyRobot> = new Map<ExclusiveRole, FriendlyRobot>();
+
+	_messaging: MessageBox;
+
+	constructor(messaging: MessageBox) {
+		this._messaging = messaging;
 	}
 
-	let roleMsgs = this._inbox.exclusiveRole()
-	let roleApplications = {}
-	for (robot, applications in pairs(roleMsgs)) {
-		for (_, application in ipairs(applications)) {
-			for (role, rating in pairs(application)) {
-				if (not roleApplications[role]) {
-					roleApplications[role] = {}
+	_chooseExclusiveRoles () {
+		let roleHysteresis = ROLE_HYSTERESIS;
+		if (Referee.isStopState()) {
+			roleHysteresis = 1;
+		}
+
+		let roleMsgs = this._messaging.receiveRepeated(MessageType.exclusiveRole);
+		let roleApplications: Map<ExclusiveRole, Map<FriendlyRobot, number>> = new Map<ExclusiveRole, Map<FriendlyRobot, number>>();
+		for (let [robot, applications] of roleMsgs.entries()) {
+			for (let [role, rating] of applications) {
+				if (!roleApplications.has(role)) {
+					roleApplications[role] = new Map<FriendlyRobot, number>();
 				}
-				roleApplications[role][robot] = rating
+				roleApplications[role]!.set(robot, rating);
 			}
 		}
-	}
 
-	let exclusiveRoles = {} // ensure that special roles are removed if no one applies
-	for (role, applications in pairs(roleApplications)) {
-		let bestRobot = nil
-		let bestRating = -Infinity
-		for (robot, rating in pairs(applications)) {
-			if (this._exclusiveRoles[role] == robot) {
-				rating = rating + roleHysteresis
+		let exclusiveRoles: Map<ExclusiveRole, FriendlyRobot> = new Map<ExclusiveRole, FriendlyRobot>(); // ensure that special roles are removed if no one applies
+		for (let [role, applications] of roleApplications.entries()) {
+			let bestRobot = undefined;
+			let bestRating = -Infinity;
+			for (let [robot, rating] of applications.entries()) {
+				if (this._exclusiveRoles[role] === robot) {
+					rating += roleHysteresis;
+				}
+				if (rating > bestRating) {
+					bestRobot = robot;
+					bestRating = rating;
+				}
 			}
-			if (rating > bestRating) {
-				bestRobot = robot
-				bestRating = rating
-			}
-		}
-		if (bestRobot) {
-			exclusiveRoles[role] = bestRobot
-			this._send[role]("all", bestRobot)
+			if (bestRobot) {
+				exclusiveRoles[role] = bestRobot;
+				this._messaging.sendBroadcast(role, bestRobot);
 
-			vis.addCircle("tr/roles: "+role, bestRobot.pos, 0.12,
-				World.TeamIsBlue ? vis.colors.blue : vis.colors.yellow, true, true)
+				vis.addCircle("tr/roles: "+role, bestRobot.pos, 0.12,
+					World.TeamIsBlue ? vis.colors.blue : vis.colors.yellow, true, true);
+			}
 		}
+		this._exclusiveRoles = exclusiveRoles;
 	}
-	this._exclusiveRoles = exclusiveRoles
 }
-
-return Roles
