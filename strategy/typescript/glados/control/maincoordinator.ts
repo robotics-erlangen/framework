5eb3b1a1d42ab3b2a81003edcf6a478d5be737c9
Andreas Wendler <andreas.wendler@robotics-erlangen.de>
2018-08-30 20:38:10 +0200

    glados: fully disable pool manual as it doesn't work yet

--- strategy/typescript/glados/control/maincoordinator.ts
+++ strategy/typescript/glados/control/maincoordinator.ts
@@ -28,7 +28,7 @@ import {MainTrainer} from "glados/trainer/maintrainer";
 class MainCoordinator extends Coordinator {
 	constructor (trainer: MainTrainer) {
 		let pools: {[name: string]: AgentPool} = {
-			//manual: new AgentPool(Agents.Manual),
+			// manual: new AgentPool(Agents.Manual),
 			// ally: new AgentPool(Agents.Ally),
 			keeper: new AgentPool(Agents.Keeper),
 			defense: new AgentPool(Agents.Defender),
@@ -36,7 +36,7 @@ class MainCoordinator extends Coordinator {
 			hidden: new AgentPool(Agents.Hidden)
 		};
 		let poolGroups: AgentPool[][] = [
-			[ pools.manual ],
+			// [ pools.manual ],
 			// [ pools.ally ],
 			[ pools.keeper ],
 			[ pools.defense, pools.attack ],
