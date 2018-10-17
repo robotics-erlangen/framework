e036b31f8ad028785d6f4368802fb975ba75d7d3
Paul Bergmann <paul.bergmann@robotics-erlangen.de>
2018-10-17 20:23:04 +0200

    typescript: Use one tsconfig for base and glados

--- strategy/typescript/glados/init.ts
+++ strategy/typescript/glados/init.ts
@@ -13,8 +13,8 @@ import * as World from "base/world";
 import "glados/control/maincoordinator";
 import "glados/observer/initReplay";
 import "glados/util/lineup";
-import "glados/test/move/index";
-import "glados/tutorials/index";
+// import "glados/test/move/index";
+// import "glados/tutorials/index";
 
 import * as Ball from "glados/observer/ball";
 import * as Error from "glados/observer/error";
