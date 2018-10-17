ecf11197e88c124f0279d216bec483ffa0cd16de
Paul Bergmann <paul.bergmann@robotics-erlangen.de>
2018-10-17 20:23:04 +0200

    typescript: move tsconfig to typescript directory

--- cmake/AddTests.cmake
+++ cmake/AddTests.cmake
@@ -2,21 +2,10 @@ add_test(NAME lua-luacheck-base
     COMMAND luacheck -q .
     WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}/strategy/lua/base")
 
-add_test(NAME lua-luacheck-marvin
-    COMMAND luacheck -q .
-    WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}/strategy/lua/marvin")
+add_test(NAME typescript-tslint
+	COMMAND tslint -p tsconfig.json -c tslint.yaml
+	WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}/strategy/typescript")
 
-add_test(NAME typescript-tslint-base
-	COMMAND tslint -p tsconfig.json -c ../tslint.yaml
-	WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}/strategy/typescript/base")
-
-add_test(NAME lua-unittest-marvin
-    COMMAND amun-cli "strategy/lua/marvin/init.lua" "Unit Tests/ all"
-    WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}")
-
-add_test(NAME lua-unittest-marvin-blue
-    COMMAND amun-cli "--as-blue" "strategy/lua/marvin/init.lua" "Unit Tests/ all"
-    WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}")
 
 # show what went wrong by default
 add_custom_target(check COMMAND ${CMAKE_CTEST_COMMAND} --output-on-failure
