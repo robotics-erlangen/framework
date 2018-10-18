b0fdc4c2731f3a91b67044453b29e007ca447c2a
Lukas Wegmann <lukas.wegmann@robotics-erlangen.de>
2018-10-18 23:03:23 +0200

    tutorial: add third tutorials assignment

---
+++ strategy/typescript/glados/tutorials/t3Ball/tutorial3.ts
@@ -0,0 +1,41 @@
+// For the third tutorial we want to create another task
+// When this task is run, a ball was shot in your robot's general direction
+// Intercept the ball and shoot it at the (opponent's) goal
+
+// Start this tutorial with the entrypoint "MoveTest" -> "Tutorial3"
+
+// Hints:
+// - You can get the current state of the ball from World.Ball
+//   (You need to import world first)
+// - World.Ball.pos returns a Vector describing the current ball position
+// - World.Ball.speed also returns a vector 
+//   This vector's length is the absolute speed and the vector's angle is the direction of the speed
+
+// - We have a module to assist all your ball-shooting needs
+//   It is located in "glados/task/ability/shoot.ts"
+// - To make use of it, import it and create a new instance of it
+// - Take a look at "glados/task/shootgoal" to find an example of how to properly use the 'shoot' ability
+
+// - We also have a module that attempts to catch balls 
+// - It is located in "glados/task/ability/catchball.lua"
+
+import * as PathHelper from "glados/trajectory/pathhelper";
+
+import { Agent, Task } from "glados/task/base";
+import { ToTarget } from "glados/trajectory/totarget";
+
+export class TutorialTask extends Task {
+
+    constructor(agent: Agent, dir: number) {
+
+		super(agent);
+
+	}
+
+	public run() {
+
+		PathHelper.setDefaultObstaclesByTable(this._robot.path, this._robot, { ignorePass: true });
+
+		this._robot.trajectory.update(ToTarget, undefined, undfined, undefined, undefined);
+	}
+}
