3791bd106fc1f2d7278237d45f16e2ce852feede
Michael Eischer <michael.eischer@robotics-erlangen.de>
2018-11-03 16:23:07 +0100

    CI: Docker containers for the CI runner
    The intention of these containers is to vary the build process
    along several different variables:
    - Different Ubuntu-Version: 16.04 and 18.04
      - This directly translates to further variations:
        - Different compiler and embedded compiler versions
        - Different Qt,SDL2,libusb-Versions
      - These include V8 libraries by default
    - Ubuntu 18.04 container without V8

---
+++ data/docker/README.md
@@ -0,0 +1,8 @@
+# Building the containers
+
+Run `copy-libs.sh` before building. Afterwards it's just standard docker building.
+e.g.
+```
+cd ubuntu-16.04
+docker build -t ubuntu-16.04 .
+```
---
+++ data/docker/copy-libs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# predictable working directory
+cd "$(dirname "$0")"
+
+for i in {ubuntu-16.04,ubuntu-18.04}; do
+	v8dir="$i/libs/v8"
+	mkdir -p "$v8dir"
+	cp -r ../../libs/v8/{build.sh,patches} "$v8dir"
+done
