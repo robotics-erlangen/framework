1aa51ecd8169b14b40d6ee6c7aa9cef086e190f9
Andreas Wendler <andreas.wendler@robotics-erlangen.de>
2018-11-11 12:33:37 +0100

    amun: add new class Integrator
    This will be used to combine status packages from the simulator and a
    log file. Note that this is currently not supported, so this class is
    mostly a stub.

---
+++ src/amun/processor/integrator.cpp
@@ -0,0 +1,49 @@
+/***************************************************************************
+ *   Copyright 2018 Andreas Wendler                                        *
+ *   Robotics Erlangen e.V.                                                *
+ *   http://www.robotics-erlangen.de/                                      *
+ *   info@robotics-erlangen.de                                             *
+ *                                                                         *
+ *   This program is free software: you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation, either version 3 of the License, or     *
+ *   any later version.                                                    *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ ***************************************************************************/
+
+#include "integrator.h"
+
+Integrator::Integrator() :
+    m_isReplay(false)
+{ }
+
+void Integrator::handleStatus(const Status &status)
+{
+    if (!m_isReplay) {
+        emit sendStatus(status);
+    }
+}
+
+void Integrator::handleReplayStatus(const Status &status)
+{
+    if (m_isReplay) {
+        emit sendReplayStatus(status);
+    }
+}
+
+void Integrator::handleCommand(const Command &command)
+{
+    if (command->has_replay()) {
+        const amun::CommandReplay &replay = command->replay();
+        if (replay.has_enable()) {
+            m_isReplay = replay.enable();
+        }
+    }
+}
