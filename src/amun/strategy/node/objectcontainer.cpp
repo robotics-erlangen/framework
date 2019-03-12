7b5bbe555165ca1045319b0cb1341d3c8805d1fd
Tobias Heineken <tobias.heineken@robotics-erlangen.de>
2019-03-12 17:47:12 +0100

    amun: node: fix indentation in objectContainer.cpp

--- src/amun/strategy/node/objectcontainer.cpp
+++ src/amun/strategy/node/objectcontainer.cpp
@@ -42,64 +42,64 @@ using v8::ObjectTemplate;
 using v8::String;
 
 Node::ObjectContainer::ObjectContainer(Isolate* isolate, const ObjectContainer* requireNamespace)
-	: m_isolate(isolate), m_requireNamespace(requireNamespace) {
+    : m_isolate(isolate), m_requireNamespace(requireNamespace) {
 }
 
 Node::ObjectContainer::~ObjectContainer() {
 }
 
 Local<Object> Node::ObjectContainer::getHandle() {
-	EscapableHandleScope handleScope(m_isolate);
-	Local<Object> object = m_handle.Get(m_isolate);
-	return handleScope.Escape(object);
+    EscapableHandleScope handleScope(m_isolate);
+    Local<Object> object = m_handle.Get(m_isolate);
+    return handleScope.Escape(object);
 }
 
 Node::ObjectContainer* Node::ObjectContainer::get(const std::string& index) const {
-	auto child = m_children.find(index);
-	if (child != m_children.end()) {
-		return child->second.get();
-	} else {
-		return nullptr;
-	}
+    auto child = m_children.find(index);
+    if (child != m_children.end()) {
+        return child->second.get();
+    } else {
+        return nullptr;
+    }
 }
 
 void Node::ObjectContainer::put(const std::string& index, std::unique_ptr<Node::ObjectContainer> object) {
-	if (!m_handle.IsEmpty()) {
-		Local<Object> ownObject = m_handle.Get(m_isolate);
-		Local<String> propertyName = String::NewFromUtf8(m_isolate, index.c_str(), NewStringType::kNormal).ToLocalChecked();
-		// what does the return value even mean?
-		USE(ownObject->Set(m_isolate->GetCurrentContext(), propertyName, object->getHandle()));
-	}
-	// TODO funktioniert das?
-	m_children.emplace(index, std::move(object));
+    if (!m_handle.IsEmpty()) {
+        Local<Object> ownObject = m_handle.Get(m_isolate);
+        Local<String> propertyName = String::NewFromUtf8(m_isolate, index.c_str(), NewStringType::kNormal).ToLocalChecked();
+        // what does the return value even mean?
+        USE(ownObject->Set(m_isolate->GetCurrentContext(), propertyName, object->getHandle()));
+    }
+    // TODO funktioniert das?
+    m_children.emplace(index, std::move(object));
 }
 
 void Node::ObjectContainer::setHandle(Local<Object> handle) {
-	m_handle.Reset(m_isolate, handle);
+    m_handle.Reset(m_isolate, handle);
 }
 
 template<> Local<ObjectTemplate> Node::ObjectContainer::createTemplateWithCallbacks(const QList<CallbackInfo>& callbackInfos) {
-	EscapableHandleScope handleScope(m_isolate);
-	Local<ObjectTemplate> resultingTemplate = ObjectTemplate::New(m_isolate);
-	for (const auto& callbackInfo : callbackInfos) {
-		auto functionTemplate = FunctionTemplate::New(m_isolate, callbackInfo.callback, External::New(m_isolate, this));
-		resultingTemplate->Set(m_isolate, callbackInfo.name, functionTemplate);
-	}
-	return handleScope.Escape(resultingTemplate);
+    EscapableHandleScope handleScope(m_isolate);
+    Local<ObjectTemplate> resultingTemplate = ObjectTemplate::New(m_isolate);
+    for (const auto& callbackInfo : callbackInfos) {
+        auto functionTemplate = FunctionTemplate::New(m_isolate, callbackInfo.callback, External::New(m_isolate, this));
+        resultingTemplate->Set(m_isolate, callbackInfo.name, functionTemplate);
+    }
+    return handleScope.Escape(resultingTemplate);
 }
 
 template<> Local<FunctionTemplate> Node::ObjectContainer::createTemplateWithCallbacks(const QList<CallbackInfo>& callbackInfos) {
-	EscapableHandleScope handleScope(m_isolate);
-	Local<FunctionTemplate> resultingTemplate = FunctionTemplate::New(m_isolate);
-	for (const auto& callbackInfo : callbackInfos) {
-		auto functionTemplate = FunctionTemplate::New(m_isolate, callbackInfo.callback, External::New(m_isolate, this));
-		resultingTemplate->Set(m_isolate, callbackInfo.name, functionTemplate);
-	}
-	return handleScope.Escape(resultingTemplate);
+    EscapableHandleScope handleScope(m_isolate);
+    Local<FunctionTemplate> resultingTemplate = FunctionTemplate::New(m_isolate);
+    for (const auto& callbackInfo : callbackInfos) {
+        auto functionTemplate = FunctionTemplate::New(m_isolate, callbackInfo.callback, External::New(m_isolate, this));
+        resultingTemplate->Set(m_isolate, callbackInfo.name, functionTemplate);
+    }
+    return handleScope.Escape(resultingTemplate);
 }
 
 void Node::ObjectContainer::throwV8Exception(const QString& message) const {
-	HandleScope handleScope(m_isolate);
-	Local<String> exceptionText = String::NewFromUtf8(m_isolate, message.toUtf8().data(), NewStringType::kNormal).ToLocalChecked();
-	m_isolate->ThrowException(exceptionText);
+    HandleScope handleScope(m_isolate);
+    Local<String> exceptionText = String::NewFromUtf8(m_isolate, message.toUtf8().data(), NewStringType::kNormal).ToLocalChecked();
+    m_isolate->ThrowException(exceptionText);
 }
