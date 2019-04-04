26a812d970d2c4ee9eb05bd9157d57f2ee0ea582
Andreas Wendler <andreas.wendler@robotics-erlangen.de>
2019-04-04 13:59:01 +0200

    Ra: add dialog for choosing logfiles

---
+++ src/ra/logfileselectiondialog.cpp
@@ -0,0 +1,50 @@
+/***************************************************************************
+ *   Copyright 2019 Andreas Wendler                                        *
+ *   Robotics Erlangen e.V.                                                *
+ *   http://www.robotics-erlangen.de/                                      *
+ *   info@robotics-erlangen.de                                             *
+ *                                                                         *
+ *   This program is free software: you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation, either version 3 of the License, or     *
+ *   any later version.                                                    *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ ***************************************************************************/
+
+#include "logfileselectiondialog.h"
+#include "ui_logfileselectiondialog.h"
+
+LogFileSelectionDialog::LogFileSelectionDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::LogFileSelectionDialog)
+{
+    ui->setupUi(this);
+    setWindowTitle("Select Logfile");
+    connect(ui->resultButtons, SIGNAL(accepted()), this, SLOT(onAccept()));
+    connect(ui->resultButtons, SIGNAL(rejected()), this, SLOT(reject()));
+    connect(ui->list, SIGNAL(itemDoubleClicked(QListWidgetItem*)), this, SLOT(onAccept()));
+}
+
+LogFileSelectionDialog::~LogFileSelectionDialog()
+{
+    delete ui;
+}
+
+void LogFileSelectionDialog::setListContent(const QList<QString> &l)
+{
+    ui->list->clear();
+    ui->list->addItems(l);
+}
+
+void LogFileSelectionDialog::onAccept()
+{
+    emit resultSelected(ui->list->currentRow());
+    accept();
+}
