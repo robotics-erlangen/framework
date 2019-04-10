e0d9c02a90ceb7eaa602fc5f034b5b00eaec205b
Tobias Heineken <tobias.heineken@robotics-erlangen.de>
2019-04-10 19:41:26 +0200

    seshat: logfilefinder: implemented
    LogFileFinder will be used to find a logfile, given a logfile::Uid.
    It offers multiple logfiles, from which the user has to chose.
    
    This commit also contains the following commit:
    
    timeline: logfilefiner: avoid using fu2
    
    While the old implementation was slightly less racy (due to the fact it kept all filedescriptors
    open while the user was asked), and slightly more easy to add new functionality (as everything
    just needed to fullfill the fu2::unique_function), it had a major issue:
    
    Logfilefinder has to live in the newly created seshat module,
    as it will create a StatusSource in the end. This makes it very annoying
    to handle the list of functions propery, one has to send a message to
    the UI, and wait for the response. The UI now has to send a message
    "nevermind" back to seshat if the user decided not to open any of these
    logs, or if the user decided for one log, it's index would have to be
    used. All of these steps seems very unstable, and seem to invite a lot
    new race conditions, for example if the user erroniously starts the
    process twice.
    
    Instead, the new approch works by using an universion ressource
    identifier, which is right now just a local path, but can easily
    modified. Instead of keeping the filedescriptors open, we close them
    and reopen the logfile as soon as the user has selected his wish.
    
    While this is racy in the sense that a user can rename the file while
    this is in progress, and will then see an error message or a wrong log
    after selecting the now outdated path in the UI, this was considered ok.
    It also simplifies the abort case: If the user doesn't like any of the
    offered logs, there's no need for any cleanup to be sent by the UI.
    
    Also, getting rid of fu2 makes it easier to maintain our project.

---
+++ src/amun/seshat/logfilefinder.cpp
@@ -0,0 +1,168 @@
+/***************************************************************************
+ *   Copyright 2019 Tobias Heineken                                        *
+ *   Robotics Erlangen e.V.                                                *
+ *   http://www.robotics-erlangen.de/                                      *
+ *   info@robotics-erlangen.de                                             *
+ *                                                                         *
+ *   This program is free software: you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation, either version 3 of the License, or     *
+ *   any later version.                                                    *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ ***************************************************************************/
+#include <iostream>
+#include <string>
+#include <QDir>
+#include <QSettings>
+
+#include "logfilefinder.h"
+#include "logfilereader.h"
+
+LogFileFinder::LogFileFinder()
+{
+}
+
+static LogFileFinder::LogFileQuality compareEntry(const logfile::UidEntry& timeline, const logfile::UidEntry& logfile)
+{
+    if (timeline.hash() != logfile.hash()) {
+        return LogFileFinder::LogFileQuality::NO_MATCH;
+    }
+    if (timeline.flags() == logfile.flags()) {
+        return LogFileFinder::LogFileQuality::PERFECT_MATCH;
+    }
+    if ((timeline.flags() | logfile.flags()) == timeline.flags()) {
+        return LogFileFinder::LogFileQuality::FULL_MATCH;
+    }
+    return LogFileFinder::LogFileQuality::PARTIAL_MATCH;
+}
+
+static bool isPerfectMatch(const logfile::Uid& timeline, const logfile::Uid& logfile) {
+    if (timeline.parts_size() != logfile.parts_size()) {
+        return false;
+    }
+    for (int i = 0; i < timeline.parts_size(); ++i) {
+        if (compareEntry(timeline.parts(i), logfile.parts(i)) != LogFileFinder::LogFileQuality::PERFECT_MATCH) {
+            return false;
+        }
+    }
+    return true;
+}
+
+static LogFileFinder::LogFileQuality compareHash(const logfile::Uid& timeline, const logfile::Uid& logfile)
+{
+    //check for perfect match
+    if (isPerfectMatch(timeline, logfile)) {
+        return LogFileFinder::LogFileQuality::PERFECT_MATCH;
+    }
+
+    //check for fullmatch: foreach timeline search FULL / PERFECT in logfile
+    bool localMatch;
+    for (int i = 0; i < timeline.parts_size(); ++i) {
+        localMatch = false;
+        for (int j = 0; j < logfile.parts_size(); ++j) {
+            LogFileFinder::LogFileQuality qual = compareEntry(timeline.parts(i), logfile.parts(j));
+            if (qual == LogFileFinder::LogFileQuality::FULL_MATCH or qual == LogFileFinder::LogFileQuality::PERFECT_MATCH) {
+                localMatch = true;
+                break;
+            }
+        }
+        if (!localMatch) {
+            break;
+        }
+    }
+    if (localMatch) {
+        return LogFileFinder::LogFileQuality::FULL_MATCH;
+    }
+
+    //check for no match: foreach timeline No_Match in any logfile -> no match
+    bool noMatch;
+    for (int i = 0; i < timeline.parts_size(); ++i) {
+        noMatch = true;
+        for (int j = 0; j < logfile.parts_size(); ++j) {
+            LogFileFinder::LogFileQuality qual = compareEntry(timeline.parts(i), logfile.parts(j));
+            if (qual != LogFileFinder::LogFileQuality::NO_MATCH) {
+                noMatch = false;
+                break;
+            }
+        }
+        if (!noMatch) {
+            break;
+        }
+    }
+    if (noMatch) {
+        return LogFileFinder::LogFileQuality::NO_MATCH;
+    }
+    return LogFileFinder::LogFileQuality::PARTIAL_MATCH;
+}
+
+void LogFileFinder::addDirectory(const QString& s, logfile::LogOffer* offers)
+{
+    QDir dir(s);
+    QFileInfoList files(dir.entryInfoList({"*.log"}, QDir::Files | QDir::Readable));
+    for(QFileInfo info : files) {
+        QString filename = info.absoluteFilePath();
+        SeqLogFileReader slfr;
+        auto* entry = offers->add_entries();
+        entry->mutable_uri()->set_path(filename.toStdString());
+        entry->set_name(info.fileName().toStdString());
+        if(!slfr.open(filename)) {
+            entry->set_quality(logfile::LogOfferEntry::UNREADABLE);
+            std::cout << slfr.errorMsg().toStdString() << std::endl; // TODO: stdout
+            continue;
+        }
+        Status s = slfr.readStatus();
+        if (s.isNull()) {
+            entry->set_quality(logfile::LogOfferEntry::UNREADABLE);
+        } else if (!s->has_log_id()) {
+            entry->set_quality(logfile::LogOfferEntry::UNKNOWN);// TODO: This way or Rehash and reask?
+        } else {
+            entry->set_quality(logfile::LogOfferEntry::PERFECT); // TODO: Offer some non-perfect matches in the future.
+            const logfile::Uid& uid = s->log_id();
+            if (!isPerfectMatch(m_hash, uid)) {
+                offers->mutable_entries()->RemoveLast();
+            }
+        }
+    }
+}
+
+Status LogFileFinder::find(const logfile::Uid& hash)
+{
+    m_hash = hash;
+    return findAll();
+}
+
+Status LogFileFinder::find(logfile::Uid&& hash)
+{
+    m_hash = std::move(hash);
+    return findAll();
+}
+
+Status LogFileFinder::findAll()
+{
+    Status s = Status::createArena();
+    logfile::LogOffer* offers = s->mutable_pure_ui_response()->mutable_log_offers();
+    findLocal(offers);
+    return s;
+}
+
+void LogFileFinder::findLocal(logfile::LogOffer* offers)
+{
+    QSettings s("ER-Force", "Ra");
+    s.beginGroup("LogLocation");
+    int size = s.beginReadArray("locations");
+    for(int i = 0; i < size; ++i){
+        s.setArrayIndex(i);
+        QString path = s.value("path").toString();
+        addDirectory(path, offers);
+    }
+    s.endArray();
+    s.endGroup();
+}
+
---
+++ src/amun/seshat/logfilefinder.h
@@ -0,0 +1,49 @@
+/***************************************************************************
+ *   Copyright 2018 Tobias Heineken                                        *
+ *   Robotics Erlangen e.V.                                                *
+ *   http://www.robotics-erlangen.de/                                      *
+ *   info@robotics-erlangen.de                                             *
+ *                                                                         *
+ *   This program is free software: you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation, either version 3 of the License, or     *
+ *   any later version.                                                    *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ ***************************************************************************/
+
+#ifndef LOGFILEFINDER_H
+#define LOGFILEFINDER_H
+
+#include <QString>
+#include <memory>
+#include "protobuf/logfile.pb.h"
+#include "protobuf/status.h"
+
+class LogFileReader;
+
+class LogFileFinder
+{
+public:
+    enum class LogFileQuality
+    {
+        PERFECT_MATCH, FULL_MATCH, PARTIAL_MATCH, UNKNOWN, NO_MATCH, NOT_READABLE
+    };
+
+    explicit LogFileFinder();
+    Status find(const logfile::Uid& m_hash);
+    Status find(logfile::Uid&& m_hash);
+private:
+    void findLocal(logfile::LogOffer* offers);
+    Status findAll();
+    void addDirectory(const QString& s, logfile::LogOffer* offers);
+    logfile::Uid m_hash;
+};
+
+#endif
