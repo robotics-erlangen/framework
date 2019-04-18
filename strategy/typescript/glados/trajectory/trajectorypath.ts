ff4e9a259f2fb2ad57816b159c8a70c9f76a6820
Andreas Wendler <andreas.wendler@robotics-erlangen.de>
2019-04-18 18:02:54 +0200

    glados: trajectorypath: project position onto last trajectory

--- strategy/typescript/glados/trajectory/trajectorypath.ts
+++ strategy/typescript/glados/trajectory/trajectorypath.ts
@@ -1,12 +1,17 @@
 import { Coordinates } from "base/coordinates";
+import * as debug from "base/debug";
 import { TrajectoryHandler, TrajectoryResult } from "base/trajectory";
 import { Position, Speed, Vector } from "base/vector";
 import * as vis from "base/vis";
 
 import { DirectRotation } from "glados/trajectory/directrotation";
 
+type Trajectory = { pos: Position, speed: Speed, time: number}[];
+
 export class TrajectoryPath extends TrajectoryHandler {
 	private rotationCalculation: DirectRotation = new DirectRotation();
+
+	private lastTrajectory: Trajectory = [];
 
 	public canHandle(...args: any[]): boolean {
 		return true;
@@ -30,13 +35,33 @@ export class TrajectoryPath extends TrajectoryHandler {
 		// find position and speed on last path
 		let startPos = robotPos;
 		let startSpeed = robotSpeed;
+		if (this.lastTrajectory.length > 0) {
+			let [testPos, testSpeed] = TrajectoryPath.calculateClosestPoint(robotPos, robotSpeed, this.lastTrajectory);
+			// TrajectoryPath.visualizeTrajectory(this.lastTrajectory, vis.colors.green);
+			vis.addCircle("trajectory-closest", Coordinates.toLocal(testPos), 0.03, vis.colors.red);
+			if (testPos.distanceTo(robotPos) < 0.2) {
+				// startPos = testPos;
+				// startSpeed = robotSpeed;
+			}
+		}
+
+		// correct start and end speed
+		if (startSpeed.length() > maxSpeed - 0.1) {
+			startSpeed.setLength(maxSpeed - 0.1);
+			// amun.log("ist " + (maxSpeed - 0.1) + " und " + startSpeed.length());
+		}
+		if (endSpeed.length() > maxSpeed - 0.1) {
+			endSpeed.setLength(maxSpeed - 0.1);
+		}
 
 		// call C++ path finding
 		let trajectory = this._robot.path.getTrajectory(startPos, startSpeed, targetPos, endSpeed,
-			this._robot.maxSpeed);
+			maxSpeed);
 		if (TRAJECTORY_PATH_DEBUG) {
-			TrajectoryPath.visualizeTrajectory(trajectory);
+			TrajectoryPath.visualizeTrajectory(trajectory, vis.colors.orange);
+			// TrajectoryPath.plotSpeed(trajectory);
 		}
+		this.lastTrajectory = trajectory;
 
 		// generate trajectory to reach path finding result
 
@@ -54,6 +79,7 @@ export class TrajectoryPath extends TrajectoryHandler {
 
 		// finish and return trajectory
 		let speed = TrajectoryPath.speedAtTime(0.12, trajectory);
+		// amun.log("Ist: " + speed.length());
 		let acc = TrajectoryPath.accAtTime(0.12, trajectory);
 		let spline = [ {t_start: 0, t_end: Infinity,
 			x: { a0: robotPos.x, a1: speed.x, a2: acc.x / 2, a3: 0 },
@@ -70,7 +96,20 @@ export class TrajectoryPath extends TrajectoryHandler {
 		// return [{spline: []}, Coordinates.toLocal(targetPos), 0];
 	}
 
-	private static speedAtTime(time: number, trajectory: { pos: Position, speed: Speed, time: number}[]): Speed {
+	private static plotSpeed(trajectory: Trajectory) {
+		let points = [];
+		for (let i = 0;i < trajectory.length;i++) {
+			let pos = new Vector(trajectory[i].speed.length(), trajectory[i].time);
+			points.push(Coordinates.toLocal(pos));
+		}
+		vis.addPath("trajectory-speeds", points, vis.colors.blue);
+		for (let i = 0;i < 5;i++) {
+			vis.addPath("trajectory-speeds", [Coordinates.toLocal(new Vector(i, 0)),
+						Coordinates.toLocal(new Vector(i, 5))], vis.colors.red);
+		}
+	}
+
+	private static speedAtTime(time: number, trajectory: Trajectory): Speed {
 		for (let i = 0;i < trajectory.length - 1;i++) {
 			let next = trajectory[i + 1];
 			if (next.time > time) {
@@ -86,7 +125,27 @@ export class TrajectoryPath extends TrajectoryHandler {
 		return new Vector(0, 0);
 	}
 
-	private static accAtTime(time: number, trajectory: { pos: Position, speed: Speed, time: number}[]): Vector {
+	private static posAtTime(time: number, trajectory: Trajectory): Position {
+		if (trajectory.length === 0) {
+			return new Vector(0, 0);
+		}
+		for (let i = 0;i < trajectory.length - 1;i++) {
+			let next = trajectory[i + 1];
+			if (next.time > time) {
+				let current = trajectory[i];
+				let tFactor = (time - current.time) / (next.time - current.time);
+				let v = current.speed + tFactor * (next.speed - current.speed);
+
+				return current.pos + (current.speed + v) * 0.5 * (time - current.time);
+			}
+		}
+		if (trajectory.length > 0) {
+			return trajectory[trajectory.length - 1].pos;
+		}
+		return new Vector(0, 0);
+	}
+
+	private static accAtTime(time: number, trajectory: Trajectory): Vector {
 		for (let i = 0;i < trajectory.length - 1;i++) {
 			let next = trajectory[i + 1];
 			if (next.time > time) {
@@ -98,12 +157,32 @@ export class TrajectoryPath extends TrajectoryHandler {
 		return new Vector(0, 0);
 	}
 
-	private static visualizeTrajectory(trajectory: { pos: Position, speed: Speed, time: number}[]) {
+	private static visualizeTrajectory(trajectory: Trajectory, color: any) {
 		let positions: Position[] = [];
 		for (let part of trajectory) {
 			positions.push(Coordinates.toLocal(part.pos));
 		}
-		vis.addPath("trajectory-fromC++", positions, vis.colors.orange);
+		vis.addPath("trajectory-fromC++", positions, color);
+	}
+
+	private static calculateClosestPoint(position: Position, speed: Speed, trajectory: Trajectory) {
+		let bestPos = position, bestSpeed = speed;
+		let bestTime = Infinity;
+		let bestDistance = Infinity;
+		for (let i = 0;i < 50;i++) {
+			let time = i / 1000;
+			let pos = TrajectoryPath.posAtTime(time, trajectory);
+			let speed = TrajectoryPath.speedAtTime(time, trajectory);
+
+			if (pos.distanceTo(position) < bestDistance) {
+				bestDistance = pos.distanceTo(position);
+				bestPos = pos;
+				bestSpeed = speed;
+				bestTime = time;
+			}
+		}
+		// bestSpeed = TrajectoryPath.speedAtTime(bestTime + 0.01, trajectory);
+		return [bestPos, bestSpeed];
 	}
 
 	private static polynomialAtOffset(a0: number, a1: number, a2: number, offset: number): [number, number, number] {
