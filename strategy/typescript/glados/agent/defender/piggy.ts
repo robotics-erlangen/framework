b0b057234a514470a1e0dbe1ad11d99e37a5583d
Johanna Wessels <johanna.wessels@robotics-erlangen.de>
2019-07-02 20:37:05 +1000

    glados: a/d/piggy: remove task breakPass
    BreakPass will be used in an independant behavior

--- strategy/typescript/glados/agent/defender/piggy.ts
+++ strategy/typescript/glados/agent/defender/piggy.ts
@@ -3,8 +3,6 @@ import { Robot } from "base/robot";
 
 import { Behavior, TaskAssignment } from "glados/agent/base/behavior";
 import { MessageType } from "glados/control/messaging";
-import * as Ball from "glados/observer/ball";
-import { BreakPass } from "glados/task/defender/breakpass";
 import { Piggy as PiggyTask } from "glados/task/defender/piggy";
 
 export class Piggy extends Behavior {
@@ -19,7 +17,7 @@ export class Piggy extends Behavior {
 		return role != undefined && role.name === "Piggy";
 	}
 
-	_updateTask(): TaskAssignment<typeof BreakPass> | TaskAssignment<typeof PiggyTask> {
+	_updateTask(): TaskAssignment<typeof PiggyTask> {
 		let assignment = this._messaging.receiveTrainer(MessageType.roleAssignment);
 		if (assignment == undefined || assignment.name !== "Piggy") {
 			throw new Error();
@@ -30,17 +28,6 @@ export class Piggy extends Behavior {
 
 		debug.set("target", this._opp.id);
 
-		let [moveDest, endSpeed, waitingTime] = BreakPass.calculateBreakPos(this._robot);
-		let breakPassThreshold = 0;
-		if (this._task instanceof BreakPass) {
-			breakPassThreshold = 0.1;
-		}
-
-		if ((Ball.receivesPass(this._opp)) && (breakPassThreshold >= waitingTime)) {
-			return [BreakPass, [ this._opp ], false];
-		} else {
-			return [PiggyTask, [ this._opp ], restartTask];
-		}
-
+		return [PiggyTask, [ this._opp ], restartTask];
 	}
 }
