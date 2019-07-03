8921ca07866e353b89e6870241fa843399bb06e9
Andreas Wendler <andreas.wendler@robotics-erlangen.de>
2019-07-03 20:46:29 +1000

    glados: tj/pathhelper: Add moving circle for opponent obstacles

--- strategy/typescript/glados/trajectory/pathhelper.ts
+++ strategy/typescript/glados/trajectory/pathhelper.ts
@@ -462,7 +462,8 @@ function addRobotObstacles(path: Path, robot: FriendlyRobot, ignoreFriendlyRobot
 						&&  r.pos.distanceTo(estimatedPosition) > 0.0001) {
 					path.addLine(r.pos.x, r.pos.y, estimatedPosition.x, estimatedPosition.y,
 							r.radius + safetyDistance, `OwnRobot_${r.id}`, Priorities.ROBOT);
-					path.addAvoidanceLine(estimatedPosition, r.pos + (estimatedPosition - r.pos) * 2, r.radius * 2, 1.2);
+					// path.addAvoidanceLine(estimatedPosition, r.pos + (estimatedPosition - r.pos) * 2, r.radius * 2, 1.2);
+					path.addMovingCircle(0, 0.8, r.pos, r.speed, new Vector(0, 0), r.radius + safetyDistance, Priorities.ROBOT);
 				} else {
 					path.addCircle(r.pos.x, r.pos.y, r.radius + safetyDistance, `OwnRobot_${r.id}`, Priorities.ROBOT);
 				}
