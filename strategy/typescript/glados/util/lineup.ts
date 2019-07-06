8996460a556580ccd6e9b5c07195a187513d0b8c
Johanna Wessels <johanna.wessels@robotics-erlangen.de>
2019-07-07 09:22:30 +1000

    glados: u/lineup: add lineup with space between pairs of robots

--- strategy/typescript/glados/util/lineup.ts
+++ strategy/typescript/glados/util/lineup.ts
@@ -1,3 +1,4 @@
+import * as Constant from "base/constants";
 import * as Entrypoints from "base/entrypoints";
 import * as geom from "base/geom";
 import { Robot } from "base/robot";
@@ -7,10 +8,11 @@ import * as World from "base/world";
 import { MoveToPos } from "glados/task/shared/movetopos";
 
 
-function generateLineup(lineStart: Position, lineupDir: number) {
+function generateLineup(lineStart: Position, lineupDir: number, space: boolean) {
 	let lineEnd = lineStart + Vector.fromAngle(lineupDir); // not really the "end" of the line, more a direction
 	let lineupDistance = 0.05; // keep 5 cm distance to make taking the robots more comfortable
 	let mindist = lineupDistance + 0.18;
+	let mindistSpace = Constant.maxRobotRadius + 2 * mindist;
 
 	let viewDir = lineupDir + Math.PI / 2;
 
@@ -34,13 +36,19 @@ function generateLineup(lineStart: Position, lineupDir: number) {
 	let friendlyIndex = 0;
 	let opponentIndex = 0;
 	while (friendlyIndex < World.FriendlyRobots.length) {
+		let mindistChoice = mindist;
+		let oddFriendlyIndex = friendlyIndex % 2 === 1 ? true : false;
+		if (space && oddFriendlyIndex) {
+			mindistChoice = mindistSpace;
+		}
+
 		let r = World.FriendlyRobots[friendlyIndex];
 		let opp = sortedOpps.length > 0 && sortedOpps[opponentIndex];
 		let intendedPos = lineStart + (lineEnd - lineStart) * distToStart;
 
-		if (opp && opp.pos.distanceTo(intendedPos) < mindist) {
+		if (opp && opp.pos.distanceTo(intendedPos) < mindistChoice) {
 			// extra distance for numeric stability
-			let [p1, p2] = geom.intersectLineCircle(lineStart, lineEnd - lineStart, opp.pos, mindist + 0.0001);
+			let [p1, p2] = geom.intersectLineCircle(lineStart, lineEnd - lineStart, opp.pos, mindistChoice + 0.0001);
 			let d1 = lineStart.distanceTo(p1!);
 			let d2 = p2 ? lineStart.distanceTo(p2) : Infinity;
 			let further = d1 > d2 ? d1 : d2;
@@ -51,7 +59,7 @@ function generateLineup(lineStart: Position, lineupDir: number) {
 			let task = new MoveToPos(<any> pseudoagent, intendedPos, viewDir);
 			task.run();
 			friendlyIndex = friendlyIndex + 1;
-			distToStart = distToStart + mindist;
+			distToStart = distToStart + mindistChoice;
 		}
 	}
 }
@@ -63,9 +71,17 @@ let oleft = new Vector(-World.Geometry.FieldWidthHalf + distToLine, World.Geomet
 let oright = new Vector(World.Geometry.FieldWidthHalf - distToLine, World.Geometry.FieldHeightHalf - distToLine);
 let mleft = new Vector(-World.Geometry.FieldWidthHalf + distToLine, 0);
 let mright = new Vector(World.Geometry.FieldWidthHalf - distToLine, 0);
-Entrypoints.add("Lineup/Friendly Left", function() { generateLineup(fleft, Math.PI / 2); return true; });
-Entrypoints.add("Lineup/Friendly Right", function() { generateLineup(fright, Math.PI / 2); return true; });
-Entrypoints.add("Lineup/Opponent Left", function() { generateLineup(oleft, -Math.PI / 2); return true; });
-Entrypoints.add("Lineup/Opponent Right", function() { generateLineup(oright, -Math.PI / 2); return true; });
-Entrypoints.add("Lineup/Middle Left", function() { generateLineup(mleft, Math.PI / 2); return true; });
-Entrypoints.add("Lineup/Middle Right", function() { generateLineup(mright, Math.PI / 2); return true; });
+
+Entrypoints.add("Lineup/Friendly Left", function() { generateLineup(fleft, Math.PI / 2, false); return true; });
+Entrypoints.add("Lineup/Friendly Right", function() { generateLineup(fright, Math.PI / 2, false); return true; });
+Entrypoints.add("Lineup/Opponent Left", function() { generateLineup(oleft, -Math.PI / 2, false); return true; });
+Entrypoints.add("Lineup/Opponent Right", function() { generateLineup(oright, -Math.PI / 2, false); return true; });
+Entrypoints.add("Lineup/Middle Left", function() { generateLineup(mleft, Math.PI / 2, false); return true; });
+Entrypoints.add("Lineup/Middle Right", function() { generateLineup(mright, Math.PI / 2, false); return true; });
+
+Entrypoints.add("Lineup_Space/Friendly Left", function() { generateLineup(fleft, Math.PI / 2, true); return true; });
+Entrypoints.add("Lineup_Space/Friendly Right", function() { generateLineup(fright, Math.PI / 2, true); return true; });
+Entrypoints.add("Lineup_Space/Opponent Left", function() { generateLineup(oleft, -Math.PI / 2, true); return true; });
+Entrypoints.add("Lineup_Space/Opponent Right", function() { generateLineup(oright, -Math.PI / 2, true); return true; });
+Entrypoints.add("Lineup_Space/Middle Left", function() { generateLineup(mleft, Math.PI / 2, true); return true; });
+Entrypoints.add("Lineup_Space/Middle Right", function() { generateLineup(mright, Math.PI / 2, true); return true; });
