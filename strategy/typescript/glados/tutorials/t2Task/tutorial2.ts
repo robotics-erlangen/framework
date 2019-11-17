e8234e6004bf18e6b3522f097445c3f4efea8896
Paul Bergmann <paul.bergmann@robotics-erlangen.de>
2019-11-17 22:04:15 +0100

    glados: tutorials: Update comments

--- strategy/typescript/glados/tutorials/t2Task/tutorial2.ts
+++ strategy/typescript/glados/tutorials/t2Task/tutorial2.ts
@@ -1,23 +1,33 @@
-// For this assignment, we also want the robots to move along triangles.
-// This time however, we have a move that assigns this task.
-// Now the task has to take care of moving in triangles.
-// Like last time, there is a commented stub in the same folder as this file.
 
-// The "number" parameter is a number from 1 to 3 and dictates your position.
-// The positions are 1m away from the middle and seperated by 120° respectively
-// So the first robot is at 120°, the second at 240° and the last at 360° (or 0°)
-// As soon as a robot arrives at its position, it should move towards the next
-// Start the move with the entrypoint "MoveTest" -> "Tutorial2"
-
-// Hints:
-// 	- if you have trouble imagining the positions, just start the move,
-//    the move will move into the correct positions first before assigning this task
-// 	- we measure angle in radians (2 * math.pi == 360°)
-// 	- the function Vector.fromAngle(angle) creates a Vector of length 1 in the specified direction
-// 	- Vector.distanceTo(vector) measures the distance between two vectors
-//    e.g. pos.distanceTo(new Vector(0,0)) would measure the distance of 'pos' from the middle in meters
-// 	- unlike moves, tasks only handle one robot (so just this._robot)
-// 	- real-life vision is never perfectly accurate, an accuracy of 10cm is sufficient (for this case, not in general!)
+/*
+ * For this assignment, we also want the robots to move along triangles.
+ * This time however, we have a move that assigns this task.
+ * Now the task has to take care of moving in triangles.
+ *
+ * Like last time, there is a commented stub in the same folder as this file.
+ *
+ * The "initialPos" parameter is a number from 0 to 2 and dictates your initial
+ * position in the triangle.
+ * The positions are 1m away from the middle and seperated by 120°
+ * respectively. So the first robots initial position is at 120°, the second at
+ * 240° and the last at 360° (or 0°).
+ * As soon as a robot arrives at its position, it should move towards the next.
+ *
+ * Execute the move with the entrypoint "Tutorial/Tutorial 2"
+ *
+ * Hints:
+ * - if you have trouble imagining the positions, just start the move,
+ *   the move will move into the correct positions first before assigning this task
+ * - we measure angle in radians (2 * math.pi == 360°)
+ *
+ * - the function Vector.fromAngle(angle) creates a Vector of length 1 in the specified direction
+ * - Vector.distanceTo(vector) measures the distance between two vectors
+ *   e.g. pos.distanceTo(new Vector(0,0)) would measure the distance of 'pos' from the middle in meters
+ *
+ * - unlike moves, tasks only handle one robot (so just this._robot)
+ * - take a look at "tutentrypoint.ts" in the same folder as this file to see how the task is called
+ * - real-life vision is never perfectly accurate, an accuracy of 10cm is sufficient (for this case, not in general!)
+ */
 
 import * as World from "base/world";
 
--- strategy/typescript/glados/tutorials/t3Ball/tutorial3.ts
+++ strategy/typescript/glados/tutorials/t3Ball/tutorial3.ts
@@ -1,23 +1,25 @@
-// For the third tutorial we want to create another task
-// When this task is run, a ball was shot in your robot's general direction
-// Intercept the ball and shoot it at the (opponent's) goal
-
-// Start this tutorial with the entrypoint "MoveTest" -> "Tutorial3"
-
-// Hints:
-// - You can get the current state of the ball from World.Ball
-//   (You need to import world first)
-// - World.Ball.pos returns a Vector describing the current ball position
-// - World.Ball.speed also returns a vector
-//   This vector's length is the absolute speed and the vector's angle is the direction of the speed
-
-// - We have a module to assist all your ball-shooting needs
-//   It is located in "glados/task/ability/shoot.ts"
-// - To make use of it, import it and create a new instance of it
-// - Take a look at "glados/task/attacker/shootgoal" to find an example of how to properly use the 'shoot' ability
-
-// - We also have a module that attempts to catch balls
-// - It is located in "glados/task/ability/catchball.lua"
+/*
+ * For the third tutorial we want you to create another task.
+ * When this task is run, a ball was shot in your robot's general direction/
+ * Intercept the ball and shoot it at the opponent's goal.
+ *
+ * Execute this tutorial with the entrypoint "Tutorials/Tutorial 3"
+ *
+ * Hints:
+ * - You can get the current state of the ball from World.Ball
+ *   (You need to import world first)
+ * - World.Ball.pos is a Vector describing the current ball position
+ * - World.Ball.speed also is a Vector
+ *   This vector's length is the absolute speed and the vector's angle is the direction of the speed
+ *
+ * - We have a module to assist all your ball-shooting needs
+ *   It is located in "glados/task/ability/shoot.ts"
+ * - To make use of it, import it and create a new instance of it
+ * - Take a look at "glados/task/shared/pass.ts" to find an example of how to properly use the 'shoot' ability
+ *
+ * - We also have a module that attempts to catch balls
+ * - It is located in "glados/task/ability/catchball.ts"
+ */
 
 import { Agent, Task } from "glados/task/base";
 import * as PathHelper from "glados/trajectory/pathhelper";
