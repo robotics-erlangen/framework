900fc1257ffd6f313d5dc5864861c02c27151227
Philipp Mathea <philipp.mathea@robotics-erlangen.de>
2019-11-20 20:39:50 +0100

    glados: a/k/handleball.ts: wait for pass suggestions

--- strategy/typescript/glados/agent/keeper/handleball.ts
+++ strategy/typescript/glados/agent/keeper/handleball.ts
@@ -1,3 +1,4 @@
+import * as debug from "base/debug";
 import * as Field from "base/field";
 import * as Referee from "base/referee";
 import { FriendlyRobot } from "base/robot";
@@ -8,15 +9,24 @@ import { Behavior, TaskAssignment } from "glados/agent/base/behavior";
 import { MessageType } from "glados/control/messaging";
 import * as Ball from "glados/observer/ball";
 import * as Physics from "glados/observer/physics";
+import { MoveToStaticBall } from "glados/task/attacker/movetostaticball";
 import { AggressiveKeeper } from "glados/task/keeper/aggressivekeeper";
 import { ChipAway as KeeperChipAway } from "glados/task/keeper/chipaway";
 import { Keeper } from "glados/task/keeper/keeper";
+import { Halt } from "glados/task/shared/halt";
 import { Pass } from "glados/task/shared/pass";
 import * as Attack from "glados/util/attack";
 
+import { Move } from "../attacker/move";
+
 export class HandleBall extends Behavior {
-	private _pass: {target?: FriendlyRobot, ballPos: Position} | undefined;
+	private _pass: {target?: FriendlyRobot, ballPos: Position, time: number} | undefined;
 	private _hysteresis: boolean = false;
+	private _timeBegin: number | undefined = undefined;
+
+	_stop() {
+		this._timeBegin = undefined;
+	}
 
 	behindCenterbacks(object: {pos: Position, radius: number}): boolean {
 		let hyst = this._hysteresis ? 0.1 : 0;
@@ -43,7 +53,7 @@ export class HandleBall extends Behavior {
 	}
 
 	_updateTask(): TaskAssignment<typeof Keeper> | TaskAssignment<typeof Pass> | TaskAssignment<typeof KeeperChipAway>
-			| TaskAssignment<typeof AggressiveKeeper> {
+			| TaskAssignment<typeof AggressiveKeeper> | TaskAssignment<typeof Halt> | TaskAssignment<typeof MoveToStaticBall> {
 		let endPos = Physics.ballAtTime(World.Ball, Infinity).pos;
 		let startInside = Field.isInFriendlyDefenseArea(World.Ball.pos, -World.Ball.radius - this._robot.radius);
 		let endInside = Field.isInFriendlyDefenseArea(endPos, -World.Ball.radius - this._robot.radius);
@@ -60,9 +70,15 @@ export class HandleBall extends Behavior {
 		} else if (startInside && endInside && !ballBehindKeeper && suggestions) {
 			// if ball is inside defense area and will not leave it -> we have time to act
 			// try to find a good pass
+			if (this._timeBegin == undefined) {
+				this._timeBegin = World.Time;
+			}
 			this._pass = Attack.choosePassFromSuggestions(this._robot, suggestions, this._pass && this._pass.ballPos, false)[0];
 			if (this._pass != undefined) { // check if there is a good pass, else chip away
-				return [Pass, [ this._pass.target, this._pass.ballPos, true ]];
+				return [Pass, [ this._pass.target, this._pass.ballPos, true, undefined, undefined, undefined, true]];
+			}
+			if (World.Time - this._timeBegin < 7) {
+				return [MoveToStaticBall];
 			} else {
 				return [KeeperChipAway];
 			}
