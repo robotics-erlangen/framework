3debe76c038c86a13cf54b818d19aefa07e8e341
Philipp Mathea <philipp.mathea@robotics-erlangen.de>
2020-01-15 20:01:14 +0100

    glados: t/d/dirtymanmarker: new manmarker task

---
+++ strategy/typescript/glados/task/defender/dirtymanmarker.ts
@@ -0,0 +1,54 @@
+import { Robot } from "base/robot";
+import { Position, Vector } from "base/vector";
+import * as World from "base/world";
+
+import { Agent, Task } from "glados/task/base";
+import { CurvedMaxAccel } from "glados/trajectory/curvedmaxaccel";
+import * as PathHelper from "glados/trajectory/pathhelper";
+import * as Defense from "glados/util/defense";
+let G = World.Geometry;
+
+const MANMARKDISTANCE = 0.3;
+const SPEEDOFFSET = 0.07;
+
+export class DirtyManMarker extends Task {
+	private targetRobot: Robot;
+	private obstacleTable: PathHelper.PathHelperParameters;
+	private dir: number | undefined;
+	private direction: Vector | undefined;
+	private oldpos: Vector | undefined;
+
+	constructor(agent: Agent, targetRobot: Robot) {
+		super(agent);
+
+		this.targetRobot = targetRobot;
+		this.obstacleTable = {
+			ignoreBall: false,
+			messaging: this._messaging
+		};
+	}
+
+	public run() {
+		let pos = new Vector(0,0);
+
+		PathHelper.setDefaultObstaclesByTable(this._robot.path, this._robot, this.obstacleTable);
+
+		if (this.targetRobot.speed.length() < 0.2) {
+			this.direction = World.Ball.pos - this.targetRobot.pos;
+			pos = this.targetRobot.pos + this.direction.copy().setLength(MANMARKDISTANCE);
+
+		} else {
+			let boundaryOne = this.targetRobot.pos + this.targetRobot.speed.copy().setLength(MANMARKDISTANCE + this.targetRobot.speed.length() * SPEEDOFFSET);
+			let boundaryTwo = this.targetRobot.pos + this.targetRobot.speed.copy().setLength(3 + this.targetRobot.speed.length() * SPEEDOFFSET);
+			let blockalpha;
+			if (this.oldpos == undefined) {
+				blockalpha = 1;
+			} else {
+				blockalpha = 0.1;
+			}
+			pos = Defense.fastestPointInInterval(this._robot, boundaryOne, boundaryTwo, this.oldpos || new Vector(0, 0), 0.1, blockalpha);
+			this.oldpos = pos;
+		}
+		this._robot.trajectory.update(CurvedMaxAccel, pos, this.dir);
+	}
+}
---
+++ strategy/typescript/glados/test/move/dirtymanmarkermove.ts
@@ -0,0 +1,34 @@
+import { FriendlyRobot } from "base/robot";
+import * as World from "base/world";
+
+import { Assignment, MessageBox, Move, MoveParameters } from "glados/group/move/base";
+import { DirtyManMarker } from "glados/task/defender/dirtymanmarker";
+
+export class DirtyManMarkerMove extends Move {
+	public static MIN_ROBOTS: number = 1;
+	public static MAX_ROBOTS: number = 1;
+	public static ALLOW_EXTRA_ATTACKERS: boolean = false;
+
+	constructor(robots: FriendlyRobot[], messaging: MessageBox) {
+		super(robots, messaging);
+	}
+
+	static canStart() {
+		return true;
+	}
+
+	_canContinue() {
+		return true;
+	}
+
+	_updateTasks(): MoveParameters {
+		let taskAssignments = new Map<FriendlyRobot, Assignment>();
+
+		taskAssignments[this._robots[0]] = { class: DirtyManMarker, params: [World.OpponentRobots[0]] };
+		return {
+			assignments: taskAssignments,
+			mainAttacker: this._robots[0]
+		};
+	}
+}
+
