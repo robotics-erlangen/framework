1f08d50834379b9afefcc548830491c91f43a38d
Andreas Wendler <andreas.wendler@robotics-erlangen.de>
2020-03-08 10:16:13 +0100

    glados: a/s/rescuefromdefensearea: move rescue position further away from goal
    During freekicks and similar, the defense area obstacle is quite large
    and there still was a chance to get stuck behind it

--- strategy/typescript/glados/agent/shared/rescuefromdefensearea.ts
+++ strategy/typescript/glados/agent/shared/rescuefromdefensearea.ts
@@ -7,7 +7,8 @@ import { Behavior, TaskAssignment } from "glados/agent/base/behavior";
 import { MoveToPos } from "glados/task/shared/movetopos";
 
 function calculateRescuePosition(robot: FriendlyRobot): Position {
-	let x = MathUtil.sign(robot.pos.x) * (World.Geometry.DefenseStretchHalf + 0.2);
+	// large extra distance for the required defense area distance during freekicks
+	let x = MathUtil.sign(robot.pos.x) * (World.Geometry.DefenseWidthHalf + 0.5);
 	let y = MathUtil.sign(robot.pos.y) * (World.Geometry.FieldHeightHalf + robot.radius + 0.02);
 	return new Vector(x, y);
 }
@@ -15,7 +16,7 @@ function calculateRescuePosition(robot: FriendlyRobot): Position {
 export class RescueFromDefenseArea extends Behavior {
 	check(): boolean {
 		return World.RefereeState !== "BallPlacementOffensive" && Math.abs(this._robot.pos.y) > World.Geometry.FieldHeightHalf  &&
-			Math.abs(this._robot.pos.x) + 0.1 < Math.abs(calculateRescuePosition(this._robot).x);
+			Math.abs(this._robot.pos.x) + 0.05 < Math.abs(calculateRescuePosition(this._robot).x);
 	}
 
 	_updateTask(): TaskAssignment<typeof MoveToPos> {
