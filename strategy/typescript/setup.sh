e5f495a0ab355ff7710f842a8a5a3175f1c35553
Paul Bergmann <paul.bergmann@robotics-erlangen.de>
2020-04-21 19:23:55 +0200

    typescript: Add a script to aid language service setup

---
+++ strategy/typescript/setup.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+FBOLD=""
+FNORMAL=""
+setup_pretty_printing() {
+	if ! command -v "tput" >/dev/null; then
+		return
+	fi
+	FBOLD="$(tput bold 2>/dev/null)"
+	if [ "$?" -ne "0" ]; then
+		FBOLD=""
+		return
+	fi
+	FNORMAL="$(tput sgr0 2>/dev/null)"
+	if [ "$?" -ne "0" ]; then
+		FBOLD=""
+		FNORMAL=""
+		return
+	fi
+}
+setup_pretty_printing
+
+die() {
+	echo "${FBOLD}ERROR:${FNORMAL} $1"
+	exit 1
+}
+
+# make sure the current working directory is the location of the script
+cd "$(dirname "$0")"
+
+# check if npm exists
+if ! command -v "npm" >/dev/null; then
+	die "'npm' not found in PATH"
+fi
+
+echo "${FBOLD}Installing packages from npm...${FNORMAL}"
+
+if ! npm install; then
+	die "Failed to run npm install"
+fi
+
+echo "${FBOLD}Copying custom typescript compiler${FNORMAL}"
+
+# check for permission to read the tsc directory
+# (and for existence thereof)
+# note that files could still be unreadable
+CUSTOM_TSC_DIR="../../libs/tsc/built/local"
+if [ ! -r "$CUSTOM_TSC_DIR" ]; then
+	die "Could not read from '$CUSTOM_TSC_DIR'"
+fi
+
+if ! cp -R "$CUSTOM_TSC_DIR/." "node_modules/typescript/lib/"; then
+	die "Failed to copy files"
+fi
+
+echo "${FBOLD}Done${FNORMAL}"
