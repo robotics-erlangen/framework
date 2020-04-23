474d8729a290d3906c4b8001c224b387352164a9
Andreas Wendler <andreas.wendler@robotics-erlangen.de>
2020-04-23 15:13:17 +0200

    glados: t/a/stopattack: position robot at target pos during ball placement

--- strategy/typescript/glados/task/attacker/stopattack.ts
+++ strategy/typescript/glados/task/attacker/stopattack.ts
@@ -2,6 +2,7 @@ import * as Constants from "base/constants";
 import * as Field from "base/field";
 import * as geom from "base/geom";
 import * as MathUtil from "base/mathutil";
+import { RefereeState } from "base/referee";
 import { Position, RelativePosition, Vector } from "base/vector";
 // import * as vis from "base/vis";
 import * as World from "base/world";
@@ -42,11 +43,18 @@ export class StopAttack extends Task {
 	}
 
 	run() {
-		let stopRadius = this._minDistToBall + this._robot.radius + POSITION_PADDING;
-		let pos: Position = World.Ball.pos + (this._focusPoint - World.Ball.pos).setLength(stopRadius);
-		let driveAngle = (World.Ball.pos - pos).angle();
 
-		let [opponentShooter, dist] = UtilDefense.getClosestRobot(World.OpponentRobots, World.Ball.pos);
+		let ballPos = World.Ball.pos;
+		// already go to the freekick position during ballplacement
+		if (World.RefereeState === RefereeState.BallPlacementDefensive && World.BallPlacementPos) {
+			ballPos = World.BallPlacementPos;
+		}
+
+		let stopRadius = this._minDistToBall + this._robot.radius + POSITION_PADDING;
+		let pos: Position = ballPos + (this._focusPoint - ballPos).setLength(stopRadius);
+		let driveAngle = (ballPos - pos).angle();
+
+		let [opponentShooter, dist] = UtilDefense.getClosestRobot(World.OpponentRobots, ballPos);
 
 		// hysteresis on distance between opponent shooter and ball
 		if (this._defenseHysteresis) {
@@ -57,7 +65,7 @@ export class StopAttack extends Task {
 		let isOpponentFreekickState = World.RefereeState === "DirectDefensive";
 		let defendGoalDistance = 4;
 		defendGoalDistance = this._defendGoalHysteresis ? defendGoalDistance + DEFEND_GOAL_HYSTERESIS : defendGoalDistance - DEFEND_GOAL_HYSTERESIS;
-		let defendOpponentPasses = ((World.Ball.pos - World.Geometry.FriendlyGoal).length() > defendGoalDistance) && isOpponentFreekickState;
+		let defendOpponentPasses = ((ballPos - World.Geometry.FriendlyGoal).length() > defendGoalDistance) && isOpponentFreekickState;
 		this._defendGoalHysteresis = !defendOpponentPasses;
 
 		let passReceivers = RobotList.excludeRobots(World.OpponentRobots, [opponentShooter!, World.OpponentKeeper!]);
@@ -65,7 +73,7 @@ export class StopAttack extends Task {
 			let minAngle = Infinity;
 			let maxAngle = -Infinity;
 			for (let robot of passReceivers) {
-				let angle = getNormalizedAngle(Field.limitToAllowedField(Physics.robotBrakePos(robot), robot.radius) - World.Ball.pos);
+				let angle = getNormalizedAngle(Field.limitToAllowedField(Physics.robotBrakePos(robot), robot.radius) - ballPos);
 				angle = geom.normalizeAnglePositive(angle);
 				if (angle < minAngle && angle >= Math.PI) {
 					minAngle = angle;
@@ -92,32 +100,32 @@ export class StopAttack extends Task {
 			const borderShift = 2;
 			const quarterBegin = 0.7;
 			const borderBegin = 1.7;
-			// vis.addPath("stopattack: MaxAngle", [World.Ball.pos, World.Ball.pos + Vector.fromAngle(maxAngle).setLength(1)], vis.colors.red);
-			// vis.addPath("stopattack: MinAngle", [World.Ball.pos, World.Ball.pos + Vector.fromAngle(minAngle).setLength(1)], vis.colors.redHalf);
-			if (World.Ball.pos.x > (quarterBegin * World.Geometry.FieldWidthQuarter) && World.Ball.pos.x < (borderBegin * World.Geometry.FieldWidthQuarter)) {
+			// vis.addPath("stopattack: MaxAngle", [ballPos, ballPos + Vector.fromAngle(maxAngle).setLength(1)], vis.colors.red);
+			// vis.addPath("stopattack: MinAngle", [ballPos, ballPos + Vector.fromAngle(minAngle).setLength(1)], vis.colors.redHalf);
+			if (ballPos.x > (quarterBegin * World.Geometry.FieldWidthQuarter) && ballPos.x < (borderBegin * World.Geometry.FieldWidthQuarter)) {
 				maxAllowedAngle -= quarterShift;
 				minAllowedAngle -= quarterShift;
-			} else if (World.Ball.pos.x < (-quarterBegin * World.Geometry.FieldWidthQuarter) && World.Ball.pos.x > (-borderBegin * World.Geometry.FieldWidthQuarter)) {
+			} else if (ballPos.x < (-quarterBegin * World.Geometry.FieldWidthQuarter) && ballPos.x > (-borderBegin * World.Geometry.FieldWidthQuarter)) {
 				maxAllowedAngle += quarterShift;
 				minAllowedAngle += quarterShift;
-			} else if (World.Ball.pos.x >= (borderBegin * World.Geometry.FieldWidthQuarter)) {
+			} else if (ballPos.x >= (borderBegin * World.Geometry.FieldWidthQuarter)) {
 				maxAllowedAngle -= borderShift;
 				minAllowedAngle -= borderShift;
-			} else if (World.Ball.pos.x <= (-borderBegin * World.Geometry.FieldWidthQuarter)) {
+			} else if (ballPos.x <= (-borderBegin * World.Geometry.FieldWidthQuarter)) {
 				maxAllowedAngle += borderShift;
 				minAllowedAngle += borderShift;
 			} else {
 				maxAllowedAngle -= middleShift;
 				minAllowedAngle += middleShift;
 			}
-			if (World.Ball.pos.y < -World.Geometry.FieldHeightQuarter) {
-				if (World.Ball.pos.x > quarterBegin) {
-					maxAllowedAngle = ((World.Geometry.FriendlyGoalRight - World.Ball.pos).angle());
+			if (ballPos.y < -World.Geometry.FieldHeightQuarter) {
+				if (ballPos.x > quarterBegin) {
+					maxAllowedAngle = ((World.Geometry.FriendlyGoalRight - ballPos).angle());
 					minAllowedAngle = (maxAllowedAngle - Math.PI / 4);
 					minAllowedAngle = minAllowedAngle / Math.PI * 8;
 					maxAllowedAngle = maxAllowedAngle / Math.PI * 8;
-				} else if (World.Ball.pos.x < -quarterBegin) {
-					minAllowedAngle = ((World.Geometry.FriendlyGoalLeft - World.Ball.pos).angle());
+				} else if (ballPos.x < -quarterBegin) {
+					minAllowedAngle = ((World.Geometry.FriendlyGoalLeft - ballPos).angle());
 					maxAllowedAngle = (minAllowedAngle + Math.PI / 4);
 					minAllowedAngle = minAllowedAngle / Math.PI * 8;
 					maxAllowedAngle = maxAllowedAngle / Math.PI * 8;
@@ -125,26 +133,26 @@ export class StopAttack extends Task {
 			}
 			maxAllowedAngle = maxAllowedAngle * Math.PI / 8;
 			minAllowedAngle = minAllowedAngle * Math.PI / 8;
-			// vis.addPath("stopattack: MaxAllowedAngle", [World.Ball.pos, World.Ball.pos + Vector.fromAngle(maxAllowedAngle).setLength(1)], vis.colors.green);
-			// vis.addPath("stopattack: MinAllowedAngle", [World.Ball.pos, World.Ball.pos + Vector.fromAngle(minAllowedAngle).setLength(1)], vis.colors.greenHalf);
+			// vis.addPath("stopattack: MaxAllowedAngle", [ballPos, ballPos + Vector.fromAngle(maxAllowedAngle).setLength(1)], vis.colors.green);
+			// vis.addPath("stopattack: MinAllowedAngle", [ballPos, ballPos + Vector.fromAngle(minAllowedAngle).setLength(1)], vis.colors.greenHalf);
 
 			maxAngle = MathUtil.bound(minAllowedAngle, maxAngle, maxAllowedAngle);
 			minAngle = MathUtil.bound(minAllowedAngle, minAngle, maxAllowedAngle);
-			// vis.addPath("stopattack: MaxAngleBounded", [World.Ball.pos, World.Ball.pos + Vector.fromAngle(maxAngle).setLength(1)], vis.colors.black);
-			// vis.addPath("stopattack: MinAngleBounded", [World.Ball.pos, World.Ball.pos + Vector.fromAngle(minAngle).setLength(1)], vis.colors.blackHalf);
+			// vis.addPath("stopattack: MaxAngleBounded", [ballPos, ballPos + Vector.fromAngle(maxAngle).setLength(1)], vis.colors.black);
+			// vis.addPath("stopattack: MinAngleBounded", [ballPos, ballPos + Vector.fromAngle(minAngle).setLength(1)], vis.colors.blackHalf);
 
-			let relativeAngle = getNormalizedAngle(World.Ball.pos - opponentShooter!.pos);
+			let relativeAngle = getNormalizedAngle(ballPos - opponentShooter!.pos);
 			let boundedAngle = MathUtil.bound(minAngle, relativeAngle, maxAngle);
 			let opponentDirection = getNormalizedAngle(Vector.fromAngle(opponentShooter!.dir));
 			let boundedOppDirection = MathUtil.bound(minAngle, opponentDirection, maxAngle);
 			let middleAngle = (boundedAngle + boundedOppDirection) / 2;
-			pos = World.Ball.pos + Vector.fromAngle(middleAngle).setLength(stopRadius);
+			pos = ballPos + Vector.fromAngle(middleAngle).setLength(stopRadius);
 			// try to reflect the ball to the opponents goal
-			let hypotheticalBall = {pos: World.Ball.pos, speed: (pos - World.Ball.pos).setLength(Constants.maxBallSpeed), maxSpeed: Constants.maxBallSpeed, posZ: 0, initSpeedZ: 0, speedZ: 0};
-			let time = Physics.ballTravelTime(hypotheticalBall, (World.Ball.pos - pos).length());
+			let hypotheticalBall = {pos: ballPos, speed: (pos - ballPos).setLength(Constants.maxBallSpeed), maxSpeed: Constants.maxBallSpeed, posZ: 0, initSpeedZ: 0, speedZ: 0};
+			let time = Physics.ballTravelTime(hypotheticalBall, (ballPos - pos).length());
 			let futureBall = Physics.ballAtTime(hypotheticalBall, time);
 			driveAngle = Volley.calcPhi(this._robot, futureBall.speed, pos, World.Geometry.OpponentGoal, Infinity)[0];
-			// vis.addPath("t/a/stopattack: reflectionNormal", [World.Ball.pos, pos, pos + Vector.fromAngle(driveAngle), pos, pos + ((World.Geometry.OpponentGoal - pos).setLength(1))]);
+			// vis.addPath("t/a/stopattack: reflectionNormal", [ballPos, pos, pos + Vector.fromAngle(driveAngle), pos, pos + ((World.Geometry.OpponentGoal - pos).setLength(1))]);
 
 			// Go back a little so the ball will hit the front side of the robot
 			pos = pos - Vector.fromAngle(driveAngle).setLength(this._robot.shootRadius);
@@ -154,7 +162,7 @@ export class StopAttack extends Task {
 			// position between ball and goal
 			this._defenseHysteresis = false;
 			if (Field.isInFriendlyDefenseArea(pos, 4 * this._robot.radius + 0.05)) {
-				let intersections = Field.intersectCircleDefenseArea(World.Ball.pos, stopRadius, 4 * this._robot.radius + 0.05, true);
+				let intersections = Field.intersectCircleDefenseArea(ballPos, stopRadius, 4 * this._robot.radius + 0.05, true);
 				if (intersections.length > 0) {
 					// pos = undefined;
 					let distanceToSqMin = Infinity;
@@ -173,9 +181,9 @@ export class StopAttack extends Task {
 					}
 				}
 			}
-			if (this._side === "left" && World.Ball.pos.x < -0.3) {
+			if (this._side === "left" && ballPos.x < -0.3) {
 				this._side = "right";
-			} else if (this._side === "right" && World.Ball.pos.x > 0.3) {
+			} else if (this._side === "right" && ballPos.x > 0.3) {
 				this._side = "left";
 			}
 
