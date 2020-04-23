37f6191a73526b0ad7398af605e9b6dca0467855
Andreas Wendler <andreas.wendler@robotics-erlangen.de>
2020-04-23 18:13:04 +0200

    glados: a/k/handleball: add chip override in Pass update target

--- strategy/typescript/glados/agent/keeper/handleball.ts
+++ strategy/typescript/glados/agent/keeper/handleball.ts
@@ -78,7 +78,7 @@ export class HandleBall extends Behavior {
 				this._pass && this._pass.ballPos, false)[0];
 			if (this._pass != undefined) { // check if there is a good pass, else chip away
 				if (this._task != undefined && this._task instanceof Pass && this._pass.target) {
-					this._task.updateTarget(this._pass.target, this._pass.ballPos);
+					this._task.updateTarget(this._pass.target, this._pass.ballPos, true);
 				}
 				return [Pass, [ this._pass.target, this._pass.ballPos, true, undefined, undefined, undefined, true]];
 			}
