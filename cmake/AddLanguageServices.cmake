ee4a74cda323015d0d783959506f4462192f0b3d
Andreas Wendler <andreas.wendler@robotics-erlangen.de>
2020-05-14 16:55:22 +0200

    Add test for conforming git name and email

--- cmake/AddLanguageServices.cmake
+++ cmake/AddLanguageServices.cmake
@@ -11,6 +11,10 @@ add_test(NAME copyright-header-exists
 	COMMAND python2 "data/scripts/check-copyright-header.py"
 	WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}")
 
+add_test(NAME git-correct-mail
+	COMMAND python3 "data/scripts/check-username-and-mail.py"
+	WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}")
+
 # show what went wrong by default
 add_custom_target(check COMMAND ${CMAKE_CTEST_COMMAND} --output-on-failure
     USES_TERMINAL)
---
+++ data/scripts/check-username-and-mail.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+#-*- coding: UTF-8 -*-
+
+import subprocess
+
+EXPECTED_MAIL_DOMAIN = "robotics-erlangen.de"
+ALLOWED_MAIL_ADRESSES = ["info@robotics-erlangen.de"]
+
+# umlaut translation
+translationTable = { ord('ä'): 'ae', ord('ö'): 'oe', ord('ü'): 'ue', ord('ß'): 'ss', ord('é'): 'e' }
+
+process = subprocess.Popen(["git", "log", "-10", "--pretty=format:'%an|%ae'"], stdout=subprocess.PIPE)
+for line in iter(process.stdout.readline, b''):
+    decoded = line.decode("utf-8").replace("'", "").replace("\n", "")
+    username = decoded.split("|")[0]
+    origUser = username
+
+    # replace umlaute in username
+    username = username.lower().translate(translationTable)
+
+    email = decoded.split("|")[1]
+
+    if email in ALLOWED_MAIL_ADRESSES:
+        continue
+
+    # compute expected mail from username
+    prefix = username.replace(" ", ".")
+    expectedMail = prefix + "@" + EXPECTED_MAIL_DOMAIN
+    if expectedMail != email.lower():
+        print("Git author " + origUser + " or mail " + email + " does not conform to the standard!")
+        exit(1)
