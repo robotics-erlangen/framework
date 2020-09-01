c30eee43fb8734e327828fdbfa4a636ac9f7aae4
Paul Bergmann <paul.bergmann@robotics-erlangen.de>
2020-09-01 21:26:05 +0200

    glados: a/a/applyformainattacker: Transform into checkable
    - ApplyForMainAttacker is not meant to be run anyway, it does not need
      inherit from Behavior
    - Changing agents requires copying over data from the old agent
      instance. Since the beOffensive field "belongs" to
      ApplyForMainAttacker, it is its responsibility to ensure this.
    
    I deleted _stop, since it was never called - ApplyForMainAttacker is
    never active.

--- strategy/typescript/glados/agent/attacker/applyformainattacker.ts
+++ strategy/typescript/glados/agent/attacker/applyformainattacker.ts
@@ -1,18 +1,31 @@
 import * as Referee from "base/referee";
+import { FriendlyRobot } from "base/robot";
 import * as World from "base/world";
 
 import { Attacker } from "glados/agent/attacker";
-import { Behavior } from "glados/agent/base/behavior";
+import { Agent } from "glados/agent/base/agent";
+import { Checkable, MainAttackerParameters } from "glados/agent/base/behavior";
 import { MessageType } from "glados/control/messaging";
 import * as Robot from "glados/observer/robot";
 import * as Attack from "glados/util/attack";
 import * as Defense from "glados/util/defense";
 
-export class ApplyForMainattacker extends Behavior {
-	_applying: boolean = false;
+export class ApplyForMainattacker implements Checkable {
+	private _agent: Attacker;
+	private _robot: FriendlyRobot;
+	private _applying = false;
+	private _mainAttackerParameters?: MainAttackerParameters;
 
-	_stop() {
-		this._applying = false;
+	private get _messaging() {
+		return this._agent.messaging();
+	}
+
+	constructor(agent: Agent) {
+		if (!(agent instanceof Attacker)) {
+			throw new Error("a/a/applyformainattacker may only be used on attackers");
+		}
+		this._agent = agent;
+		this._robot = agent.robot();
 	}
 
 	check(): undefined {
@@ -30,21 +43,21 @@ export class ApplyForMainattacker extends Behavior {
 		let applying = false;
 		let [sender, passInfoTable] = this._messaging.receiveSingleSender(MessageType.passInfo, true);
 		if (Attack.currentPlannedMainAttacker(sender, passInfoTable!) === this._robot) {
-			this._applyForMainAttacker(undefined, undefined, 2);
-			(this._agent as Attacker).beOffensive = true;
+			this._mainAttackerParameters = [undefined, undefined, 2];
+			this._agent.beOffensive = true;
 			applying = true;
 		} else {
 			if (!Defense.dangerousBallTowardsDefense(true)) {
-				this._applyForMainAttacker();
-				(this._agent as Attacker).beOffensive = false;
+				this._mainAttackerParameters = [undefined, undefined, undefined];
+				this._agent.beOffensive = false;
 				applying = true;
 			} else {
 				let robotDistToGoal = this._robot.pos.distanceTo(World.Geometry.OpponentGoal);
 				let ballDistToGoal = World.Ball.pos.distanceTo(World.Geometry.OpponentGoal);
 				let maxDistDiff = (this._applying ? -1 : 1) * (World.Ball.radius + this._robot.shootRadius);
 				if (robotDistToGoal - ballDistToGoal > maxDistDiff) {
-					this._applyForMainAttacker();
-					(this._agent as Attacker).beOffensive = false;
+					this._mainAttackerParameters = [undefined, undefined, undefined];
+					this._agent.beOffensive = false;
 					applying = true;
 				}
 			}
@@ -53,7 +66,26 @@ export class ApplyForMainattacker extends Behavior {
 		return undefined;
 	}
 
-	_updateTask(): any {
-		throw new Error("This behavior is not supposed to run");
+	mainAttackerParameters(): [MainAttackerParameters | undefined, false] {
+		return [this._mainAttackerParameters, false];
+	}
+
+	clearMainAttackerParameters() {
+		this._mainAttackerParameters = undefined;
+	}
+
+	agent() {
+		return this._agent;
+	}
+
+	setAgent(agent: Agent) {
+		if (this._agent.robot() !== agent.robot()) {
+			throw new Error("Can't reuse checkable with different robot");
+		}
+		if (!(agent instanceof Attacker)) {
+			throw new Error("a/a/applyformainattacker may only be used on attackers");
+		}
+		agent.beOffensive = this._agent.beOffensive;
+		this._agent = agent;
 	}
 }
