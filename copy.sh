f08a1811b82f1502c22dc971e017db91d16bbc56
Tobias Heineken <tobias.heineken@robotics-erlangen.de>
2020-10-29 20:01:50 +0100

    copy.sh: addded
    copy.sh copies all necessary files into ../software-cpy, s.t. one is able to pack and upload software-cpy to nas

---
+++ copy.sh
@@ -0,0 +1,13 @@
+mkdir -p ../software-cpy/build/bin
+mkdir -p ../software-cpy/libs/v8/v8
+cp -r strategy ../software-cpy
+cp build/bin/ra ../software-cpy/build/bin
+cp build/bin/icudtl.dat ../software-cpy/build/bin
+cp build/bin/natives_blob.bin ../software-cpy/build/bin
+cp build/bin/snapshot_blob.bin ../software-cpy/build/bin
+cp -r libs/v8/v8 ../software-cpy/libs/v8
+cp -r config ../software-cpy
+cp -r data ../software-cpy
+echo "#!/bin/bash" >../software-cpy/start.sh
+echo "LD_LIBRARY_PATH=libs/v8/v8/out/x64.release build/bin/ra" >>../software-cpy/start.sh
+chmod +x ../software-cpy/start.sh
