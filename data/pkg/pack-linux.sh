7693c664787988e196f5a6056ff855313b89396f
Paul Bergmann <paul.bergmann@robotics-erlangen.de>
2020-10-30 16:03:23 +0100

    data: Move copy.sh to pkg folder and autopack tarballs

--- data/pkg/pack-linux.sh
+++ data/pkg/pack-linux.sh
@@ -2,6 +2,7 @@
 # make sure the current working directory is the location of the script
 # `bash foobar.sh` seems to have foobar.sh in $0
 cd "$(dirname "$0")"
+cd ../..
 
 mkdir -p ../software-cpy/build/bin
 mkdir -p ../software-cpy/libs/v8/v8
@@ -13,6 +14,13 @@ cp build/bin/snapshot_blob.bin ../software-cpy/build/bin
 cp -r libs/v8/v8 ../software-cpy/libs/v8
 cp -r config ../software-cpy
 cp -r data ../software-cpy
-echo "#!/bin/bash" >../software-cpy/start.sh
-echo "LD_LIBRARY_PATH=libs/v8/v8/out/x64.release build/bin/ra" >>../software-cpy/start.sh
-chmod +x ../software-cpy/start.sh
+
+cd ../software-cpy
+echo "#!/bin/bash" > start.sh
+echo "LD_LIBRARY_PATH=libs/v8/v8/out/x64.release build/bin/ra" >> start.sh
+chmod +x start.sh
+
+tar cfz software-linux.tar.gz *
+mv software-linux.tar.gz ..
+cd ..
+rm -rf software-cpy
