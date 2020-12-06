74b4ebcf0c793f99c05aef4a9d8a88000da18614
Paul Bergmann <paul.bergmann@robotics-erlangen.de>
2020-12-06 16:29:34 +0100

    CI: Add CI runner for Ubuntu 20.04

--- data/docker/copy-libs.sh
+++ data/docker/copy-libs.sh
@@ -3,7 +3,7 @@
 # predictable working directory
 cd "$(dirname "$0")"
 
-for i in {ubuntu-16.04,ubuntu-18.04}; do
+for i in {ubuntu-16.04,ubuntu-18.04,ubuntu-20.04}; do
 	v8dir="$i/libs/v8"
 	mkdir -p "$v8dir"
 	cp -r ../../libs/v8/{build.sh,patches} "$v8dir"
