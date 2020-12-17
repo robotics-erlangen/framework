79d3a8879286250d863b0cc0dcc341058a4e2f5d
Tobias Heineken <tobias.heineken@robotics-erlangen.de>
2020-12-17 19:21:48 +0100

    Ra: logfileselectiondialog: sort by quality
    Otherwise the list is totally useless

--- src/ra/logfileselectiondialog.cpp
+++ src/ra/logfileselectiondialog.cpp
@@ -37,14 +37,54 @@ LogFileSelectionDialog::~LogFileSelectionDialog()
     delete ui;
 }
 
+
+static int mapQuality(logfile::LogOfferEntry::QUALITY q)
+{
+    switch(q) {
+        case logfile::LogOfferEntry::PERFECT: return 0;
+        case logfile::LogOfferEntry::UNKNOWN: return 1;
+        case logfile::LogOfferEntry::UNREADABLE: return 1000;
+    }
+}
+
+struct LogEntry {
+    std::string path;
+    std::string name;
+    logfile::LogOfferEntry::QUALITY q;
+    LogEntry(std::string p_path, std::string p_name, logfile::LogOfferEntry::QUALITY p_q): path(std::move(p_path)), name(std::move(p_name)), q(p_q) {}
+
+    std::string toString() const
+    {
+        return name + " (" + logfile::LogOfferEntry::QUALITY_descriptor()->FindValueByNumber(q)->name() + ")";
+    }
+
+    bool operator<(const LogEntry& other) const
+    {
+        int q1 = mapQuality(q);
+        int qO = mapQuality(other.q);
+        if (q1 < qO)
+            return true;
+        if (q1 > qO)
+            return false;
+        return name < other.name;
+
+    }
+};
+
 void LogFileSelectionDialog::setListContent(const logfile::LogOffer &l)
 {
     ui->list->clear();
     paths = {};
-    const auto& qualityDescriptor = logfile::LogOfferEntry::QUALITY_descriptor();
+    std::vector<LogEntry> items;
+    items.reserve(l.entries_size());
     for (const auto& item : l.entries()) {
-        ui->list->addItem(QString::fromStdString(item.name() + "(" + qualityDescriptor->FindValueByNumber(item.quality())->name() + ")"));
-        paths.append(QString::fromStdString(item.uri().path()));
+        items.emplace_back(item.uri().path(), item.name(), item.quality());
+    }
+    std::sort(items.begin(), items.end());
+
+    for(const auto& item: items) {
+         ui->list->addItem(QString::fromStdString(item.toString()));
+         paths.append(QString::fromStdString(item.path));
     }
 }
 
