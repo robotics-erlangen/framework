ff78df7df6ffb5e03d5b73d02bacb03f890678f0
Andreas Wendler <andreas.wendler@robotics-erlangen.de>
2020-12-27 21:59:21 +0100

    path/trajectorycli: add way to declare constants to be optimized by the trajectory-cli
    The targets for this were as follows:
    * The definition of a new parameter should be as easy as possible and
    remaing in a single file
    * In actual usage with ra, the generated code should be the as if the
    constant were just inserted directly into the code (meaning macros have
    to be used). This is quite important since some of these constants
    reside in the innermost optimization loops, even array lookups might
    incur noticable cost
    
    While this solution is slightly clunky and occurs some performance
    penalties during parameter optimization, it should work quite well.
    Tradeoffs are:
    * the optimization must be performed single-threadedly, since a
    singleton is used
    * There must only ever be one parameter declaration per line in each
    file

---
+++ src/amun/strategy/path/include/path/parameterization.h
@@ -0,0 +1,99 @@
+/***************************************************************************
+ *   Copyright 2020 Andreas Wendler                                        *
+ *   Robotics Erlangen e.V.                                                *
+ *   http://www.robotics-erlangen.de/                                      *
+ *   info@robotics-erlangen.de                                             *
+ *                                                                         *
+ *   This program is free software: you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation, either version 3 of the License, or     *
+ *   any later version.                                                    *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ ***************************************************************************/
+
+#pragma once
+
+#include <vector>
+#include <map>
+#include <string>
+#include <iostream>
+
+
+#ifndef ACTIVE_PATHFINDING_PARAMETER_OPTIMIZATION
+
+// every parameter MUST be defined in a separate line
+#define PARAMETER(category, rangeMin, currentValue, rangeMax) currentValue
+
+#else
+
+enum ParameterCategory {
+    AlphaTimeTrajectoryParameter,
+    EndInObstacleSamplerParameter,
+    None
+};
+
+#define PARAMETER(category, rangeMin, currentValue, rangeMax) DynamicSearchParameters::getParameter(category ## Parameter, rangeMin, currentValue, rangeMax, __FILE__, __LINE__, __COUNTER__)
+
+// file in which the paramter is defined and the corresponding line in that file
+using ParameterIdentifier = std::pair<const char*, int>;
+
+struct ParameterDefinition {
+    float rangeMin, rangeMax;
+    float defaultValue;
+    ParameterIdentifier identifier;
+    int counter;
+};
+
+
+// a singleton class providing the search parameters while they are being optimized
+// must only be used in single-threaded execution, not during normal usage of ra
+class DynamicSearchParameters {
+public:
+    DynamicSearchParameters(const DynamicSearchParameters &other) = delete;
+    DynamicSearchParameters(DynamicSearchParameters &&other) = delete;
+    void operator=(const DynamicSearchParameters &other) = delete;
+    void operator=(DynamicSearchParameters &&other) = delete;
+
+    inline static float getParameter(ParameterCategory category, float rangeMin, float currentDefault, float rangeMax, const char* file, int line, int counter) {
+        if (category != instance.m_currentlyOptimizing) {
+            return currentDefault;
+        }
+        return getParameterRegistering(rangeMin, currentDefault, rangeMax, file, line, counter);
+    }
+
+    static float getParameterRegistering(float rangeMin, float currentDefault, float rangeMax, const char* file, int line, int counter);
+
+    static void setParameters(const std::vector<std::pair<ParameterIdentifier, float>> &parameters) {
+        instance.m_parameters = parameters;
+    }
+
+    static void beginRegistering(ParameterCategory toOptimize) {
+        instance.m_currentlyRegistering = true;
+        instance.m_currentlyOptimizing = toOptimize;
+    }
+
+    static std::vector<ParameterDefinition> stopRegistering() {
+        instance.m_currentlyRegistering = false;
+        return instance.m_parameterDefinitions;
+    }
+
+private:
+    DynamicSearchParameters() = default;
+
+    static DynamicSearchParameters instance;
+
+    std::vector<std::pair<ParameterIdentifier, float>> m_parameters;
+
+    bool m_currentlyRegistering = false;
+    ParameterCategory m_currentlyOptimizing = ParameterCategory::None;
+    std::vector<ParameterDefinition> m_parameterDefinitions;
+};
+
+#endif
---
+++ src/amun/strategy/path/parameterization.cpp
@@ -0,0 +1,58 @@
+/***************************************************************************
+ *   Copyright 2020 Andreas Wendler                                        *
+ *   Robotics Erlangen e.V.                                                *
+ *   http://www.robotics-erlangen.de/                                      *
+ *   info@robotics-erlangen.de                                             *
+ *                                                                         *
+ *   This program is free software: you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation, either version 3 of the License, or     *
+ *   any later version.                                                    *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ ***************************************************************************/
+
+#include "parameterization.h"
+
+#ifdef ACTIVE_PATHFINDING_PARAMETER_OPTIMIZATION
+DynamicSearchParameters DynamicSearchParameters::instance;
+
+float DynamicSearchParameters::getParameterRegistering(float rangeMin, float currentDefault, float rangeMax, const char* file, int line, int counter)
+{
+    ParameterIdentifier id(file, line);
+    if (instance.m_currentlyRegistering) {
+        for (const auto &def : instance.m_parameterDefinitions) {
+            if (def.identifier == id) {
+                if (def.counter != counter) {
+                    std::cerr <<"ERROR: Two paramters must not be defined in the same line!!"<<std::endl;
+                    exit(1);
+                }
+                return currentDefault;
+            }
+        }
+        ParameterDefinition definition;
+        definition.rangeMin = rangeMin;
+        definition.rangeMax = rangeMax;
+        definition.defaultValue = currentDefault;
+        definition.identifier = id;
+        definition.counter = counter;
+        instance.m_parameterDefinitions.push_back(definition);
+        return currentDefault;
+    } else {
+        for (const auto &parameter : instance.m_parameters) {
+            if (parameter.first == id) {
+                return parameter.second;
+            }
+        }
+        // should not happen if the parameters are properly collected and registered
+        return currentDefault;
+    }
+}
+
+#endif
