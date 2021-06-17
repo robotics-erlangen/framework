95e3afdb9677ccb894f79ee8796f4f752870b01f
Tobias Heineken <tobias.heineken@robotics-erlangen.de>
2021-06-17 19:34:20 +0200

    glados: g/m/ballplacement: remove SHOOTER_OBSTACLES during GET_INTO_POSITION
    The very large obstacle is something pathfinding will not reliable find a way around

--- strategy/typescript/glados/group/move/ballplacement.ts
+++ strategy/typescript/glados/group/move/ballplacement.ts
@@ -199,7 +199,6 @@ export class BallPlacement extends Move {
 					params: [{
 						pos: this._computedShooterPos,
 						ignoreDefaultObstacles: true,
-						customObstacles: SHOOTER_OBSTACLES,
 						ignoreBallPlacement: true,
 					}],
 					restart: true
