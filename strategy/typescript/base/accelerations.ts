775974f0586268d7fd9843f24917e0dde7945819
Tobias Heineken <tobias.heineken@robotics-erlangen.de>
2021-06-21 14:22:48 +0200

    base: accelerations: Add a new file for accelerations

--- strategy/typescript/base/accelerations.ts
+++ strategy/typescript/base/accelerations.ts
@@ -17,22 +17,88 @@
 *   You should have received a copy of the GNU General Public License     *
 *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
 **************************************************************************/
-export interface RobotAccelerationProfile {
-	aSpeedupFMax: number;
-	aSpeedupSMax: number;
-	aSpeedupPhiMax: number;
-	aBrakeFMax: number;
-	aBrakeSMax: number;
-	aBrakePhiMax: number;
-}
+import { RobotAccelerationProfile } from "base/trajectory";
 
-/** Interface describing basic movement information of a team. */
 export interface RobotSpecs {
 	profile: RobotAccelerationProfile;
 	vmax: number;
 	vangular: number;
 }
 
-/** This map is meant to be filled with the measured RobotSpecs of opponent teams during tournaments. */
 export let accelerationsByTeam: Map<string, RobotSpecs> = new Map();
 
+accelerationsByTeam["RoboIME"] = { profile: {
+	aSpeedupFMax: 6.3,
+	aSpeedupSMax: 6.3,
+	aSpeedupPhiMax: 60,
+	aBrakeFMax: 7.3,
+	aBrakeSMax: 7.3,
+	aBrakePhiMax: 60
+},
+	vmax: 4.2,
+	vangular: 20,
+};
+
+accelerationsByTeam["RoboTeam Twente"] = {
+	profile: {
+		aSpeedupFMax: 5.9,
+		aSpeedupSMax: 5.9,
+		aSpeedupPhiMax: 55,
+		aBrakeFMax: 7,
+		aBrakeSMax: 7,
+		aBrakePhiMax: 50},
+	vmax: 3.8,
+	vangular: 4.8,
+};
+
+accelerationsByTeam["TIGERs Mannheim"] = {
+	profile: {
+		aSpeedupFMax: 3.5,
+		aSpeedupSMax: 3.5,
+		aSpeedupPhiMax: 55,
+		aBrakeFMax: 3.5,
+		aBrakeSMax: 3.5,
+		aBrakePhiMax: 55
+	},
+	vmax: 4.5,
+	vangular: 25
+};
+
+accelerationsByTeam["RobôCIn"] = {
+	profile: {
+		aSpeedupFMax: 4.9,
+		aSpeedupSMax: 4.9,
+		aSpeedupPhiMax: 55,
+		aBrakeFMax: 6,
+		aBrakeSMax: 6,
+		aBrakePhiMax: 55
+	},
+	vmax: 3.5,
+	vangular: 25
+};
+
+accelerationsByTeam["RoboFEI"] = {
+	profile: {
+		aSpeedupFMax: 4.9,
+		aSpeedupSMax: 4.9,
+		aSpeedupPhiMax: 55,
+		aBrakeFMax: 6,
+		aBrakeSMax: 6,
+		aBrakePhiMax: 55
+	},
+	vmax: 3.5,
+	vangular: 25
+};
+
+accelerationsByTeam["UBC Thunderbots"] = {
+	profile: {
+		aSpeedupFMax: 3.3,
+		aSpeedupSMax: 3.3,
+		aSpeedupPhiMax: 38.6,
+		aBrakeFMax: 3.3,
+		aBrakeSMax: 3.3,
+		aBrakePhiMax: 38.6,
+	},
+	vmax: 4.83,
+	vangular: 56.8,
+};
