b686ae4c659dbc00b74e259b08a9ce91f5b28dc0
Paul Bergmann <paul.bergmann@robotics-erlangen.de>
2021-06-22 22:20:54 +0200

    glados: u/volley: Change damping parameters used during simulation
    ~Magic~ experiments have shown these values to be better in simulated
    games.

--- strategy/typescript/glados/util/volley.ts
+++ strategy/typescript/glados/util/volley.ts
@@ -20,7 +20,7 @@ interface DampingFactor {
  */
 const DEFAULT_DAMPING_FACTOR = (): Readonly<DampingFactor> =>
 	World.WorldStateSource !== pb.world.WorldSource.REAL_LIFE
-		? { mu_x: 0.93, mu_y: 0.26 }
+		? { mu_x: 1.10, mu_y: 0.0 }
 		: { mu_x: 0.70, mu_y: 0.05 };
 
 type DampingIndex = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | "opp";
