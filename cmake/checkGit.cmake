da7ba3b0895f1ec0331df756ac7642856a4e2256
Tobias Heineken <tobias.heineken@robotics-erlangen.de>
2021-07-13 21:46:18 +0200

    cmake: add new library to retrieve compiletime git information

--- cmake/checkGit.cmake
+++ cmake/checkGit.cmake
@@ -1,5 +1,5 @@
 # ***************************************************************************
-# *   Copyright 2015 Michael Bleier, Philipp Nordhus                        *
+# *   Copyright 2021 Tobias Heineken                                        *
 # *   Robotics Erlangen e.V.                                                *
 # *   http://www.robotics-erlangen.de/                                      *
 # *   info@robotics-erlangen.de                                             *
@@ -18,26 +18,31 @@
 # *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
 # ***************************************************************************
 
-# firmware
-add_subdirectory(firmware)
 
-# libs
-add_subdirectory(config)
-add_subdirectory(git)
-add_subdirectory(core)
-add_subdirectory(protobuf)
-add_subdirectory(amun)
-add_subdirectory(visionlog)
-add_subdirectory(timeline)
+# This cmake file is build as a script that is triggered by src/config/CMakeLists.txt
+# to make sure the git-stuff is not done at configure time, but for every build time.
+# It requires the following Variables:
+# TARGET (The location of the file after processing), GIT_BASE_DIR (CMAKE_SOURCE_DIR), SOURCE (The file to be processed), STAMP (The file to be touched)
 
-# apps
-add_subdirectory(ra)
-add_subdirectory(logplayer)
-add_subdirectory(amuncli)
-add_subdirectory(replaycli)
-add_subdirectory(visionanalyzer)
-add_subdirectory(logcuttercli)
-add_subdirectory(loganalyzer)
-add_subdirectory(trajectorycli)
-add_subdirectory(tests)
-add_subdirectory(simulator)
+# It created the following file:
+# TARGET
+
+# It touches the following file: (to be run again next time even if no file has changed)
+# STAMP
+
+execute_process(WORKING_DIRECTORY ${GIT_BASE_DIR}
+    OUTPUT_VARIABLE GIT_DIFF
+    COMMAND git diff-files -p --no-color ${GIT_BASE_DIR}/src
+)
+
+execute_process(WORKING_DIRECTORY ${GIT_BASE_DIR}
+    OUTPUT_VARIABLE GIT_COMMIT
+    COMMAND git rev-parse HEAD
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+
+string(REGEX REPLACE "\n" "\\\\n" GIT_DIFF "${GIT_DIFF}")
+
+configure_file(${SOURCE} ${TARGET} ESCAPE_QUOTES)
+
+execute_process(COMMAND ${CMAKE_COMMAND} -E touch ${STAMP})
