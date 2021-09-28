0c9cdc9b53e73659b9bc9dc2399cfdddfee6f247
Michel Schmid <michel.schmid@robotics-erlangen.de>
2021-09-28 17:17:44 +0200

    glados:hwchallenges: add experimental new dribble task for hw challenge

--- strategy/typescript/glados/group/move/hardwarechallenges/dribblechallenge.ts
+++ strategy/typescript/glados/group/move/hardwarechallenges/dribblechallenge.ts
@@ -1,26 +1,154 @@
+import * as debug from "base/debug";
 import { FriendlyRobot } from "base/robot";
-import { Vector } from "base/vector";
+import { Position, Vector } from "base/vector";
+import * as World from "base/world";
 
 import { MessageBox } from "glados/control/messaging";
 import { Assignment, MoveParameters } from "glados/group/move/base";
 import { HardwareChallengeBase } from "glados/group/move/hardwarechallenges/base";
 import * as Scenarios from "glados/group/move/hardwarechallenges/scenarios";
-import { MoveToPos } from "glados/task/shared/movetopos";
+import * as BallObserver from "glados/observer/ball";
+import { DribbleToPos } from "glados/task/shared/dribbletopos";
+import { Obstacle } from "glados/task/shared/movetopos";
 
 export class DribbleChallenge extends HardwareChallengeBase {
 	protected challengeNumber: 1 | 2 | 3 | 4 | undefined = 3;
 
+	private static gates: [Position, Vector][] = [];
+	private static currentGateIndex: number = 0;
+	private static moveToMidwayPos: boolean = true;
+	private static nextGateImminent: boolean = false;
+	private static currentTargetPosition: Position = new Vector(0, 0);
+	private customObstacles: Obstacle[];
+	private static lastGate: number = -1;
+	private left: boolean = false;
+
 	constructor(robots: FriendlyRobot[], messaging: MessageBox) {
 		super(robots, messaging, Scenarios.challenge3);
+
+		for (let i = this.opponentTransforms.length - 1; i > 0; --i) {
+			const posA = this.opponentTransforms[i].pos;
+			const posB = this.opponentTransforms[i - 1].pos;
+			const relativePosition = posB - posA;
+			DribbleChallenge.gates[this.opponentTransforms.length - i - 1] = [posA, relativePosition];
+		}
+
+		this.customObstacles = this.opponentTransforms.map((transform) => {
+			let name: string = "dribbleObstacleY" + transform.pos.x;
+			return {
+				type: "circle",
+				x: transform.pos.x,
+				y: transform.pos.y,
+				radius: this._robots[0].radius * 1.1,
+				name: name
+			};
+		});
+
+		DribbleChallenge.currentTargetPosition = DribbleChallenge.moveToMidwayPos ? this.getMidwayPosition() : this.getCurrentGatePosition();
+	}
+
+	private getCurrentGatePosition(): Position {
+		const currentGate = DribbleChallenge.gates[DribbleChallenge.currentGateIndex];
+		return currentGate[0] + 0.5 * currentGate[1];
+	}
+
+	private getNextGateIndex(): number {
+		// if currentGate is the last gate return currentGate as the nextGate
+		return Math.min(DribbleChallenge.gates.length - 1, DribbleChallenge.currentGateIndex + 1);
+	}
+
+	private getNextGatePosition(): Position {
+		const nextGate = DribbleChallenge.gates[this.getNextGateIndex()];
+		return nextGate[0] + 0.5 * nextGate[1];
+	}
+
+	private getMidwayPosition(): Position {
+		const currentGate = DribbleChallenge.gates[DribbleChallenge.currentGateIndex];
+		const yOffset = 0.5;
+		if (this.left) {
+			return currentGate[0] + new Vector(0, -yOffset);
+		} else {
+			return currentGate[0] + new Vector(0, yOffset);
+		}
+	}
+
+	private getNextMidwayPosition(): Position {
+		const currentGate = DribbleChallenge.gates[this.getNextGateIndex()];
+		const yOffset = 0.5;
+		if (this.left) {
+			return currentGate[0] + new Vector(0, -yOffset);
+		} else {
+			return currentGate[0] + new Vector(0, yOffset);
+		}
+	}
+
+	private getBallPosition(): Position {
+		if (World.Ball.isPositionValid()) {
+			return BallObserver.getRealisticBallPos();
+		} else {
+			return this._robots[0].pos + Vector.fromAngle(this._robots[0].dir) * (this._robots[0].radius - World.Ball.radius);
+		}
+	}
+
+	private checkIfGatePassed(): boolean {
+		const gatePosition = this.getCurrentGatePosition();
+		let ballPosition: Position = this.getBallPosition();
+		const currentGate = DribbleChallenge.gates[DribbleChallenge.currentGateIndex];
+		const betweenObstacles = ballPosition.x > currentGate[0].x && ballPosition.x < (currentGate[0] + currentGate[1]).x;
+		const crossingThreshold = World.Ball.radius + 0.05;
+		let crossed = false;
+		if (this.left) {
+			crossed = ballPosition.y - gatePosition.y < -crossingThreshold;
+		} else {
+			crossed = ballPosition.y - gatePosition.y > crossingThreshold;
+		}
+		return betweenObstacles && crossed;
+	}
+
+	// returns true if position changed
+	private updateTargetPosition(): boolean {
+		if (DribbleChallenge.moveToMidwayPos) {
+			if (this._robots[0].pos.x - DribbleChallenge.currentTargetPosition.x > -(0.01 + this._robots[0].radius)) {
+				DribbleChallenge.moveToMidwayPos = false;
+				DribbleChallenge.currentTargetPosition = this.getCurrentGatePosition();
+
+				return true;
+			}
+		} else {
+			if ((DribbleChallenge.currentTargetPosition - this._robots[0].pos).length() < 0.1) {
+				DribbleChallenge.moveToMidwayPos = true;
+				DribbleChallenge.currentTargetPosition = this.getNextMidwayPosition();
+
+				return true;
+			}
+		}
+		return false;
 	}
 
 	public challengeSpecificUpdateTask(): MoveParameters {
 		let taskAssignments = new Map<FriendlyRobot, Assignment>();
 
+		let restart = false;
+
+		if (this.checkIfGatePassed()) {
+			DribbleChallenge.currentGateIndex = this.getNextGateIndex();
+			restart = true;
+		}
+
+		if (this.left && this.getBallPosition().y - this.getCurrentGatePosition().y < -0.1) {
+			this.left = false;
+		} else if (!this.left && this.getBallPosition().y - this.getCurrentGatePosition().y > 0.1) {
+			this.left = true;
+		}
+
+		amun.log("current gate", DribbleChallenge.currentGateIndex);
+
+		restart = restart || this.updateTargetPosition();
+
 		taskAssignments[this._robots[0]] = Assignment.create({
-			class: MoveToPos,
-			params: [{pos: new Vector(5, 4)}],
-			restart: true
+			class: DribbleToPos,
+			params: [{pos: DribbleChallenge.currentTargetPosition, customObstacles: this.customObstacles, useCMA: true}],
+			restart: restart
 		});
 
 		return {assignments: taskAssignments};
---
+++ strategy/typescript/glados/task/shared/dribbletopos.ts
@@ -0,0 +1,128 @@
+import { Position } from "base/vector";
+import * as World from "base/world";
+
+import { Behavior } from "glados/agent/base/behavior";
+import * as BallObserver from "glados/observer/ball";
+import { Task } from "glados/task/base";
+import { Obstacle } from "glados/task/shared/movetopos";
+import { CurvedMaxAccel } from "glados/trajectory/curvedmaxaccel";
+import * as PathHelper from "glados/trajectory/pathhelper";
+import { ToTarget } from "glados/trajectory/totarget";
+
+export type Parameters = {
+	pos: Position,
+	dir?: number,
+	endSpeedLength?: number,
+	customObstacles?: Obstacle[],
+	ignoreDefaultObstacles?: boolean,
+	ignoreBallPlacement?: boolean
+	ignoreBall?: boolean,
+	useCMA?: boolean
+};
+
+export class DribbleToPos extends Task {
+	private pos: Position;
+	private dir: number;
+	private endSpeedLength: number;
+	private obstacleTable: PathHelper.PathHelperParameters;
+	private customObstacles: Obstacle[];
+	private useCMA: boolean;
+
+	private currentlyDribbling: boolean = false;
+
+	// customObstacles is a table of obstacle tables
+	// An obstacle table contains a string field called type and parameters relevant for Path:addX
+	// Type can be "circle", "line", "rect" and "triangle"
+	constructor(behavior: Behavior, params: Parameters) {
+		super(behavior);
+
+		this.pos = params.pos;
+
+		if (params.dir === undefined) {
+			params.dir = (World.Ball.pos - params.pos).angle();
+		}
+		this.dir = params.dir;
+		if (params.endSpeedLength === undefined) {
+			params.endSpeedLength = 0;
+		}
+		this.endSpeedLength = params.endSpeedLength;
+		if (params.customObstacles === undefined) {
+			params.customObstacles = [];
+		}
+		this.customObstacles = params.customObstacles;
+
+		const ignore = params.ignoreDefaultObstacles === true;
+		this.obstacleTable = {
+			ignoreBall: ignore || params.ignoreBall === true,
+			ignoreDefenseArea: ignore,
+			ignoreOpponentDefenseArea: ignore,
+			task: this,
+			ignorePass: this._messaging == undefined || ignore,
+			ignoreBallPlacementObstacle: params.ignoreBallPlacement === true
+		};
+
+		this.useCMA = params.useCMA === true;
+
+		// let offset = this._robot.shootRadius + World.Ball.radius + 0.1;
+	}
+
+	public run() {
+		let targetPos = this.pos;
+
+		let ballDirection = BallObserver.getRealisticBallPos() - this._robot.pos;
+		const distanceToBall = ballDirection.length() - this._robot.radius + World.Ball.radius;
+		ballDirection = ballDirection.normalized();
+		const targetDirection = (targetPos - this._robot.pos).normalized();
+
+		const ballAngle = targetDirection.dot(ballDirection);
+
+		if (!World.Ball.isPositionValid() || (distanceToBall < 0.025 && ballAngle > 0.95)) {
+			this.currentlyDribbling = true;
+		} else {
+			this.currentlyDribbling = false;
+		}
+
+		if (!this.currentlyDribbling) {
+			if (World.Ball.isPositionValid()) {
+				this.dir = ballDirection.angle();
+				this.obstacleTable.ignoreBall = ballAngle > 0.8;
+			} else {
+				this.obstacleTable.ignoreBall = true;
+			}
+			let directionGateToBall = (BallObserver.getRealisticBallPos() - this.pos).normalized();
+			targetPos = BallObserver.getRealisticBallPos() + this._robot.radius * directionGateToBall;
+			this._robot.setDribblerSpeed(0.8);
+		} else {
+			// this.dir = Math.PI + (this._robot.pos - this.pos).angle();
+			this.dir = this._robot.speed.angle();
+		}
+
+		PathHelper.setDefaultObstaclesByTable(this._robot.path, this._robot, this.obstacleTable);
+
+		for (let obstacle of this.customObstacles) {
+			this._addCustomObstacle(obstacle);
+		}
+
+		let maxSpeed = 0.2;
+		let endSpeed = (this.pos - this._robot.pos).withLength(this.endSpeedLength);
+		if (this.useCMA) {
+			this._robot.trajectory.update(CurvedMaxAccel, targetPos, this.dir, maxSpeed, endSpeed);
+		} else {
+			this._robot.trajectory.update(ToTarget, targetPos, this.dir, maxSpeed, endSpeed);
+		}
+	}
+
+	private _addCustomObstacle(obstInfo: Obstacle) {
+		let path = this._robot.path;
+		// If this gets changed, the comment before _init also needs to be updated
+		if (obstInfo.type === "circle") {
+			path.addCircle(obstInfo.x, obstInfo.y, obstInfo.radius, obstInfo.name);
+		} else if (obstInfo.type === "line") {
+			path.addLine(obstInfo.start_x, obstInfo.start_y, obstInfo.end_x, obstInfo.end_y, obstInfo.radius, obstInfo.name);
+		} else if (obstInfo.type === "rect") {
+			path.addRect(obstInfo.start_x, obstInfo.start_y, obstInfo.end_x, obstInfo.end_y, obstInfo.radius, obstInfo.name);
+		} else if (obstInfo.type === "triangle") {
+			path.addTriangle(obstInfo.x1, obstInfo.y1, obstInfo.x2, obstInfo.y2, obstInfo.x3, obstInfo.y3, obstInfo.lineWidth, obstInfo.name);
+		}
+	}
+}
