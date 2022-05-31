ef60154b09f8a929caf7175a48a0ebd8908d863a
Andreas Wendler <andreas.wendler@robotics-erlangen.de>
2022-05-31 18:35:05 +0200

    amun: processor/networktransceiver: simulation_limits can not be converted

--- src/amun/processor/networktransceiver.cpp
+++ src/amun/processor/networktransceiver.cpp
@@ -181,9 +181,9 @@ static void convertSpecs(const robot::Specs& in, T outGen, bool blueTeam, bool* 
         rsef.set_dribbler_height(in.dribbler_height());
         useSpecialFields = true;
     }
+    // ignore simulation_limits
     if (useSpecialFields) {
         out->add_custom()->PackFrom(rsef);
-
     }
     *success = true;
 }
@@ -245,7 +245,7 @@ void NetworkTransceiver::handleCommand(const Command &command)
     }
     static bool sendMessage = true;
     if (sendMessage) {
-        const int highest_field = 20;
+        const int highest_field = 21;
         const int expected_fields = highest_field - 1;
         auto* desc = robot::Specs::descriptor();
         int real_fields = desc->field_count();
