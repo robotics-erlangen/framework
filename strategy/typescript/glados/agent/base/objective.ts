a93c76a5ffdcb7e99fad8064be51263df5075c7e
Michel Schmid <michel.schmid@robotics-erlangen.de>
2022-06-16 18:37:36 +0200

    glados:supporterzones: refactor zone division
    Do more in the base class for shared code in future objectives that use
    similar heuristics.

--- strategy/typescript/glados/agent/base/objective.ts
+++ strategy/typescript/glados/agent/base/objective.ts
@@ -1,11 +1,12 @@
+import * as geom from "base/geom";
 import * as Referee from "base/referee";
 import { FriendlyRobot } from "base/robot";
-import { Position } from "base/vector";
+import { Position, Vector } from "base/vector";
 import * as World from "base/world";
 
 import { Agent } from "glados/agent/base/agent";
 import { Checkable, CheckableConstructor } from "glados/agent/base/behavior";
-import { Zone } from "glados/util/zone";
+import { Boundaries, getRandomPosition, Zone } from "glados/util/zone";
 
 /** Different checkables to be used by the main and support attackers */
 export interface RunnerCtors {
@@ -16,6 +17,11 @@ export interface RunnerCtors {
 	/** To be used by support attackers */
 	support: CheckableConstructor;
 }
+
+	interface SplitZone {
+		boundaries: Boundaries;
+		splitCount: number;
+	}
 
 /**
  * The base class for objectives.
@@ -79,6 +85,148 @@ export abstract class Objective {
 
 	toString() {
 		return this._toString();
+	}
+
+	protected splitZonesClosestToMainAttacker(mainAttackerPos: Position, remainingZones: number, startBoundaries: Boundaries[]): Zone[] {
+		const DEFENSE_AREA_CUT_OFF_Y = World.Geometry.FieldHeightHalf - World.Geometry.DefenseHeight;
+		const LOWER_DEFENSE_AREA_CUT_OFF_X = -World.Geometry.DefenseWidthHalf;
+		const UPPER_DEFENSE_AREA_CUT_OFF_X = World.Geometry.DefenseWidthHalf;
+
+		let boundaryList: SplitZone[] = [];
+		if (remainingZones > 0) {
+			let startSplitCount = Math.floor(startBoundaries.length / 2);
+			boundaryList = startBoundaries.map((boundaries) => {
+					return { boundaries, splitCount: startSplitCount }
+				});
+
+			/* The basic idea of the following is to split the zones repeatedly.
+			 * The zones split next is the one closest to the main attacker, unless it already has been split MAX_SPLIT times,
+			 * to keep the zones at a useful size.
+			 * If the zone contains the defense area lines, split it along those lines, otherwise split it in the middle.
+			 * In the case one of the resulting zones contains the defense area and can be split into two new zones a way that
+			 * one zone lies completely inside the defense area and the other one completely outside the defense area do this
+			 * and only keep the zone outside the defense area. */
+			for (let i = 1; i < remainingZones; ++i) {
+				const boundaryIndex = this.findClosestZoneToMainAttacker(mainAttackerPos, boundaryList);
+
+				const currentSplitZone = boundaryList[boundaryIndex];
+				const currentBoundary = currentSplitZone.boundaries;
+				const splitCount = currentSplitZone.splitCount;
+				const newSplitCount = splitCount + 1;
+				const rightLeft = currentBoundary.right - currentBoundary.left;
+				const topBottom = currentBoundary.top - currentBoundary.bottom;
+				const rightLeftBigger = Math.abs(rightLeft) > Math.abs(topBottom);
+				// choose splitting axis
+				if (rightLeftBigger) {
+					let newBoundaryMidRightLeft: number;
+					if (currentBoundary.top > DEFENSE_AREA_CUT_OFF_Y
+						&& currentBoundary.right > UPPER_DEFENSE_AREA_CUT_OFF_X
+						&& currentBoundary.left < UPPER_DEFENSE_AREA_CUT_OFF_X) {
+
+						// check if whole defense area is inside boundaries
+						if (currentBoundary.left < LOWER_DEFENSE_AREA_CUT_OFF_X) {
+							const mainAttackerPosToLowerDefenseArea = Math.abs(mainAttackerPos.x - LOWER_DEFENSE_AREA_CUT_OFF_X);
+							const mainAttackerPosToUpperDefenseArea = Math.abs(mainAttackerPos.x - UPPER_DEFENSE_AREA_CUT_OFF_X);
+							if (mainAttackerPosToLowerDefenseArea < mainAttackerPosToUpperDefenseArea) {
+								newBoundaryMidRightLeft = LOWER_DEFENSE_AREA_CUT_OFF_X;
+							} else {
+								newBoundaryMidRightLeft = UPPER_DEFENSE_AREA_CUT_OFF_X;
+							}
+						} else {
+							newBoundaryMidRightLeft = UPPER_DEFENSE_AREA_CUT_OFF_X;
+						}
+					} else if (currentBoundary.top > DEFENSE_AREA_CUT_OFF_Y
+								&& currentBoundary.right > LOWER_DEFENSE_AREA_CUT_OFF_X
+								&& currentBoundary.left < LOWER_DEFENSE_AREA_CUT_OFF_X) {
+						newBoundaryMidRightLeft = LOWER_DEFENSE_AREA_CUT_OFF_X;
+					} else {
+						newBoundaryMidRightLeft = rightLeft / 2 + currentBoundary.left;
+					}
+
+					const boundaryContainsDefenseArea = currentBoundary.top > DEFENSE_AREA_CUT_OFF_Y
+						&& currentBoundary.bottom < DEFENSE_AREA_CUT_OFF_Y;
+					// check if the new boundary can be reduced to not contain the defense area
+					const cutoutDefenseArea0 = boundaryContainsDefenseArea
+						&& newBoundaryMidRightLeft === UPPER_DEFENSE_AREA_CUT_OFF_X
+						&& currentBoundary.left === LOWER_DEFENSE_AREA_CUT_OFF_X;
+					const newBoundary0Top = cutoutDefenseArea0 ? DEFENSE_AREA_CUT_OFF_Y : currentBoundary.top;
+
+					const newBoundary0 = { boundaries: { left: currentBoundary.left, right: newBoundaryMidRightLeft,
+						top: newBoundary0Top, bottom: currentBoundary.bottom }, splitCount: newSplitCount };
+
+					// check if the new boundary can be reduced to not contain the defense area
+					const cutoutDefenseArea1 = boundaryContainsDefenseArea
+						&& currentBoundary.right === UPPER_DEFENSE_AREA_CUT_OFF_X
+						&& newBoundaryMidRightLeft === LOWER_DEFENSE_AREA_CUT_OFF_X;
+					const newBoundary1Top = cutoutDefenseArea1 ? DEFENSE_AREA_CUT_OFF_Y : currentBoundary.top;
+					const newBoundary1 = { boundaries: { left: newBoundaryMidRightLeft, right: currentBoundary.right,
+						top: newBoundary1Top, bottom: currentBoundary.bottom }, splitCount: newSplitCount };
+
+					boundaryList[boundaryIndex] = newBoundary0;
+					boundaryList.push(newBoundary1);
+				} else {
+					const containsDefenseArea = currentBoundary.top > DEFENSE_AREA_CUT_OFF_Y
+						&& currentBoundary.bottom < DEFENSE_AREA_CUT_OFF_Y;
+					const newBoundaryTopBottom = containsDefenseArea ? DEFENSE_AREA_CUT_OFF_Y : topBottom / 2 + currentBoundary.bottom;
+					const newBoundary0 = { boundaries: { left: currentBoundary.left, right: currentBoundary.right,
+						top: newBoundaryTopBottom, bottom: currentBoundary.bottom }, splitCount: newSplitCount };
+
+					// check if the new boundary can be reduced to not contain the defense area
+					const newBoundary1ContainsDefenseAreaUpper = currentBoundary.left > UPPER_DEFENSE_AREA_CUT_OFF_X
+						&& currentBoundary.right < UPPER_DEFENSE_AREA_CUT_OFF_X;
+					const newBoundary1ContainsDefenseAreaLower = currentBoundary.left > LOWER_DEFENSE_AREA_CUT_OFF_X
+						&& currentBoundary.right < LOWER_DEFENSE_AREA_CUT_OFF_X;
+					const newBoundary1Left: number = newBoundary1ContainsDefenseAreaUpper
+						? UPPER_DEFENSE_AREA_CUT_OFF_X
+						: currentBoundary.left;
+					const newBoundary1Right: number = newBoundary1ContainsDefenseAreaLower
+						? LOWER_DEFENSE_AREA_CUT_OFF_X
+						: currentBoundary.right;
+					const newBoundary1 = { boundaries: { left: newBoundary1Left, right: newBoundary1Right,
+						top: currentBoundary.top, bottom: newBoundaryTopBottom }, splitCount: newSplitCount };
+
+					boundaryList[boundaryIndex] = newBoundary0;
+					boundaryList.push(newBoundary1);
+				}
+			}
+		}
+
+		const newZones = boundaryList.map((splitZone) => {
+			const defaultPos = getRandomPosition(splitZone.boundaries);
+			return { boundaries: splitZone.boundaries, defaultPos };
+		});
+
+		return newZones;
+	}
+
+	protected findClosestZoneToMainAttacker(mainAttackerPos: Position, boundaryList: SplitZone[]): number {
+		const MAX_SPLIT = 3;
+		let minDist = Infinity;
+		let boundaryIndex: number = 0;
+		for (let j = 0; j < boundaryList.length; ++j) {
+			const splitZone = boundaryList[j];
+			if (splitZone.splitCount >= MAX_SPLIT) {
+				continue;
+			}
+			const corner1 = new Vector(splitZone.boundaries.left, splitZone.boundaries.bottom);
+			const corner2 = new Vector(splitZone.boundaries.right, splitZone.boundaries.top);
+			// if zone contains mainAttackerPos it's obviously always the closest
+			if (geom.insideRect(corner1, corner2, mainAttackerPos)) {
+				boundaryIndex = j;
+				break;
+			}
+
+			const distX = Math.min(Math.abs(mainAttackerPos.x - splitZone.boundaries.right),
+									Math.abs(mainAttackerPos.x - splitZone.boundaries.left));
+			const distY = Math.min(Math.abs(mainAttackerPos.y - splitZone.boundaries.top),
+									Math.abs(mainAttackerPos.y - splitZone.boundaries.bottom));
+			const dist = Math.min(distX, distY);
+			if (dist < minDist) {
+				minDist = dist;
+				boundaryIndex = j;
+			}
+		}
+		return boundaryIndex;
 	}
 }
 
--- strategy/typescript/glados/agent/objective/striker.ts
+++ strategy/typescript/glados/agent/objective/striker.ts
@@ -1,7 +1,6 @@
-import * as geom from "base/geom";
 import { FriendlyRobot } from "base/robot";
 import { parameterizeClass } from "base/types";
-import { Position, Vector } from "base/vector";
+import { Position } from "base/vector";
 import * as World from "base/world";
 
 import { DoubleTouchGuard } from "glados/agent/attacker/doubletouchguard";
@@ -12,17 +11,11 @@ import { Support } from "glados/agent/attacker/support";
 import { Agent } from "glados/agent/base/agent";
 import { CheckableList } from "glados/agent/base/behavior";
 import { BallLike, Objective } from "glados/agent/base/objective";
-import { MessageType } from "glados/control/messaging";
 import { StrikerSampling } from "glados/task/ability/strikersampling";
 import { defaultRatePass } from "glados/util/attack";
-import { Boundaries, getRandomPosition, Zone } from "glados/util/zone";
+import { getRandomPosition, Zone } from "glados/util/zone";
 
 const G = World.Geometry;
-
-interface SplitZone {
-	boundaries: Boundaries;
-	splitCount: number;
-}
 
 export class Striker extends Objective {
 	constructor(maAgent: Agent) {
@@ -99,137 +92,12 @@ export class Striker extends Objective {
 				zones.push({ boundaries, defaultPos });
 			}
 		} else {
-			const DEFENSE_AREA_CUT_OFF_Y = G.FieldHeightHalf - G.DefenseHeight;
-			const LOWER_DEFENSE_AREA_CUT_OFF_X = -G.DefenseWidthHalf;
-			const UPPER_DEFENSE_AREA_CUT_OFF_X = G.DefenseWidthHalf;
-
-			let boundaryList: SplitZone[] = [];
-			if (remainingZones > 0) {
-				let firstBoundary = { boundaries: { left: TOTAL_LEFT, right: TOTAL_RIGHT, top: TOTAL_TOP, bottom: MIDFIELD_OFFENSIVE_SPLIT }, splitCount: 0};
-				boundaryList.push(firstBoundary);
-				/* The basic idea of the following is to split the zones repeatedly.
-				 * The zones split next is the one closest to the main attacker, unless it already has been split MAX_SPLIT times,
-				 * to keep the zones at a useful size.
-				 * If the zone contains the defense area lines, split it along those lines, otherwise split it in the middle.
-				 * In the case one of the resulting zones contains the defense area and can be split into two new zones a way that
-				 * one zone lies completely inside the defense area and the other one completely outside the defense area do this
-				 * and only keep the zone outside the defense area. */
-				for (let i = 1; i < remainingZones; ++i) {
-					const boundaryIndex = this.findClosestZoneToMainAttacker(mainAttackerPos, boundaryList);
-
-					const currentSplitZone = boundaryList[boundaryIndex];
-					const currentBoundary = currentSplitZone.boundaries;
-					const splitCount = currentSplitZone.splitCount;
-					const newSplitCount = splitCount + 1;
-					const rightLeft = currentBoundary.right - currentBoundary.left;
-					const topBottom = currentBoundary.top - currentBoundary.bottom;
-					const rightLeftBigger = Math.abs(rightLeft) > Math.abs(topBottom);
-					// choose splitting axis
-					if (rightLeftBigger) {
-						let newBoundaryMidRightLeft: number;
-						if (currentBoundary.right > UPPER_DEFENSE_AREA_CUT_OFF_X && currentBoundary.left < UPPER_DEFENSE_AREA_CUT_OFF_X) {
-							// check if whole defense area is inside boundaries
-							if (currentBoundary.left < LOWER_DEFENSE_AREA_CUT_OFF_X) {
-								const mainAttackerPosToLowerDefenseArea = Math.abs(mainAttackerPos.x - LOWER_DEFENSE_AREA_CUT_OFF_X);
-								const mainAttackerPosToUpperDefenseArea = Math.abs(mainAttackerPos.x - UPPER_DEFENSE_AREA_CUT_OFF_X);
-								if (mainAttackerPosToLowerDefenseArea < mainAttackerPosToUpperDefenseArea) {
-									newBoundaryMidRightLeft = LOWER_DEFENSE_AREA_CUT_OFF_X;
-								} else {
-									newBoundaryMidRightLeft = UPPER_DEFENSE_AREA_CUT_OFF_X;
-								}
-							} else {
-								newBoundaryMidRightLeft = UPPER_DEFENSE_AREA_CUT_OFF_X;
-							}
-						} else if (currentBoundary.right > LOWER_DEFENSE_AREA_CUT_OFF_X && currentBoundary.left < LOWER_DEFENSE_AREA_CUT_OFF_X) {
-							newBoundaryMidRightLeft = LOWER_DEFENSE_AREA_CUT_OFF_X;
-						} else {
-							newBoundaryMidRightLeft = rightLeft / 2 + currentBoundary.left;
-						}
-
-						const boundaryContainsDefenseArea = currentBoundary.top > DEFENSE_AREA_CUT_OFF_Y
-													&& currentBoundary.bottom < DEFENSE_AREA_CUT_OFF_Y;
-						// check if the new boundary can be reduced to not contain the defense area
-						const cutoutDefenseArea0 = boundaryContainsDefenseArea
-													&& newBoundaryMidRightLeft === UPPER_DEFENSE_AREA_CUT_OFF_X
-													&& currentBoundary.left === LOWER_DEFENSE_AREA_CUT_OFF_X;
-						const newBoundary0Top = cutoutDefenseArea0 ? DEFENSE_AREA_CUT_OFF_Y : currentBoundary.top;
-
-						const newBoundary0 = { boundaries: { left: currentBoundary.left, right: newBoundaryMidRightLeft,
-							top: newBoundary0Top, bottom: currentBoundary.bottom }, splitCount: newSplitCount };
-
-						// check if the new boundary can be reduced to not contain the defense area
-						const cutoutDefenseArea1 = boundaryContainsDefenseArea
-													&& currentBoundary.right === UPPER_DEFENSE_AREA_CUT_OFF_X
-													&& newBoundaryMidRightLeft === LOWER_DEFENSE_AREA_CUT_OFF_X;
-						const newBoundary1Top = cutoutDefenseArea1 ? DEFENSE_AREA_CUT_OFF_Y : currentBoundary.top;
-						const newBoundary1 = { boundaries: { left: newBoundaryMidRightLeft, right: currentBoundary.right,
-							top: newBoundary1Top, bottom: currentBoundary.bottom }, splitCount: newSplitCount };
-						boundaryList[boundaryIndex] = newBoundary0;
-						boundaryList.push(newBoundary1);
-					} else {
-						const containsDefenseArea = currentBoundary.top > DEFENSE_AREA_CUT_OFF_Y
-													&& currentBoundary.bottom < DEFENSE_AREA_CUT_OFF_Y;
-						const newBoundaryTopBottom = containsDefenseArea ? DEFENSE_AREA_CUT_OFF_Y : topBottom / 2 + currentBoundary.bottom;
-						const newBoundary0 = { boundaries: { left: currentBoundary.left, right: currentBoundary.right,
-							top: newBoundaryTopBottom, bottom: currentBoundary.bottom }, splitCount: newSplitCount };
-
-						// check if the new boundary can be reduced to not contain the defense area
-						const newBoundary1ContainsDefenseAreaUpper = currentBoundary.left > UPPER_DEFENSE_AREA_CUT_OFF_X
-																		&& currentBoundary.right < UPPER_DEFENSE_AREA_CUT_OFF_X;
-						const newBoundary1ContainsDefenseAreaLower = currentBoundary.left > LOWER_DEFENSE_AREA_CUT_OFF_X
-																		&& currentBoundary.right < LOWER_DEFENSE_AREA_CUT_OFF_X;
-						const newBoundary1Left: number = newBoundary1ContainsDefenseAreaUpper
-														? UPPER_DEFENSE_AREA_CUT_OFF_X
-														: currentBoundary.left;
-						const newBoundary1Right: number = newBoundary1ContainsDefenseAreaLower
-														? LOWER_DEFENSE_AREA_CUT_OFF_X
-														: currentBoundary.right;
-						const newBoundary1 = { boundaries: { left: newBoundary1Left, right: newBoundary1Right,
-							top: currentBoundary.top, bottom: newBoundaryTopBottom }, splitCount: newSplitCount };
-						boundaryList[boundaryIndex] = newBoundary0;
-						boundaryList.push(newBoundary1);
-					}
-				}
-			}
-
-			const newZones = boundaryList.map((splitZone) => {
-				const defaultPos = getRandomPosition(splitZone.boundaries);
-				return { boundaries: splitZone.boundaries, defaultPos };
-			});
+			const startBoundaries = { left: TOTAL_LEFT, right: TOTAL_RIGHT, top: TOTAL_TOP, bottom: MIDFIELD_OFFENSIVE_SPLIT };
+			const newZones = this.splitZonesClosestToMainAttacker(mainAttackerPos, remainingZones, [startBoundaries]);
 
 			zones = zones.concat(newZones);
 		}
 
 		return zones;
-	}
-
-	private findClosestZoneToMainAttacker(mainAttackerPos: Position, boundaryList: SplitZone[]): number {
-		const MAX_SPLIT = 3;
-		let minDist = Infinity;
-		let boundaryIndex: number = 0;
-		for (let j = 0; j < boundaryList.length; ++j) {
-			const splitZone = boundaryList[j];
-			if (splitZone.splitCount >= MAX_SPLIT) {
-				continue;
-			}
-			const corner1 = new Vector(splitZone.boundaries.left, splitZone.boundaries.bottom);
-			const corner2 = new Vector(splitZone.boundaries.right, splitZone.boundaries.top);
-			// if zone contains mainAttackerPos it's obviously always the closest
-			if (geom.insideRect(corner1, corner2, mainAttackerPos)) {
-				boundaryIndex = j;
-				break;
-			}
-
-			const distX = Math.min(Math.abs(mainAttackerPos.x - splitZone.boundaries.right),
-									Math.abs(mainAttackerPos.x - splitZone.boundaries.left));
-			const distY = Math.min(Math.abs(mainAttackerPos.y - splitZone.boundaries.top),
-									Math.abs(mainAttackerPos.y - splitZone.boundaries.bottom));
-			const dist = Math.min(distX, distY);
-			if (dist < minDist) {
-				minDist = dist;
-				boundaryIndex = j;
-			}
-		}
-		return boundaryIndex;
 	}
 }
