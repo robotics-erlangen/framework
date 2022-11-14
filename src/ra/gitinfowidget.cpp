25c0a97e460bc726f6a2e75a9af44122e3844f43
Michel Schmid <michel.schmid@robotics-erlangen.de>
2022-11-14 23:33:51 +0100

    ra: gitinfowidget: add default path and improve error message

--- src/ra/gitinfowidget.cpp
+++ src/ra/gitinfowidget.cpp
@@ -23,6 +23,7 @@
 #include "git/gitconfig.h"
 
 #include <QSettings>
+#include <QDir>
 
 enum class DiffOptions {
     ORIGINAL, MIN_HASH, HEAD, MASTER, CUSTOM
@@ -73,6 +74,11 @@ void GitInfoWidget::updateWidget()
     } else {
         const auto newDiff = gitconfig::calculateDiff(m_relativePath.c_str(), m_hash.c_str(), m_diff.c_str(), m_diffHash.c_str());
         diffText = QString::fromStdString(newDiff);
+
+        if (diffText.startsWith("error")) {
+            diffText = QString("Encountered %1\n(This may be caused by Relative Path not pointing to a valid git repository for this tab)")
+                .arg(diffText);
+        }
     }
 
     QString escaped = diffText.toHtmlEscaped();
@@ -137,7 +143,28 @@ void GitInfoWidget::load()
 {
     const auto pathName = "GitInfo/" + name;
     QSettings s;
-    ui->relativePathEdit->setText(s.value(pathName + "/relativePath").toString());
+
+    auto relativePath = s.value(pathName + "/relativePath").toString();
+    if (relativePath == "") {
+        auto home = QDir::home();
+        home.setNameFilters({"robotics", "Robotics"});
+        const auto roboticsDir = home.entryList(QDir::Dirs);
+        if (roboticsDir.size() > 0) {
+            QString subDir = "software/";
+            if (name.contains("ra", Qt::CaseInsensitive)) {
+                subDir += "src";
+            } else if (name.contains("autoref", Qt::CaseInsensitive)) {
+                subDir += "autoref";
+            } else if (name.contains("blue", Qt::CaseInsensitive)
+                    || name.contains("yellow", Qt::CaseInsensitive)) {
+                subDir += "strategy";
+            }
+            const QStringList pathParts{ home.path(), roboticsDir[0], subDir };
+            relativePath = pathParts.join("/");
+        }
+    }
+
+    ui->relativePathEdit->setText(relativePath);
     m_relativePath = ui->relativePathEdit->text().toStdString();
     ui->customDiffHashEdit->setText(s.value(pathName + "/customDiffHash").toString());
     m_diffHash = ui->customDiffHashEdit->text().toStdString();
