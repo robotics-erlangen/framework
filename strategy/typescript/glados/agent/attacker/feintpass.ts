79ba1670de81db636390fbc050b1f154be61f552
Marco Wiedmann <marco.wiedmann@robotics-erlangen.de>
2023-03-27 23:19:48 +0200

    glados: feintpass: Adds behavior and task for feintpass
        The behavior checks all timing considerations for if
        feinting a pass is possible. Currently only the positions
        at 50 and 75 percent of the passline are sampled for possible
        feints. Additionally, AcceptPass will be executed instead
        of the FeintPassTask if we need to accept a pass.
    
        The task handles the switch between 'stand inside pass line'
        and 'evacuate the passline'. The ball is evaded by switching
        on an obstacle. Also, passes are suggested inside the zone
        assigned to this robot.

---
+++ strategy/typescript/glados/agent/attacker/feintpass.ts
@@ -0,0 +1,294 @@
+import * as constants from "base/constants";
+import * as debug from "base/debug";
+import { FriendlyRobot } from "base/robot";
+import { Position, Vector } from "base/vector";
+import * as World from "base/world";
+
+import { Agent } from "glados/agent/base/agent";
+import { Behavior, CONTINUE_TASK, TaskAssignment } from "glados/agent/base/behavior";
+import { MessageType } from "glados/control/messaging";
+import * as Physics from "glados/observer/physics";
+import { AcceptPass } from "glados/task/attacker/acceptpass";
+import { FeintPassTask } from "glados/task/attacker/feintpass";
+import { SupportParameters } from "glados/task/attacker/support";
+import { checkPassInfos, PassInfo } from "glados/util/attack";
+import { isInZone, Zone } from "glados/util/zone";
+
+const PASS_DISTANCE_HYSTERESIS = 0.2;
+const PASS_DIST_SMALL_THRESHOLD = 3;
+const PASS_DIST_LARGE_THRESHOLD = 5;
+const IS_REACHABLE_TIMEDIFF = 0.5;
+
+export class FeintPass extends Behavior {
+
+	private lastFeintSamplings = new Map<PassInfo, number>();
+	private supportParams: SupportParameters;
+
+	private restartTask: boolean = false;
+	private nextPassInfo: PassInfo | undefined;
+	private nextSender: FriendlyRobot | undefined;
+	private nextFeintpos: Position | undefined;
+	private nextAttackPosition: Position | undefined;
+
+	constructor(agent: Agent, supportParams: SupportParameters) {
+		super(agent);
+		this.supportParams = supportParams;
+	}
+
+	_stop() {
+		this.lastFeintSamplings = new Map<PassInfo, number>();
+		this.restartTask = false;
+		this.nextPassInfo = undefined;
+		this.nextSender = undefined;
+		this.nextFeintpos = undefined;
+		this.nextAttackPosition = undefined;
+	}
+
+	private getFeintPosition(passInfoTable: ReadonlyRec<PassInfo[]>, attackPosition: Position, plannedAttackTime: number | undefined, zone: Zone):
+	[PassInfo | undefined, Position | undefined] {
+		let relevantPassInfoMessage = undefined;
+		let bestTime = Infinity;
+		let bestPosition = undefined;
+		let feintSamplings = new Map<PassInfo, number>();
+
+		for (let passInfo of passInfoTable) {
+
+			let passPos = passInfo.ballPos;
+			let passSpeed = passInfo.passSpeed;
+			let passDist = passPos.distanceTo(attackPosition);
+			let hysteresis = 0;
+			if (this.lastFeintSamplings.has(passInfo)) {
+				hysteresis = PASS_DISTANCE_HYSTERESIS;
+			}
+
+			let possibleFeintPositions = [];
+			debug.set("passDistance", passDist);
+			if (passDist <= PASS_DIST_SMALL_THRESHOLD - hysteresis) {
+				feintSamplings[passInfo] = 0;
+				continue;
+			} else if (passDist > PASS_DIST_SMALL_THRESHOLD + hysteresis && passDist <= PASS_DIST_LARGE_THRESHOLD - hysteresis) {
+				feintSamplings[passInfo] = 1;
+			} else if (passDist > PASS_DIST_LARGE_THRESHOLD + hysteresis) {
+				feintSamplings[passInfo] = 2;
+			} else {
+				feintSamplings[passInfo] = this.lastFeintSamplings[passInfo]!;
+			}
+
+			if (feintSamplings[passInfo]! > 0) {
+				possibleFeintPositions.push(0.5 * (attackPosition + passPos));
+			}
+			if (feintSamplings[passInfo]! > 1) {
+				possibleFeintPositions.push(attackPosition + 0.75 * (passPos - attackPosition));
+			}
+			if (feintSamplings[passInfo]! > 2) {
+				throw new Error("Currently only up to two feint positions per pass are supported");
+			}
+
+			debug.set("possibleFeintPositions", possibleFeintPositions.length);
+
+			for (let feintPos of possibleFeintPositions) {
+
+				// Check if possibly inside zone
+				if (!isInZone(feintPos, zone)) {
+					continue;
+				}
+
+				// Check if we can reach feintPos in time
+				if (plannedAttackTime == undefined) {
+					plannedAttackTime = World.Time;
+				}
+
+				let ballTime;
+				if (this._task instanceof FeintPassTask && this._task.passWasShot) {
+					ballTime = Physics.ballRollTime(World.Ball, World.Ball.pos.distanceTo(feintPos));
+				} else {
+					let ball = { pos: attackPosition, speed: (passPos - attackPosition).withLength(passSpeed), maxSpeed: constants.maxBallSpeed };
+					ballTime = (plannedAttackTime - World.Time) + Physics.ballRollTime(ball, (feintPos - attackPosition).length());
+				}
+
+				let robotTime = Physics.robotTimeToPos(this._robot, feintPos, new Vector(0, 0))[0];
+
+				let isReachableTimediff;
+				if (this._task instanceof FeintPassTask && !this._task.complete) {
+					isReachableTimediff = 0;
+				} else {
+					isReachableTimediff = IS_REACHABLE_TIMEDIFF;
+				}
+
+				if (robotTime <= bestTime && robotTime + isReachableTimediff < ballTime) {
+					relevantPassInfoMessage = passInfo;
+					bestTime = robotTime;
+					bestPosition = feintPos;
+					debug.set("robotTimeToFeintPos", robotTime);
+					debug.set("ballTimeToFeintPos", ballTime);
+				}
+
+			}
+		}
+		this.lastFeintSamplings = feintSamplings;
+		return [relevantPassInfoMessage, bestPosition];
+	}
+
+	check(): Behavior | undefined {
+		let [sender, passInfoTable] = this._messaging.receiveSingleSender(MessageType.passInfo);
+		let attackPosition = this._messaging.receiveSingleSender(MessageType.attackPosition)[1] || World.Ball.pos;
+		let plannedAttackTime = this._messaging.receiveSingleSender(MessageType.plannedAttackTime)[1];
+
+		// If we don't have a zone yet, do something else
+		let zone = this._messaging.receiveTrainer(MessageType.supportZone);
+		if (zone == undefined) {
+			return undefined;
+		}
+
+		// Check if we currently plan a pass
+		let passPlanned = passInfoTable !== undefined && attackPosition !== undefined && plannedAttackTime !== undefined;
+
+		// Check if we needed to accept the next planned pass last frame
+		let prevRobotTime = (this._task instanceof AcceptPass) ? this._task.getLastTime() : undefined;
+		let [acceptingPass, _timeLeft] = passInfoTable ? checkPassInfos(this._robot, passInfoTable, false, prevRobotTime) : [false, undefined];
+		let escapeToAccept = acceptingPass && this.nextAttackPosition && this.nextPassInfo;
+
+		debug.set("passPlanned", passPlanned);
+
+		// If we are doing a feint in the last frame
+		if (this._task instanceof FeintPassTask && !this._task.complete) {
+			debug.set("doingFeint", undefined);
+
+			// Update wasPassShot and evacuate
+			this._task.updateState(attackPosition);
+
+			// Never abort during evacuation
+			if (this._task.evacuate) {
+				this._applyForMainAttacker(undefined, undefined, 0);
+				this.restartTask = false;
+				return this;
+			}
+
+			// Check if the pass of the feint was already shot
+			let wasPassShot = this._task.passWasShot;
+			debug.set("wasPassShot", wasPassShot);
+
+			// Get info on the pass that we are feinting
+			let lastPassInfo = this._task.relevantPassInfo;
+			let lastPassRobot = this._task.passRobot;
+
+			// If we are planning a pass
+			if (passPlanned) {
+				let [passInfo, feintPos] = this.getFeintPosition(passInfoTable!, attackPosition, plannedAttackTime, zone);
+
+				debug.set("isReachable", passInfo != undefined);
+
+				// If we can reach the planned pass in time
+				if (passInfo !== undefined && feintPos !== undefined) {
+					let samePass = lastPassInfo.target === passInfo.target && sender === lastPassRobot;
+					// And it is the same pass that we already feint
+					debug.set("samePass", samePass);
+					if (samePass && !wasPassShot) {
+						// Update info on this pass and continue
+						this._task.updatePass(passInfo, feintPos, attackPosition);
+						this._applyForMainAttacker(undefined, undefined, 0);
+						this.restartTask = false;
+						return this;
+					} else {
+						// If it is a new pass
+						if (wasPassShot) {
+							// But our pass was already shot ---> keep feinting the current pass
+							this._applyForMainAttacker(undefined, undefined, 0);
+							this.restartTask = false;
+							return this;
+						} else {
+							// Our pass wasn't shot yet ---> it is probably dead. Feint the new pass
+							this._applyForMainAttacker(undefined, undefined, 0);
+							this.restartTask = true;
+							this.nextAttackPosition = attackPosition;
+							this.nextFeintpos = feintPos;
+							this.nextSender = sender;
+							this.nextPassInfo = passInfo;
+							return this;
+						}
+					}
+				// If we can't reach the new pass, but the old one already was shot
+				} else if (wasPassShot) {
+					// Check if we can still reach our old pass
+					let ballTime = Physics.ballRollTime(World.Ball, World.Ball.pos.distanceTo(this._task.feintPos));
+					let robotTime = Physics.robotTimeToPos(this._robot, this._task.feintPos, new Vector(0, 0))[0];
+
+					let isOldPassReachable = robotTime < ballTime;
+					debug.set("isOldPassReachable", isOldPassReachable);
+					debug.set("feintPos", this._task.feintPos);
+					debug.set("ballTime", ballTime);
+					debug.set("robotTime", robotTime);
+					if (isOldPassReachable) {
+						// Continue feinting the old pass
+						this._applyForMainAttacker(undefined, undefined, 0);
+						this.restartTask = false;
+						return this;
+					}
+				}
+				// We can't reach the new one and the old pass is either dead or unreachable ---> do something else
+			// We aren't planning a pass, but our old pass was already shot
+			} else if (wasPassShot && this._task.feintPos !== undefined) {
+				// Check if we can still reach our old pass
+				let ballTime = Physics.ballRollTime(World.Ball, World.Ball.pos.distanceTo(this._task.feintPos));
+				let robotTime = Physics.robotTimeToPos(this._robot, this._task.feintPos, new Vector(0, 0))[0];
+
+				let isOldPassReachable = robotTime < ballTime;
+				debug.set("isOldPassReachable", isOldPassReachable);
+				debug.set("feintPos", this._task.feintPos);
+				debug.set("ballTime", ballTime);
+				debug.set("robotTime", robotTime);
+				if (isOldPassReachable) {
+					// Continue feinting the old pass
+					this._applyForMainAttacker(undefined, undefined, 0);
+					this.restartTask = false;
+					return this;
+				}
+			}
+			// We aren't planning a new pass and the old one is dead ---> do something else
+		// If we need to escape the passline now in order to reach our next pass
+		} else if (escapeToAccept) {
+			// Use the extra obstacle to escape the passline asap but also get to the passDest
+			// That means we need to stay in this behavior until the passline is evacuated again
+
+			// This is very hacky
+			if (this._robot.pos.orthogonalDistance(this.nextAttackPosition!, this.nextPassInfo!.ballPos) < this._robot.radius + World.Ball.radius + 0.1) {
+				this._robot.path.addLine(World.Ball.pos.x, World.Ball.pos.y, this.nextPassInfo!.ballPos.x, this.nextPassInfo!.ballPos.y, this._robot.radius, "PassEvacuation", 1);
+				return this;
+			}
+
+		// We aren't already doing a feint but a pass is being planned
+		} else if (passPlanned) {
+			let [passInfo, feintPos] = this.getFeintPosition(passInfoTable!, attackPosition, plannedAttackTime!, zone);
+			debug.set("newPassReachable", passInfo !== undefined);
+			// if the new pass is reachable
+			if (passInfo !== undefined && feintPos !== undefined) {
+				// Feint the pass
+				this._applyForMainAttacker(undefined, undefined, 0);
+				this.restartTask = true;
+				this.nextAttackPosition = attackPosition;
+				this.nextFeintpos = feintPos;
+				this.nextSender = sender;
+				this.nextPassInfo = passInfo;
+				return this;
+			}
+		}
+		return undefined;
+	}
+
+	_updateTask(): TaskAssignment<typeof FeintPassTask> | TaskAssignment<typeof AcceptPass> | typeof CONTINUE_TASK {
+		let passInfoTable = this._messaging.receiveSingleSender(MessageType.passInfo)[1];
+		let prevRobotTime = (this._task instanceof AcceptPass) ? this._task.getLastTime() : undefined;
+		let [acceptingPass, _timeLeft] = passInfoTable ? checkPassInfos(this._robot, passInfoTable, false, prevRobotTime) : [false, undefined];
+
+		if (acceptingPass) {
+			return [AcceptPass];
+		}
+
+		if (this.restartTask) {
+			this.restartTask = false;
+			return [FeintPassTask, [this.supportParams, this.nextPassInfo!, this.nextSender!, this.nextFeintpos!, this.nextAttackPosition!], true];
+		} else {
+			return CONTINUE_TASK;
+		}
+	}
+}
---
+++ strategy/typescript/glados/task/attacker/feintpass.ts
@@ -0,0 +1,256 @@
+import * as debug from "base/debug";
+import * as Field from "base/field";
+import * as geom from "base/geom";
+import { FriendlyRobot } from "base/robot";
+import { Position, Vector } from "base/vector";
+import * as vis from "base/vis";
+import * as World from "base/world";
+
+import { Behavior } from "glados/agent/base/behavior";
+import { MessageType } from "glados/control/messaging";
+import * as Ball from "glados/observer/ball";
+import * as Physics from "glados/observer/physics";
+import * as Robot from "glados/observer/robot";
+import { SuggestPass } from "glados/task/ability/suggestpass";
+import { Sampling, SupportParameters } from "glados/task/attacker/support";
+import { Task } from "glados/task/base";
+import * as PathHelper from "glados/trajectory/pathhelper";
+import { TrajectoryPath } from "glados/trajectory/trajectorypath";
+import { PassInfo } from "glados/util/attack";
+import { Zone } from "glados/util/zone";
+
+const G = World.Geometry;
+
+const obstacleTable: PathHelper.PathHelperParameters = {
+	ignoreBall: false,
+	ignorePass: true,
+	ignoreDefenseArea: false,
+	ignoreOpponentDefenseArea: false,
+};
+
+export class FeintPassTask extends Task {
+
+	public passWasShot: boolean = false;
+	public evacuate: boolean = false;
+	public complete: boolean = false;
+
+	public relevantPassInfo: PassInfo;
+	public passRobot: FriendlyRobot;
+	public feintPos: Position;
+
+	private attackPosition: Position;
+	private _zone?: Zone;
+
+	private _reEvaluateTimestamp: number;
+	private _suggestPass: SuggestPass;
+	private _sampling: Sampling;
+	private _passDestSuggestion: Position | undefined;
+
+	constructor(behavior: Behavior, supportParameters: SupportParameters, relevantPassInfo: PassInfo, passRobot: FriendlyRobot, feintPos: Position, attackPosition: Position) {
+		super(behavior);
+
+		this.relevantPassInfo = relevantPassInfo;
+		this.passRobot = passRobot;
+		this.feintPos = feintPos;
+		this.attackPosition = attackPosition;
+
+		this._reEvaluateTimestamp = World.Time;
+		this._suggestPass = new SuggestPass(this);
+		this._sampling = new supportParameters.samplingCtor(this);
+		this.updateState(attackPosition);
+	}
+
+	// Copied from supportTask for now because it can't be moved to util/attack because it uses internal states
+	private _reEvaluatePassDest(): boolean {
+		let timestamps = this._messaging.receive(MessageType.supportSamplingTimestamp, true);
+		let nextCandidate = undefined;
+		let nextCandidateTimestamp = Infinity;
+		for (let [r, time] of timestamps.entries()) {
+			if (nextCandidate == undefined || time < nextCandidateTimestamp
+					|| time === nextCandidateTimestamp && r.id < nextCandidate.id) {
+				nextCandidate = r;
+				nextCandidateTimestamp = time;
+			}
+		}
+
+		let revaluate = this._robot === nextCandidate;
+		if (revaluate) {
+			this._reEvaluateTimestamp = World.Time;
+		}
+
+		this._messaging.sendBroadcast(MessageType.supportSamplingTimestamp, this._reEvaluateTimestamp);
+
+		return revaluate;
+	}
+
+	private _searchForPassDest() {
+		this._sampling.precalculate();
+
+		let grid_point_count_x = 16;
+		let grid_point_count_y = 14;
+
+		let grid_point_dist_x = G.FieldWidth / grid_point_count_x;
+		let grid_point_dist_y = G.FieldHeight / grid_point_count_y;
+
+		let boundaries = this._zone!.boundaries;
+		let left = boundaries.left;
+		let right = boundaries.right;
+		let top = boundaries.top;
+		let bottom = boundaries.bottom;
+
+		// TODO hysteresis
+		// TODO only consider well-timed pass positions
+		// TODO am strafraum stehen ist geil! -> score anpassen
+
+		let bestPoint = undefined;
+		let bestScore = -Infinity;
+		for (let x = grid_point_dist_x * 0.5 - G.FieldWidthHalf; x <= G.FieldWidthHalf; x += grid_point_dist_x) {
+			if (x <= left || x >= right) {
+				continue;
+			}
+
+			for (let y = grid_point_dist_y * 0.5 - G.FieldHeightHalf; y <= G.FieldHeightHalf; y += grid_point_dist_y) {
+				if (y <= bottom || y >= top || (G.FieldHeightHalf - Math.abs(y) < 4 && Field.isInFriendlyDefenseArea(World.Ball.pos, World.Ball.radius))) {
+					continue;
+				}
+
+				let candidatePoint = new Vector(x, y);
+				candidatePoint = Field.limitToAllowedField(candidatePoint, -(3 * this._robot.radius + 0.1));
+				if (!geom.insideRect(new Vector(left, bottom), new Vector(right, top), candidatePoint)) {
+					continue;
+				}
+
+				let score = this._sampling.evalLocation(candidatePoint, bestScore);
+				let passInfoTable = this._messaging.receiveSingleSender(MessageType.passInfo)[1];
+				let firstHysteresis = false;
+				if (passInfoTable) {
+					for (let passInfo of passInfoTable) {
+						if (passInfo.ballPos.distanceToSq(candidatePoint) < 0.01 * 0.01) {
+							score = score + 0.1;
+							firstHysteresis = true;
+						}
+					}
+				}
+				if (!firstHysteresis && this._passDestSuggestion &&
+						this._passDestSuggestion.distanceToSq(candidatePoint) < 0.01 * 0.01) {
+					score += 0.05;
+				}
+				if (score > bestScore) {
+					bestScore = score;
+					bestPoint = candidatePoint;
+				}
+			}
+		}
+
+		this._passDestSuggestion = bestPoint;
+	}
+
+	updatePass(relevantPassInfo: PassInfo, feintPos: Position, attackPosition: Position): void {
+		this.relevantPassInfo = relevantPassInfo;
+		this.feintPos = feintPos;
+		this.attackPosition = attackPosition;
+	}
+
+	run(): void {
+		this._messaging.sendToTrainerRepeated(MessageType.groupApplication, { name: "support" });
+
+		// retrieve the assigned zone from the support group
+		this._zone = this._messaging.receiveTrainer(MessageType.supportZone);
+		if (this._zone == undefined) {
+			return;
+		}
+
+		// search for a good pass dest
+		if (this._reEvaluatePassDest()) {
+			this._searchForPassDest();
+		}
+
+		PathHelper.setDefaultObstaclesByTable(this._robot.path, this._robot, obstacleTable);
+		let color = this.evacuate ? vis.colors.green : vis.colors.red;
+		vis.addCircle("t/a/feintpass: evacuate", this._robot.pos, 0.05, color, true);
+		debug.set("FeintpassTask/passWasShot", this.passWasShot);
+		debug.set("FeintpassTask/evacuate", this.evacuate);
+		debug.set("FeintpassTask/completed", this.complete);
+		debug.set("FeintpassTask/passRobot", this.passRobot.id);
+
+		let attackPosition = this._messaging.receiveSingleSender(MessageType.attackPosition)[1];
+		if (this._passDestSuggestion && attackPosition) {
+			this._suggestPass._suggestPass(this._passDestSuggestion, attackPosition, Physics.robotTimeToPos(this._robot, this._passDestSuggestion, new Vector(0, 0))[0]);
+		}
+
+		let path = this._robot.path;
+
+		if (this.evacuate) {
+			if (this.passWasShot) {
+				if (attackPosition) {
+					// Feinted pass is already shot --> new attackPosition is where the ball will be catched
+					path.addLine(World.Ball.pos.x, World.Ball.pos.y, attackPosition.x, attackPosition.y, this._robot.radius, "PassEvacuation", 1);
+				} else {
+					path.addLine(World.Ball.pos.x, World.Ball.pos.y, this.relevantPassInfo.ballPos.x, this.relevantPassInfo.ballPos.y, this._robot.radius, "PassEvacuation", 1);
+				}
+			} else {
+				if (attackPosition) {
+					path.addLine(attackPosition.x, attackPosition.y, this.relevantPassInfo.ballPos.x, this.relevantPassInfo.ballPos.y, this._robot.radius, "PassEvacuation", 1);
+				} else {
+					// This case should probably never happen tm
+					path.addLine(World.Ball.pos.x, World.Ball.pos.y, this.relevantPassInfo.ballPos.x, this.relevantPassInfo.ballPos.y, this._robot.radius, "PassEvacuation", 1);
+				}
+			}
+		}
+
+		// We want to look at the ball so we can quickly stop it in case the behavior changes and feintpass dies
+		// But we do not want to rotate if we evacuate because evacuating while rotating is slow
+		let targetDir = this.evacuate ? this._robot.dir : (World.Ball.pos - this.feintPos).angle();
+
+		this._robot.trajectory.update(TrajectoryPath, this.feintPos, targetDir);
+	}
+
+	updateState(attackPosition: Position): void {
+		let plannedAttackTime = this._messaging.receiveSingleSender(MessageType.plannedAttackTime)[1];
+
+		let futureShotBall = { pos: attackPosition,
+			speed: (this.relevantPassInfo.ballPos - this.attackPosition).withLength(this.relevantPassInfo.passSpeed),
+			maxSpeed: this.relevantPassInfo.passSpeed,
+			radius: World.Ball.radius, posZ: 0 } as Physics.BallLike;
+
+		let ballTime = this.passWasShot ? Physics.ballRollTime(World.Ball, World.Ball.pos.distanceTo(this.feintPos)) : Physics.ballRollTime(futureShotBall, futureShotBall.pos.distanceTo(this.feintPos));
+		let triggerEvacuate = false;
+
+		let orthoProjection = this._robot.pos.orthogonalProjection(attackPosition, this.relevantPassInfo.ballPos)[0];
+		let dummyEvacuatePos = orthoProjection - (this._robot.pos - orthoProjection).withLength(2 * this._robot.radius);
+		vis.addCircle("t/a/feintpass: dummyEvacuatePos", dummyEvacuatePos, 0.05, vis.colors.red, true);
+		let robotTime = Physics.robotTimeToPos(this._robot, dummyEvacuatePos, new Vector(0, 0))[0];
+
+		debug.set("FeintpassTask/robotTime", robotTime);
+		debug.set("FeintpassTask/ballTime", ballTime);
+		if (plannedAttackTime !== undefined) {
+			debug.set("FeintpassTask/plannedAttackTime", plannedAttackTime - World.Time);
+		}
+
+		if ((Robot.hadBall(this.passRobot, 0.3) && Ball.wasShot(0.1))) {
+			this.passWasShot = true;
+		}
+
+		if (this.passWasShot || this.evacuate || (
+			!Ball.isStanding()
+			&& Math.abs(World.Ball.speed.angleDiff(this.relevantPassInfo.ballPos - this.attackPosition)) < (Math.PI / 180) * 10
+			&& Math.abs((World.Ball.pos - this.attackPosition).angleDiff(this.relevantPassInfo.ballPos - this.attackPosition)) < (Math.PI / 180) * 10
+		)) {
+			if (ballTime < robotTime) {
+				triggerEvacuate = true;
+			}
+		} else {
+			if (plannedAttackTime !== undefined && ballTime + plannedAttackTime < World.Time + robotTime) {
+				triggerEvacuate = true;
+			}
+		}
+
+		if (triggerEvacuate) {
+			this.evacuate = true;
+		}
+
+		if (Ball.lastBallOwner() !== this.passRobot && this.passWasShot) {
+			this.complete = true;
+		}
+	}
+}
