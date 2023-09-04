59366c894da96c16583f8bbdc85e701eb891e137
Christoph Schmidtmeier <christoph.schmidtmeier@robotics-erlangen.de>
2023-09-04 17:45:07 +0200

    glados: te/u/listutil: print custom error messages for debugging

--- strategy/typescript/glados/test/unit/base/listutil.ts
+++ strategy/typescript/glados/test/unit/base/listutil.ts
@@ -71,19 +71,34 @@ export class BaseListUtil extends UnitTest {
 			const lin = ListUtil.linspace(len, min, max);
 
 			// check length
-			this.assert_equal(lin.length, len);
+			this.assert_equal(
+				lin.length, len,
+				() => `linspace(${len}, ${min}, ${max}) = [${lin}] is not of length ${len}`,
+			);
 
 			// check only for lists with at least two elements
 			if (len >= 2) {
 				// check min and max
-				this.assert_equal_eps(Math.min(...lin), min, 1e-6);
-				this.assert_equal_eps(Math.max(...lin), max, 1e-6);
+				this.assert_equal_eps(
+					Math.min(...lin), min, 1e-6,
+					() => `linspace(${len}, ${min}, ${max}) = [${lin}] does not contain ${min}`,
+				);
+				this.assert_equal_eps(
+					Math.max(...lin), max, 1e-6,
+					() => `linspace(${len}, ${min}, ${max}) = [${lin}] does not contain ${max}`,
+				);
 
 				// check ascending order and step size
 				const step = lin[1] - lin[0];
 				for (let i = 0; i < len - 1; i++) {
-					this.assert_true(lin[i] < lin[i + 1]);
-					this.assert_equal_eps(lin[i + 1] - lin[i], step, 1e-6);
+					this.assert_less_than(
+						lin[i], lin[i + 1],
+						() => `linspace(${len}, ${min}, ${max}) = [${lin}] is not strictly increasing from index ${i} to ${i + 1}`,
+					);
+					this.assert_equal_eps(
+						lin[i + 1] - lin[i], step, 1e-6,
+						() => `linspace(${len}, ${min}, ${max}) = [${lin}] is not equally spaced between index ${i} to ${i + 1}`,
+					);
 				}
 			}
 		}
