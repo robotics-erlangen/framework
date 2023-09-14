9ea13da9d1840d6b57f36eab162e97be048e2f16
Christoph Schmidtmeier <christoph.schmidtmeier@robotics-erlangen.de>
2023-09-14 14:48:51 +0200

    base: hyst: write GreaterThanHyst

--- strategy/typescript/glados/test/unit/base/hyst.ts
+++ strategy/typescript/glados/test/unit/base/hyst.ts
@@ -1,4 +1,4 @@
-import { Hyst, LessThanHyst } from "base/hyst";
+import { Hyst, LessThanHyst, GreaterThanHyst } from "base/hyst";
 
 import { UnitTest } from "glados/test/unit/unittest";
 
@@ -6,6 +6,7 @@ export class BaseHyst extends UnitTest {
 	constructor() {
 		super();
 		this.addTest("LessThanHyst", this.testLessThanHyst);
+		this.addTest("GreaterThanHyst", this.testGreaterThanHyst);
 	}
 
 	private testHystSequence<In, Out>(hyst: Hyst<In, Out>, io: [In, Out][], assert_fn: (got: Out, expected: Out, input: In) => void) {
@@ -34,6 +35,35 @@ export class BaseHyst extends UnitTest {
 
 		for (const initialState of [false, true]) {
 			const hyst = new LessThanHyst(THRESHOLD, HYST, initialState);
+			this.assert_eq(hyst.state, initialState);
+			this.testHystSequence(
+				hyst,
+				IN_OUT,
+				(got, expected, input) => this.assert_eq(got, expected, () => `${hyst}.update(${input}) returned ${got} instead of ${expected}`)
+			);
+		}
+	}
+
+	private testGreaterThanHyst() {
+		const IN_OUT: [number, boolean][] = [
+			[-2, false],
+			[-1, false],
+			[0, false],
+			[1, true],
+			[2, true],
+			[1, true],
+			[1, true],
+			[0, true],
+			[0, true],
+			[1, true],
+			[-1, false],
+			[-2, false],
+		];
+		const THRESHOLD: number = 0;
+		const HYST: number = 0.5;
+
+		for (const initialState of [false, true]) {
+			const hyst = new GreaterThanHyst(THRESHOLD, HYST, initialState);
 			this.assert_eq(hyst.state, initialState);
 			this.testHystSequence(
 				hyst,
