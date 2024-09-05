db13d76f0f706d5e0bfed3366a51dde0aad9ac4b
Steffen Probst <steffen.probst@robotics-erlangen.de>
2024-09-05 16:48:38 +0200

    glados: t/direct: added docstrings and improved readability

--- strategy/typescript/glados/trajectory/direct.ts
+++ strategy/typescript/glados/trajectory/direct.ts
@@ -4,10 +4,25 @@ import * as MathUtil from "base/mathutil";
 import { TrajectoryHandler, TrajectoryResult } from "base/trajectory";
 import { Speed, Vector } from "base/vector";
 
+/**
+ * The Direct class extends the TrajectoryHandler to provide direct trajectory updates.
+ *
+ * This class allows for specifying velocities directly, rather than target positions.
+ * It can handle either a target direction or a rotational speed, but not both simultaneously.
+ */
 export class Direct extends TrajectoryHandler {
 
-	// only targetDir or rotateSpeed may be passed!
-	// accel is optional
+	/**
+	 * Updates the trajectory based on the given parameters.
+	 * Only targetDir or rotateSpeed may be passed. accel is optional.
+	 *
+	 * @param speed - The desired speed of the robot in global coordinates.
+	 * @param targetDir - The desired target direction (optional).
+	 * @param rotateSpeed - The desired rotational speed (optional).
+	 * @param accel - The acceleration vector (optional, default is a zero vector).
+	 * @returns The result of the trajectory update including the spline, robot position, and a zero value.
+	 * @throws Error if both targetDir and rotateSpeed are passed or if neither are provided.
+	 */
 	public update(speed: Speed, targetDir?: number, rotateSpeed?: number, accel: Vector = new Vector(0, 0)): TrajectoryResult {
 		speed = Coordinates.toGlobal(speed);
 		accel = Coordinates.toGlobal(accel);
@@ -18,6 +33,7 @@ export class Direct extends TrajectoryHandler {
 
 		let robotPos = Coordinates.toGlobal(this._robot.pos);
 		let robotDir = Coordinates.toGlobal(this._robot.dir);
+
 		if (targetDir != undefined && rotateSpeed != undefined) {
 			throw new Error("rotating while having a fixed direction makes no sense");
 		}
@@ -33,7 +49,9 @@ export class Direct extends TrajectoryHandler {
 			rotateSpeed = MathUtil.bound(-limitRot, error_phi * k_omega, limitRot);
 		}
 
-		let spline = [{ t_start: 0, t_end: Infinity,
+		let spline = [{
+			t_start: 0,
+			t_end: Infinity,
 			x: { a0: robotPos.x, a1: speed.x, a2: accel.x / 2, a3: 0 },
 			y: { a0: robotPos.y, a1: speed.y, a2: accel.y / 2, a3: 0 },
 			phi: { a0: robotDir, a1: rotateSpeed, a2: 0, a3: 0 }
@@ -42,7 +60,7 @@ export class Direct extends TrajectoryHandler {
 		return [{ spline: spline }, this._robot.pos, 0];
 	}
 
-	public canHandle(...args: any[]) {
+	public canHandle(...args: any[]): boolean {
 		return true;
 	}
 }
