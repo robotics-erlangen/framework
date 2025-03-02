96e26d94d9c2f0c946f36651189f5d3a11474987
Paul Bergmann <paul.bergmann@robotics-erlangen.de>
2025-03-02 19:18:03 +0100

    core: timer: Always use a monotonic clock as system time
    In contrast to QueryPerformanceCounter, both CLOCK_REALTIME and
    gettimeofday are not monotonic. Calls performed later (in terms of wall
    time) may return smaller values than earlier calls, for example due to
    NTP or system administrator adjustments. This would lead to negative
    time deltas, corrupting logfiles and causing issues in the tracking.
    While this is unlikely to have happened often over the last 14 years, it
    is preferrable to use an actual monotonic clock.
    
    We use std::steady_clock instead of a custom implementation, since it is
    more simple and offers a high precision clock on all modern systems (in
    fact, it uses CLOCK_MONOTONIC on Posix systems in both libstdc++ and
    libc++. The mingw-w64 clock_gettime CLOCK_MONOTONIC uses
    QueryPerformanceCounter)
    
    Note that while the underlying clock is now monotonic, the Timer itself
    still is not, since it allows setting its time backwards. This is mostly
    used for log replay and analysis, so it should not cause issues under
    normal conditions.

--- src/core/timer.cpp
+++ src/core/timer.cpp
@@ -20,18 +20,8 @@
 
 #include "timer.h"
 
-#if _POSIX_TIMERS > 0
-    #define WITH_POSIX_TIMERS
-#endif
-
-#ifdef WITH_POSIX_TIMERS
-    #include <time.h>
-#else
-    #include <sys/time.h>
-    #ifdef Q_OS_WIN
-        #include <windows.h>
-    #endif // Q_OS_WIN
-#endif
+#include <chrono>
+#include <ratio>
 
 /*!
  * \class Timer
@@ -95,57 +85,7 @@ void Timer::setTime(qint64 time, double scaling)
  */
 qint64 Timer::systemTime()
 {
-#ifdef WITH_POSIX_TIMERS
-    timespec ts;
-    if (clock_gettime(CLOCK_REALTIME, &ts) == -1)
-        return 0;
-
-    return qint64(ts.tv_sec) * 1000000000LL + qint64(ts.tv_nsec);
-#else // WITH_POSIX_TIMERS
-    #ifdef Q_OS_WIN
-    static bool isInitialized = false;
-
-    static quint64 timerFrequency;
-    static quint64 startTick;
-    static quint64 startTime;
-
-    if (!isInitialized) {
-        isInitialized = true;
-        timerFrequency = 0; // !!! disable QueryPerformanceCounter for initialization
-        startTime = Timer::systemTime(); // get time via gettimeofday
-
-        // the timing provided by this code is far from optimal, but it should do
-        // as the time deltas are what we're interested in.
-        LARGE_INTEGER freq;
-        // set timerFrequency to zero if QueryPerformanceCounter can't be used
-        if (!QueryPerformanceFrequency(&freq)) {
-            timerFrequency = 0;
-        } else {
-            timerFrequency = freq.QuadPart;
-        }
-        if (timerFrequency > 0) {
-            LARGE_INTEGER ticks;
-            if (QueryPerformanceCounter(&ticks)) {
-                startTick = ticks.QuadPart;
-            } else {
-                timerFrequency = 0;
-            }
-        }
-    }
-
-    if (timerFrequency > 0) {
-        LARGE_INTEGER ticks;
-        if (QueryPerformanceCounter(&ticks)) {
-            return (ticks.QuadPart - startTick) * 1000000000LL / timerFrequency + startTime;
-        }
-    }
-    #endif // Q_OS_WIN
-
-    timeval tv;
-    if (gettimeofday(&tv, NULL) != 0) {
-        return 0;
-    }
-
-    return qint64(tv.tv_sec) * 1000000000LL + qint64(tv.tv_usec) * 1000LL;
-#endif // WITH_POSIX_TIMERS
+    using namespace std::chrono;
+    const auto t = steady_clock::now().time_since_epoch();
+    return duration_cast<nanoseconds>(t).count();
 }
