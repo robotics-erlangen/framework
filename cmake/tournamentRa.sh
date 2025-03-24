aacec7bf7f1731a8f51c3f9ee987ed92de53c47e
Steffen Probst <steffen.probst@robotics-erlangen.de>
2025-03-24 14:17:37 +0100

    cmake: tournamentRa.sh: Made script independent from working directory

--- cmake/tournamentRa.sh
+++ cmake/tournamentRa.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 ulimit -c unlimited
+
+# Get the directory where the script is located
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 while true; do
-		bin/ra -t
+		"$SCRIPT_DIR/bin/ra" -t
 		if [ $? -eq 0 ]; then
 				break;
 		fi
