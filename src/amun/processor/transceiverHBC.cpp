e8d1365ccc7c0e1beaed0555b7e7858c3de20106
Christoph Schmidtmeier <christoph.schmidtmeier@robotics-erlangen.de>
2025-05-10 23:07:36 +0200

    amun: processor/transceiverHBC: duplicate code instead of inheriting for easier modification in the future

--- src/amun/processor/transceiverHBC.cpp
+++ src/amun/processor/transceiverHBC.cpp
@@ -23,7 +23,6 @@
 
 #include "core/timer.h"
 #include "radio_address.h"
-#include "transceiver2015.h"
 #include "firmware-interface/radiocommand.h"
 #include "transceiverlayer.h"
 #include "usbdevice.h"
@@ -32,9 +31,17 @@
 #include <QString>
 #include <QtGlobal>
 #include <cstring>
+#include "firmware-interface/radiocommand2014.h"
+#include "firmware-interface/radiocommandpasta.h"
+#include "firmware-interface/transceiver2012.h"
 #include <libusb.h>
 #include <memory>
+#include <optional>
+#include <QEventLoop>
+#include <qdebug.h>
 #include <qeventloop.h>
+#include <QTimer>
+#include <qglobal.h>
 #include <qtimer.h>
 
 using namespace Radio;
@@ -43,6 +50,8 @@ typedef struct
 {
     int64_t time;
 } __attribute__ ((packed)) TransceiverPingData;
+
+const int PROTOCOL_VERSION = 5;
 
 constexpr qint16 HBC_VENDOR_ID  = 0x09fb;
 constexpr qint16 HBC_PRODUCT_ID_PRIMARY = 0x0de2;
@@ -111,11 +120,161 @@ std::pair<std::vector<std::unique_ptr<TransceiverLayer>>, std::vector<Transceive
     return {std::move(transceivers), std::move(errors)};
 }
 
-TransceiverHBC::TransceiverHBC(USBDevice *device,  const Timer *timer, QString debugName, Kind kind) :
-    Transceiver2015(device, timer, debugName),
-    m_kind(kind)
+TransceiverHBC::TransceiverHBC(USBDevice *device, const Timer *timer, QString debugName, Kind kind) :
+    TransceiverLayer(),
+    m_device(device),
+    m_timer(timer),
+    m_kind(kind),
+    m_debugName(debugName)
 {
+    connect(m_device, &USBDevice::readyRead, this, &TransceiverHBC::onReadyRead);
 }
+
+void TransceiverHBC::onReadyRead()
+{
+    const auto readError = read();
+    if (readError.has_value()) {
+        emit errorOccurred(*readError);
+    }
+}
+
+TransceiverHBC::~TransceiverHBC()
+{
+    delete m_device;
+}
+
+std::optional<std::vector<TransceiverError>> TransceiverHBC::tryConnect(QObject* parent)
+{
+    connect(this, SIGNAL(sendStatus(Status)), parent, SIGNAL(sendStatus(Status)));
+    std::vector<TransceiverError> deviceError;
+    const auto gatherErrorConnection = connect(this, &TransceiverLayer::errorOccurred, [&deviceError] (const TransceiverError& error) {
+        deviceError.emplace_back(error);
+    });
+    connect(this, SIGNAL(sendRawRadioResponses(qint64, QList<QByteArray>)), parent, SLOT(onRawRadioResponse(qint64, QList<QByteArray>)));
+    // TODO We should keep a per device timeout
+    connect(this, SIGNAL(deviceResponded(QString)), parent, SLOT(transceiverResponded(QString)));
+
+    // try to open the communication channel
+    if (!m_device->open(QIODevice::ReadWrite)) {
+        return {{TransceiverError(m_debugName, m_device->errorString())}};
+    }
+
+    const auto maybeError = sendInitPacket();
+    if (maybeError.has_value()) {
+        return {{*maybeError}};
+    }
+    QEventLoop loop;
+    connect(this, &TransceiverHBC::connectionSucceeded, &loop, &QEventLoop::quit, Qt::ConnectionType::DirectConnection);
+    QTimer timer;
+    connect(&timer, &QTimer::timeout, &loop, &QEventLoop::quit, Qt::ConnectionType::DirectConnection);
+    timer.setSingleShot(true);
+    timer.start(100);
+    loop.exec();
+    QObject::disconnect(gatherErrorConnection);
+
+    // only add transceiver if it is connected after handshake
+    if (!deviceError.empty()) {
+        return deviceError;
+    } else if (!isOpen()) {
+        return {{TransceiverError(m_debugName, "Handshake timed out!")}};
+    } else {
+        // technically we could lose an error that happens between the disconnect above and here,
+        // but it *probably* will never lead to any issues. Hopefully.
+        connect(this, SIGNAL(errorOccurred(TransceiverError)), parent, SLOT(transceiverErrorOccurred(TransceiverError)));
+        return {};
+    }
+}
+
+std::optional<TransceiverError> TransceiverHBC::read()
+{
+    const int maxSize = 512;
+    char buffer[maxSize];
+    QList<QByteArray> rawResponses;
+
+
+    // read until no further data is available
+    const int size = m_device->read(buffer, maxSize);
+    if (size == -1) {
+        return TransceiverError(m_debugName, m_device->errorString());
+    }
+
+    const qint64 receiveTime = m_timer->currentTime();
+
+    int pos = 0;
+    while (pos < size) {
+        // check header size
+        if (pos + (int)sizeof(TransceiverResponsePacket) > size) {
+            break;
+        }
+
+        const TransceiverResponsePacket *header = (const TransceiverResponsePacket*) &buffer[pos];
+        // check command content size
+        if (pos + (int)sizeof(TransceiverResponsePacket) + header->size > size) {
+            break;
+        }
+
+        pos += sizeof(TransceiverResponsePacket);
+        // handle command
+        switch (header->command) {
+        case COMMAND_INIT_REPLY: {
+                const auto result = handleInitPacket(&buffer[pos], header->size);
+                if (result.has_value()) {
+                    return result;
+                } else {
+                    emit connectionSucceeded();
+                }
+                break;
+            }
+        case COMMAND_PING_REPLY:
+            handlePingPacket(&buffer[pos], header->size);
+            break;
+        case COMMAND_STATUS_REPLY:
+            handleStatusPacket(&buffer[pos], header->size);
+            break;
+        case COMMAND_REPLY_FROM_ROBOT:
+            rawResponses.append(buildRawRadioResponse(&buffer[pos], header->size));
+            break;
+        case COMMAND_DATAGRAM_RECEIVED:
+            qDebug() << "HBC received DATAGRAM?";
+            break;
+        }
+
+        pos += header->size;
+    }
+
+    emit sendRawRadioResponses(receiveTime, rawResponses);
+
+    return {};
+}
+
+std::optional<TransceiverError> TransceiverHBC::sendInitPacket()
+{
+    // publish transceiver status
+    Status status(new amun::Status);
+    status->mutable_transceiver()->set_active(true);
+    status->mutable_transceiver()->set_name(m_debugName.toStdString());
+    status->mutable_transceiver()->set_error("Handshake");
+    emit sendStatus(status);
+
+    // send protocol handshake
+    // the transceiver should return the highest supported version <= hostConfig->protocolVersion
+    // if the version is higher/less than supported by the host, then the connection will fail
+    m_connectionState = State::HANDSHAKE;
+
+    // send init handshake
+    TransceiverCommandPacket senderCommand;
+    senderCommand.command = COMMAND_INIT;
+    senderCommand.size = sizeof(TransceiverInitPacket);
+
+    TransceiverInitPacket config;
+    config.protocolVersion = PROTOCOL_VERSION;
+
+    QByteArray usb_packet;
+    usb_packet.append((const char *) &senderCommand, sizeof(senderCommand));
+    usb_packet.append((const char *) &config, sizeof(config));
+    return write(usb_packet);
+}
+
 
 void TransceiverHBC::addSendCommand(const Radio::Address &target, size_t expectedResponseSize, const char *data, size_t len)
 {
@@ -125,8 +284,71 @@ void TransceiverHBC::addSendCommand(const Radio::Address &target, size_t expecte
         return;
     }
 
-    Transceiver2015::addSendCommand(target, expectedResponseSize, data, len);
+    TransceiverCommandPacket senderCommand;
+    senderCommand.command = COMMAND_SEND_NRF24;
+    senderCommand.size = len + sizeof(TransceiverSendNRF24Packet);
+
+    const auto getTargetAddress = [&](int broadcastGenerationTag, const uint8_t unicastAddressTemplate[], size_t addressTemplateLength) -> TransceiverSendNRF24Packet {
+        TransceiverSendNRF24Packet targetAddress;
+
+        if (target.isBroadcast()) {
+            // robot_datagram is used to broadcast both at generation 2014 and 2018
+            memcpy(targetAddress.address, robot_datagram, sizeof(robot_datagram));
+            // broadcast (0x1f) to generation with broadcastGenerationTag
+            targetAddress.address[0] |= 0x1f | broadcastGenerationTag;
+        } else {
+            memcpy(targetAddress.address, unicastAddressTemplate, addressTemplateLength);
+            targetAddress.address[0] |= target.unicastTarget();
+        }
+
+        targetAddress.expectedResponseSize = expectedResponseSize;
+
+        return targetAddress;
+    };
+
+    TransceiverSendNRF24Packet targetAddress;
+    switch (target.generation) {
+    case Radio::Generation::Gen2014:
+        targetAddress = getTargetAddress(
+            0x20, robot2014_address, sizeof(robot2014_address)
+        );
+        break;
+    case Radio::Generation::GenPasta:
+        targetAddress = getTargetAddress(
+            robotPasta_address[0], robotPasta_address, sizeof(robotPasta_address)
+        );
+        break;
+    }
+
+    m_packet.append((const char*) &senderCommand, sizeof(senderCommand));
+    m_packet.append((const char*) &targetAddress, sizeof(targetAddress));
+    m_packet.append((const char*) data, len);
 }
+
+void TransceiverHBC::addPingPacket(qint64 time)
+{
+    // Append ping packet with current timestamp
+    TransceiverCommandPacket senderCommand;
+    senderCommand.command = COMMAND_PING;
+    senderCommand.size = sizeof(TransceiverPingData);
+
+    TransceiverPingData ping;
+    ping.time = time;
+
+    m_packet.append((const char*) &senderCommand, sizeof(senderCommand));
+    m_packet.append((const char*) &ping, sizeof(ping));
+}
+
+void TransceiverHBC::addStatusPacket()
+{
+    // request count of dropped usb packets
+    TransceiverCommandPacket senderCommand;
+    senderCommand.command = COMMAND_STATUS;
+    senderCommand.size = 0;
+
+    m_packet.append((const char*) &senderCommand, sizeof(senderCommand));
+}
+
 
 QByteArray TransceiverHBC::buildRawRadioResponse(const char *data, uint size) {
     QByteArray rawRadioResponse{(int)size + 1, 0};
@@ -134,5 +356,87 @@ QByteArray TransceiverHBC::buildRawRadioResponse(const char *data, uint size) {
     rawRadioResponse[0] = ResponseCommand::RESPONSE_2025_DEFAULT;
     std::memcpy(rawRadioResponseData + 1, data, size);
     return rawRadioResponse;
+}
+
+std::optional<TransceiverError> TransceiverHBC::handleInitPacket(const char *data, uint size)
+{
+    // only allowed during handshake
+    if (m_connectionState != State::HANDSHAKE || size < 2) {
+        return TransceiverError(m_debugName, "Invalid reply from transceiver", (qint64) 10 * 1000 * 1000 * 1000);
+    }
+
+    const TransceiverInitPacket *handshake = (const TransceiverInitPacket *)data;
+    if (handshake->protocolVersion < PROTOCOL_VERSION) {
+        return TransceiverError(m_debugName, "Outdated firmware", (qint64) 10 * 1000 * 1000 * 1000);
+    } else if (handshake->protocolVersion > PROTOCOL_VERSION) {
+        return TransceiverError(m_debugName, "Not yet supported transceiver firmware", (qint64) 10 * 1000 * 1000 * 1000);
+    }
+
+    m_connectionState = State::CONNECTED;
+    emit deviceResponded(m_debugName);
+
+    Status status(new amun::Status);
+    status->mutable_transceiver()->set_active(true);
+    status->mutable_transceiver()->set_name(m_debugName.toStdString());
+    emit sendStatus(status);
+
+    return {};
+}
+
+void TransceiverHBC::handlePingPacket(const char *data, uint size)
+{
+    if (size < sizeof(TransceiverPingData)) {
+        return;
+    }
+
+    const TransceiverPingData *ping = (const TransceiverPingData *) data;
+    Status status(new amun::Status);
+    status->mutable_timing()->set_transceiver_rtt((Timer::systemTime() - ping->time) * 1E-9f);
+    emit sendStatus(status);
+
+    emit deviceResponded(m_debugName);
+}
+
+void TransceiverHBC::handleStatusPacket(const char *data, uint size)
+{
+    if (size < sizeof(TransceiverStatusPacket)) {
+        return;
+    }
+
+    const TransceiverStatusPacket *transceiverStatus = (const TransceiverStatusPacket *)data;
+    Status status(new amun::Status);
+    status->mutable_transceiver()->set_active(true);
+    status->mutable_transceiver()->set_name(m_debugName.toStdString());
+    status->mutable_transceiver()->set_dropped_usb_packets(transceiverStatus->droppedPackets);
+    emit sendStatus(status);
+
+    emit deviceResponded(m_debugName);
+}
+
+std::optional<TransceiverError> TransceiverHBC::handleCommand(const Command &command)
+{
+    return {};
+}
+
+std::optional<TransceiverError> TransceiverHBC::write(const QByteArray &packet)
+{
+    // close radio link on errors
+    // transmission usually either succeeds completely or fails horribly
+    // write does not actually guarantee complete delivery!
+    if (m_device->write(packet) < 0) {
+        return TransceiverError(m_debugName, m_device->errorString());
+    }
+
+    return {};
+}
+
+std::optional<TransceiverError> TransceiverHBC::flush(qint64 time)
+{
+    // Workaround for usb problems if packet size is a multiple of transfer size
+    if (m_packet.size() % 64 == 0) {
+        addPingPacket(time);
+    }
+
+    return write(m_packet);
 }
 
--- src/amun/processor/transceiverHBC.h
+++ src/amun/processor/transceiverHBC.h
@@ -22,18 +22,21 @@
 #ifndef TRANSCEIVER_HBC_H
 #define TRANSCEIVER_HBC_H
 
-#include "transceiver2015.h"
+#include "protobuf/command.h"
+#include "protobuf/status.h"
+#include "transceiverlayer.h"
 #include <QByteArray>
 #include <QObject>
 #include <QString>
-#include <variant>
+#include <memory>
+#include <optional>
 
 class Timer;
 class USBDevice;
 class USBThread;
 namespace Radio { class Address; }
 
-class TransceiverHBC : public Transceiver2015
+class TransceiverHBC : public TransceiverLayer
 {
     Q_OBJECT
 
@@ -42,26 +45,75 @@ private:
         Primary, // sends commands to ids 0-7
         Secondary, // sends commands to ids 8-15
     };
+    enum class State {
+        DISCONNECTED,
+        HANDSHAKE,
+        CONNECTED
+    };
+
 public:
-    static std::pair<std::vector<std::unique_ptr<TransceiverLayer>>, std::vector<TransceiverError>> tryOpen(USBThread * context, const Timer *timer, QObject *parent = nullptr);
+    [[nodiscard]] static std::pair<std::vector<std::unique_ptr<TransceiverLayer>>, std::vector<TransceiverError>> tryOpen(USBThread * context, const Timer *timer, QObject *parent = nullptr);
 
     TransceiverHBC(const TransceiverHBC&) = delete;
     TransceiverHBC& operator=(const TransceiverHBC&) = delete;
 
+    ~TransceiverHBC() override;
+
+    bool isOpen() const final {
+        return m_connectionState == State::CONNECTED;
+    }
+
+    void newCycle() final { m_packet.resize(0); }
+
+    static bool openDevice();
+
     void addSendCommand(const Radio::Address &target, size_t expectedResponseSize, const char *data, size_t len) final;
 
-protected:
-    QByteArray buildRawRadioResponse(const char *data, uint size) override;
+    void addPingPacket(qint64 time) final;
+    void addStatusPacket() final;
+
+    [[nodiscard]] std::optional<TransceiverError> flush(qint64 time) final;
+    [[nodiscard]] std::optional<TransceiverError> handleCommand(const Command &command) final;
 
 private:
-    static std::variant<TransceiverHBC, TransceiverError> tryOpen(USBThread * context, Kind kind, const Timer *timer, QObject *parent = nullptr);
     explicit TransceiverHBC(USBDevice *device, const Timer *timer, QString debugName, Kind kind);
+    [[nodiscard]] std::optional<TransceiverError> read();
+    [[nodiscard]] std::optional<TransceiverError> sendInitPacket();
+    [[nodiscard]] std::optional<std::vector<TransceiverError>> tryConnect(QObject* parent);
+
+    QByteArray buildRawRadioResponse(const char *data, uint size);
+
+    [[nodiscard]] std::optional<TransceiverError> write(const QByteArray &packet);
+
+    [[nodiscard]] std::optional<TransceiverError> handleInitPacket(const char *data, uint size);
+    void handlePingPacket(const char *data, uint size);
+    void handleStatusPacket(const char *data, uint size);
+    void handleDatagramPacket(const char *data, uint size);
+
+    [[nodiscard]] std::optional<TransceiverError> sendTransceiverConfiguration();
+
+private slots:
+    void onReadyRead();
+
+signals:
+    void connectionSucceeded();
+
+protected:
+    USBDevice *m_device = nullptr;
+    State m_connectionState = State::DISCONNECTED;
 
 private:
-    /** Both Transceiver2015 and HBC use the same command protocol with
+    USBThread *m_context = nullptr;
+    const Timer *m_timer = nullptr;
+
+    /** Both TransceiverHBC and HBC use the same command protocol with
      * different Vendor and Product IDs
      */
     Kind m_kind;
+
+    QByteArray m_packet;
+
+    QString m_debugName;
 };
 
 #endif // TRANSCEIVER_HBC_H
