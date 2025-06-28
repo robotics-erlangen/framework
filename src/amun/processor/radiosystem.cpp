27e054b45853563ed1c39e689236948076f3126c
Christoph Schmidtmeier <christoph.schmidtmeier@robotics-erlangen.de>
2025-06-28 12:48:25 +0200

    amun: processor/radiosystem: dont write to other transceivers after one reported an error
    When two HBC transceivers are connected, and the first one in the
    transceivers list reported an error, the transceiver list was
    immediately cleared, and then the second transceivers was used.
    
    This obviously resulted in a crash an is easily fixed by simply no
    using the second transceiver anymore after the first one raised an
    error.

--- src/amun/processor/radiosystem.cpp
+++ src/amun/processor/radiosystem.cpp
@@ -663,9 +663,11 @@ void RadioSystem::sendCommand(const QList<robot::RadioCommand> &commands, bool c
             const auto maybeError = transceiver->flush(time);
             if (maybeError.has_value()) {
                 transceiverErrorOccurred(*maybeError);
+                goto BREAK; // goto to exit the two nested loops
             }
         }
     }
+BREAK:
 
     // only restart timeout if not yet active
     if (!m_timeoutTimer->isActive()) {
