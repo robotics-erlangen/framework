d198b3e8a3f33062501c12f823ebe78c315801b8
Christoph Schmidtmeier <christoph.schmidtmeier@robotics-erlangen.de>
2026-07-27 19:47:47 +0200

    add copyright headers to all strategy files

--- strategy/typescript/glados/tutorials/t2Task/tutorial2.ts
+++ strategy/typescript/glados/tutorials/t2Task/tutorial2.ts
@@ -28,6 +28,25 @@
  * - real-life vision is never perfectly accurate, an accuracy of 10cm is sufficient (for this case, not in general!)
  */
 
+/**************************************************************************
+*   Copyright 2026 Robotics Erlangen e.V., Tobias Heineken                *
+*   http://www.robotics-erlangen.de/                                      *
+*   info@robotics-erlangen.de                                             *
+*                                                                         *
+*   This program is free software: you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation, either version 3 of the License, or     *
+*   any later version.                                                    *
+*                                                                         *
+*   This program is distributed in the hope that it will be useful,       *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+*   GNU General Public License for more details.                          *
+*                                                                         *
+*   You should have received a copy of the GNU General Public License     *
+*   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+**************************************************************************/
+
 /**
  * We import several necessary classes for path finding, etc.
  */
